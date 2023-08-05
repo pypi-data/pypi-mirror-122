7b1555f638840b17a677d4ccc35c3bac6ac5bc14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:31 +0000

    Add package artefacts/0.1.0rc0/artefacts-0.1.0rc0.tar.gz

---
+++ packages/artefacts/artefacts-0.1.0rc0.tar.gz/artefacts-0.1.0rc0/artefacts/nodes.py
@@ -0,0 +1,42 @@
+from typing import Union, Dict, List
+from pydantic import BaseModel
+from artefacts.mixins import ContextReader
+from artefacts.exceptions import NodeNotFoundException
+
+
+class BaseNode(ContextReader, BaseModel):
+
+    unique_id: str
+    
+    class Config:
+        extra = 'allow'
+        arbitrary_types_allowed = True
+
+    def __hash__(self):
+        return hash(self.unique_id)
+
+    def __str__(self):
+        return self.unique_id
+
+    def __repr__(self):
+        return f"<Node {self.unique_id}>"
+
+
+class BaseNodeReference(ContextReader, str):
+
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if not isinstance(v, str):
+            raise TypeError("NodeReferences must be strings")
+        else:
+            return cls(v)
+
+    def __str__(self):
+        return self.unique_id
+
+    def __repr__(self):
+        return f"<Node {self.unique_id}>"
