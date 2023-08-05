92f7c13d2b0c87d1541b3c31d5cb7c6e31857bb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:22 +0000

    Add package datakund-bot-studio/1.1.3/datakund_bot_studio-1.1.3.tar.gz

---
+++ packages/datakund-bot-studio/datakund_bot_studio-1.1.3.tar.gz/datakund_bot_studio-1.1.3/datakund_bot_studio/__init__.py
@@ -0,0 +1,2 @@
+from datakund_bot_studio.datakund_bot_studio import *
+bot_studio=bot_studio()
---
+++ packages/datakund-bot-studio/datakund_bot_studio-1.1.3.tar.gz/datakund_bot_studio-1.1.3/datakund_bot_studio/dk_studio.py
@@ -0,0 +1,5 @@
+import subprocess
+import os
+from datakund_bot_studio import start_studio
+def main():
+    start_studio()
