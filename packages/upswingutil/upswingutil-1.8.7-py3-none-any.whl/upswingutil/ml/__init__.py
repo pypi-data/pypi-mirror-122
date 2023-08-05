98b50ece1c3275917214bfbf85e5f834dda03f01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:31 +0000

    Add package upswingutil/1.8.7/upswingutil-1.8.7-py3-none-any.whl

---
+++ packages/upswingutil/upswingutil-1.8.7-py3-none-any.whl/upswingutil/ml/__init__.py
@@ -0,0 +1,4 @@
+from .reservation_count_forecasting import ReservationCountForecast
+from .revenue_forecasting_v2 import RevenueForecasting
+from .cancellation_risk_score import CancellationRiskScore
+# from .classification_cancel_noShow_others import Classification_Cancellation_NoShow_Others
