f5fedda9d21bf7addc4585399a7b9d138a77413a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:36 +0000

    Add package PySimpleProperties/0.2.0/PySimpleProperties-0.2.0.tar.gz

---
+++ packages/PySimpleProperties/PySimpleProperties-0.2.0.tar.gz/PySimpleProperties-0.2.0/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+import os
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+VERSION = '0.2.0'
+DESCRIPTION = 'A simple adaptation of java properties'
+LONG_DESCRIPTION = """A simple adaptation of java properties.
+Includes a Properties objects manager to help in managing multiple objects, useful for example if you have multi language support.
+
+New in Version 0.2.0:
+- Linux support
+- Comments in-file Support
+- Directory Support enhanced
+
+**Tutorial to come**
+
+GitHub repo: https://github.com/JariBou/PySimpleProperties
+Feel free to contact me at my mail: bourdietome@gmail.com
+
+-JariBou (Tomé Bourdié)"""
+
+# Setting up
+setup(
+    name="PySimpleProperties",
+    license='LICENSE',
+    version=VERSION,
+    author="JariBou (Tomé Bourdié)",
+    author_email="<bourdietome@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    url='https://github.com/JariBou/PySimpleProperties',
+    install_requires=[],
+    keywords=['python', 'properties', 'file manager'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux",
+        ]
+    )
