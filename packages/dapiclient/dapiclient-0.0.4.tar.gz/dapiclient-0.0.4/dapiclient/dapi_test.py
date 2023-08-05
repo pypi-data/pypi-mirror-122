261e8a8c09cfa3678f22e8e14dbc9f5998e6530f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:08 +0000

    Add package dapiclient/0.0.4/dapiclient-0.0.4.tar.gz

---
+++ packages/dapiclient/dapiclient-0.0.4.tar.gz/dapiclient-0.0.4/dapiclient/dapi_test.py
@@ -0,0 +1,44 @@
+import re
+from dapiclient.client import DAPIClient
+import cbor2
+import base58
+
+
+
+
+def getDocuments(data, seeds):
+    client = DAPIClient()
+    docs = client.getDocuments(
+        data['contract_id'],
+        data['document_type'],
+        data['where'],
+        limit=2,
+    )
+
+    documents = []
+    
+    for  doc in docs:
+        documents.append(cbor2.loads(doc))
+    
+    return documents[0] # Only one document
+
+
+def main():
+    data = {
+        'contract_id': base58.b58decode('D6tjxCZzZobDQztc4S1PK7EDwm4CegLARpiKZn6jQc1R'),
+        'document_type': 'thumbnailField',
+        'where': cbor2.dumps([
+            ['ownerId', '==', base58.b58decode('26AxVi5bvYYaC94GmeTmqX21vzsSxar2a4imxSE8ULUQ')],
+            ['$updatedAt', '==', 1627948894242],
+        ]),
+    }
+    result = getDocuments(data)    
+    print(result)
+    
+   
+if __name__ == "__main__":
+    main()
+
+
+
+
