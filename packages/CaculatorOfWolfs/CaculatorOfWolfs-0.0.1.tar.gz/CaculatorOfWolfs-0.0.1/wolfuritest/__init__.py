c504bfe2e90e90e36f5fb09a477178d1fc94834c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:32 +0000

    Add package CaculatorOfWolfs/0.0.1/CaculatorOfWolfs-0.0.1.tar.gz

---
+++ packages/CaculatorOfWolfs/CaculatorOfWolfs-0.0.1.tar.gz/CaculatorOfWolfs-0.0.1/wolfuritest/__init__.py
@@ -0,0 +1,13 @@
+def add_numbers(num1, num2):
+    return num1 + num2
+
+def sus_numbers(num1, num2):
+    return num1 - num2
+
+def mul_numbers(num1, num2):
+    return num1 * num2
+
+def devide_numbers(num1, num2):
+    return num1 / num2
+
+
