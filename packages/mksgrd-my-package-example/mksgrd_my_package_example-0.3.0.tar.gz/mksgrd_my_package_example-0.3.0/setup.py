dc59e020447bbe3412b8c9d56ef4a286f86364b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:05:59 +0000

    Add package mksgrd-my-package-example/0.3.0/mksgrd_my_package_example-0.3.0.tar.gz

---
+++ packages/mksgrd-my-package-example/mksgrd_my_package_example-0.3.0.tar.gz/mksgrd_my_package_example-0.3.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='mksgrd_my_package_example',
+    version='0.3.0',
+    description='An example Python package',
+    author='Maxim Gordienko',
+    author_email='maksgord-152@yandex.ru',
+    url='https://github.com/mksgrd',
+    license='MIT',
+    install_requires=['pytz<2017'],
+)
