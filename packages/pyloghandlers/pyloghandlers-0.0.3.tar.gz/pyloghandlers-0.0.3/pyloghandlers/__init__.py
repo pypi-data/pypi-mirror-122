8db05e4ddd1ea1bfef47f5d434475786c816a47a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:10 +0000

    Add package pyloghandlers/0.0.3/pyloghandlers-0.0.3.tar.gz

---
+++ packages/pyloghandlers/pyloghandlers-0.0.3.tar.gz/pyloghandlers-0.0.3/pyloghandlers/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+__title__ = 'pyloghandlers'
+__description__ = 'A python log handler packages with supported Multi-process'
+__version__ = '0.0.3'
+__url__ = 'https://github.com/ugvibib/pyloghandlers'
+__author__ = 'ugvibib'
+__author_email__ = 'ugvibib@163.com'
+__license__ = 'Apache License Version 2.0, January 2004'
+__copyright__ = 'Copyright 2020 ugvibib'
