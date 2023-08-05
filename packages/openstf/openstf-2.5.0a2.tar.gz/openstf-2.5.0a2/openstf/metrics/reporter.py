55f3ff1da72431aeeac9e415f1009e9f768028df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:56 +0000

    Add package openstf/2.5.0a2/openstf-2.5.0a2.tar.gz

---
+++ packages/openstf/openstf-2.5.0a2.tar.gz/openstf-2.5.0a2/openstf/metrics/reporter.py
@@ -0,0 +1,93 @@
+# SPDX-FileCopyrightText: 2017-2021 Alliander N.V. <korte.termijn.prognoses@alliander.com> # noqa E501>
+#
+# SPDX-License-Identifier: MPL-2.0
+import os
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Dict
+
+import pandas as pd
+import structlog
+from plotly.graph_objects import Figure
+from sklearn.base import RegressorMixin
+
+from openstf.metrics import figure
+
+
+@dataclass
+class Report:
+    feature_importance_figure: Figure
+    data_series_figures: Dict[str, Figure]
+    logger = structlog.get_logger("Report")
+
+    def save_figures(self, save_path):
+        save_path = Path(save_path)
+        os.makedirs(save_path, exist_ok=True)
+
+        self.feature_importance_figure.write_html(str(save_path / "weight_plot.html"))
+
+        for key, fig in self.data_series_figures.items():
+            fig.write_html(str(save_path / f"{key}.html"), auto_open=False)
+        self.logger.info(f"Saved figures to {save_path}")
+
+
+class Reporter:
+    def __init__(
+        self,
+        pj: dict = None,
+        train_data: pd.DataFrame = None,
+        validation_data: pd.DataFrame = None,
+        test_data: pd.DataFrame = None,
+    ) -> None:
+
+        """Initializes reporter
+
+        Args:
+            pj:
+            train_data:
+            validation_data:
+            test_data:
+        """
+        self.pj = pj
+        self.horizons = train_data.horizon.unique()
+        self.predicted_data_list = []
+        self.input_data_list = [train_data, validation_data, test_data]
+
+    def generate_report(
+        self,
+        model: RegressorMixin,
+    ) -> Report:
+
+        data_series_figures = self._make_data_series_figures(model)
+        feature_importance_figure = figure.plot_feature_importance(
+            model.feature_importance_dataframe
+        )
+
+        report = Report(
+            data_series_figures=data_series_figures,
+            feature_importance_figure=feature_importance_figure,
+        )
+
+        return report
+
+    def _make_data_series_figures(self, model: RegressorMixin) -> dict:
+
+        # Make model predictions
+        for data_set in self.input_data_list:
+            # First ("load") and last ("horizon") are removed here
+            # as they are not expected by the model as prediction input
+            model_forecast = model.predict(data_set.iloc[:, 1:-1])
+            forecast = pd.DataFrame(
+                index=data_set.index, data={"forecast": model_forecast}
+            )
+            self.predicted_data_list.append(forecast)
+
+        # Make cufflinks plots for the data series
+        return {
+            f"Predictor{horizon}": figure.plot_data_series(
+                data=self.input_data_list,
+                predict_data=self.predicted_data_list,
+                horizon=horizon,
+            )
+            for horizon in self.horizons
+        }
---
+++ packages/openstf/openstf-2.5.0a2.tar.gz/openstf-2.5.0a2/openstf/pipeline/optimize_hyperparameters.py
@@ -0,0 +1,129 @@
+# SPDX-FileCopyrightText: 2017-2021 Alliander N.V. <korte.termijn.prognoses@alliander.com> # noqa E501>
+#
+# SPDX-License-Identifier: MPL-2.0
+from typing import List
+
+import optuna
+import pandas as pd
+import structlog
+
+
+from openstf.feature_engineering.feature_applicator import TrainFeatureApplicator
+from openstf.exceptions import (
+    InputDataInsufficientError,
+    InputDataWrongColumnOrderError,
+)
+
+from openstf.model.model_creator import ModelCreator
+from openstf.model.objective_creator import ObjectiveCreator
+from openstf.validation import validation
+
+# This is required to disable the default optuna logger and pass the logs to our own
+# structlog logger
+optuna.logging.enable_propagation()  # Propagate logs to the root logger.
+optuna.logging.disable_default_handler()  # Stop showing logs in sys.stderr.
+
+logger = structlog.get_logger(__name__)
+
+# See https://optuna.readthedocs.io/en/stable/reference/generated/optuna.study.Study.html#optuna.study.Study.optimize
+N_TRIALS: int = 8  # The number of trials.
+TIMEOUT: int = 200  # Stop study after the given number of second(s).
+TRAIN_HORIZONS: List[float] = [0.25, 24.0]
+
+
+def optimize_hyperparameters_pipeline(
+    pj: dict,
+    input_data: pd.DataFrame,
+    horizons: List[float] = TRAIN_HORIZONS,
+    n_trials: int = N_TRIALS,
+) -> dict:
+    """Optimize hyperparameters pipeline.
+
+    Expected prediction job key's: "name", "model"
+
+    Args:
+        pj (dict): Prediction job
+        input_data (pd.DataFrame): Raw training input data
+        horizons (List[float]): horizons for feature engineering.
+        n_trials (int, optional): The number of trials. Defaults to N_TRIALS.
+        timeout (int, optional): Stop study after the given number of second(s).
+            Defaults to TIMEOUT. Will give an exception if the optimization is only
+            1 trial.
+
+    Raises:
+        ValueError: If the input_date is insufficient.
+
+    Returns:
+        dict: Optimized hyperparameters.
+    """
+    if input_data.empty:
+        raise InputDataInsufficientError("Input dataframe is empty")
+    elif "load" not in input_data.columns:
+        raise InputDataWrongColumnOrderError(
+            "Missing the load column in the input dataframe"
+        )
+
+    # Validate and clean data
+    validated_data = validation.clean(validation.validate(pj["id"], input_data))
+
+    # Check if sufficient data is left after cleaning
+    if not validation.is_data_sufficient(validated_data):
+        raise InputDataInsufficientError(
+            f"Input data is insufficient for {pj['name']} "
+            f"after validation and cleaning"
+        )
+
+    input_data_with_features = TrainFeatureApplicator(horizons=horizons).add_features(
+        input_data
+    )
+
+    # Create objective (NOTE: this is a callable class)
+    objective = ObjectiveCreator.create_objective(model_type=pj["model"])
+
+    model_type = pj["model"]
+    model = ModelCreator.create_model(model_type)
+
+    objective = objective(
+        model,
+        input_data_with_features,
+    )
+
+    study = optuna.create_study(
+        study_name=model_type,
+        pruner=optuna.pruners.MedianPruner(n_warmup_steps=5),
+        direction="minimize",
+    )
+
+    study.optimize(
+        objective,
+        n_trials=n_trials,
+        callbacks=[_log_study_progress],
+        show_progress_bar=False,
+        timeout=TIMEOUT,
+    )
+
+    optimized_hyperparams = study.best_params
+    optimized_error = study.best_value
+
+    logger.info(
+        f"Finished hyperparameter optimization, error objective {optimized_error} "
+        f"and params {optimized_hyperparams}"
+    )
+
+    return optimized_hyperparams
+
+
+def _log_study_progress(
+    study: optuna.study.Study, trial: optuna.trial.FrozenTrial
+) -> None:
+    # Collect study and trial data
+    trial_index = study.trials.index(trial)
+    best_trial_index = study.trials.index(study.best_trial)
+    value = trial.value
+    params = trial.params
+    duration = (trial.datetime_complete - trial.datetime_start).total_seconds()
+    # Log information about this trial
+    logger.debug(
+        f"Trial {trial_index} finished with value: {value} and parameters: {params}."
+        f"Best trial is {best_trial_index}. Iteration took {duration} s"
+    )
