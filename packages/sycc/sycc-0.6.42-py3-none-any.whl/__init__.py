c6d7de6798b17ece5e3c12287d254f77a6a3945f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:10 +0000

    Add package sycc/0.6.42/sycc-0.6.42-py3-none-any.whl

---
+++ packages/sycc/sycc-0.6.42-py3-none-any.whl/__init__.py
@@ -0,0 +1,9 @@
+# __init__.py
+#from . import *
+from colorama import init
+from datetime import datetime as sj
+from time import sleep as dd
+from math import pi as pai1
+from sys import exit as tc
+start=sj.now()
+from p.pdsys import *
