6342556ea6f1cfdbb395e3529749b1824fed3640
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:44 +0000

    Add package mmds/0.0.1.dev20211003162211/mmds-0.0.1.dev20211003162211.tar.gz

---
+++ packages/mmds/mmds-0.0.1.dev20211003162211.tar.gz/mmds-0.0.1.dev20211003162211/mmds/modalities/rgbs.py
@@ -0,0 +1,44 @@
+import attr
+import logging
+from PIL import Image
+from typing import Callable
+
+from .ts import TimeSeriesModality
+
+logger = logging.getLogger(__name__)
+
+dumb_image = Image.new("RGB", (32, 32))
+
+
+def _load_pil(path):
+    if path is None:
+        return dumb_image
+
+    try:
+        image = Image.open(path)
+    except:
+        logger.warning(f"Open {path} failed, use an empty picture instead.")
+        image = dumb_image
+
+    return image
+
+
+@attr.define
+class RgbsModality(TimeSeriesModality):
+    """A rgb sequence modality for video."""
+
+    transform: Callable
+    aggragate: Callable
+
+    @property
+    def duration(self):
+        return len(self.paths) / self.sample_rate
+
+    def fetch(self, *, info={}):
+        paths = self._slice(self.paths, info.get("t0", None), info.get("t1", None))
+        frames = list(map(self.transform, map(_load_pil, paths)))
+        return self.aggragate(frames)
+
+    @staticmethod
+    def _pad_fn(x, n):
+        return x + [None] * n
