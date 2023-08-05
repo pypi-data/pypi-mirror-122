cbf097dc359559ed6d8920cbd7c37514e984c414
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:13 +0000

    Add package finetuner/0.0.1/finetuner-0.0.1.tar.gz

---
+++ packages/finetuner/finetuner-0.0.1.tar.gz/finetuner-0.0.1/README.md
@@ -0,0 +1,11 @@
+<p align="center">
+<img src="https://github.com/jina-ai/finetuner/blob/main/docs/_static/logo-light.svg?raw=true" alt="Finetuner logo: Finetuner allows one to finetune any deep Neural Network for better embedding on search tasks. It accompanies Jina to deliver the last mile of performance-tuning for neural search applications." width="150px">
+</p>
+
+
+<p align="center">
+<b>Finetuning any deep neural network for better embedding on neural search tasks</b>
+</p>
+
+
+## [Documentation](https://finetuner.jina.ai)
---
+++ packages/finetuner/finetuner-0.0.1.tar.gz/finetuner-0.0.1/tests/unit/tailor/test_keras.py
@@ -0,0 +1,165 @@
+import pytest
+import tensorflow as tf
+
+from finetuner.tailor.keras import KerasTailor
+
+
+@pytest.fixture
+def dense_model():
+    model = tf.keras.models.Sequential()
+    model.add(tf.keras.layers.InputLayer(input_shape=(128,)))  # (None, 128)
+    model.add(tf.keras.layers.Dense(128, activation='relu'))  # (None, 128)
+    model.add(tf.keras.layers.Dense(64, activation='relu'))  # (None, 64)
+    model.add(tf.keras.layers.Dense(32, activation='relu'))  # (None, 32)
+    model.add(tf.keras.layers.Dense(10, activation='softmax'))  # (None, 10)
+    return model
+
+
+@pytest.fixture
+def simple_cnn_model():
+    model = tf.keras.models.Sequential()
+    model.add(tf.keras.layers.InputLayer(input_shape=(28, 28, 1)))
+    model.add(tf.keras.layers.Conv2D(32, 3, (1, 1), activation='relu'))
+    model.add(tf.keras.layers.Conv2D(64, 3, (1, 1), activation='relu'))
+    model.add(tf.keras.layers.MaxPool2D(2))
+    model.add(tf.keras.layers.Dropout(0.25))
+    model.add(tf.keras.layers.Flatten())
+    model.add(tf.keras.layers.Dense(128))
+    model.add(tf.keras.layers.Dropout(0.25))
+    model.add(tf.keras.layers.Dense(10, activation='softmax'))
+    return model
+
+
+@pytest.fixture
+def vgg16_cnn_model():
+    return tf.keras.applications.vgg16.VGG16(
+        include_top=True,
+        weights='imagenet',
+        input_tensor=None,
+        input_shape=None,
+        pooling=None,
+        classes=1000,
+        classifier_activation='softmax',
+    )
+
+
+@pytest.fixture
+def stacked_lstm():
+    model = tf.keras.models.Sequential()
+    model.add(tf.keras.layers.Embedding(1000, 1024, input_length=128))
+    model.add(
+        tf.keras.layers.LSTM(256, return_sequences=True)
+    )  # this layer will not considered as candidate layer
+    model.add(tf.keras.layers.LSTM(256, return_sequences=True))
+    model.add(
+        tf.keras.layers.LSTM(256, return_sequences=False)
+    )  # this layer will be considered as candidate layer
+    model.add(tf.keras.layers.Dense(256, activation='relu'))
+    model.add(tf.keras.layers.Dense(5, activation='softmax'))
+    return model
+
+
+@pytest.fixture
+def bidirectional_lstm():
+    return tf.keras.Sequential(
+        [
+            tf.keras.layers.Embedding(input_dim=5000, output_dim=64),
+            tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64)),
+            tf.keras.layers.Dense(32),
+        ]
+    )
+
+
+@pytest.fixture(
+    params=[
+        'dense_model',
+        'simple_cnn_model',
+        'vgg16_cnn_model',
+        'stacked_lstm',
+        'bidirectional_lstm',
+    ]
+)
+def model(request):
+    return request.getfixturevalue(request.param)
+
+
+@pytest.mark.parametrize(
+    'model, layer_idx',
+    [
+        ('dense_model', 10),  # 10th layer does not exist
+        ('simple_cnn_model', 2),  # 2nd layer is a convolutional layer
+        ('vgg16_cnn_model', 4),  # 4th layer is a convolutional layer
+        ('stacked_lstm', 10),  # 10th layer does not exist
+        ('bidirectional_lstm', 5),  # 5th layer does not exist
+    ],
+    indirect=['model'],
+)
+def test_trim_fail_given_unexpected_layer_idx(model, layer_idx):
+    with pytest.raises(IndexError):
+        keras_tailor = KerasTailor(model, layer_idx)
+        keras_tailor._trim()
+
+
+@pytest.mark.parametrize(
+    'model, layer_idx, expected_output_shape',
+    [
+        ('dense_model', 3, (None, 32)),
+        ('simple_cnn_model', 5, (None, 9216)),
+        ('vgg16_cnn_model', 21, (None, 4096)),
+        ('stacked_lstm', 4, (None, 256)),
+        ('bidirectional_lstm', 2, (None, 64 * 2)),  # bi-directional
+    ],
+    indirect=['model'],
+)
+def test_trim(model, layer_idx, expected_output_shape):
+    keras_tailor = KerasTailor(model, layer_idx)
+    keras_tailor._trim()
+    assert keras_tailor.model.output_shape == expected_output_shape
+
+
+@pytest.mark.parametrize(
+    'model',
+    [
+        'dense_model',
+        'simple_cnn_model',
+        'vgg16_cnn_model',
+        'stacked_lstm',
+        'bidirectional_lstm',
+    ],
+    indirect=['model'],
+)
+def test_freeze(model):
+    keras_tailor = KerasTailor(model)
+    for layer in keras_tailor.model.layers:
+        assert layer.trainable
+    keras_tailor._freeze_weights()
+    for layer in keras_tailor.model.layers:
+        assert not layer.trainable
+
+
+def test_keras_model_parser():
+    user_model = tf.keras.Sequential(
+        [
+            tf.keras.layers.Flatten(input_shape=(28, 28), name='l1'),
+            tf.keras.layers.Dense(128, activation='relu', name='l2'),
+            tf.keras.layers.Dense(32, name='l3'),
+        ]
+    )
+
+    keras_tailor = KerasTailor(user_model)
+
+    r = keras_tailor.candidate_layers
+    assert len(r) == 3
+    assert r[0]['name'] == 'l1'
+    assert r[1]['name'] == 'l2'
+    assert r[2]['name'] == 'l3'
+
+    # flat layer can be a nonparametric candidate
+    assert r[0]['output_features'] == 784
+    assert r[0]['params'] == 0
+
+    assert r[1]['output_features'] == 128
+    assert r[1]['params'] == 100480
+
+    assert r[2]['output_features'] == 32
+    assert r[2]['params'] == 4128
