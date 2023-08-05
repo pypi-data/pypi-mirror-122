40d1472a462c0dcdca2dd841e0fdfb6eb53cad6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:03 +0000

    Add package hamlet/9.6.4.dev3/hamlet-9.6.4.dev3.tar.gz

---
+++ packages/hamlet/hamlet-9.6.4.dev3.tar.gz/hamlet-9.6.4.dev3/hamlet/backend/automation_tasks/transfer_image/__init__.py
@@ -0,0 +1,74 @@
+from hamlet.backend.automation_tasks.base import AutomationRunner
+from hamlet.backend.automation import (
+    properties_file,
+    set_automation_context,
+    manage_build_references,
+    construct_tree,
+)
+from hamlet.backend.automation.properties_file import get_automation_properties
+
+
+class TransferImageAutomationRunner(AutomationRunner):
+    def __init__(
+        self,
+        source_account,
+        source_environment,
+        deployment_unit,
+        build_reference,
+        image_format,
+        **kwargs
+    ):
+        super().__init__(**kwargs)
+
+        self._source_account = source_account
+
+        self._context_env = {
+            "DEPLOYMENT_UNITS": deployment_unit,
+            "IMAGE_FORMATS": image_format,
+            "GIT_COMMIT": build_reference,
+            **kwargs,
+        }
+
+        self._context_env["FROM_ACCOUNT"] = source_account
+        self._context_env["FROM_ENVIRONMENT"] = source_environment
+
+        self._script_list = [
+            {
+                "func": construct_tree.run,
+                "args": {
+                    "use_existing_tree": True,
+                    "_is_cli": True,
+                },
+            },
+            {
+                "func": properties_file.get_automation_properties,
+                "args": {**self._context_env},
+            },
+            {
+                "func": set_automation_context.run,
+                "args": {"_is_cli": True, "release_mode": "promotion"},
+            },
+            {
+                "func": manage_build_references.run,
+                "args": {
+                    "verify": "latest",
+                    "code_commits": build_reference,
+                    "deployment_units": deployment_unit,
+                    "image_formats": image_format,
+                    "_is_cli": True,
+                },
+            },
+        ]
+
+    def run(self):
+
+        source_args = {
+            **self._context_env,
+            **{
+                "account": self._source_account,
+            },
+        }
+        source_automation_properties = get_automation_properties(**source_args)
+        self._context_env.update(source_automation_properties)
+
+        return super().run()
