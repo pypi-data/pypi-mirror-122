722df9a9bb24d7f804684968189eb755ebe633a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:21 +0000

    Add package napari-ccp4map/1.0/napari-ccp4map-1.0.tar.gz

---
+++ packages/napari-ccp4map/napari-ccp4map-1.0.tar.gz/napari-ccp4map-1.0/napari_ccp4map/_reader.py
@@ -0,0 +1,65 @@
+"""
+This module is an example of a barebones numpy reader plugin for napari.
+
+It implements the ``napari_get_reader`` hook specification, (to create
+a reader plugin) but your plugin may choose to implement any of the hook
+specifications offered by napari.
+see: https://napari.org/docs/dev/plugins/hook_specifications.html
+
+Replace code below accordingly.  For complete documentation see:
+https://napari.org/docs/dev/plugins/for_plugin_developers.html
+"""
+import numpy as np
+import gemmi
+from napari_plugin_engine import napari_hook_implementation
+
+
+@napari_hook_implementation
+def napari_get_reader(path):
+    """A basic implementation of the napari_get_reader hook specification.
+
+    Parameters
+    ----------
+    path : str or list of str
+        Path to file, or list of paths.
+
+    Returns
+    -------
+    function or None
+        If the path is a recognized format, return a function that accepts the
+        same path or list of paths, and returns a list of layer data tuples.
+    """
+    if isinstance(path, str) and path.endswith((".map",".ccp4")):
+        return map_reader
+
+
+def map_reader(path: str):
+    """Take a path or list of paths and return a list of LayerData tuples.
+
+    Readers are expected to return data as a list of tuples, where each tuple
+    is (data, [add_kwargs, [layer_type]]), "add_kwargs" and "layer_type" are
+    both optional.
+
+    Parameters
+    ----------
+    path : str or list of str
+        Path to file, or list of paths.
+
+    Returns
+    -------
+    layer_data : list of tuples
+        A list of LayerData tuples where each tuple in the list contains
+        (data, metadata, layer_type), where data is a numpy array, metadata is
+        a dict of keyword arguments for the corresponding viewer.add_* method
+        in napari, and layer_type is a lower-case string naming the type of layer.
+        Both "meta", and "layer_type" are optional. napari will default to
+        layer_type=="image" if not provided
+    """
+    ccp4_map = gemmi.read_ccp4_map(path)
+    vol = np.array(ccp4_map.grid, copy=False)
+
+    # optional kwargs for the corresponding viewer.add_* method
+    add_kwargs = {}
+
+    layer_type = "image"  # optional, default is "image"
+    return [(vol, add_kwargs, layer_type)]
