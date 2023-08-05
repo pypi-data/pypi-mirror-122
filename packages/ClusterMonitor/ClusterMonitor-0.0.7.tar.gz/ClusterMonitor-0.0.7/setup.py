1702d01df6bda1edc5b7875492407ca9fc3c067b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:01 +0000

    Add package ClusterMonitor/0.0.7/ClusterMonitor-0.0.7.tar.gz

---
+++ packages/ClusterMonitor/ClusterMonitor-0.0.7.tar.gz/ClusterMonitor-0.0.7/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+VERSION = '0.0.7'
+DESCRIPTION = 'Simple script which can be used to monitor and log CPU and RAM usage of submitted cluster jobs'
+this_directory = Path(__file__).parent
+LONG_DESCRIPTION = (this_directory / "README.md").read_text()
+
+# Setting up
+setup(
+    name="ClusterMonitor",
+    version=VERSION,
+    author="nickhir",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=['matplotlib'],
+    keywords=['python', 'cluster', 'monitor', 'CPU', 'RAM', 'usage', 'SLURM'],
+    classifiers=["Operating System :: Unix"],
+    scripts=['bin/cpu_ram_log', 'bin/visualize_log']
+)
