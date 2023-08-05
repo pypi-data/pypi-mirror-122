adcd2f057eaec3efcb67c8f0b632518f602ba05a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:39 +0000

    Add package nice-prompts/0.3.0/nice-prompts-0.3.0.tar.gz

---
+++ packages/nice-prompts/nice-prompts-0.3.0.tar.gz/nice-prompts-0.3.0/test/test.py
@@ -0,0 +1,20 @@
+import nice_prompts
+
+n = nice_prompts.NicePrompt()
+
+print(n.number(float, 0, 1))
+
+print(n.number(int, 0, 100))
+
+print(n.selection({"I like pizza": "Good taste",
+    "I respectfully disagree with the opinion of liking pizza": "Fair enough, good day",
+    "I hate pizza": "Bad sport 👎"})) # Select one from the keys, return the value
+
+print(n.multiselection({"I like pizza": "Good taste",
+    "I respectfully disagree with the opinion of liking pizza": "Fair enough, good day",
+    "I hate pizza": "Bad sport 👎"}, amount=2, required=2)) # Select multiple from the keys, return the values. You must select 2
+
+
+print(n.multiselection({"I like pizza": "Good taste",
+    "I respectfully disagree with the opinion of liking pizza": "Fair enough, good day",
+    "I hate pizza": "Bad sport 👎"}, required=0))  # Select multiple from the keys, return the values. No max, can be left blank
