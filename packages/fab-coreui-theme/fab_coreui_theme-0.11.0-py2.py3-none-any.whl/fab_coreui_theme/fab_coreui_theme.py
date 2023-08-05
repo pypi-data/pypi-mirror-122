862348f948b08b6f27812c30acdfb32a54f215c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:24 +0000

    Add package fab-coreui-theme/0.11.0/fab_coreui_theme-0.11.0-py2.py3-none-any.whl

---
+++ packages/fab-coreui-theme/fab_coreui_theme-0.11.0-py2.py3-none-any.whl/fab_coreui_theme/fab_coreui_theme.py
@@ -0,0 +1,62 @@
+"""Main module."""
+
+from flask_appbuilder import (
+    action,
+    aggregate_count,
+    BaseView,
+    expose,
+    GroupByChartView,
+    has_access,
+    ModelView,
+)
+from flask import Blueprint, request, jsonify, Response, session
+from flask_appbuilder.views import MultipleView, SimpleFormView, PublicFormView
+import os
+
+TEMPLATE_FOLDER = os.path.join(os.path.dirname(__file__), "templates")
+STATIC_FOLDER = os.path.join(os.path.dirname(__file__), "static")
+STATIC_URL_PATH = "/static/coreui"
+
+
+# Use the Flask Blueprint to create an appized app and setup our routing
+# https://flask.palletsprojects.com/en/2.0.x/blueprints/#templates
+# You must register this blueprint on your app!
+
+coreui_bp = Blueprint(
+    "coreui_theme",
+    __name__,
+    template_folder=TEMPLATE_FOLDER,
+    static_folder=STATIC_FOLDER,
+    static_url_path=STATIC_URL_PATH,
+)
+
+
+class CoreUIBaseView(BaseView):
+    template_folder = TEMPLATE_FOLDER
+    static_folder = STATIC_FOLDER
+    static_url_path = STATIC_URL_PATH
+
+
+class CoreUIModelView(ModelView):
+    template_folder = TEMPLATE_FOLDER
+    static_folder = STATIC_FOLDER
+    static_url_path = STATIC_URL_PATH
+    form_template = "coreui/general/model/edit.html"
+    edit_template = "coreui/general/model/edit.html"
+    list_template = "coreui/general/model/list.html"
+    show_template = "coreui/general/model/show.html"
+    add_template = "coreui/general/model/add.html"
+
+
+class CoreUISimpleFormView(SimpleFormView):
+    template_folder = TEMPLATE_FOLDER
+    static_folder = STATIC_FOLDER
+    static_url_path = STATIC_URL_PATH
+    form_template = "coreui/general/model/edit.html"
+
+
+class CoreUIPublicFormView(PublicFormView):
+    template_folder = TEMPLATE_FOLDER
+    static_folder = STATIC_FOLDER
+    static_url_path = STATIC_URL_PATH
+    form_template = "coreui/general/model/edit.html"
