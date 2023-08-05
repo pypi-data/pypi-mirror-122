e22c3f0fd1d3c7062de117c06e6cf9e65682028b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:04 +0000

    Add package bavard-ml-utils/0.1.25/bavard-ml-utils-0.1.25.tar.gz

---
+++ packages/bavard-ml-utils/bavard-ml-utils-0.1.25.tar.gz/bavard-ml-utils-0.1.25/bavard_ml_utils/types/nlu.py
@@ -0,0 +1,29 @@
+import typing as t
+
+from pydantic import BaseModel
+
+from bavard_ml_utils.ml.dataset import LabeledDataset
+
+
+class TrainingTag(BaseModel):
+    """A tag as it appears in NLU training data."""
+
+    tagType: str
+    start: int
+    end: int
+
+
+class NLUExample(BaseModel):
+    intent: t.Optional[str]
+    text: str
+    tags: t.Optional[t.List[TrainingTag]]
+    # `True` if `text` does not belong to any `intent` in the dataset.
+    isOOD = False
+
+
+class NLUExampleDataset(LabeledDataset[NLUExample]):
+    def get_label(self, item: NLUExample) -> t.Optional[str]:
+        return item.intent
+
+    def unique_tag_types(self) -> t.Set[str]:
+        return set(tag.tagType for ex in self for tag in ex.tags or [])
