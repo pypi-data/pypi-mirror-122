e89479e0ff6c31e43bda2d53449e82b709a14797
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:07 +0000

    Add package PYTHON-TRANSFORMATION/0.0.3/PYTHON_TRANSFORMATION-0.0.3.tar.gz

---
+++ packages/PYTHON-TRANSFORMATION/PYTHON_TRANSFORMATION-0.0.3.tar.gz/PYTHON_TRANSFORMATION-0.0.3/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.6',
+]
+
+setup(
+    name='PYTHON_TRANSFORMATION',
+    version='0.0.3',
+    description='Transformations to a dataframe using python',
+    long_description=open('README.rst').read(),
+    url='',
+    author='Anjana V',
+    author_email='anjana.v@bdb.ai',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='python transformation',
+    packages=find_packages(),
+    install_requires=[
+          'pandas',
+        'numpy',
+        'hashlib',
+        'num2words',
+        'datetime',
+        'math'
+      ]
+)
