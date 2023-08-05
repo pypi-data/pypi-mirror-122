7ffe36338f8f50988f14d12e908ee5c56c067bbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:14:21 +0000

    Add package bigo_test/0.1.0/bigo_test-0.1.0.tar.gz

---
+++ packages/bigo_test/bigo_test-0.1.0.tar.gz/bigo_test-0.1.0/bigo_test/assertions/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from bigo_test.assertions.assert_big_o import assertBigO
+
+
+__all__ = ["assertBigO"]
---
+++ packages/bigo_test/bigo_test-0.1.0.tar.gz/bigo_test-0.1.0/bigo_test/exceptions.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+class WrongTimeComplexity(ValueError):
+    """Raised when the one of the assertion functions gets wrong time complexity
+    """
