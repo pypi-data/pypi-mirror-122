ca19a77338e7894fb8bcb1c094d0868fee9ecd83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:13 +0000

    Add package django-mass-edit/3.3.0/django_mass_edit-3.3.0-py3-none-any.whl

---
+++ packages/django-mass-edit/django_mass_edit-3.3.0-py3-none-any.whl/tests/site.py
@@ -0,0 +1,7 @@
+from django.contrib.admin.sites import AdminSite
+
+class CustomAdminSite(AdminSite):
+    def each_context(self, request):
+        context = super(CustomAdminSite, self).each_context(request)
+        context['custom_variable'] = 'custom_value'
+        return context
