a26b32696419b8784c401ee5a2e80acb593565ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:13:34 +0000

    Add package euros/1.3/euros-1.3.tar.gz

---
+++ packages/euros/euros-1.3.tar.gz/euros-1.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='euros',
+      version='1.3',
+      description='Converts any amount of euros from numbers into letters',
+      author='Thibaut Spriet',
+      author_email='thibaut@spriet.online',
+      url='https://github.com/ThbtSprt/euros',
+      package_dir = {'': 'src'},
+      packages=['euros']
+     )
