8e67d793d40456d3ed3f4488ea535f0282db254c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:20 +0000

    Add package spacepackets/0.2.1/spacepackets-0.2.1-py3-none-any.whl

---
+++ packages/spacepackets/spacepackets-0.2.1-py3-none-any.whl/spacepackets/ecss/conf.py
@@ -0,0 +1,59 @@
+import enum
+
+
+class PusVersion(enum.IntEnum):
+    # ESA PSS-07-101. Not supported by this package!
+    ESA_PUS = 0
+    # ECSS-E-70-41A
+    PUS_A = 1
+    # ECSS-E-ST-70-41C
+    PUS_C = 2
+    GLOBAL_CONFIG = 98
+    UNKNOWN = 99
+
+
+class EcssConfKeys(enum.IntEnum):
+    ECSS_APID = 0,
+    PUS_TM_TYPE = 1,
+    PUS_TC_TYPE = 2,
+    ECSS_TM_APID = 3
+
+
+ECSS_DICT = {
+    EcssConfKeys.ECSS_APID: 0xef,
+    EcssConfKeys.PUS_TM_TYPE: PusVersion.PUS_C,
+    EcssConfKeys.PUS_TC_TYPE: PusVersion.PUS_C,
+    EcssConfKeys.ECSS_TM_APID: 0xef
+}
+
+
+def set_default_apid(default_apid: int):
+    ECSS_DICT[EcssConfKeys.ECSS_APID] = default_apid
+
+
+def get_default_apid() -> int:
+    return ECSS_DICT[EcssConfKeys.ECSS_APID]
+
+
+def set_pus_tc_version(pus_type: PusVersion):
+    ECSS_DICT[EcssConfKeys.PUS_TC_TYPE] = pus_type
+
+
+def get_pus_tc_version() -> PusVersion:
+    return ECSS_DICT[EcssConfKeys.PUS_TC_TYPE]
+
+
+def set_pus_tm_version(pus_type: PusVersion):
+    ECSS_DICT[EcssConfKeys.PUS_TM_TYPE] = pus_type
+
+
+def get_pus_tm_version() -> PusVersion:
+    return ECSS_DICT[EcssConfKeys.PUS_TM_TYPE]
+
+
+def insert_tm_apid(tm_apid: int):
+    ECSS_DICT[EcssConfKeys.ECSS_TM_APID] = tm_apid
+
+
+def get_tm_apid() -> int:
+    return ECSS_DICT[EcssConfKeys.ECSS_TM_APID]
