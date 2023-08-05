b6f32f2e220d876dca9371c6bdc01590f8338e82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:48 +0000

    Add package QNCP/1.2.2/QNCP-1.2.2.tar.gz

---
+++ packages/QNCP/QNCP-1.2.2.tar.gz/QNCP-1.2.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='QNCP',
+    version='1.2.2',
+    license='MIT',
+    author="Leonardo Castillo Veneros and Guodong Cui",
+    author_email='frozenbooger@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/frozenbooger/QNCP',
+    keywords='Quantum Network Control Panel',
+    install_requires=[
+        'matplotlib',
+        'numpy',
+        'numpydoc',
+        'paramiko',
+        'pyvis',
+        'PyVISA',
+        'PyVISA-py',
+        'regex',
+        'scipy',
+        'scp',
+        'visa',
+        'python-vxi11'],
+
+)
