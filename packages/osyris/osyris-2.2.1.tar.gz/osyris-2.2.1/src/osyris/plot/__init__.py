d09d7dc3539c34b197cd35584d45c7620be210d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:42 +0000

    Add package osyris/2.2.1/osyris-2.2.1.tar.gz

---
+++ packages/osyris/osyris-2.2.1.tar.gz/osyris-2.2.1/src/osyris/plot/__init__.py
@@ -0,0 +1,9 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2021 Osyris contributors (https://github.com/nvaytet/osyris)
+
+# flake8: noqa
+
+from .histogram import histogram
+from .render import render
+from .plane import plane
+from .scatter import scatter
