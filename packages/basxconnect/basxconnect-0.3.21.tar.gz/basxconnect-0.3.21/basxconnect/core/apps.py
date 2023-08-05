02ebc8132f8cff176b1db601f0ab923021a9d6d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:41 +0000

    Add package basxconnect/0.3.21/basxconnect-0.3.21.tar.gz

---
+++ packages/basxconnect/basxconnect-0.3.21.tar.gz/basxconnect-0.3.21/basxconnect/core/apps.py
@@ -0,0 +1,32 @@
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+
+class CoreConfig(AppConfig):
+    name = "basxconnect.core"
+
+    def ready(self):
+        import sys
+
+        from .models import Vocabulary
+
+        if "migrate" in sys.argv or "makemigrations" in sys.argv:
+            return
+
+        pre_installed_vocabulary = {
+            "tag": _("Tags"),
+            "title": _("Titles"),
+            "salutation": _("Salutations"),
+            "form_of_address": _("Forms Of Address"),
+            "gender": _("Genders"),
+            "naturaltype": _("Natural Person Types"),
+            "legaltype": _("Legal Person Types"),
+            "associationtype": _("Association Person Types"),
+            "addressstatus": _("Address statuses"),
+            "emailtype": _("Email Types"),
+            "urltype": _("URL Types"),
+            "phonetype": _("Phone Types"),
+            "addresstype": _("Address Types"),
+        }
+        for slug, name in pre_installed_vocabulary.items():
+            Vocabulary.objects.get_or_create(slug=slug, defaults={"name": name})
