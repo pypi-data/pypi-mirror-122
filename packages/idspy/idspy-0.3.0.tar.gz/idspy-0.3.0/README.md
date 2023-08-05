9676e70f5e886ed08813086926b1aba9d682d177
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:05:49 +0000

    Add package idspy/0.3.0/idspy-0.3.0.tar.gz

---
+++ packages/idspy/idspy-0.3.0.tar.gz/idspy-0.3.0/README.md
@@ -0,0 +1,5 @@
+# idspy
+
+[![Build Status](https://github.com/intercontinental-dictionary-series/idspy/workflows/tests/badge.svg)](https://github.com/intercontinental-dictionary-series/idspy/actions?query=workflow%3Atests)
+[![codecov](https://codecov.io/gh/intercontinental-dictionary-series/idspy/branch/master/graph/badge.svg)](https://codecov.io/gh/intercontinental-dictionary-series/idspy)
+[![PyPI](https://img.shields.io/pypi/v/idspy.svg)](https://pypi.org/project/idspy)
---
+++ packages/idspy/idspy-0.3.0.tar.gz/idspy-0.3.0/setup.py
@@ -0,0 +1,54 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='idspy',
+    version='0.3.0',
+    author='Robert Forkel and Hans-Jörg Bibiko',
+    author_email='dlce.rdm@eva.mpg.de',
+    description='Python library providing shared code for IDS datasets',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    keywords='',
+    license='Apache 2.0',
+    url='https://github.com/intercontinental-dictionary-series/idspy',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+        "cldfbench.commands": [
+            "idspy=idspy.commands",
+        ]
+    },
+    platforms='any',
+    python_requires='>=3.6',
+    install_requires=[
+        'clldutils',
+        'pybtex',
+        'pylexibank>=2.8.2',
+        'openpyxl',
+    ],
+    extras_require={
+        'dev': ['flake8', 'wheel', 'twine'],
+        'test': [
+            'pytest>=5',
+            'pytest-mock',
+            'pytest-cov',
+            'coverage>=4.2',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+)
