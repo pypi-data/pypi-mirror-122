39d3835a1e61774fcb529d7550ed0dc3acca8e50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:16 +0000

    Add package namen/0.3.2/namen-0.3.2.tar.gz

---
+++ packages/namen/namen-0.3.2.tar.gz/namen-0.3.2/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['namen']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'namen',
+    'version': '0.3.2',
+    'description': '',
+    'long_description': None,
+    'author': 'Dennis van Bohemen',
+    'author_email': 'dennis.van.bohemen@profit4cloud.nl',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
