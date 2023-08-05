013fd6ebe252fde69c435098692d25db3a89c3c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:43 +0000

    Add package dsnd-distributions-cbui/0.1/dsnd_distributions_cbui-0.1.tar.gz

---
+++ packages/dsnd-distributions-cbui/dsnd_distributions_cbui-0.1.tar.gz/dsnd_distributions_cbui-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd_distributions_cbui',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      author = 'Cuong Bui',
+      author_email = 'buithecuong@gmail.com',
+      zip_safe=False)
