61fc44bc3b76e00bd56b2d69b431786854c531ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:04 +0000

    Add package mlog-extended/0.0.6/mlog_extended-0.0.6.tar.gz

---
+++ packages/mlog-extended/mlog_extended-0.0.6.tar.gz/mlog_extended-0.0.6/mlog_extended/__init__.py
@@ -0,0 +1,5 @@
+"""Mindustry Logic Extended: Provide extra instructions for mindustry logic.
+"""
+from .compilation_error import CompilationError
+from .basic_compiler import BasicCompiler
+from .extended_compiler import ExtendedCompiler
