bec9cec91730b5cd8302c6363e957688978ec698
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:14:05 +0000

    Add package ontospy/1.9.9.1/ontospy-1.9.9.1-py2.py3-none-any.whl

---
+++ packages/ontospy/ontospy-1.9.9.1-py2.py3-none-any.whl/ontospy/tests/quick.py
@@ -0,0 +1,66 @@
+# !/usr/bin/env python
+#  -*- coding: UTF-8 -*-
+"""
+Unit test stub for ontosPy
+
+Test Quick: use this file to quickly run scripts/tests which will then be integrated into proper tests
+
+"""
+
+import click 
+
+import unittest, os, sys
+from .. import *
+from ..core import *
+from ..core.utils import *
+
+
+from .context import TEST_RDF_FOLDER, TEST_SHAPES_FOLDER
+
+
+# sanity check
+print("-------------------\nOntospy ", VERSION, "\n-------------------")
+
+
+
+@click.command()
+@click.argument('test_number', nargs=1)
+def main(test_number):
+
+	test_number = int(test_number)
+
+
+	if test_number == 1:
+
+		print("=================\n*** QUICK TEST 1 ***\n=================\n")
+
+		f = TEST_RDF_FOLDER + "paper.jsonld"
+
+		o = Ontospy(f, verbose=True, rdf_format="json-ld", hide_implicit_types=False, hide_base_schemas=False, hide_implicit_preds=False)
+		print(f)			
+
+
+
+	if test_number == 2:
+
+		print("=================\n*** QUICK TEST 2 ***\n=================\n")
+
+		uri, title = "http://examples.com", "My ontology"
+		printDebug(click.style("[%d]" % 1, fg='blue') +
+				click.style(uri + " ==> ", fg='black') +
+				click.style(title, fg='red'))
+
+
+		from colorama import Fore, Style
+
+		printDebug(Fore.BLUE + Style.BRIGHT + "[%d]" % 1 + 
+              Style.RESET_ALL + uri + " ==> " + Fore.RED + title + 
+              Style.RESET_ALL)
+
+
+
+
+
+if __name__ == '__main__':
+	main()
+
