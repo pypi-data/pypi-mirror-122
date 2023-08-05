8651bc5160d661bb268a313c4508e07e4179ea48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:41 +0000

    Add package cirq-core/0.13.0.dev20211004030023/cirq_core-0.13.0.dev20211004030023-py3-none-any.whl

---
+++ packages/cirq-core/cirq_core-0.13.0.dev20211004030023-py3-none-any.whl/cirq/qis/channels_test.py
@@ -0,0 +1,156 @@
+# Copyright 2021 The Cirq Developers
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Tests for channels."""
+from typing import Iterable
+
+import numpy as np
+import pytest
+
+import cirq
+
+
+def apply_channel(channel: cirq.SupportsKraus, rho: np.ndarray) -> np.ndarray:
+    ks = cirq.kraus(channel)
+    d_out, d_in = ks[0].shape
+    assert rho.shape == (d_in, d_in)
+    out = np.zeros((d_out, d_out), dtype=np.complex128)
+    for k in ks:
+        out += k @ rho @ k.conj().T
+    return out
+
+
+def generate_standard_operator_basis(d_out: int, d_in: int) -> Iterable[np.ndarray]:
+    for i in range(d_out):
+        for j in range(d_in):
+            e_ij = np.zeros((d_out, d_in))
+            e_ij[i, j] = 1
+            yield e_ij
+
+
+def compute_choi(channel: cirq.SupportsKraus) -> np.ndarray:
+    ks = cirq.kraus(channel)
+    d_out, d_in = ks[0].shape
+    d = d_in * d_out
+    c = np.zeros((d, d), dtype=np.complex128)
+    for e in generate_standard_operator_basis(d_in, d_in):
+        c += np.kron(apply_channel(channel, e), e)
+    return c
+
+
+def compute_superoperator(channel: cirq.SupportsKraus) -> np.ndarray:
+    ks = cirq.kraus(channel)
+    d_out, d_in = ks[0].shape
+    m = np.zeros((d_out * d_out, d_in * d_in), dtype=np.complex128)
+    for k, e_in in enumerate(generate_standard_operator_basis(d_in, d_in)):
+        m[:, k] = np.reshape(apply_channel(channel, e_in), d_out * d_out)
+    return m
+
+
+@pytest.mark.parametrize(
+    'kraus_operators, expected_choi',
+    (
+        ([np.eye(2)], np.array([[1, 0, 0, 1], [0, 0, 0, 0], [0, 0, 0, 0], [1, 0, 0, 1]])),
+        (cirq.kraus(cirq.depolarize(0.75)), np.eye(4) / 2),
+        (
+            [
+                np.array([[1, 0, 0], [0, 0, 1]]) / np.sqrt(2),
+                np.array([[1, 0, 0], [0, 0, -1]]) / np.sqrt(2),
+            ],
+            np.diag([1, 0, 0, 0, 0, 1]),
+        ),
+    ),
+)
+def test_kraus_to_choi(kraus_operators, expected_choi):
+    """Verifies that cirq.kraus_to_choi computes the correct Choi matrix."""
+    assert np.allclose(cirq.kraus_to_choi(kraus_operators), expected_choi)
+
+
+@pytest.mark.parametrize(
+    'channel',
+    (
+        cirq.I,
+        cirq.X,
+        cirq.CNOT,
+        cirq.depolarize(0.1),
+        cirq.depolarize(0.1, n_qubits=2),
+        cirq.amplitude_damp(0.2),
+    ),
+)
+def test_operation_to_choi(channel):
+    """Verifies that cirq.operation_to_choi correctly computes the Choi matrix."""
+    n_qubits = cirq.num_qubits(channel)
+    actual = cirq.operation_to_choi(channel)
+    expected = compute_choi(channel)
+    assert np.isclose(np.trace(actual), 2 ** n_qubits)
+    assert np.all(actual == expected)
+
+
+def test_choi_for_completely_dephasing_channel():
+    """Checks cirq.operation_to_choi on the completely dephasing channel."""
+    assert np.all(cirq.operation_to_choi(cirq.phase_damp(1)) == np.diag([1, 0, 0, 1]))
+
+
+@pytest.mark.parametrize(
+    'kraus_operators, expected_superoperator',
+    (
+        ([np.eye(2)], np.eye(4)),
+        (
+            cirq.kraus(cirq.depolarize(0.75)),
+            np.array([[1, 0, 0, 1], [0, 0, 0, 0], [0, 0, 0, 0], [1, 0, 0, 1]]) / 2,
+        ),
+        (
+            [
+                np.array([[0, 1, 0], [0, 0, 1]]) / np.sqrt(2),
+                np.array([[0, 1, 0], [0, 0, -1]]) / np.sqrt(2),
+            ],
+            np.array(
+                [
+                    [0, 0, 0, 0, 1, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 1],
+                ]
+            ),
+        ),
+    ),
+)
+def test_kraus_to_superoperator(kraus_operators, expected_superoperator):
+    """Verifies that cirq.kraus_to_superoperator computes the correct channel matrix."""
+    assert np.allclose(cirq.kraus_to_superoperator(kraus_operators), expected_superoperator)
+    with cirq.testing.assert_deprecated(deadline='v0.14'):
+        assert np.allclose(cirq.kraus_to_channel_matrix(kraus_operators), expected_superoperator)
+
+
+@pytest.mark.parametrize(
+    'channel',
+    (
+        cirq.I,
+        cirq.X,
+        cirq.CNOT,
+        cirq.depolarize(0.1),
+        cirq.depolarize(0.1, n_qubits=2),
+        cirq.amplitude_damp(0.2),
+    ),
+)
+def test_operation_to_superoperator(channel):
+    """Verifies that cirq.operation_to_superoperator correctly computes the channel matrix."""
+    expected = compute_superoperator(channel)
+    assert np.all(expected == cirq.operation_to_superoperator(channel))
+    with cirq.testing.assert_deprecated(deadline='v0.14'):
+        assert np.all(expected == cirq.operation_to_channel_matrix(channel))
+
+
+def test_superoperator_for_completely_dephasing_channel():
+    """Checks cirq.operation_to_superoperator on the completely dephasing channel."""
+    assert np.all(cirq.operation_to_superoperator(cirq.phase_damp(1)) == np.diag([1, 0, 0, 1]))
