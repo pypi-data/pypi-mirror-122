b2c5c295597591b71aec6021d9c5d68d1c5445f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:26 +0000

    Add package pyshell-msg/0.1.1/pyshell-msg-0.1.1.tar.gz

---
+++ packages/pyshell-msg/pyshell-msg-0.1.1.tar.gz/pyshell-msg-0.1.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+def readme():
+    with open("Readme.md", "r") as f:
+        return f.read()
+
+VERSION = '0.1.1'
+DESCRIPTION = 'Log/print your status and messages on python shell or terminal'
+LONG_DESCRIPTION = readme()
+
+# Setting up
+setup(
+    name="pyshell-msg",
+    version=VERSION,
+    author="Ellipsion (Ashish Kumawat)",
+    author_email="<ellipsion@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'pyshell_msg', 'status', 'shell', 'messages', 'application', 'logs', 'status', 'print', 'format'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
