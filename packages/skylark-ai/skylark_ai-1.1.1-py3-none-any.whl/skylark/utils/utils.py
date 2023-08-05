155520f4877be4e8547bd5f0c94e8623d87093a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:05 +0000

    Add package skylark-ai/1.1.1/skylark_ai-1.1.1-py3-none-any.whl

---
+++ packages/skylark-ai/skylark_ai-1.1.1-py3-none-any.whl/skylark/utils/utils.py
@@ -0,0 +1,23 @@
+from constants.constants import WEBSOCKET_AUTH_API
+import requests
+import socket
+
+def get_websocket_auth_token(token):
+    try:
+        '''
+        changes --jash jain
+        '''
+        #changed the response so that when an api key(token) is passed a websocket token is retrieved
+        response = requests.post(WEBSOCKET_AUTH_API,data={'api_key':token})
+        if response.status_code == 201:
+            print("hello")
+            print(response.json())
+            return response.json().get('key')
+        else:
+            return None
+    except socket.gaierror:
+        print("Internet not found")
+
+
+def get_bytes_from_file(filename):
+    return open(filename, "rb").read()
