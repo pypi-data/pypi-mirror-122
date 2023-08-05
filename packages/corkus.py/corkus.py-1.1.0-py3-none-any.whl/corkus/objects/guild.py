cc2469b61ec487098de5d16eb268c910574f65e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:15 +0000

    Add package corkus.py/1.1.0/corkus.py-1.1.0-py3-none-any.whl

---
+++ packages/corkus.py/corkus.py-1.1.0-py3-none-any.whl/corkus/objects/guild.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+from .member import Member
+from typing import List
+from .base_guild import BaseGuild
+
+class Guild(BaseGuild):
+    """`Guilds <https://wynncraft.fandom.com/wiki/Guilds>`_ are Wynncraft communities of people
+    that work together to achieve their goals.
+    """
+    @property
+    def members(self) -> List[Member]:
+        """List of all members currently in guild."""
+        return [Member(self._corkus, self , m) for m in self._attributes.get("members", [])]
+
+    @property
+    def level_progress(self) -> float:
+        """Progress to next :py:attr:`level` in precentage 0-100%
+
+        .. caution::
+            This property is currently bugged and return invalid precentage. See:
+            `Wynncraft/WynncraftAPI#61 <https://github.com/Wynncraft/WynncraftAPI/issues/61>`_.
+        """
+        return self._attributes.get("xp", 0)
+
+    def __repr__(self) -> str:
+        return f"<Guild name={self.name!r} tag={self.tag!r} members={len(self.members)}>"
