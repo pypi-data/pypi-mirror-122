23056bcc3f4f8dcbca8404023abc35cb2d0683b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:41 +0000

    Add package openspace/0.2.1/openspace-0.2.1.tar.gz

---
+++ packages/openspace/openspace-0.2.1.tar.gz/openspace-0.2.1/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+setup(
+	name="openspace",
+	version="0.2.1",
+	description="Public package for various space operations applications",
+	long_description=long_description,
+	author="Brandon Sexton",
+	author_email="brandon.sexton.1@outlook.com",
+	entry_points={'console_scripts': ['openspace=openspace.geoplanner:run',
+	'reach_demo=openspace.reach_demo:run',
+	'sda_demo=openspace.sda_demo:run']},
+	packages=find_packages(),
+	include_package_data=True,
+	install_requires=[]
+	)
