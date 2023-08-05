bff3eacf907b9969fb262e9a80c576036951e404
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:10 +0000

    Add package distributions-ahmed/1.0/distributions_ahmed-1.0.tar.gz

---
+++ packages/distributions-ahmed/distributions_ahmed-1.0.tar.gz/distributions_ahmed-1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name = 'distributions_ahmed',
+version = '1.0',
+description = 'Gausssian and Binomaial distribution',
+packages = ['distributions_ahmed'],
+zip_safe = False)
