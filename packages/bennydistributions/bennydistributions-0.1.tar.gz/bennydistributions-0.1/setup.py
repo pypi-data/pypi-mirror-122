c565e33b98371d35dfedd88ab5096c5f9f03e18d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:17 +0000

    Add package bennydistributions/0.1/bennydistributions-0.1.tar.gz

---
+++ packages/bennydistributions/bennydistributions-0.1.tar.gz/bennydistributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='bennydistributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['bennydistributions'],
+      zip_safe=False)
