acf96b8f884557f06f243ea6135945beb99d1221
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:14:06 +0000

    Add package elowen-distributions/0.1/elowen_distributions-0.1.tar.gz

---
+++ packages/elowen-distributions/elowen_distributions-0.1.tar.gz/elowen_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='elowen_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['elowen_distributions'],
+      zip_safe=False)
