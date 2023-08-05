c7733b853a57dcf0e9fa95e3c2ddd40ed0777039
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:13:34 +0000

    Add package csvio/0.2.0/csvio-0.2.0-py3-none-any.whl

---
+++ packages/csvio/csvio-0.2.0-py3-none-any.whl/csvio/csvreader.py
@@ -0,0 +1,161 @@
+# MIT License
+#
+# csvio: A library for conveniently processing CSV files.
+#
+# Copyright (c) 2021 Salman Raza <raza.salman@gmail.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+import csv
+import traceback
+from typing import Any, Dict, List
+
+from .csvbase import CSVBase
+
+
+class CSVReader(CSVBase):
+    """
+    This object represents a CSV file for reading.
+
+    :param filename: Full path to the CSV file for reading.
+    :type filename: required
+
+    :param fieldnames:
+        A list of strings representing the column headings for the CSV
+        file.
+        If this list is specified while initiating an Object of this class
+        then it is used as the column headings. This is handy when the CSV
+        to read does not have column headings.
+        Otherwise this list is populated from the CSV that is set in the
+        *filename* argument of this Class's constructor.
+    :type fieldnames: optional
+
+    :param open_kwargs:
+        A dictionary of key, value pairs that should be passed to the open
+        method within this class.
+    :type open_kwargs: optional
+
+    :param csv_kwargs:
+        A dictionary of key, value pairs that should be passed to the
+        DictReader constructor within this class.
+    :type csv_kwargs: optional
+
+    Usage:
+
+    .. doctest::
+
+        >>> from csvio import CSVReader
+        >>> reader = CSVReader("fruit_stock.csv")
+        >>> reader.fieldnames
+        ['Supplier', 'Fruit', 'Quantity']
+
+        >>> len(reader.rows)
+        4
+
+        >>> import json
+        >>> print(json.dumps(reader.rows, indent=4))
+        [
+            {
+                "Supplier": "Big Apple",
+                "Fruit": "Apple",
+                "Quantity": "1"
+            },
+            {
+                "Supplier": "Big Melons",
+                "Fruit": "Melons",
+                "Quantity": "2"
+            },
+            {
+                "Supplier": "Big Mangoes",
+                "Fruit": "Mango",
+                "Quantity": "3"
+            },
+            {
+                "Supplier": "Small Strawberries",
+                "Fruit": "Strawberry",
+                "Quantity": "4"
+            }
+        ]
+
+    CSV file contents:
+
+    .. code-block:: bash
+
+            Supplier,Fruit,Quantity
+            Big Apple,Apple,1
+            Big Melons,Melons,2
+            Long Mangoes,Mango,3
+            Small Strawberries,Strawberry,4
+
+    """
+
+    def __init__(
+        self,
+        filename: str,
+        fieldnames: List[str] = [],
+        open_kwargs: Dict[str, str] = {},
+        csv_kwargs: Dict[str, Any] = {},
+    ) -> None:
+
+        super().__init__(filename, open_kwargs, csv_kwargs)
+
+        self.fieldnames = fieldnames or self.__get_fieldnames()
+        self.rows = self.__get_rows()
+
+    def __get_fieldnames(self) -> List[str]:
+
+        fieldnames: List[str] = []
+
+        try:
+            with open(self.filepath, "r", **self.open_kwargs) as fh:
+                csvreader = csv.DictReader(fh, **self.csv_kwargs)
+                fieldnames = csvreader.fieldnames  # type: ignore
+
+        except FileNotFoundError:
+            print("File to read not found: {}".format(self.filepath))
+            exit()
+
+        return fieldnames
+
+    def __get_rows(self) -> List[Dict[str, Any]]:
+
+        rows: List[Dict[str, Any]] = []
+
+        try:
+            with open(self.filepath, "r", **self.open_kwargs) as fh:
+
+                csv_reader = csv.DictReader(
+                    fh, fieldnames=self.fieldnames, **self.csv_kwargs
+                )
+                next(csv_reader)
+
+                for row in csv_reader:
+
+                    row_dict: Dict[str, Any] = {}
+
+                    for fieldname in self.fieldnames:
+                        row_dict[fieldname] = row[fieldname]
+
+                    rows.append(row_dict)
+
+        except csv.Error:
+
+            print("\nCSV Reader Error: {}\n".format(self.filepath))
+            traceback.print_exc()
+
+        return rows
---
+++ packages/csvio/csvio-0.2.0-py3-none-any.whl/csvio/csvwriter.py
@@ -0,0 +1,239 @@
+# MIT License
+#
+# csvio: A library for conveniently processing CSV files.
+#
+# Copyright (c) 2021 Salman Raza <raza.salman@gmail.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+import csv
+import traceback
+from typing import Any, Dict, List, Union
+
+from .csvbase import CSVBase
+
+
+class CSVWriter(CSVBase):
+    """
+    This object represents a CSV file for writing.
+
+    :param filename: Full path to the CSV file for writing.
+    :type filename: required
+
+    :param fieldnames:
+        A list of strings representing the column headings for the CSV
+        file.
+    :type fieldnames: required
+
+    :param open_kwargs:
+        A dictionary of key, value pairs that should be passed to the open
+        method within this class.
+    :type open_kwargs: optional
+
+    :param csv_kwargs:
+        A dictionary of key, value pairs that should be passed to the
+        DictReader constructor within this class.
+    :type csv_kwargs: optional
+
+    """
+
+    def __init__(
+        self,
+        filename: str,
+        fieldnames: List[str],
+        open_kwargs: Dict[str, str] = {},
+        csv_kwargs: Dict[str, Any] = {},
+    ) -> None:
+
+        super().__init__(filename, open_kwargs, csv_kwargs)
+
+        self._pending_rows: List[Dict[str, Any]] = []
+        self.fieldnames: List[str] = fieldnames
+        self._fieldnames_written: bool = False
+
+    @property
+    def pending_rows(self) -> List[Dict[str, Any]]:
+        """
+        :return: List of rows not flushed yet and are pending to be written
+        """
+        return self._pending_rows
+
+    @pending_rows.setter
+    def pending_rows(self, rows: List[Dict[str, Any]]) -> None:
+        self._pending_rows = rows
+
+    def __write_field_headings(self) -> bool:
+
+        if self.fieldnames:
+
+            with open(
+                self.filepath, "w", newline="", **self.open_kwargs
+            ) as wf:
+
+                writer = csv.DictWriter(
+                    wf, fieldnames=self.fieldnames, **self.csv_kwargs
+                )
+                writer.writeheader()
+
+            self._fieldnames_written = True
+
+            return True
+
+        else:
+            return False
+
+    def add_rows(
+        self, rows: Union[Dict[str, Any], List[Dict[str, Any]]]
+    ) -> None:
+        """
+        Add rows for writing to the output CSV.
+
+        All the rows to be written to the output CSV are collected using this
+        method.
+
+        This only collects the rows to be written without writing anything to
+        the output CSV. The rows are written to output only when the method
+        :py:meth:`csvio.CSVWriter.flush` is called.
+
+        :param rows:
+            A single dictionary or a list of dictionaries that repsresent the
+            row(s) to be written to the output CSV.
+        :type rows: required
+
+        Usage:
+
+        .. doctest::
+
+            >>> from csvio import CSVWriter
+            >>> writer = CSVWriter("fruit_stock.csv", fieldnames=["Supplier", "Fruit", "Quantity"])
+            >>> row1 = {"Supplier": "Big Apple", "Fruit": "Apple", "Quantity": 1}
+            >>> writer.add_rows(row1)
+            >>> rows2_3_4 = [
+            ...     {"Supplier": "Big Melons", "Fruit": "Melons", "Quantity": 2},
+            ...     {"Supplier": "Long Mangoes", "Fruit": "Mango", "Quantity": 3},
+            ...     {"Supplier": "Small Strawberries", "Fruit": "Strawberry", "Quantity": 4}
+            ... ]
+            >>> writer.add_rows(rows2_3_4)
+            >>> len(writer.pending_rows)
+            4
+
+            >>> len(writer.rows)
+            0
+
+        Notice that the :py:attr:`csvio.CSVWriter.rows` property is still
+        empty. This property is incremented by the number of currently pending
+        rows once they are flushed.
+
+        """
+
+        if isinstance(rows, dict):
+            rows = [rows]
+        elif isinstance(rows, list):
+            pass
+        else:
+            return None
+
+        for row in rows:
+            self.pending_rows.append(row)
+
+    def __write_rows(self) -> None:
+
+        row_to_write = None
+
+        try:
+            with open(
+                self.filepath, "a", newline="", **self.open_kwargs
+            ) as wf:
+
+                writer = csv.DictWriter(
+                    wf, fieldnames=self.fieldnames, **self.csv_kwargs
+                )
+
+                for pending_row in self.pending_rows:
+                    row_to_write = pending_row
+                    writer.writerow(row_to_write)
+
+        except csv.Error:
+            print(f"\nCSV Writer Error: {self.filepath}\n")
+            print(f"\nProblem Row: {row_to_write}\n")
+            traceback.print_exc()
+
+    def flush(self) -> None:
+        """
+        Write pending rows to the output CSV and reset the
+        :py:attr:`CSVWriter.pending_rows` property to an empty :obj:`list`
+
+        Usage:
+
+        .. doctest::
+
+            >>> from csvio import CSVWriter
+            >>> writer = CSVWriter("fruit_stock.csv", fieldnames=["Supplier", "Fruit", "Quantity"])
+            >>> row1 = {"Supplier": "Big Apple", "Fruit": "Apple", "Quantity": 1}
+            >>> writer.add_rows(row1)
+            >>> rows2_3_4 = [
+            ...     {"Supplier": "Big Melons", "Fruit": "Melons", "Quantity": 2},
+            ...     {"Supplier": "Long Mangoes", "Fruit": "Mango", "Quantity": 3},
+            ...     {"Supplier": "Small Strawberries", "Fruit": "Strawberry", "Quantity": 4}
+            ... ]
+            >>> writer.add_rows(rows2_3_4)
+            >>> len(writer.pending_rows)
+            4
+
+            >>> len(writer.rows)
+            0
+
+            >>> writer.flush()
+            >>> len(writer.pending_rows)
+            0
+
+            >>> len(writer.rows)
+            4
+
+        Once flush is called a CSV file with the name *fruit_stock.csv* will be
+        written with the following contents.
+
+        .. code-block:: bash
+
+            Supplier,Fruit,Quantity
+            Big Apple,Apple,1
+            Big Melons,Melons,2
+            Long Mangoes,Mango,3
+            Small Strawberries,Strawberry,4
+
+        """
+
+        if self.pending_rows:
+
+            if not self._fieldnames_written:
+                self.__write_field_headings()
+
+            self.__write_rows()
+            self.rows += self.pending_rows
+            self.pending_rows = []
+
+    def write_blank_csv(self) -> None:
+        """
+        Write a blank CSV with only the column headings.
+
+        If the CSV already exists with any rows in it, it will be overwritten
+        and its contents will be replaced with only the column headings.
+        """
+
+        if not self._fieldnames_written:
+            self.__write_field_headings()
