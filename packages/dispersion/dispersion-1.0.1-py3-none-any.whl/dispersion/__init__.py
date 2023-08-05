181c2170730cb3ef7978d3a7209101c5e52a875b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:59 +0000

    Add package dispersion/1.0.1/dispersion-1.0.1-py3-none-any.whl

---
+++ packages/dispersion/dispersion-1.0.1-py3-none-any.whl/dispersion/__init__.py
@@ -0,0 +1,29 @@
+import numpy as np
+import sys
+import warnings
+from dispersion.spectrum import Spectrum
+from dispersion.spectral_data import *
+from dispersion.io import Writer, Reader
+from dispersion.material import Material
+from dispersion.catalogue import Catalogue, rebuild_catalogue
+from dispersion.config import get_config
+
+__version__ = "1.0.1"
+__all__ = ["Spectrum", "Material", "Catalogue", "get_config",
+           "Constant", "Interpolation", "Extrapolation",
+           "Sellmeier", "Sellmeier2", "Polynomial",
+           "RefractiveIndexInfo", "Cauchy", "Gases",
+           "Herzberger", "Retro", "Exotic", "Drude",
+           "DrudeLorentz", "rebuild_catalogue", "Writer", "Reader"]
+
+config = get_config()
+
+try:
+    from ruamel.yaml import YAML
+    from ruamel.yaml import scalarstring
+    from ruamel.yaml.comments import CommentedMap
+except ModuleNotFoundError as exc:
+    if config['WarnMissingPackages']:
+        warnings.warn("preferred yaml package ruamel.yaml not installed, " +
+                      "falling back to PyYAML, writing yaml files may give " +
+                      "inconsistent round trip results")
