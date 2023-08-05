9103cecfaaada5bc046d596c426a80e73c18ef0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:59 +0000

    Add package elementary-lineage/0.0.11/elementary_lineage-0.0.11-py3-none-any.whl

---
+++ packages/elementary-lineage/elementary_lineage-0.0.11-py3-none-any.whl/lineage/query_history.py
@@ -0,0 +1,61 @@
+from datetime import datetime, timedelta
+from typing import Optional
+
+from lineage.query_context import QueryContext
+from lineage.utils import is_flight_mode_on
+import json
+import os
+
+
+class QueryHistory(object):
+
+    INFORMATION_SCHEMA_QUERY_HISTORY = None
+
+    LATEST_QUERY_HISTORY_FILE = './latest_query_history.json'
+
+    def __init__(self, con, should_export_query_history: bool = True) -> None:
+        self.con = con
+        self.should_export_query_history = should_export_query_history
+
+    def _serialize_query_history(self, queries: [str]) -> None:
+        if self.should_export_query_history:
+            with open(self.LATEST_QUERY_HISTORY_FILE, 'w') as query_history_file:
+                serialized_queries = []
+                for query, query_context in queries:
+                    serialized_queries.append((query, query_context.to_dict()))
+                json.dump(serialized_queries, query_history_file)
+
+    def _deserialize_query_history(self) -> [str]:
+        queries = []
+        if os.path.exists(self.LATEST_QUERY_HISTORY_FILE):
+            with open(self.LATEST_QUERY_HISTORY_FILE, 'r') as query_history_file:
+                deserialized_queries = json.load(query_history_file)
+                for query, query_context_dict in deserialized_queries:
+                    queries.append((query, QueryContext.from_dict(query_context_dict)))
+        return queries
+
+    @staticmethod
+    def _include_end_date(end_date: datetime) -> Optional[datetime]:
+        if end_date is not None and (end_date.hour, end_date.minute, end_date.second) == (0, 0, 0):
+            return end_date + timedelta(hours=23, minutes=59, seconds=59)
+
+        return end_date
+
+    def extract_queries(self, start_date: datetime, end_date: datetime) -> [tuple]:
+        if is_flight_mode_on():
+            queries = self._deserialize_query_history()
+        else:
+            queries = self._query_history_table(start_date, end_date)
+
+            self._serialize_query_history(queries)
+
+        return queries
+
+    def _query_history_table(self, start_date: datetime, end_date: datetime) -> [tuple]:
+        pass
+
+    def get_database_name(self):
+        pass
+
+    def get_schema_name(self):
+        pass
