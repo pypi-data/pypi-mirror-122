b9e81d31643fed2c45ea06ac254547d54d44f4d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:47 +0000

    Add package git-interface/0.2.0/git-interface-0.2.0.tar.gz

---
+++ packages/git-interface/git-interface-0.2.0.tar.gz/git-interface-0.2.0/git_interface/exceptions.py
@@ -0,0 +1,27 @@
+"""
+Exceptions that could be raised during one of the git commands
+"""
+
+
+class GitException(Exception):
+    pass
+
+
+class NoCommitsException(GitException):
+    pass
+
+
+class NoBranchesException(GitException):
+    pass
+
+
+class AlreadyExistsException(GitException):
+    pass
+
+
+class NoLogsException(GitException):
+    pass
+
+
+class UnknownRevisionException(GitException):
+    pass
