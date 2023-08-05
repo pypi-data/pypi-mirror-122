a957c1ad3b9f073c210570b90cb9bf757ce403ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:32 +0000

    Add package data-preparation/0.0.1/data_preparation-0.0.1.tar.gz

---
+++ packages/data-preparation/data_preparation-0.0.1.tar.gz/data_preparation-0.0.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.6',
+]
+
+setup(
+    name='data_preparation',
+    version='0.0.1',
+    description='Transformations to a dataframe using python',
+    long_description=open('README.rst').read(),
+    url='',
+    author='Anjana V',
+    author_email='anjana.v@bdb.ai',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='python transformation',
+    packages=find_packages(),
+    install_requires=[
+          'pandas',
+        'numpy',
+        'num2words'
+      ]
+)
