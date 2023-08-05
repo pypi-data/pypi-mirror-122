50f2a39ed1a1f6d3e818ee288d25f9122f5b420d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:30 +0000

    Add package gndbd-probability/1.0/gndbd_probability-1.0.tar.gz

---
+++ packages/gndbd-probability/gndbd_probability-1.0.tar.gz/gndbd_probability-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name ='gndbd_probability',
+      version ='1.0',
+      description ='Gaussian or normal distributions and binomial distribution',
+      packages =['gndbd_probability'],
+      author = 'Injamul Hoque',
+      author_email = 'injamulhoque005@gmail.com',
+      zip_safe = False)
