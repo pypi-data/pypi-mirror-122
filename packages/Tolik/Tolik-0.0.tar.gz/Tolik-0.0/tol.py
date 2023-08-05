142c745cd41d32ff1c3e419ecb647e579eb214ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:20 +0000

    Add package Tolik/0.0/Tolik-0.0.tar.gz

---
+++ packages/Tolik/Tolik-0.0.tar.gz/Tolik-0.0/tol.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(name='Tolik',
+      version='0.0',
+      description='Gaussian and Binomial distributions',
+      packages=['Tolik'],
+      author_email='KirillGlaz13@yandex.ru',
+      zip_safe=False)
+
+
+class Tolik:
+    def толик(a):
+        print(a)
+        
+        
+    def все_толики(a):
+        return range(a)
+        
+        
+    def толик_число(a):
+        return int(a)
+        
