39314055d2ceb9fc2d418270150dbd73859df1d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:02 +0000

    Add package livemark/0.74.1/livemark-0.74.1.tar.gz

---
+++ packages/livemark/livemark-0.74.1.tar.gz/livemark-0.74.1/livemark/plugins/cards/script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+  for (const link of $("a")) {
+    const href = $(link).attr("href");
+    if (href.startsWith("#card=")) {
+      const code = href.split("=")[1];
+      $(link).click(async () => {
+        // TODO: handle errors
+        const response = await fetch(`/assets/cards/${code}.html`);
+        const html = await response.text();
+        $("#livemark-cards .modal-title").html("");
+        $("#livemark-cards .modal-body").html(html);
+        $("#livemark-cards h1").appendTo("#livemark-cards .modal-title");
+        $("#livemark-cards .modal").modal();
+      });
+    }
+  }
+});
