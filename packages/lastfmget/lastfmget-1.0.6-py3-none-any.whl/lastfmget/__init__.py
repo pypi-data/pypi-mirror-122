87def1dfed976737b5d59a5ed8795a88aafeb245
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:56 +0000

    Add package lastfmget/1.0.6/lastfmget-1.0.6-py3-none-any.whl

---
+++ packages/lastfmget/lastfmget-1.0.6-py3-none-any.whl/lastfmget/__init__.py
@@ -0,0 +1,13 @@
+"""
+A Python package for accessing Last.fm scrobble data using the public API.
+
+### PyPi
+`pypi.org/project/lastfmget`
+
+### Github
+`github.com/D3r3k23/LastFmGet`
+"""
+from .core        import init
+from .methods     import *
+from .raw_methods import *
+from .errors      import *
