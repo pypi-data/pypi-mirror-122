593fb6c2e8ffaaa851d73a5587bfcc7cc4aa1798
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:23 +0000

    Add package tower-defense/3.6.0/tower_defense-3.6.0-py3-none-any.whl

---
+++ packages/tower-defense/tower_defense-3.6.0-py3-none-any.whl/tower_defense/functions.py
@@ -0,0 +1,28 @@
+from tower_defense.constants import *
+
+
+def updateDict(d: dict, l: list) -> dict:
+    """Re-order the items in d based on the items in l and deletes redundant items."""
+
+    newDict = {}
+    oldDict = d.copy()
+
+    keys = list(oldDict.keys())
+    values = list(oldDict.values())
+
+    for item in l:
+        try:
+            index = keys.index(item)
+            newDict[keys[index]] = values[index]
+        except ValueError:
+            continue
+
+    return newDict
+
+
+def removeCharset(s, charset) -> str:
+    """Remove all charaters in charset from s and returns the result."""
+    for char in charset:
+        s = s.replace(char, '')
+
+    return s
