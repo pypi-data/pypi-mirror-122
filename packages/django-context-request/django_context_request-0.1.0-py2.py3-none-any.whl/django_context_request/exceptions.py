c5aea2aae1127d9b29e91d8ef2f6cdf3a5fa4de1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:59 +0000

    Add package django-context-request/0.1.0/django_context_request-0.1.0-py2.py3-none-any.whl

---
+++ packages/django-context-request/django_context_request-0.1.0-py2.py3-none-any.whl/django_context_request/exceptions.py
@@ -0,0 +1,16 @@
+class RequestContextProxyError(object):
+
+    class WrongWrappedFunc(Exception):
+        pass
+
+    class ObjNotFound(Exception):
+        pass
+
+    class ObjReadOnly(Exception):
+        pass
+
+
+class RequestContextError(object):
+
+    class ObjExisted(Exception):
+        pass
