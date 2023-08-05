6e97cc54ab86bd7009efd97ef8b349d16cf5135d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:07 +0000

    Add package fpql/0.4.10/fpql-0.4.10.tar.gz

---
+++ packages/fpql/fpql-0.4.10.tar.gz/fpql-0.4.10/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fpql", # Replace with your own username
+    version="0.4.10",
+    author="ROSLI MOHD SANI",
+    author_email="romsey67@gmail.com",
+    description="A python package for financial instruments using QuantLib",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/romsey67/fpql-project.git",
+    packages=setuptools.find_packages(),
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires = ["QuantLib",
+                        "pydantic",
+                        "typing-extensions",
+                        'numpy',
+                        'matplotlib',
+                        
+    ],
+    python_requires='>=3.6',
+)
