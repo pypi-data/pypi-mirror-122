ed364350a70764b2a739cc55037d89668b0e8374
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:13 +0000

    Add package django-mass-edit/3.3.0/django-mass-edit-3.3.0.tar.gz

---
+++ packages/django-mass-edit/django-mass-edit-3.3.0.tar.gz/django-mass-edit-3.3.0/massadmin/urls.py
@@ -0,0 +1,9 @@
+from django.urls import re_path
+from .massadmin import mass_change_view
+
+
+urlpatterns = [
+    re_path(r'(?P<app_name>[^/]+)/(?P<model_name>[^/]+)-masschange/(?P<object_ids>[\w,\.\-]+)/$',
+     mass_change_view,
+     name='massadmin_change_view'),
+]
