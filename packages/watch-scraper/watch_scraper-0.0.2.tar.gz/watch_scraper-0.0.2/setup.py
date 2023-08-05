9433e577d1af1d3f295dc9b055bd939eb0f677dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:03 +0000

    Add package watch-scraper/0.0.2/watch_scraper-0.0.2.tar.gz

---
+++ packages/watch-scraper/watch_scraper-0.0.2.tar.gz/watch_scraper-0.0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='watch_scraper',
+    version='0.0.2',
+    description='Aicore project that allows the user to scrape mens watch data from the gold smiths website',
+    author='Tafadzwa Zama',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=['selenium', 'tqdm', 'sqlalchemy', 'pandas', 'boto3', 'psycopg2', 'numpy'] 
+)
