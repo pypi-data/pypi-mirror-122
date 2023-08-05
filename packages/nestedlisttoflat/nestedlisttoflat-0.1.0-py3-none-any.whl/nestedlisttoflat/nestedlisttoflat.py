2ceba688799b36fb239fe38aed1a3b7a475bcdc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:39 +0000

    Add package nestedlisttoflat/0.1.0/nestedlisttoflat-0.1.0-py3-none-any.whl

---
+++ packages/nestedlisttoflat/nestedlisttoflat-0.1.0-py3-none-any.whl/nestedlisttoflat/nestedlisttoflat.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  8 12:37:45 2021
+
+@author: avik_
+"""
+
+def flatten_list(_2d_list):
+    flat_list = []
+    # Iterate through the outer list
+    for element in _2d_list:
+        if type(element) is list:
+            # If the element is of type list, iterate through the sublist
+            for item in element:
+                flat_list.append(item)
+        else:
+            flat_list.append(element)
+    return flat_list
