f95c39cab4af6673968a3ca9f14393ffde65cddb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:17 +0000

    Add package Newname/0.0.1/Newname-0.0.1.tar.gz

---
+++ packages/Newname/Newname-0.0.1.tar.gz/Newname-0.0.1/car_scraper/__main__.py
@@ -0,0 +1,11 @@
+#__package__ = None
+from car_scraping_1 import Carscraper
+from rds_uploader.rds_scraped_ebay_cars import DataHandling
+
+
+
+simulate = DataHandling()
+scrap_upload = simulate.run_data()
+# simulate = Carscraper()
+#
+# car_property = simulate.get_car_details()
