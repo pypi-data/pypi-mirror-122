cab267ed88bd9894e0af55b2fe941fc038faaebb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:14:22 +0000

    Add package oneNeuron-pypi-AnkitJha16/0.0.1/oneNeuron_pypi-AnkitJha16-0.0.1.tar.gz

---
+++ packages/oneNeuron-pypi-AnkitJha16/oneNeuron_pypi-AnkitJha16-0.0.1.tar.gz/oneNeuron_pypi-AnkitJha16-0.0.1/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+PROJECT_NAME = "oneNeuron_pypi"
+USERNAME = "AnkitJha16"
+
+
+setuptools.setup(
+    name=f"{PROJECT_NAME}-{USERNAME}",
+    version="0.0.1",
+    author=USERNAME,
+    author_email="freelancer.ankit.jha@gmail.com",
+    description="Its an Implementation of perceptron",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{USERNAME}/{PROJECT_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{USERNAME}/{PROJECT_NAME}/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7",
+    install_requires=["numpy","logging","tqdm"]
+)
