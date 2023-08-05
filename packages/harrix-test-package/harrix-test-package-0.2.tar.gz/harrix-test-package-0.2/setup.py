18d66eb87cafd036ccdd826e042d26d60ad55b76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:40 +0000

    Add package harrix-test-package/0.2/harrix-test-package-0.2.tar.gz

---
+++ packages/harrix-test-package/harrix-test-package-0.2.tar.gz/harrix-test-package-0.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+import pathlib
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+setup(
+    name="harrix-test-package",
+    version="0.2",
+    description="Test package",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/Harrix/harrix-test-package",
+    author="Anton Sergienko",
+    author_email="anton.b.sergienko@gmail.com",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+)
---
+++ packages/harrix-test-package/harrix-test-package-0.2.tar.gz/harrix-test-package-0.2/src/harrixtestpackage/functions.py
@@ -0,0 +1,8 @@
+def multiply_2(x):
+    return x * 2
+
+def multiply_10(x):
+    return x * 10
+
+def multiply_20(x):
+    return x * 20
