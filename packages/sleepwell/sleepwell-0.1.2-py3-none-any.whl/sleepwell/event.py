02d6c409345fd6bc90095bd70027eb81767f7e20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:05:49 +0000

    Add package sleepwell/0.1.2/sleepwell-0.1.2-py3-none-any.whl

---
+++ packages/sleepwell/sleepwell-0.1.2-py3-none-any.whl/sleepwell/event.py
@@ -0,0 +1,47 @@
+"""
+define the event objects 
+
+format data to be sent 
+"""
+import json
+import requests
+from sleepwell import state
+from utils import JSONEncoder
+
+
+class Event:
+    """
+
+    Event class for recording events to be sent on Sleep well API
+
+    """
+
+    def __init__(self, data={}):
+        self._data = data
+
+    def add_field(self, key, value):
+        self._data[key] = value
+
+    def add(self, data):
+        try:
+            for k, v in data.items():
+                self.add_field(k, v)
+
+        except AttributeError:
+            raise TypeError("add requires a dict-like argument")
+
+    def is_empty(self):
+        return len(self._data) == 0
+
+    def __str__(self):
+        return json.dumps(self._data, sort_keys=True, indent=1, cls=JSONEncoder)
+
+    def send(self):
+        if state.URL == None:
+            return Exception("No url provided")
+
+        requests.post(
+            state.URL,
+            headers={"Content-type": "application/json"},
+            data=json.dumps(self._data, sort_keys=True, indent=1, cls=JSONEncoder),
+        )
