7d32ecbf67a54b9751552af58bcb451dc048f636
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:02 +0000

    Add package gauscalc/1.0/gauscalc-1.0.tar.gz

---
+++ packages/gauscalc/gauscalc-1.0.tar.gz/gauscalc-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gauscalc',
+      version='1.0',
+      description='Calculate and Plot Gaussian and Binomail distributions',
+      packages=['gauscalc'],
+      author = 'Abdullah Alam Tapadar',
+      author_email = 'abdullah.alam.tapadar2000@gmail.com',
+      zip_safe=False)
