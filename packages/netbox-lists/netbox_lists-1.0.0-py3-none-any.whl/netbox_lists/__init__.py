bf2eac3e4d1ccc104c18439e864e9cabb8a3d421
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:17 +0000

    Add package netbox-lists/1.0.0/netbox_lists-1.0.0-py3-none-any.whl

---
+++ packages/netbox-lists/netbox_lists-1.0.0-py3-none-any.whl/netbox_lists/__init__.py
@@ -0,0 +1,19 @@
+__version__ = '0.1.0'
+
+from extras.plugins import PluginConfig
+
+
+class ListsPluginConfig(PluginConfig):
+    name = "netbox_lists"
+    verbose_name = "NetBox Lists"
+    version = "0.1.0"
+    author = "Devon Mar"
+    base_url = "lists"
+    required_settings = []
+    default_settings = {
+        "as_cidr": True,
+        "service_primary_ips": True
+    }
+
+
+config = ListsPluginConfig
