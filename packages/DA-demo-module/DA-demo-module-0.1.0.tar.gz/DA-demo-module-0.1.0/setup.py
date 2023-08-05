ca0237179b4f595f06291bde0180ae4887a76cb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:21 +0000

    Add package DA-demo-module/0.1.0/DA-demo-module-0.1.0.tar.gz

---
+++ packages/DA-demo-module/DA-demo-module-0.1.0.tar.gz/DA-demo-module-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='DA-demo-module',
+    version='0.1.0',    
+    description='A example Python package',
+    author='D-Korobkov',
+    author_email='email@email.com',
+    url='https://colab.research.google.com/drive/1e42VW-f5vJM1BxK_Fw2kh1sjJfSV1vGs#scrollTo=loose-emerald',
+    packages=['DA-demo-module'],
+    install_requires=['numpy==1.20.3'],
+
+    classifiers=['Programming Language :: Python :: 3.6'],
+)
