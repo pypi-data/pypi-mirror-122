f4e2e23f16c04ff1d120ef869793f88d5d37214d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:11 +0000

    Add package zcls/0.13.1/zcls-0.13.1.tar.gz

---
+++ packages/zcls/zcls-0.13.1.tar.gz/zcls-0.13.1/zcls/config/key_word.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+"""
+@date: 2020/12/28 上午10:35
+@file: key_word.py
+@author: zj
+@description: 
+"""
+
+KEY_OUTPUT = 'output'
+KEY_LOSS = 'loss'
+
+KEY_IMGS = 'imgs'
+KEY_TARGETS = 'targets'
+KEY_CLASSES = 'classes'
