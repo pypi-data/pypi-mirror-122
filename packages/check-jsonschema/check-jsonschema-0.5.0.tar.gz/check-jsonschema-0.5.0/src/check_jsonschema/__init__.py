42aeccd50f147d539dfe384a3d5a53ea56abe1d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:56 +0000

    Add package check-jsonschema/0.5.0/check-jsonschema-0.5.0.tar.gz

---
+++ packages/check-jsonschema/check-jsonschema-0.5.0.tar.gz/check-jsonschema-0.5.0/src/check_jsonschema/__init__.py
@@ -0,0 +1,33 @@
+import sys
+
+import jsonschema
+
+from .loaders import InstanceLoader, SchemaLoader
+from .parse_cli import parse_args
+
+
+def main():
+    args = parse_args()
+
+    schema_loader = SchemaLoader(args.schemafile, args.cache_filename, args.no_cache)
+    validator = schema_loader.get_validator()
+
+    instances = InstanceLoader(
+        args.instancefiles, default_filetype=args.default_filetype
+    )
+
+    failures = {}
+    for filename, doc in instances.iter_files():
+        try:
+            validator.validate(instance=doc)
+        except jsonschema.ValidationError as err:
+            failures[filename] = err
+    if failures:
+        print("Schema validation errors were encountered.")
+        for filename, err in failures.items():
+            path = [str(x) for x in err.path] or ["<root>"]
+            path = ".".join(x if "." not in x else f'"{x}"' for x in path)
+            print(f"  \033[0;33m{filename}::{path}: \033[0m{err.message}")
+        sys.exit(1)
+
+    print("ok -- validation done")
