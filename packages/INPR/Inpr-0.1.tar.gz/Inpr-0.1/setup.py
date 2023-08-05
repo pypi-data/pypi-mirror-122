ae537f6ff1c0fbdad258a683c4ca935dafc27d22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:11 +0000

    Add package INPR/0.1/Inpr-0.1.tar.gz

---
+++ packages/INPR/Inpr-0.1.tar.gz/Inpr-0.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+import setuptools
+
+with open("requirements.txt",'r') as f:
+    requirements = f.read().splitlines()
+
+with open("README.md",'r') as f:
+    long_description = f.read()
+
+
+setup(
+    name='Inpr',
+    include_package_data=True,
+    version='0.1',
+    description='A deep learning based project to detect indian number plates',
+    author='Prathmesh Patil',
+    author_email='prathmesh.patil8899@gmail.com',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages= ['inpr'],
+    keywords=['Deep Learning','Computer Vision','Pytorch','Detectron2'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    py_modules=['INPR'],
+    install_requires=requirements
+    
+)
