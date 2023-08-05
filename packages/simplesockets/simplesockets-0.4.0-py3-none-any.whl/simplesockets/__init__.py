ccacee85fd4096d9594451b6dac6346fcdcdca59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:02 +0000

    Add package simplesockets/0.4.0/simplesockets-0.4.0-py3-none-any.whl

---
+++ packages/simplesockets/simplesockets-0.4.0-py3-none-any.whl/simplesockets/__init__.py
@@ -0,0 +1,3 @@
+from .simple_sockets import TCPServer, TCPClient
+from .secure_sockets import SecureServer, SecureClient
+from . import typehints
---
+++ packages/simplesockets/simplesockets-0.4.0-py3-none-any.whl/tests/test_simple_sockets.py
@@ -0,0 +1,44 @@
+import simplesockets.simple_sockets as s
+
+
+def test_data_exchange():
+
+    Client = s.TCPClient()
+    Server = s.TCPServer(1)
+
+    pr = lambda client, data: client.send(data.response)
+    on_connect = lambda *args: print("connect", args)
+    on_receive = lambda *args: print(args)
+    on_disconnect = lambda *args: print(args)
+
+    Server.setup(on_receive=pr, on_connect=on_connect, on_disconnect=on_disconnect)
+    Client.setup("localhost", on_connect=on_connect, on_receive=on_receive, on_disconnect=on_disconnect)
+    Server.start()
+    Client.connect()
+
+    Client.autorecv()
+
+    text = b'test'
+
+    Client.send_data(text)
+
+    event, value = Client.await_event(disable_on_functions=True, timeout=3000)
+
+    Client.close()
+    Server.close()
+    if Server.event.exception.occurred:
+        print(Server.return_exceptions())
+
+    assert event == Client.EVENT_RECEIVED and value[0].response == text
+
+def test_reconnect():
+    Client = s.TCPClient()
+    Server = s.TCPServer(1)
+
+    Server.setup()
+    Client.setup("localhost")
+
+    Server.start()
+    Client.reconnect()
+
+    assert Client.event.connected is True
