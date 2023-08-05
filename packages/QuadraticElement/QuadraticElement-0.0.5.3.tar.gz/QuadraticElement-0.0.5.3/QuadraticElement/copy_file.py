5ad7d3714b1d8a98a7b47547cf43141d4bf95abc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:29 +0000

    Add package QuadraticElement/0.0.5.3/QuadraticElement-0.0.5.3.tar.gz

---
+++ packages/QuadraticElement/QuadraticElement-0.0.5.3.tar.gz/QuadraticElement-0.0.5.3/QuadraticElement/copy_file.py
@@ -0,0 +1,17 @@
+import os
+import shutil
+
+def mycopyfile(srcfile,dstpath):
+    if not os.path.isfile(srcfile):
+        print ("%s not exist!"%(srcfile))
+    else:
+        fpath,fname=os.path.split(srcfile)
+        if not os.path.exists(dstpath):
+            os.makedirs(dstpath)
+        shutil.copy(srcfile, dstpath + fname)
+        print ("copy %s -> %s"%(srcfile, dstpath + fname))
+ 
+ 
+src_dir = 'ME.py'
+dst_dir = os.path.dirname(__file__)
+mycopyfile(src_dir, dst_dir+"\\kkk\\")
