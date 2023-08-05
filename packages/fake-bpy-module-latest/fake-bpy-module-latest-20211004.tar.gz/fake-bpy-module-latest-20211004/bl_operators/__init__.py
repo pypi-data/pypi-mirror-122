7b4a04249ef8bcfb4a75ef389edea8485fff60cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:52 +0000

    Add package fake-bpy-module-latest/20211004/fake-bpy-module-latest-20211004.tar.gz

---
+++ packages/fake-bpy-module-latest/fake-bpy-module-latest-20211004.tar.gz/fake-bpy-module-latest-20211004/bl_operators/__init__.py
@@ -0,0 +1,46 @@
+import sys
+import typing
+from . import rigidbody
+from . import view3d
+from . import constraint
+from . import geometry_nodes
+from . import object_align
+from . import vertexpaint_dirt
+from . import uvcalc_lightmap
+from . import spreadsheet
+from . import object_quick_effects
+from . import screen_play_rendered_anim
+from . import sequencer
+from . import mesh
+from . import uvcalc_follow_active
+from . import file
+from . import object_randomize_transform
+from . import freestyle
+from . import clip
+from . import console
+from . import add_mesh_torus
+from . import assets
+from . import userpref
+from . import bmesh
+from . import object
+from . import image
+from . import presets
+from . import node
+from . import anim
+from . import wm
+
+
+def register():
+    ''' 
+
+    '''
+
+    pass
+
+
+def unregister():
+    ''' 
+
+    '''
+
+    pass
---
+++ packages/fake-bpy-module-latest/fake-bpy-module-latest-20211004.tar.gz/fake-bpy-module-latest-20211004/freestyle/__init__.py
@@ -0,0 +1,8 @@
+import sys
+import typing
+from . import shaders
+from . import predicates
+from . import types
+from . import utils
+from . import functions
+from . import chainingiterators
