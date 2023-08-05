8c457660ee6a557decac65345e5c8a32e8221a57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:53 +0000

    Add package chromilo-distributions/0.1/chromilo-distributions-0.1.tar.gz

---
+++ packages/chromilo-distributions/chromilo-distributions-0.1.tar.gz/chromilo-distributions-0.1/README.md
@@ -0,0 +1,11 @@
+# chromilo-distribution package
+
+Summary of the package
+
+# Files
+The distribution packages are from the AWS ML Foundation course
+These py programs calculate Binomial and Gaussian 
+
+
+
+
---
+++ packages/chromilo-distributions/chromilo-distributions-0.1.tar.gz/chromilo-distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='chromilo-distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['chromilo-distributions'],
+	  author='Chromilo Amin',
+	  author_email='chromiloamin@gmail.com',
+      zip_safe=False)
