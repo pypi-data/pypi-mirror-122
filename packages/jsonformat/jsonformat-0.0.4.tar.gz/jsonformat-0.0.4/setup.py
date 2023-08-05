f4b922a45c6152b9889a33a36500599837887b76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:49 +0000

    Add package jsonformat/0.0.4/jsonformat-0.0.4.tar.gz

---
+++ packages/jsonformat/jsonformat-0.0.4.tar.gz/jsonformat-0.0.4/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+
+setuptools.setup(
+    name='jsonformat',
+    version='0.0.4',
+    license="MIT",
+    entry_points={
+        'console_scripts': ['jsonformat=jsonformat:cli'],
+    },
+    author='Alex Pylypenko',
+    description='Simple command line utility to format json files.',
+    packages=['jsonformat'],
+    install_requires=[
+        'setuptools',
+        'pip'
+    ],
+    python_requires='>=3.3'
+)
