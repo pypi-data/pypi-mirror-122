d0414e3fc90565324f1481aa50910a49ff32cd5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:20 +0000

    Add package dtrs-gaussian-binomial/1.2/dtrs_gaussian_binomial-1.2.tar.gz

---
+++ packages/dtrs-gaussian-binomial/dtrs_gaussian_binomial-1.2.tar.gz/dtrs_gaussian_binomial-1.2/dtrs_gaussian_binomial/__init__.py
@@ -0,0 +1,5 @@
+from .Gaussiandistribution import Gaussian
+
+# TODO: import the Binomial class from the Binomialdistribution module
+from .Gaussiandistribution import Distribution
+from .Binomialdistribution import Binomial
---
+++ packages/dtrs-gaussian-binomial/dtrs_gaussian_binomial-1.2.tar.gz/dtrs_gaussian_binomial-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dtrs_gaussian_binomial',
+      version='1.2',
+      description='Gaussian and Binomial distributions',
+      packages=['dtrs_gaussian_binomial'],
+      author ='ons cherif',
+      author_email ='oscheriff@gmail.com',
+      zip_safe=False)
