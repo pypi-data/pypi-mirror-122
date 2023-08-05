36bf43650b2e0da8ba46928fd1465622db106745
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:38 +0000

    Add package CEA-Wrap/1.3.1/CEA_Wrap-1.3.1.tar.gz

---
+++ packages/CEA-Wrap/CEA_Wrap-1.3.1.tar.gz/CEA_Wrap-1.3.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup( name='CEA_Wrap',
+                  version='1.3.1',
+                  description='A Python-Based wrapper for the NASA CEA Thermochemical Code',
+                  url='https://github.com/civilwargeeky/CEA_Wrap',
+                  author='civilwargeeky',
+                  author_email='klingerd@purdue.edu',
+                  license_files=["LICENSE"],
+                  install_requires=[],
+                  packages=setuptools.find_packages(),
+                  python_requires=">=3.6",
+                  package_data={
+                    "": ["FCEA2.exe", "thermo.lib", "trans.lib", "thermo_spg.inp", "thermo_spg.out"],
+                  },
+                  include_package_data=True, # Add in pdfs and executables
+                  zip_safe=False)
