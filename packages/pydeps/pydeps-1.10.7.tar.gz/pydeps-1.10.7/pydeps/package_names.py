5d969af2f77aaaba45c1173854f9bf436758b74c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:01 +0000

    Add package pydeps/1.10.7/pydeps-1.10.7.tar.gz

---
+++ packages/pydeps/pydeps-1.10.7.tar.gz/pydeps-1.10.7/pydeps/package_names.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+import os
+import site
+
+
+def find_package_names():
+    site_package_dirs = [site.getusersitepackages()]
+    site_package_dirs += site.getsitepackages()
+    # initialize with well-known packages that don't seem to have a top_level.txt
+    res = {
+        'yaml': 'PyYAML',
+        'Crypto': 'pycrypto',
+    }
+    for site_packages in reversed(site_package_dirs):
+        if not os.path.isdir(site_packages):
+            continue
+        for pth in os.listdir(site_packages):
+            if not pth.endswith('.dist-info'):
+                continue
+            pkgname = pth.split('-', 1)[0].replace('_', '-')
+            top_level_fname = os.path.join(site_packages, pth, 'top_level.txt')
+            if not os.path.exists(top_level_fname):
+                if pkgname not in res.values():
+                    print("ERR:", pth, 'has not top_level.txt')
+                continue
+
+            with open(top_level_fname) as fp:
+                modnames = fp.read().split()
+
+            for modname in modnames:
+                modname = modname.replace('/', '.')
+                if modname.startswith(r'win32\lib'):
+                    modname = modname.rsplit('\\')[1]
+                res[modname] = pkgname
+
+    return res
+
+
+if __name__ == "__main__":
+    import json
+    print(json.dumps(find_package_names(), indent=4, sort_keys=True))
