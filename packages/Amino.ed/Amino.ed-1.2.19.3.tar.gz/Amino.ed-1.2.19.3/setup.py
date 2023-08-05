750ca799dccc4dca6aeaa8b85885145282c7468a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:59 +0000

    Add package Amino.ed/1.2.19.3/Amino.ed-1.2.19.3.tar.gz

---
+++ packages/Amino.ed/Amino.ed-1.2.19.3.tar.gz/Amino.ed-1.2.19.3/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as stream:
+    long_description = stream.read()
+
+setup(
+    name = 'Amino.ed',
+    version = '1.2.19.3',
+    url = 'https://github.com/Alert-Aigul/Amino.ed',
+    download_url = 'https://github.com/Alert-Aigul/Amino.ed/tarball/master',
+    license = 'MIT',
+    author = 'Alert Aigul',
+    author_email = 'alertaigul@gmail.com',
+    description = 'A library to create Amino bots.',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    keywords = [
+        'aminoapps',
+        'amino-py',
+        'amino',
+        'amino-bot',
+        'narvii',
+        'api',
+        'python',
+        'python3',
+        'python3.x',
+        'slimakoi',
+        'unofficial',
+        "alert",
+        "fix"
+    ],
+    install_requires = [
+        'setuptools',
+        'requests',
+        'six',
+        'websocket-client==0.57.0',
+        'json_minify'
+    ],
+    setup_requires = [
+        'wheel'
+    ],
+    packages = find_packages()
+)
