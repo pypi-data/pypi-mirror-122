1fc75a6075f18a4a2ef5b7356caa54e1773c0598
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:58 +0000

    Add package trace-feature/0.1/trace_feature-0.1.tar.gz

---
+++ packages/trace-feature/trace_feature-0.1.tar.gz/trace_feature-0.1/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+
+      name='trace_feature',
+      packages = ['trace_feature'],
+      version='0.1',
+      description='A lib to trace bdd features.',
+      url='https://github.com/vitorbribas/trace_feature',
+      download_url = 'https://github.com/vitorbribas/trace_feature/archive/refs/tags/v_02.tar.gz',
+      author='Rafael Fazzolino',
+      author_email='fazzolino29@gmail.com',
+      license='MIT',
+      keywords = ['BDD', 'Trace'],
+      py_modules=['trace_feature'],
+      install_requires=[
+            'Click',
+      ]
+)
---
+++ packages/trace-feature/trace_feature-0.1.tar.gz/trace_feature-0.1/trace_feature/__init__.py
@@ -0,0 +1,1 @@
+from trace_feature.trace_feature import *
