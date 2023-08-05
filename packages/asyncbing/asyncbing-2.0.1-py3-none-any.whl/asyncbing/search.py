8902849841efad1c2ed46f957cb8f22cc5a54599
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:37 +0000

    Add package asyncbing/2.0.1/asyncbing-2.0.1-py3-none-any.whl

---
+++ packages/asyncbing/asyncbing-2.0.1-py3-none-any.whl/asyncbing/search.py
@@ -0,0 +1,38 @@
+import aiohttp
+from .searchresponse import SearchResponse
+
+class Search:
+    """The searching part of asyncbing"""
+    def __init__(self, auth: str, *, session: aiohttp.ClientSession=None):
+        self.headers = {'Ocp-Apim-Subscription-Key': auth}
+        self.bing = 'https://api.bing.microsoft.com/v7.0/search'
+
+    async def fetch(self, search: str) -> SearchResponse:
+        """|coro|
+        Searches with the bing api for the search string provided, with the global market set."""
+        async with self.session.get(self.bing, headers=self.headers, params={'q': search}) as resp:
+            return SearchResponse((await resp.json()))
+
+    # "alias"
+    async def search(self, *args, **kwargs) -> fetch:
+        """|coro|
+        An alias for Search.fetch()"""
+        return await self.fetch(*args, **kwargs)
+    
+    async def __aenter__(self):
+        if not hasattr(self, 'session'):
+            async with aiohttp.ClientSession() as session:
+                self.session = session
+        return self
+
+    async def __aexit__(self, *args):
+        pass
+
+def search(auth: str, *, session: aiohttp.ClientSession=None):
+    """Takes a `auth` token, as well as an optional aiohttp session.
+        example:
+        ``
+        async with asyncbing.Search('AUTHTOKEN') as searching:
+            await searching.fetch...
+        ``"""
+    return Search(auth, session=session)
---
+++ packages/asyncbing/asyncbing-2.0.1-py3-none-any.whl/asyncbing/searchresponse.py
@@ -0,0 +1,46 @@
+from .exceptions import NotEnoughResults
+from typing import Tuple
+from .searchresult import SearchResult
+
+class SearchResponse:
+    """Represents a Bing Response (not a search result response)
+    
+    ```{caution}
+    You must not manually initialize this!
+    ```"""
+    def __init__(self, data):
+        self.data = data
+        self.resulNum = 0
+
+    @property
+    def originalQuery(self) -> str:
+        """Returns a string if for some reason you forgot what you searched for"""
+        return self.data['queryContext']['originalQuery']
+
+    @property
+    def matches(self) -> int:
+        """Returns the amount of **estimated** matches found for your search query"""
+        return self.data['webPages']['totalEstimatedMatches']
+
+    @property
+    def searchUrl(self) -> str:
+        """Returns the bing search url you would get if you were to type in the query in the bing website directly."""
+        return self.data['webPages']['webSearchUrl']
+
+    def getOne(self) -> SearchResult:
+        """Gets the top search result and returns a :class:`asyncbing.SearchResult`."""
+        if not len(self.data['webPages']['value']) >= 1:
+            raise NotEnoughResults("There weren't enough search results to give a BingSearchResult.")
+        return SearchResult((self.data['webPages']['value'][0]))
+
+    def getThree(self) -> Tuple[SearchResult, SearchResult, SearchResult]:
+        """Gets the top three search results and returns a tuple of :class:`asyncbing.SearchResult`"""
+        if not len(self.data['webPages']['value']) >= 3:
+            raise NotEnoughResults("There weren't enough search results to give a BingSearchResult.")
+        return (SearchResult(self.data['webPages']['value'][0]), SearchResult(self.data['webPages']['value'][1]), SearchResult(self.data['webPages']['value'][2]))
+    
+    def getMany(self, amount: int, *, start: int=0) -> Tuple[SearchResult]:
+        """Returns as many :class:`asyncbing.SearchResult` as you want as the max defined by the int passed into the function. has an optional ``start`` kwarg. Defaults to 0."""
+        if not len(self.data['webPages']['value']) >= amount:
+            raise NotEnoughResults("There weren't enough search results to give a BingSearchResult for the specified amount.")
+        return ([SearchResult(self.data['webPages']['value'][x]) for x in range(start, amount)])
