088a5f843eda2dd63bfc32d3b2bf2e1ce49b6375
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:18 +0000

    Add package debinux/0.0.0/debinux-0.0.0.tar.gz

---
+++ packages/debinux/debinux-0.0.0.tar.gz/debinux-0.0.0/README.md
@@ -0,0 +1,7 @@
+Run linux (debian) commands from [debinux] Commmand line.
+
+some thing like, git bash - but with virtual enviroment
+
+execute program 
+takes input with linux like prompt everytime
+and gives output like linux shell (bash) gives
---
+++ packages/debinux/debinux-0.0.0.tar.gz/debinux-0.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+import os
+
+
+VERSION = '0.0.0'
+DESCRIPTION = 'Debinux | Comming soon'
+
+
+# Setting up
+setup(
+    name="debinux",
+    version=VERSION,
+    author="Divinemonk",
+    author_email="<v1b7rc8eb@relay.firefox.com>",
+    description=DESCRIPTION,
+    include_package_data=True,
+)
