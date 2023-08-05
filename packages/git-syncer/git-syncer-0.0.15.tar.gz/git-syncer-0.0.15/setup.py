2d838f236b47c659754657e81c6a2ce67ac0c22a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:15 +0000

    Add package git-syncer/0.0.15/git-syncer-0.0.15.tar.gz

---
+++ packages/git-syncer/git-syncer-0.0.15.tar.gz/git-syncer-0.0.15/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name="git-syncer",
+    version="0.0.15",
+    description="Allows cron job control and command execution on a remote machine using git infrastructure",
+    author="Asaf Kali",
+    author_email="unknown@gmail.com",
+    url="https://github.com/asaf-kali/git-syncer",
+    install_requires=["pycron>=3.0.0", "jinja2>=3.0.0"],
+    entry_points={
+        "console_scripts": [
+            "init-syncer = git_syncer.initializer:initialize_syncer",
+        ]
+    },
+    include_package_data=True,
+    license="https://github.com/asaf-kali/git-syncer/blob/main/LICENSE",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
