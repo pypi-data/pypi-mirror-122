1cc5d8da265b921039c636b34b9845388bfe9e4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:14:33 +0000

    Add package pincer/0.8.0/pincer-0.8.0.tar.gz

---
+++ packages/pincer/pincer-0.8.0.tar.gz/pincer-0.8.0/pincer/middleware/message_create.py
@@ -0,0 +1,19 @@
+# Copyright Pincer 2021-Present
+# Full MIT License can be found in `LICENSE` at the project root.
+
+"""sent when a message is created in a subscribed text channel"""
+
+from ..core.dispatch import GatewayDispatch
+from ..objects import UserMessage
+
+
+async def message_create_middleware(self, payload: GatewayDispatch):
+    return "on_message", [
+        UserMessage.from_dict(
+            {"_client": self, "_http": self.http, **payload.data}
+        )
+    ]
+
+
+def export():
+    return message_create_middleware
