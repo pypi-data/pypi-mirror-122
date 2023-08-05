7743036398469bd02aa4741ab4f3d191081f8ea0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:16 +0000

    Add package justhacking/2.0.2/justhacking-2.0.2.tar.gz

---
+++ packages/justhacking/justhacking-2.0.2.tar.gz/justhacking-2.0.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import os
+
+
+VERSION = '2.0.2'
+DESCRIPTION = 'Just Hacking is a Python CLI script that stimulates as you are hacking.'
+
+
+# Setting up
+setup(
+    name="justhacking",
+    version=VERSION,
+    author="Divinemonk",
+    author_email="<v1b7rc8eb@relay.firefox.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['rich'],
+    keywords=['python', 'justhacking', 'divinemonk', 'hacking stimulation', 'cli', 'python hacking', 'console'],
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "justhacking=justhacking.__main__:main",
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "License :: OSI Approved :: MIT License"
+    ]
+)
