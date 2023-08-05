5ff1b3e5c04dc38bb64b86d5a122e233daba728a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:12 +0000

    Add package from-ge-to-allure-mapper/0.0.1.4/from_ge_to_allure_mapper-0.0.1.4.tar.gz

---
+++ packages/from-ge-to-allure-mapper/from_ge_to_allure_mapper-0.0.1.4.tar.gz/from_ge_to_allure_mapper-0.0.1.4/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='from_ge_to_allure_mapper',
+    version='0.0.1.4',
+    author='Bogdan Volodarskiy',
+    author_email='hesherus@gmail.com',
+    description='Package for mapping test report format from Great Expectations to Allure',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/provectus/from_ge_to_allure_mapper',
+    download_url='https://github.com/provectus/from_ge_to_allure_mapper/archive/refs/tags/v_01.4.tar.gz',
+    keywords=['DATA_QA', 'GREAT_EXPECTATIONS', 'ALLURE'],
+    license='MIT',
+    data_files=[('', ['from_ge_to_allure_mapper/config.json'])],
+    packages=['from_ge_to_allure_mapper'],
+    install_requires=['great_expectations'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+)
