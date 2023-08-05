2e8212b0f418c0207e0da8acf85ae6731e8ec08e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:11 +0000

    Add package zcls/0.13.1/zcls-0.13.1-py2.py3-none-any.whl

---
+++ packages/zcls/zcls-0.13.1-py2.py3-none-any.whl/zcls/data/datasets/general_dataset.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+"""
+@date: 2021/4/4 下午2:55
+@file: general_dataset.py
+@author: zj
+@description: 
+"""
+
+import numpy as np
+from PIL import Image
+
+from torch.utils.data import Dataset
+import torchvision.datasets as datasets
+
+from .evaluator.general_evaluator import GeneralEvaluator
+
+
+class GeneralDataset(Dataset):
+
+    def __init__(self, root, transform=None, target_transform=None, top_k=(1, 5)):
+        self.data_set = datasets.ImageFolder(root)
+        self.classes = self.data_set.classes
+        self.root = root
+        self.transform = transform
+        self.target_transform = target_transform
+        self._update_evaluator(top_k)
+
+    def __getitem__(self, index: int):
+        image, target = self.data_set.__getitem__(index)
+        if isinstance(image, Image.Image):
+            image = np.array(image)
+
+        if self.transform is not None:
+            image = self.transform(image)
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return image, target
+
+    def __len__(self) -> int:
+        return self.data_set.__len__()
+
+    def _update_evaluator(self, top_k):
+        self.evaluator = GeneralEvaluator(self.classes, top_k=top_k)
+
+    def get_classes(self):
+        return self.classes
+
+    def __repr__(self):
+        return self.__class__.__name__ + ' (' + self.root + ')'
