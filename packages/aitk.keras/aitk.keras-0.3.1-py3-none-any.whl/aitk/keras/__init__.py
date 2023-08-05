ad5681ae8e37f2a09debb3c33241b9a6c708f6f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:11 +0000

    Add package aitk.keras/0.3.1/aitk.keras-0.3.1-py3-none-any.whl

---
+++ packages/aitk.keras/aitk.keras-0.3.1-py3-none-any.whl/aitk/keras/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# **************************************************************
+# aitk.keras: A Python Keras model API
+#
+# Copyright (c) 2021 AITK Developers
+#
+# https://github.com/ArtificialIntelligenceToolkit/aitk.keras
+#
+# **************************************************************
+
+"""A module of basic building blcoks for constructing neural networks"""
+from . import utils
+from . import losses
+from . import activations
+from . import schedulers
+from . import optimizers
+from . import wrappers
+from . import layers
+from . import initializers
+from . import modules
+from . import models
+from . import datasets
