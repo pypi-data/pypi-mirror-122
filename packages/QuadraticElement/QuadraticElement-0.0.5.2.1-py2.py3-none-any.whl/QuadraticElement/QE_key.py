599f7066889d326d24d390ddec72f390199daa52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:26 +0000

    Add package QuadraticElement/0.0.5.2.1/QuadraticElement-0.0.5.2.1-py2.py3-none-any.whl

---
+++ packages/QuadraticElement/QuadraticElement-0.0.5.2.1-py2.py3-none-any.whl/QuadraticElement/QE_key.py
@@ -0,0 +1,38 @@
+#coding:gbk
+import os
+try:
+    import keyboard
+except:
+    os.system("pip install keyboard")
+from QuadraticElement.jk_key import ME
+
+def key_user_message():
+    ME.user_message()
+
+def key_Poli():
+    ME.run_Poli()
+
+def key_dow():
+    ME.dow()
+
+def key_quit():
+    return exit()
+
+def key_new_user():
+    ME.new_user()
+
+def key_open_files():
+    ME.open_files()
+def main():
+    message = "¿ì½İ¼ü´°¿Ú>"
+    print(message)
+    keyboard.add_hotkey('ctrl+u', key_user_message)
+    keyboard.add_hotkey('ctrl+p', key_Poli)
+    keyboard.add_hotkey('ctrl+d', key_dow)
+    keyboard.add_hotkey('ctrl+n', key_new_user)
+    keyboard.add_hotkey('ctrl+q', key_quit)
+    keyboard.add_hotkey('ctrl+f', key_open_files)
+    keyboard.wait()
+    return message
+if __name__ == '__main__':
+    main()
