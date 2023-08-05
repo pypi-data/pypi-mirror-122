6a4c0b3dda2820cdcaa7e196dfbaa418daf5a487
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:52 +0000

    Add package simpl-cloud/0.36/simpl_cloud-0.36-py3-none-any.whl

---
+++ packages/simpl-cloud/simpl_cloud-0.36-py3-none-any.whl/simpl/schema/__init__.py
@@ -0,0 +1,23 @@
+import graphene
+
+from . import mutations, query
+from .external import mutations as external_mutations
+from .external.query import Query as ExternalQuery
+
+
+class Mutation(graphene.ObjectType):
+    balance_teams = mutations.BalanceTeams.Field()
+    alter_player = mutations.AlterPlayer.Field()
+
+    run = external_mutations.SimplRun.Field()
+    players = external_mutations.SimplPlayers.Field()
+    managers = external_mutations.SimplManagers.Field()
+    simpl_class = external_mutations.SimplClass.Field()
+    alter_user = external_mutations.AlterUser.Field()
+
+
+class Query(query.Query, ExternalQuery):
+    pass
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
