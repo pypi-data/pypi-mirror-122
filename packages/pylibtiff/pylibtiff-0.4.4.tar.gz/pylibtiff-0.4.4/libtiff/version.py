360a8bd71ff352eb28e8d61053a0fd50d03b3e39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:13:03 +0000

    Add package pylibtiff/0.4.4/pylibtiff-0.4.4.tar.gz

---
+++ packages/pylibtiff/pylibtiff-0.4.4.tar.gz/pylibtiff-0.4.4/libtiff/version.py
@@ -0,0 +1,9 @@
+
+# THIS FILE IS GENERATED FROM PYLIBTIFF SETUP.PY
+short_version = '0.4.4'
+version = '0.4.4'
+full_version = '0.4.4'
+git_revision = '892df2482c869777798f37cdac29bf6dee221a5a'
+release = True
+if not release:
+    version = full_version
