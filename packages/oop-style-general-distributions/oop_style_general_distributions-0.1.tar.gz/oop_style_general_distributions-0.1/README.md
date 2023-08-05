d0016c7eabbd5bfc64e94c44f80d105fd71b4815
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:01 +0000

    Add package oop-style-general-distributions/0.1/oop_style_general_distributions-0.1.tar.gz

---
+++ packages/oop-style-general-distributions/oop_style_general_distributions-0.1.tar.gz/oop_style_general_distributions-0.1/README.md
@@ -0,0 +1,9 @@
+# General distributions package
+
+Summary of the package
+
+# Files
+
+Details of files in the package
+
+# Installation
---
+++ packages/oop-style-general-distributions/oop_style_general_distributions-0.1.tar.gz/oop_style_general_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='oop_style_general_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      author='Bidila Timotei',
+      author_email='bidila.timotei@gmail.com',
+      zip_safe=False)
