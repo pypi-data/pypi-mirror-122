d51c3f7f0a7ca4baf6d2739dfae5b98cc69f432a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:14:11 +0000

    Add package HydDown/0.16.1/HydDown-0.16.1-py3-none-any.whl

---
+++ packages/HydDown/HydDown-0.16.1-py3-none-any.whl/HydDown-0.16.1.data/scripts/hyddown_main.py
@@ -0,0 +1,35 @@
+# HydDown hydrogen/other gas depressurisation
+# Copyright (c) 2021 Anders Andreasen
+# Published under an MIT license
+
+import yaml
+import sys
+import time
+
+try:
+    from hyddown import HydDown
+except:
+    import sys
+    import os
+    hyddown_path = os.path.join(os.path.abspath(os.path.dirname(__file__)),"..","src")
+    sys.path.append(os.path.abspath(hyddown_path))
+    
+    from hyddown import HydDown
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        input_filename = sys.argv[1]
+    else:
+        input_filename = "input.yml"
+
+    with open(input_filename) as infile:
+        input = yaml.load(infile, Loader=yaml.FullLoader)
+
+
+    hdown=HydDown(input)
+    start = time.time()
+    hdown.run()
+    end = time.time()
+    print('Elapsed time: ',end-start,' sec.')
+    hdown.plot()
