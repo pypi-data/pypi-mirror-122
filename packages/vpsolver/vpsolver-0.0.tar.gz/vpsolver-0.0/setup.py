42e0e7af23d0864bec2caa4f1f1217681c52de1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:10 +0000

    Add package vpsolver/0.0/vpsolver-0.0.tar.gz

---
+++ packages/vpsolver/vpsolver-0.0.tar.gz/vpsolver-0.0/setup.py
@@ -0,0 +1,19 @@
+# encoding: utf8
+
+from setuptools import setup
+
+
+setup(
+    author='Filipe Brandao',
+    author_email='fdabrandao@dcc.fc.up.pt',
+    description='Arc-flow Vector Packing Solver (VPSolver)',
+    install_requires=['pyvpsolver'],
+    long_description='Use `pyvpsolver <https://pypi.python.org/pypi/pyvpsolver/>`_ instead.',
+    maintainer='Filipe Brandao',
+    maintainer_email='fdabrandao@gmail.com',
+    name='vpsolver',
+    platforms=['all'],
+    url='https://pypi.python.org/pypi/pyvpsolver/',
+    version='0.0',
+    zip_safe=False,
+)
