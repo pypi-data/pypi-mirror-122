a01bbdb28a8530a2075145dbd1a6aaa87cbcde52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:39 +0000

    Add package deemon/2.1b2/deemon-2.1b2.tar.gz

---
+++ packages/deemon/deemon-2.1b2.tar.gz/deemon-2.1b2/deemon/utils/dataprocessor.py
@@ -0,0 +1,43 @@
+import logging
+from csv import reader
+
+logger = logging.getLogger(__name__)
+
+
+def read_file_as_csv(file):
+    with open(file, 'r', encoding="utf-8", errors="replace") as f:
+        make_csv = f.read()
+        csv_to_list = make_csv.split('\n')
+        sorted_list = sorted(list(filter(None, csv_to_list)))
+        return sorted_list
+
+
+def process_input_file(artist_list):
+    logger.debug("Processing file contents")
+    try:
+        artists = [int(x) for x in artist_list]
+    except ValueError:
+        artists = [x for x in artist_list]
+    return artists
+
+
+def csv_to_list(all_artists) -> list:
+    """
+    Separate artists and replace delimiter to find artists containing commas in their name
+    """
+    all_artists = [x for x in all_artists]
+    processed_artists = []
+    for artist in all_artists:
+        if artist[-1] == ',':
+            processed_artists.append(artist[:-1] + "|")
+        else:
+            processed_artists.append(artist)
+    processed_artists = ' '.join(processed_artists)
+    processed_artists = processed_artists.split('|')
+
+    result = []
+    csv_artists = reader(processed_artists, delimiter="|")
+    for line in csv_artists:
+        combined_line = ([x.lstrip() for x in line])
+        result.append(','.join(combined_line))
+    return (result)
