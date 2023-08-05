e03e3d6785eba908fd665585fd2c68d52461ba3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:13 +0000

    Add package flask-mixins/0.0.3/flask-mixins-0.0.3.tar.gz

---
+++ packages/flask-mixins/flask-mixins-0.0.3.tar.gz/flask-mixins-0.0.3/src/flask_mixins/__init__.py
@@ -0,0 +1,20 @@
+from distutils.version import LooseVersion
+
+from .view_mixins.misc_mixins import JsonifyMixin, StatusCodeMixin
+from .view_mixins.permission_mixin import PermissionMixin
+from .view_mixins.schema_mixin import SchemaMixin
+from .view_mixins.service_mixin import ServiceMixin
+from .views import ResourcesView, ResourceView
+
+__all__ = [
+    "JsonifyMixin",
+    "StatusCodeMixin",
+    "PermissionMixin",
+    "SchemaMixin",
+    "ServiceMixin",
+    "ResourceView",
+    "ResourcesView",
+]
+
+__version__ = "0.0.3"
+__version_info__ = tuple(LooseVersion(__version__).version)
