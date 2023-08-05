f012a2d084c92043de51a687af989e632a5a54dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:47 +0000

    Add package QGUI/0.3/QGUI-0.3.tar.gz

---
+++ packages/QGUI/QGUI-0.3.tar.gz/QGUI-0.3/setup.py
@@ -0,0 +1,24 @@
+# Author: Acer Zhang
+# Datetime: 2021/9/17 
+# Copyright belongs to the author.
+# Please indicate the source for reprinting.
+
+from setuptools import setup
+from setuptools import find_packages
+
+__version__ = "0.3"
+
+# python setup.py sdist bdist_wheel
+setup(
+    name='QGUI',
+    version=__version__,
+    packages=find_packages(),
+    url='https://github.com/QPT-Family/QGUI',
+    license='MIT',
+    author='GT-ZhangAcer',
+    author_email='zhangacer@foxmail.com',
+    description='QWebSite',
+    install_requires=["ttkbootstrap", "pillow"],
+    python_requires='>3.5',
+    include_package_data=True,
+)
