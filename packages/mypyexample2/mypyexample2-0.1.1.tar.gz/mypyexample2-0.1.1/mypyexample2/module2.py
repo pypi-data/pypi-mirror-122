8008e50f73fd0bc51e150e5942e2f9db8f6bb1f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:14 +0000

    Add package mypyexample2/0.1.1/mypyexample2-0.1.1.tar.gz

---
+++ packages/mypyexample2/mypyexample2-0.1.1.tar.gz/mypyexample2-0.1.1/mypyexample2/module2.py
@@ -0,0 +1,8 @@
+import numpy as np
+import numpy as pd
+
+
+def df_mean(a):
+    print(np.mean(a))
+
+
---
+++ packages/mypyexample2/mypyexample2-0.1.1.tar.gz/mypyexample2-0.1.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='mypyexample2',
+    version='0.1.1',
+    description='A example Python package',
+    url='https://github.com/shuds13/pyexample',
+    author='Stephen Hudson',
+    author_email='shudson@anl.gov',
+    license='BSD 2-clause',
+    packages=['mypyexample2'],
+    install_requires=['mpi4py>=2.0',
+                      'numpy<=1.17.2'
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
