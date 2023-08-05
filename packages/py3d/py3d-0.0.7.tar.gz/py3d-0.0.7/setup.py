db61a2c63f9fa1ce030c561b92100c79ed37987b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:08 +0000

    Add package py3d/0.0.7/py3d-0.0.7.tar.gz

---
+++ packages/py3d/py3d-0.0.7.tar.gz/py3d-0.0.7/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="py3d",
+    version="0.0.7",
+    description="a 3d library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Tumiz",
+    author_email="hh11698@163.com",
+    python_requires=">=3.5.0",
+    url="https://github.com/Tumiz/scenario",
+    install_requires=["numpy","towebi==0.0.7"],
+    packages=find_packages(),
+    py_modules=["py3d"],
+    license="GPL-3.0 License"
+)
