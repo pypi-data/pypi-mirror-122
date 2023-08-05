0c15725085478ff632a59bf1bb6e2f2046840c3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:32 +0000

    Add package hardcode-house-model/0.0.9/hardcode-house-model-0.0.9.tar.gz

---
+++ packages/hardcode-house-model/hardcode-house-model-0.0.9.tar.gz/hardcode-house-model-0.0.9/src/hardcode_house_model/statistics/common.py
@@ -0,0 +1,5 @@
+from hardcode_house_model.util.mongo_mixin import MongoMixin
+from mongoengine import Document
+from mongoengine.fields import (DateTimeField, DecimalField, DictField,
+                                EmbeddedDocumentField, FloatField, IntField,
+                                ListField, StringField, URLField)
