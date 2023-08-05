29ca370a21b8c2805efeed434777fd487287e2e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:26 +0000

    Add package tankobon/2021.10.2/tankobon-2021.10.2.tar.gz

---
+++ packages/tankobon/tankobon-2021.10.2.tar.gz/tankobon-2021.10.2/create_docs.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+pydoc-markdown \
+  -m tankobon.core \
+  -m tankobon.exceptions \
+  -m tankobon.iso639 \
+  -m tankobon.models \
+  -m tankobon.utils \
+  -m tankobon.sources.base \
+  --render-toc > API.md
