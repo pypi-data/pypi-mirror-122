6e871cb3d85e7bd4a4ea7dd1e513484567fb5b04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:52 +0000

    Add package iridi/1.2.0/iridi-1.2.0-py3-none-any.whl

---
+++ packages/iridi/iridi-1.2.0-py3-none-any.whl/iridi/presets.py
@@ -0,0 +1,19 @@
+# Taken from uigradients.com
+from .iridi import preset
+
+
+class presets:
+    kye_meh = preset(["#7350b3", "#2ebf91"])
+    wiretap = preset(["#8A2387", "#E94057", "#F27121"])
+    aquatic = preset(["#00C9FF", "#92FE9D"])
+    martini = preset(["#FDFC47", "#24FE41"])
+    amethyst = preset(["#9D50BB", "#6E48AA"])
+    dance_to_forget = preset(["#FF4E50", "#F9D423"])
+    instagram = preset(['#833ab4', '#fd1d1d', '#fcb045'])
+    pastel = preset(['#74ebd5', '#74ecd5'])
+    retro = preset(['#3f51b1', '#5a55ae', '#7b5fac', '#8f6aae', '#a86aa4', '#cc6b8e', '#f18271', '#f3a469', '#f7c978'])
+    cherryblossoms = preset(["#FBD3E9", "#BB377D"])
+    candy = preset(["#D3959B", "#BFE6BA"])
+    nelson = preset(["#f2709c", "#ff9472"])
+    kyoto = preset(["#c21500", "#ffc500"])
+    wedding_day_blues = preset(["#40E0D0", "#FF8C00", "#FF0080"])
