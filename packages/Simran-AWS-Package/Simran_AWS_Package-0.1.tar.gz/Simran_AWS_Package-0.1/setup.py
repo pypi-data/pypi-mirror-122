566b2e5455a80f45093908d244b6c066f8bd207b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:13 +0000

    Add package Simran-AWS-Package/0.1/Simran_AWS_Package-0.1.tar.gz

---
+++ packages/Simran-AWS-Package/Simran_AWS_Package-0.1.tar.gz/Simran_AWS_Package-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='Simran_AWS_Package',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      zip_safe=False)
