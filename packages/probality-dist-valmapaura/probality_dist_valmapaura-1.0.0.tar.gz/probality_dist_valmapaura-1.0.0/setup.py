5b7ae1ff597e62c63fc4539925e8dad5ee0780d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:21 +0000

    Add package probality-dist-valmapaura/1.0.0/probality_dist_valmapaura-1.0.0.tar.gz

---
+++ packages/probality-dist-valmapaura/probality_dist_valmapaura-1.0.0.tar.gz/probality_dist_valmapaura-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='probality_dist_valmapaura',
+      version='1.0.0',
+      description='Gaussian and Binomial distributions',
+      packages=['probality_dist_valmapaura'],
+      author='Valentine Mapaura',
+      author_email='valmapaura@gmail.com',
+      zip_safe=False)
