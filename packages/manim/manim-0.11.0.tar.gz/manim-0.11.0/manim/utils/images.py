144e0cad96b4c85e024c5c09255f65fddde75858
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:12 +0000

    Add package manim/0.11.0/manim-0.11.0.tar.gz

---
+++ packages/manim/manim-0.11.0.tar.gz/manim-0.11.0/manim/utils/images.py
@@ -0,0 +1,33 @@
+"""Image manipulation utilities."""
+
+__all__ = ["get_full_raster_image_path", "drag_pixels", "invert_image"]
+
+
+import numpy as np
+from PIL import Image
+
+from .. import config
+from ..utils.file_ops import seek_full_path_from_defaults
+
+
+def get_full_raster_image_path(image_file_name):
+    return seek_full_path_from_defaults(
+        image_file_name,
+        default_dir=config.get_dir("assets_dir"),
+        extensions=[".jpg", ".jpeg", ".png", ".gif", ".ico"],
+    )
+
+
+def drag_pixels(frames):
+    curr = frames[0]
+    new_frames = []
+    for frame in frames:
+        curr += (curr == 0) * np.array(frame)
+        new_frames.append(np.array(curr))
+    return new_frames
+
+
+def invert_image(image):
+    arr = np.array(image)
+    arr = (255 * np.ones(arr.shape)).astype(arr.dtype) - arr
+    return Image.fromarray(arr)
