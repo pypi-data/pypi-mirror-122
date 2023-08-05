2ff9c23e39f021766f41609f3de5164936c96bf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:30 +0000

    Add package pypowsybl/0.10.0/pypowsybl-0.10.0-cp39-cp39-win_amd64.whl

---
+++ packages/pypowsybl/pypowsybl-0.10.0-cp39-cp39-win_amd64.whl/pypowsybl/loadflow.py
@@ -0,0 +1,54 @@
+#
+# Copyright (c) 2020, RTE (http://www.rte-france.com)
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+import _pypowsybl
+from _pypowsybl import LoadFlowParameters as Parameters
+from _pypowsybl import LoadFlowComponentResult as ComponentResult
+from _pypowsybl import LoadFlowComponentStatus as ComponentStatus
+from _pypowsybl import ConnectedComponentMode
+from _pypowsybl import BalanceType
+from _pypowsybl import VoltageInitMode
+from pypowsybl.network import Network as _Network
+
+
+Parameters.__repr__ = lambda self: f"{self.__class__.__name__}("\
+                                   f"voltage_init_mode={self.voltage_init_mode.name}"\
+                                   f", transformer_voltage_control_on={self.transformer_voltage_control_on!r}"\
+                                   f", no_generator_reactive_limits={self.no_generator_reactive_limits!r}"\
+                                   f", phase_shifter_regulation_on={self.phase_shifter_regulation_on!r}"\
+                                   f", twt_split_shunt_admittance={self.twt_split_shunt_admittance!r}"\
+                                   f", simul_shunt={self.simul_shunt!r}"\
+                                   f", read_slack_bus={self.read_slack_bus!r}"\
+                                   f", write_slack_bus={self.write_slack_bus!r}"\
+                                   f", distributed_slack={self.distributed_slack!r}"\
+                                   f", balance_type={self.balance_type.name}"\
+                                   f", dc_use_transformer_ratio={self.dc_use_transformer_ratio!r}"\
+                                   f", countries_to_balance={self.countries_to_balance}"\
+                                   f", connected_component_mode={self.connected_component_mode!r}"\
+                                   f")"
+
+ComponentResult.__repr__ = lambda self: f"{self.__class__.__name__}("\
+                                        f"connected_component_num={self.connected_component_num!r}"\
+                                        f", synchronous_component_num={self.synchronous_component_num!r}"\
+                                        f", status={self.status.name}"\
+                                        f", iteration_count={self.iteration_count!r}"\
+                                        f", slack_bus_id={self.slack_bus_id!r}"\
+                                        f", slack_bus_active_power_mismatch={self.slack_bus_active_power_mismatch!r}"\
+                                        f")"
+
+
+def run_ac(network: _Network, parameters: Parameters = None, provider='OpenLoadFlow'):
+    p = parameters
+    if parameters is None:
+        p = Parameters()
+    return list(_pypowsybl.run_load_flow(network._handle, False, p, provider))
+
+
+def run_dc(network: _Network, parameters: Parameters = None, provider='OpenLoadFlow'):
+    p = parameters
+    if parameters is None:
+        p = Parameters()
+    return list(_pypowsybl.run_load_flow(network._handle, True, p, provider))
