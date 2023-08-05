02ddb5106e7ba78dfcde2eee67972dcc13fba85f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:20 +0000

    Add package git-substatus/0.2.8/git_substatus-0.2.8-py3-none-any.whl

---
+++ packages/git-substatus/git_substatus-0.2.8-py3-none-any.whl/git_substatus/base.py
@@ -0,0 +1,17 @@
+# Base file - global settings & modules for the entire git_substatus module.
+
+from typing import (
+    List,
+    Tuple,
+    Dict,
+    Union,
+    Iterator,
+    Any,
+    Optional,
+    TypedDict,
+    Callable,
+)
+
+import os
+import sys
+import subprocess
