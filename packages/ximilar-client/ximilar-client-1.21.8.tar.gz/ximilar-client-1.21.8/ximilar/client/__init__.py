227f931d9beb9584dad4629554e264dbb1299045
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:02 +0000

    Add package ximilar-client/1.21.8/ximilar-client-1.21.8.tar.gz

---
+++ packages/ximilar-client/ximilar-client-1.21.8.tar.gz/ximilar-client-1.21.8/ximilar/client/__init__.py
@@ -0,0 +1,17 @@
+from .client import RestClient
+from .recognition import RecognitionClient
+from .tagging import GenericTaggingClient, FashionTaggingClient, HomeDecorTaggingClient
+from .colors import DominantColorProductClient, DominantColorGenericClient
+from .detection import DetectionClient
+from .search import (
+    SimilarityPhotosClient,
+    SimilarityProductsClient,
+    SimilarityFashionClient,
+    SimilarityCustomClient,
+    ImageMatchingSearchClient,
+)
+from .flows import FlowsClient
+from .exceptions import XimilarClientException
+from .removebg import RemoveBGClient
+from .similarity import CustomSimilarityClient
+from .upscaler import UpscaleClient
