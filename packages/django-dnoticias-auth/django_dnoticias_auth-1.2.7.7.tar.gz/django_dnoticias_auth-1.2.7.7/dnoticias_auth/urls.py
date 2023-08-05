8349f59cf695eb1bcb84c995b444f46a2ea3ba3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:12 +0000

    Add package django-dnoticias-auth/1.2.7.7/django_dnoticias_auth-1.2.7.7.tar.gz

---
+++ packages/django-dnoticias-auth/django_dnoticias_auth-1.2.7.7.tar.gz/django_dnoticias_auth-1.2.7.7/dnoticias_auth/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('silent-check-sso/', views.SilentCheckSSOView.as_view(), name="silent-check-sso"),
+    path('logout/', views.DnoticiasOIDCLogoutView.as_view(), name="dnoticias-auth-logout"),
+]
