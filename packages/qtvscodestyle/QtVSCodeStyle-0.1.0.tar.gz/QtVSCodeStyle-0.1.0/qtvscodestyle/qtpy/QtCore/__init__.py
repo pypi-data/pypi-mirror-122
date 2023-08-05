4d2df7613548df4839457c7b55d436470e2de14e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:26 +0000

    Add package qtvscodestyle/0.1.0/QtVSCodeStyle-0.1.0.tar.gz

---
+++ packages/qtvscodestyle/QtVSCodeStyle-0.1.0.tar.gz/QtVSCodeStyle-0.1.0/qtvscodestyle/qtpy/QtCore/__init__.py
@@ -0,0 +1,16 @@
+from ..qt_compat import QT_API
+
+if QT_API == "PySide6":
+    from PySide6.QtCore import *
+elif QT_API == "PyQt6":
+    from PyQt6.QtCore import *
+
+    Slot = pyqtSlot  # noqa: F405
+    Signal = pyqtSignal  # noqa: F405
+elif QT_API == "PyQt5":
+    from PyQt5.QtCore import *
+
+    Slot = pyqtSlot  # noqa: F405
+    Signal = pyqtSignal  # noqa: F405
+elif QT_API == "PySide2":
+    from PySide2.QtCore import *
