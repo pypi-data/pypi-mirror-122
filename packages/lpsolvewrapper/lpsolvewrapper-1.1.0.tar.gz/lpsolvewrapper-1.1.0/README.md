9ee635315654a4e4b1cea9c74b107c8a78022caf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:47 +0000

    Add package lpsolvewrapper/1.1.0/lpsolvewrapper-1.1.0.tar.gz

---
+++ packages/lpsolvewrapper/lpsolvewrapper-1.1.0.tar.gz/lpsolvewrapper-1.1.0/README.md
@@ -0,0 +1,26 @@
+# lpsolvewrapper
+Python3.6 wrapper for compiled lpsolve bindings
+
+C source code compiled for python 3.6 (64-bit) and wrapper to avoid multiple fragmented library files needing to be correctly located in linux environment. This has been tested on Ubuntu and Windows 7/10. See official [lpsolve website](http://lpsolve.sourceforge.net/5.5/) for more information.
+
+## Installation
+
+`pip install lpsolvewrapper`
+
+**Install from Git:**
+`pip install git+https://github.com/theweiyu/lpsolvewrapper`
+
+**Install from Wheel:**
+`pip install dist/lpsolvewrapper-1.0.0-py3-none-any.whl`
+
+## Usage
+`from lpsolvewrapper import lp_solve`
+
+## Compile from source (linux)
+1. (Optional) First set up a python venv: `python3.6 -m venv venv1`. And activate this.
+2. First build the liblpsolve55.so binary: `sh src/lpsolve55/ccc`. This is dynamically loaded by the lpsolve driver (lpsolve55.so) as a dependency
+3. Update src/extra/Python/setup.py so that `LPSOLVE55=` points to the build directory containing liblpsolve55.so
+4. To compile the driver: `python src/extra/Python/setup.py install`
+
+## Troubleshooting
+`ModuleNotFoundError: No module named 'lpsolve55'` Make sure you are using Python 3.6 64-bit. If you continue to have this issue, check that the lpsolve55 binaries are successfully copied into site-packages upon importing lpsolvewrapper for the first time. If you want to use a different version of python you'll need to re-compile from source (see above).
---
+++ packages/lpsolvewrapper/lpsolvewrapper-1.1.0.tar.gz/lpsolvewrapper-1.1.0/setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup
+import sys
+
+lpsolvewrapper_files = []
+
+if sys.platform == 'win32':  
+    if sys.version_info.major == 3 and sys.version_info.minor == 6:
+        lpsolvewrapper_files.append('lpsolve55.cp36-win_amd64.pyd')
+    elif sys.version_info.major == 3 and sys.version_info.minor == 7:
+        lpsolvewrapper_files.append('lpsolve55.cp37-win_amd64.pyd')
+    elif sys.version_info.major == 3 and sys.version_info.minor == 8:
+        lpsolvewrapper_files.append('lpsolve55.cp38-win_amd64.pyd')
+    else:
+        raise ValueError(f'python version not supported')
+elif sys.platform == 'linux':
+    lpsolvewrapper_files.append('liblpsolve55.so')
+    if sys.version_info.major == 3 and sys.version_info.minor == 6:
+        lpsolvewrapper_files.append('lpsolve55.cpython-36m-x86_64-linux-gnu.so')
+    elif sys.version_info.major == 3 and sys.version_info.minor == 7:
+        lpsolvewrapper_files.append('lpsolve55.cpython-37m-x86_64-linux-gnu.so')
+    elif sys.version_info.major == 3 and sys.version_info.minor == 8:
+        lpsolvewrapper_files.append('lpsolve55.cpython-38-x86_64-linux-gnu.so')
+    else:
+        raise ValueError(f'python version not supported')
+else:
+    raise ValueError(f'operating system not supported')
+    
+setup(
+    name='lpsolvewrapper',
+    version='1.1.0',
+    author='Weiyu Chen',
+    keywords='lpsolve python3 wrapper compiled',
+    packages=['lpsolvewrapper'],
+    package_data={'lpsolvewrapper': lpsolvewrapper_files},
+    description='Python wrapper for compiled lpsolve bindings',
+    long_description='C source code compiled for python 3.6/3.7/3.8 and wrapper to avoid multiple fragmented library files needing to be correctly located in linux environment.',
+    long_description_content_type='text/plain',
+    url='https://github.com/theweiyu/lpsolvewrapper',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+        'Programming Language :: Python :: 3.6'
+    ],
+    zip_safe = False,
+)
+
+
