65234b5eae1bbf713a07985622fbc599a68412e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:43 +0000

    Add package synology-api/0.3.0/synology-api-0.3.0.tar.gz

---
+++ packages/synology-api/synology-api-0.3.0.tar.gz/synology-api-0.3.0/synology_api/__init__.py
@@ -0,0 +1,13 @@
+from . import \
+    audiostation,\
+    auth,\
+    base_api_core,\
+    directory_server,\
+    core_active_backup,\
+    core_backup,\
+    core_certificate,\
+    core_sys_info,\
+    downloadstation,\
+    filestation,\
+    virtualization
+
