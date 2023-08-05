bb8180782a13322da8b0ab483dcc412dbc5a5759
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:13:24 +0000

    Add package gnome-pomodoro-tracking/4.0.1/gnome-pomodoro-tracking-4.0.1.tar.gz

---
+++ packages/gnome-pomodoro-tracking/gnome-pomodoro-tracking-4.0.1.tar.gz/gnome-pomodoro-tracking-4.0.1/gnome_pomodoro_tracking/__main__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2021 The Project GNOME Pomodoro Tracking Authors
+import sys
+sys.path.insert(0, "/home/jhernandez/gp/gnome-pomodoro-tracking")
+import os
+from gnome_pomodoro_tracking.tracking import Tracking
+
+def main():
+    config = "%s/.local/share/gnome-pomodoro/tracking.config" % os.path.expanduser("~")
+    gpt = Tracking(config)
+    gpt.load_plugin()
+    gpt.add_parse_args()
+    gpt.cli()
+
+if __name__ == "__main__":
+    main()
+    
+
