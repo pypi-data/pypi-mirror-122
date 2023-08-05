8324620134b25850870264a1a0531d7bac68a600
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:32 +0000

    Add package NaMAZU/0.0.56/NaMAZU-0.0.56-py3-none-any.whl

---
+++ packages/NaMAZU/NaMAZU-0.0.56-py3-none-any.whl/NaMAZU/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "0.0.56"
+
+import NaMAZU.lightning_wingman
+import NaMAZU.functional
+import NaMAZU.st_integration
---
+++ packages/NaMAZU/NaMAZU-0.0.56-py3-none-any.whl/NaMAZU/functional/__init__.py
@@ -0,0 +1,25 @@
+from .image_control import *
+from .file_control import *
+from .data_science import *
+from .text_control import *
+from .coreml_control import *
+
+__all__ = [
+    "train_linear_regressor",
+    "rename_file",
+    "collect_file_pathes_by_ext",
+    "zip_files",
+    "img_to_npy",
+    "npy_to_png",
+    "apply_mask_to",
+    "split_image",
+    "compose_two_png",
+    "apply_to_all",
+    "change_frame_rates_in",
+    "save_all_frames",
+    "export_list_str_as",
+    "search_word_from",
+    "collect_images",
+    "drop_negative",
+]
+
