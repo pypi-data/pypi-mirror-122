6483272ed85926620cb88e921eba98f8f99cc962
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:17 +0000

    Add package namen/0.3.3/namen-0.3.3.tar.gz

---
+++ packages/namen/namen-0.3.3.tar.gz/namen-0.3.3/namen/name_resolver.py
@@ -0,0 +1,4 @@
+def resolve_name_by_index(index: int):
+    names = ["Dennis", "Michael", "Michel", "Tom", "Dick", "Pascal",
+             "Lange Frans", "Erik", "Rik", "Sander", "Ilia"]
+    return names[index]
