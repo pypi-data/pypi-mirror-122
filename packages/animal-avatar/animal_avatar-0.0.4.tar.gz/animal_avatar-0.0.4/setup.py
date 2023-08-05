1cc9c79faaa8b631ed284fd05796b3b332267371
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:14 +0000

    Add package animal-avatar/0.0.4/animal_avatar-0.0.4.tar.gz

---
+++ packages/animal-avatar/animal_avatar-0.0.4.tar.gz/animal_avatar-0.0.4/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+setup(
+    name='animal_avatar',
+    version='0.0.4',
+    description='SVG avatar generator.',
+    long_description=
+    """
+    Python implementation of Animal Avatar Generator
+    (https://github.com/roma-lukashik/animal-avatar-generator).
+    Python >= 3.7
+
+    License: MIT
+    """,
+    author_email='johnherbertdillinger@ukr.net',
+    license='MIT',
+    platforms='any',
+    url='https://github.com/Nacht1gall/animal-avatar-generator',
+    download_url='https://github.com/Nacht1gall/animal-avatar-generator/archive/refs/heads/master.zip',
+    keywords=['AVATAR', 'SVG'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
---
+++ packages/animal-avatar/animal_avatar-0.0.4.tar.gz/animal_avatar-0.0.4/src/animal_avatar/animal_avatar.py
@@ -0,0 +1,46 @@
+import random
+from typing import Sequence
+
+from animal_avatar.palette import AVATAR_COLORS, BACKGROUND_COLORS
+from animal_avatar.shapes import (
+    EMPTY_SHAPE, BROWS, EARS,
+    EYES, FACES, HAIRS,
+    MUZZLES, PATTERNS
+)
+from animal_avatar.utils.svg import create_svg, create_background, create_blackout
+
+
+class Avatar:
+    def __init__(self, seed: str, size: int = 150,
+                 avatar_colors: Sequence = AVATAR_COLORS, background_colors: Sequence = BACKGROUND_COLORS,
+                 blackout: bool = True, is_round: bool = True):
+        self.rng = random.Random(seed)
+        self.size = size
+        self.avatar_color = self.rng.choice(avatar_colors)
+        self.background_color = self.rng.choice(background_colors)
+        self.blackout = blackout
+        self.is_round = is_round
+        self.avatar = None
+
+        self.shapes = [
+            FACES, self.optional(PATTERNS), EARS,
+            self.optional(HAIRS), MUZZLES, EYES,
+            BROWS
+        ]
+
+    def optional(self, shapes) -> Sequence:
+        if self.rng.randrange(100) < 50:
+            return shapes
+        return (EMPTY_SHAPE, )
+
+    def set_avatar(self) -> None:
+        self.avatar = ''.join(map(lambda shape: self.rng.choice(shape)(self.avatar_color), self.shapes))
+
+    def create_avatar(self) -> str:
+        self.set_avatar()
+        return create_svg(
+            self.size,
+            create_background(self.is_round, self.background_color),
+            self.avatar,
+            create_blackout(self.is_round) if self.blackout else ''
+        )
