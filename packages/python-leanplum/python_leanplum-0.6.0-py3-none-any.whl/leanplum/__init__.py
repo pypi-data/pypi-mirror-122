5b40250a472299d704fe08abd36ce22da78692f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:25 +0000

    Add package python-leanplum/0.6.0/python_leanplum-0.6.0-py3-none-any.whl

---
+++ packages/python-leanplum/python_leanplum-0.6.0-py3-none-any.whl/leanplum/__init__.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import, division, print_function
+
+__version__ = "0.6.1"
+
+# errors
+# TODO: Remove from global namespace
+from leanplum.errors import *
+
+# Configuration variables
+api_base = "https://www.leanplum.com/api"
+api_version = "1.0.6"
+default_http_client = None
+default_logger = None
