6d5398ffd1d5807589700e0fa4abf0063ca15547
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:02 +0000

    Add package tools-jsyoo61/2021.10.3.1/tools_jsyoo61-2021.10.3.1-py3-none-any.whl

---
+++ packages/tools-jsyoo61/tools_jsyoo61-2021.10.3.1-py3-none-any.whl/tools/__init__.py
@@ -0,0 +1,14 @@
+
+__version__ = "2021.10.3.1"
+# from . import tools, data, my_random
+from .tools import *
+from . import os
+# from . import my_random as random
+
+'''
+For third-party packages, import manually by something like:
+import tools.pytorch
+import tools.pytorch as tpytorch
+import tools.pytorch as tpt
+import tools.pytorch as Tpt
+'''
