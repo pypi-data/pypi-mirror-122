e79e762e4f49bccf08c9031147ae24f7c9dba816
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:00 +0000

    Add package oop-probability/0.1/oop_probability-0.1.tar.gz

---
+++ packages/oop-probability/oop_probability-0.1.tar.gz/oop_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='oop_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['oop_probability'],
+      author = 'Amani Ezzat',
+      author_email = 'amanyezat@gmail.com',
+      zip_safe=False)
