be62c36d37d38af3d9acbc4f87f9388b1a2ac5e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:55 +0000

    Add package linque/1.0.1/linque-1.0.1-py3-none-any.whl

---
+++ packages/linque/linque-1.0.1-py3-none-any.whl/linque/__init__.py
@@ -0,0 +1,12 @@
+#  Created byMartin.cz
+#  Copyright (c) Martin Strohalm. All rights reserved.
+
+version = (1, 0, 1)
+
+# import utils
+from .iters import aggregate, bisect, chunk, concat, count, index
+from .iters import first, last, skip, skip_while, take, take_while
+from .iters import distinct, exclude, group, intersect, union
+
+# import main class
+from .linque import Linque
