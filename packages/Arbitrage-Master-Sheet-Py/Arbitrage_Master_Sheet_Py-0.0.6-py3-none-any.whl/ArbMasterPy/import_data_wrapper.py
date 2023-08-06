3646c8388b5603a29b0edf62c48a81771eac14e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:14 +0000

    Add package Arbitrage-Master-Sheet-Py/0.0.6/Arbitrage_Master_Sheet_Py-0.0.6-py3-none-any.whl

---
+++ packages/Arbitrage-Master-Sheet-Py/Arbitrage_Master_Sheet_Py-0.0.6-py3-none-any.whl/ArbMasterPy/import_data_wrapper.py
@@ -0,0 +1,5 @@
+from Excelutilities.importing_data_helpers import add_col_data_from_another_sheet
+import decimal
+import datetime
+def import_data():
+    add_col_data_from_another_sheet("ASIN data", "INPUT DATA", [float, int, decimal.Decimal,datetime.datetime, str])
