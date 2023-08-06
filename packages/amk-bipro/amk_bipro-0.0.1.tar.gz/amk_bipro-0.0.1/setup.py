7b6a753b8c89d666ef9001f4c7dfd15665fcfd77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:39 +0000

    Add package amk-bipro/0.0.1/amk_bipro-0.0.1.tar.gz

---
+++ packages/amk-bipro/amk_bipro-0.0.1.tar.gz/amk_bipro-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='amk_bipro',
+      version='0.0.1',
+      description='Gaussian distributions',
+      packages=['amk_bipro'],
+      author='Kololi Milimo',
+      author_email='kololimilimo@gmail.com',
+      zip_safe=False)
