b914056530b4d5fe0f23d03bb36f953e2618f9cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:41:42 +0000

    Add package dvk-archive/0.10.8/dvk_archive-0.10.8-py3-none-any.whl

---
+++ packages/dvk-archive/dvk_archive-0.10.8-py3-none-any.whl/dvk_archive/test/file/file_tests.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+"""
+Combined unit tests for the file package
+"""
+
+from dvk_archive.test.file.test_dvk import all_tests as test_dvk
+from dvk_archive.test.file.test_dvk_handler import all_tests as test_handler
+from dvk_archive.test.file.test_reformat import all_tests as test_reformat
+from dvk_archive.test.file.test_rename import all_tests as test_rename
+from dvk_archive.test.file.test_sequencing import all_tests as test_sequencing
+
+
+def test_all():
+    """
+    Runs all file tests.
+    """
+    test_sequencing()
+    test_handler()
+    test_dvk()
+    test_reformat()
+    test_rename()
