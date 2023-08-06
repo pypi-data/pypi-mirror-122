3a88e6986381b0377ecc6008e31cb25e95ea556a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:41 +0000

    Add package HALdata/0.1.23/HALdata-0.1.23-py3-none-any.whl

---
+++ packages/HALdata/HALdata-0.1.23-py3-none-any.whl/Server/ServerHandler.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+'''
+MIT License
+Copyright (c) 2021 Mikhail Hyde & Cole Crescas
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
+
+'''
+This program takes in a dataframe, checks for validity and then returns a boolean if the file is ready to be sent to S3
+
+return: boolean if file can be sent
+'''
+
+import pandas as pd
+import numpy as np
+import pickle
+import hmac
+import hashlib
+from ErrorHandler import ErrorHandler
+
+"""
+Server validity check:
+"""
+
+
+
+class ServerHandler(ErrorHandler):
+    def __init__(self):
+        super.__init__()
+        
+
