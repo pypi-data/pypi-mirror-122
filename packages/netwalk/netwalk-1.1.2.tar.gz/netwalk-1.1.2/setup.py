4b1fa0979822834c420750045aedbea8e22b2bd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:44 +0000

    Add package netwalk/1.1.2/netwalk-1.1.2.tar.gz

---
+++ packages/netwalk/netwalk-1.1.2.tar.gz/netwalk-1.1.2/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "netwalk",
+    version = "1.1.2",
+    author = "icovada",
+    author_email = "ftabbo@gmail.com",
+    description = "Network discovery and analysis tool",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/icovada/netwalk",
+    project_urls = {
+        "Bug Tracker": "https://github.com/icovada/netwalk/issues"
+    },
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent"
+    ],
+    packages = setuptools.find_packages(),
+    python_requires = ">=3.6",
+    install_requires=[
+        "ciscoconfparse>=1.5.30",
+        "napalm>=3.2.0"
+    ],
+    include_package_data=True
+)
