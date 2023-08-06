836fc9e145c5fc9e9ade717def0729c5ef01247f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:46 +0000

    Add package double-prob-dist/0.1/double_prob_dist-0.1.tar.gz

---
+++ packages/double-prob-dist/double_prob_dist-0.1.tar.gz/double_prob_dist-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='double_prob_dist',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['double_prob_dist'],
+      author='Eric Paskey',
+      email='eric.paskey@gmail.com',
+      zip_safe=False)
