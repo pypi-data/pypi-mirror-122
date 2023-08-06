4874a81c82a99a5d26aa7b908bec8d816ed21348
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:31 +0000

    Add package djan-dsnd-probability/0.1/djan_dsnd_probability-0.1.tar.gz

---
+++ packages/djan-dsnd-probability/djan_dsnd_probability-0.1.tar.gz/djan_dsnd_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='djan_dsnd_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['djan_dsnd_probability'],
+      author= 'Djan Magno',
+      author_email= 'djan.magno@gmail.com',
+      zip_safe=False)
