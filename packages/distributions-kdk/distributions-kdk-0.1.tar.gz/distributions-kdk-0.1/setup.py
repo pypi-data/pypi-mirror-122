d4dc228371d82d41a55a173a40c68d6e61ba0026
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:54 +0000

    Add package distributions-kdk/0.1/distributions-kdk-0.1.tar.gz

---
+++ packages/distributions-kdk/distributions-kdk-0.1.tar.gz/distributions-kdk-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions-kdk',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions-kdk'],
+      zip_safe=False)
