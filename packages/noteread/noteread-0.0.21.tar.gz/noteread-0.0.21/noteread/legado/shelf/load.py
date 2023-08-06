9f36f00b13b6e8fb28bdc2b294566314d424f9ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:32 +0000

    Add package noteread/0.0.21/noteread-0.0.21.tar.gz

---
+++ packages/noteread/noteread-0.0.21.tar.gz/noteread-0.0.21/noteread/legado/shelf/load.py
@@ -0,0 +1,34 @@
+import json
+import logging
+
+import requests
+from noteread.legado.shelf.base import BookSource, BookSourceCorrect
+from noteread.legado.shelf.libs.core import load_source
+from noteread.legado.shelf.libs.correct import correct_source
+from notetool.log import logger
+from tqdm import tqdm
+
+urls = [
+    "https://cdn.jsdelivr.net/gh/bushixuanqi/book-source/%E4%B9%A6%E6%BA%90%E5%90%88%E9%9B%86.json",
+
+    "https://guaner001125.coding.net/p/coding-code-guide/d/booksources/git/raw/master/sources/guaner.json",
+    "https://haxc.coding.net/p/booksrc/d/booksrc/git/raw/master/Book3.0Source.json",
+
+    "https://namofree.gitee.io/yuedu3/legado3_booksource_by_Namo.json",
+    "https://pbpobing.coding.net/p/yueduyuan/d/sy/git/raw/master/syhj.json",
+    "https://pbpobing.coding.net/p/yueduyuan/d/sy/git/raw/master/yshj.json",
+
+    "http://shuyuan.miaogongzi.net/shuyuan/1624832786.json",
+    "https://shuyuan.miaogongzi.net/shuyuan/1630342495.json",
+    "http://shuyuan.miaogongzi.net/shuyuan/1623355431.json",
+
+    "https://tangguochaotian.coding.net/p/tangguoshuyuan1015/d/tangguo/git/raw/master/exportBookSource.json",
+    "https://tianyuzhange.coding.net/p/booksource/d/shuyuan/git/raw/master/2.0shuyuan.json",
+]
+
+
+book = BookSource(lanzou_fid=4147049)
+book_correct = BookSourceCorrect(lanzou_fid=4147049)
+load_source(urls=urls, book=book)
+correct_source(book, book_correct)
+print(book.db_save())
