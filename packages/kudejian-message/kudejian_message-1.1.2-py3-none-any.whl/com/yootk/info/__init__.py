d1a011cc42ab6d08c5126456d26c101d0dbe55ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:52 +0000

    Add package kudejian-message/1.1.2/kudejian_message-1.1.2-py3-none-any.whl

---
+++ packages/kudejian-message/kudejian_message-1.1.2-py3-none-any.whl/com/yootk/info/__init__.py
@@ -0,0 +1,10 @@
+# coding:UTF-8
+# P110_实例：修改com yootk info __init__.py配置文件
+__all__ = ["message"]
+
+"""
+[程序运行结果]：
+
+[重要提示]：
+
+"""
---
+++ packages/kudejian-message/kudejian_message-1.1.2-py3-none-any.whl/com/yootk/info/message.py
@@ -0,0 +1,14 @@
+# coding:UTF-8
+# 定义“com.yootk.info.message.py”程序文件
+def get_info():     # 定义模块函数
+    return "沐言优拓：www.yootk.com"     # 返回数据信息
+_url = "www.yootk.com"
+
+"""
+[程序运行结果]：
+
+[重要提示]：
+要求main.py所在根目录与message.py主目录com同级
+模块定义只需将单独定义的程序文件保存到相应的包中即可。
+而包的本质就是文件目录，该目录允许有多级，目录名称要求全部采用小写字母。
+"""
