c5c64e80ac2640856ff7bfe55421c305867e6fdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:40 +0000

    Add package pub.tools/4.0/pub.tools-4.0.tar.gz

---
+++ packages/pub.tools/pub.tools-4.0.tar.gz/pub.tools-4.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+
+with open('VERSION.txt') as version_file:
+    version = version_file.read().strip()
+
+setup(name='pub.tools',
+      version=version,
+      description="Package of tools for formatting publication data and accessing data from PubMed's Entrez tool",
+      long_description=open("README.md").read() + "\n" + open("CHANGES.md").read(),
+      long_description_content_type='text/markdown',
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "Topic :: Scientific/Engineering",
+          "Topic :: Scientific/Engineering :: Bio-Informatics",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
+      keywords='',
+      author='Eric Wohnlich',
+      author_email='wohnlice@imsweb.com',
+      url='http://git.imsweb.com/wohnlice/pub.tools',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['pub'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'biopython>=1.73',
+          'unidecode',
+          'lxml',
+          'requests'
+      ],
+      )
