6043e6ce5f99a9c201afd9d15c542bf922247498
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:39 +0000

    Add package ftp-himawari8-hsd/1.0.10/ftp_himawari8_hsd-1.0.10.tar.gz

---
+++ packages/ftp-himawari8-hsd/ftp_himawari8_hsd-1.0.10.tar.gz/ftp_himawari8_hsd-1.0.10/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ftp_himawari8_hsd",
+    version="1.0.10",
+    author="Sasikala Gnanamuthu",
+    author_email="serskg@nus.edu.sg",
+    description="Sateliite Imagery Download Package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    package_dir={"": "src"},
+    url="https://github.com/gSasikala/Python_JAXA_Himawari8_Imagery_Downloader.git",
+    packages=setuptools.find_packages(where="src"),
+    install_requires=[
+        'argparse==1.4.0',
+        'dateparser==1.0.0',
+        'DateTime==4.3',
+        'wget==3.2',
+        'bz2file==0.98',
+        'python-dateutil==2.8.1',
+        'pathlib==1.0.1',
+        'regex==2020.11.13',
+        'futures3==1.0.0',
+        'pandas~=1.2.4'
+    ],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
