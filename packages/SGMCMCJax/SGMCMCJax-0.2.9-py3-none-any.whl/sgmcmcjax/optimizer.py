9b2b7761c519aedcadcf2666aa9cfefb6e5f39a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:05 +0000

    Add package SGMCMCJax/0.2.9/SGMCMCJax-0.2.9-py3-none-any.whl

---
+++ packages/SGMCMCJax/SGMCMCJax-0.2.9-py3-none-any.whl/sgmcmcjax/optimizer.py
@@ -0,0 +1,56 @@
+from typing import Tuple, Callable
+from jax.tree_util import tree_map
+from jax.experimental.optimizers import adam
+from jax import jit, lax, random
+import jax.numpy as jnp
+import optax
+
+from .gradient_estimation import build_gradient_estimation_fn
+from .util import progress_bar_scan, build_grad_log_post
+
+
+def build_adam_optimizer(dt: float, loglikelihood: Callable, logprior: Callable, data: Tuple, batch_size: int) -> Callable:
+    grad_log_post = build_grad_log_post(loglikelihood, logprior, data, with_val=True)
+    estimate_gradient, _ = build_gradient_estimation_fn(grad_log_post, data, batch_size)
+
+    init_fn, update, get_params = adam(dt)
+
+    @jit
+    def body(carry, i):
+        key, state = carry
+        key, subkey = random.split(key)
+        (lp_val, param_grad), _ = estimate_gradient(i, subkey, get_params(state))
+        neg_param_grad = tree_map(lambda x: -x, param_grad)
+        state = update(i, neg_param_grad, state)
+        return (key, state), lp_val
+
+    def run_adam(key, Niters, params_IC):
+        state = init_fn(params_IC)
+        body_pbar = progress_bar_scan(Niters)(body)
+        (_, state_opt), logpost_array = lax.scan(body_pbar, (key, state), jnp.arange(Niters))
+        return get_params(state_opt), logpost_array
+    return run_adam
+
+
+
+def build_optax_optimizer(optimizer: optax.GradientTransformation, loglikelihood: Callable, logprior: Callable, data: Tuple, batch_size: int) -> Callable:
+    grad_log_post = build_grad_log_post(loglikelihood, logprior, data, with_val=True)
+    estimate_gradient, _ = build_gradient_estimation_fn(grad_log_post, data, batch_size)
+ 
+    @jit
+    def body(carry, i):
+        key, state, params = carry
+        key, subkey = random.split(key)
+        (lp_val, param_grad), _ = estimate_gradient(i, subkey, params)
+        neg_param_grad = tree_map(lambda x: -x, param_grad)
+        updates, state = optimizer.update(neg_param_grad, state)
+        params = optax.apply_updates(params, updates)
+        return (key, state, params), lp_val
+
+
+    def run_optimizer(key, Niters, params):
+        state = optimizer.init(params)
+        body_pbar = progress_bar_scan(Niters)(body)
+        (key, state, params), logpost_array = lax.scan(body_pbar, (key, state, params), jnp.arange(Niters))
+        return params, logpost_array
+    return run_optimizer
