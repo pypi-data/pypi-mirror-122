709550ac9e49708b7ca4beaead1fb802d441045f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:41 +0000

    Add package streamlit-text-label/0.1.0/streamlit_text_label-0.1.0.tar.gz

---
+++ packages/streamlit-text-label/streamlit_text_label-0.1.0.tar.gz/streamlit_text_label-0.1.0/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="streamlit_text_label",
+    version="0.1.0",
+    author="Basis AI",
+    author_email="developers@basis-ai.com",
+    description="Components for labelling text document",
+    long_description=long_description,
+    long_description_content_type="text/plain",
+    url="https://github.com/basisai/streamlit-text-label",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        # By definition, a Custom Component depends on Streamlit.
+        # If your component has other Python dependencies, list
+        # them here.
+        "streamlit >= 0.63",
+    ],
+)
