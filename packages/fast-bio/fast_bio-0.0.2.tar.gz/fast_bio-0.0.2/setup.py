6654f4f4e54f3b5bb848868f1820838c20f03ec3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:53 +0000

    Add package fast-bio/0.0.2/fast_bio-0.0.2.tar.gz

---
+++ packages/fast-bio/fast_bio-0.0.2.tar.gz/fast_bio-0.0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+from Cython.Build import cythonize
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="fast_bio",
+    version="0.0.2",
+    packages=find_packages(),
+    author="Cadel Watson",
+    description="Fast utilities for bioinformatics, written in Cython",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    ext_modules=cythonize(["fast_bio/__init__.pyx"]),
+    install_requires=[],
+)
