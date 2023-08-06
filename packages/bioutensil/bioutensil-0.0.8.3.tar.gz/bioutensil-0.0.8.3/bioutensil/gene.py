b68b00ffda89c68fb8305bef0a4ec8d7b35ec51a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:06:08 +0000

    Add package bioutensil/0.0.8.3/bioutensil-0.0.8.3.tar.gz

---
+++ packages/bioutensil/bioutensil-0.0.8.3.tar.gz/bioutensil-0.0.8.3/bioutensil/gene.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+    gene
+    ~~~~~~~~~~~~~~
+
+    function for gene
+
+    @Copyright: (c) 2018-04 by Lingxi Chen (chanlingxi@gmail.com).
+    @License: LICENSE_NAME, see LICENSE for more details.
+"""
+import os
+
+from bioutensil import basic
+
+
+def read_gene_list(fn, sep='\t'):
+    if not fn:
+        return []
+    gene_list = []
+    for row in basic.safe_open(fn, 'r'):
+        if row.startswith('#'):
+            continue
+        gene = row.strip().split(sep)[0]
+        if gene:
+            gene_list.append(gene)
+
+    return gene_list
+
+def read_gene_list_from_dir(in_dir):
+    gene_set = set()
+    for gs in read_gene_dict_from_dir(in_dir).values():
+        gene_set = set(gs) | gene_set
+    return list(gene_set)
+
+def read_gene_dict_from_dir(in_dir):
+    if not in_dir:
+        return {}
+    if os.path.isfile(in_dir):
+        _, fn = os.path.split(in_dir)
+        return {fn: read_gene_list(fn)}
+    gene_dict = {}
+    for fn in os.listdir(in_dir):
+        in_fn = os.path.join(in_dir, fn)
+        gene_dict[fn] = read_gene_list(in_fn)
+
+    return gene_dict
---
+++ packages/bioutensil/bioutensil-0.0.8.3.tar.gz/bioutensil-0.0.8.3/setup.py
@@ -0,0 +1,28 @@
+
+import setuptools
+from bioutensil.version import __version__
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bioutensil",
+    version=__version__,
+    author="Lingxi Chen",
+    author_email="chanlingxi@gmail.com",
+    description="Bioinformatics utensil",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/paprikachan/bioutensil",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    include_package_data=True,
+    package_data={
+        "bioutensil": ["bio_database/*"],
+        "bioutensil": ["bio_database/*/*"],
+    },
+)
