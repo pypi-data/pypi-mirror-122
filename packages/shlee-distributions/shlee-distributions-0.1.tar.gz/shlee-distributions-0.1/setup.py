5539784ceb08a7229115a42e563bec95dadb661c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:18 +0000

    Add package shlee-distributions/0.1/shlee-distributions-0.1.tar.gz

---
+++ packages/shlee-distributions/shlee-distributions-0.1.tar.gz/shlee-distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='shlee-distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['shlee-distributions'],
+      zip_safe=False)
