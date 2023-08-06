38a7136508578874a2ae93b8370b95830f400fda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:37 +0000

    Add package primes-eip-f/1.0/primes_eip_f-1.0-py3-none-any.whl

---
+++ packages/primes-eip-f/primes_eip_f-1.0-py3-none-any.whl/primes_eip_f/__init__.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  7 10:14:22 2021
+
+@author: feemjo
+"""
+
+import rpy2.robjects as ro
+
+
+def es_primo(n):
+    res = True
+    for i in range(2,n):
+        if(n%i==0):
+            res=False
+    return res
+
+def numeros_primos(n):
+    for i in range(1, n):
+        if(es_primo(i)):
+            print("El numero", i, "es primo")
+
+primos_r = """
+primos <- function(n){
+    print("Numeros primos")
+    for(i in 2:n){
+        res<-TRUE
+        for(j in 2:i){
+            if(i%%j==0 && j!=i){
+                res<-FALSE
+            }
+        }
+        if(res==TRUE){
+            print(i)
+        }
+    }
+}
+"""
+
+# numeros_primos(10)
+# ro.r(primos_r)
+# primos_py = ro.globalenv['primos']
+# primos_py(10)
