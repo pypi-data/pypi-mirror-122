cd439f0332281e6a883894545bcb32f16665d1ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:43:52 +0000

    Add package ColabTurtlePlus/2.0.1/ColabTurtlePlus-2.0.1.tar.gz

---
+++ packages/ColabTurtlePlus/ColabTurtlePlus-2.0.1.tar.gz/ColabTurtlePlus-2.0.1/setup.py
@@ -0,0 +1,25 @@
+import pathlib
+from distutils.core import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
+    name='ColabTurtlePlus',
+    version='2.0.1',
+    packages=['ColabTurtlePlus'],
+    url='https://github.com/mathriddle/ColabTurtlePlus',
+    license='MIT',
+    author='Larry Riddle',
+    author_email='lriddle@agnesscott.edu',
+    description='An HTML based Turtle implementation with classes for Google Colab and Jupyter Labs',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+)
