5387bde72789e22fd17032093978f9797dc18f28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:43 +0000

    Add package beanie/1.6.1/beanie-1.6.1-py3-none-any.whl

---
+++ packages/beanie/beanie-1.6.1-py3-none-any.whl/beanie/odm/interfaces/update.py
@@ -0,0 +1,100 @@
+from abc import abstractmethod
+from typing import Dict, Mapping, Union, Any, Optional
+
+from pymongo.client_session import ClientSession
+
+from beanie.odm.fields import ExpressionField
+from beanie.odm.operators.update.general import (
+    Set,
+    CurrentDate,
+    Inc,
+)
+
+
+class UpdateMethods:
+    """
+    Update methods
+    """
+
+    @abstractmethod
+    def update(
+        self,
+        *args: Mapping[str, Any],
+        session: Optional[ClientSession] = None,
+        **kwargs
+    ):
+        return self
+
+    def set(
+        self,
+        expression: Dict[Union[ExpressionField, str], Any],
+        session: Optional[ClientSession] = None,
+        **kwargs
+    ):
+        """
+        Set values
+
+        Example:
+
+        ```python
+
+        class Sample(Document):
+            one: int
+
+        await Document.find(Sample.one == 1).set({Sample.one: 100})
+
+        ```
+
+        Uses [Set operator](https://roman-right.github.io/beanie/api/operators/update/#set)
+
+        :param expression: Dict[Union[ExpressionField, str], Any] - keys and
+        values to set
+        :param session: Optional[ClientSession] - pymongo session
+        :return: self
+        """
+        return self.update(Set(expression), session=session, **kwargs)
+
+    def current_date(
+        self,
+        expression: Dict[Union[ExpressionField, str], Any],
+        session: Optional[ClientSession] = None,
+        **kwargs
+    ):
+        """
+        Set current date
+
+        Uses [CurrentDate operator](https://roman-right.github.io/beanie/api/operators/update/#currentdate)
+
+        :param expression: Dict[Union[ExpressionField, str], Any]
+        :param session: Optional[ClientSession] - pymongo session
+        :return: self
+        """
+        return self.update(CurrentDate(expression), session=session, **kwargs)
+
+    def inc(
+        self,
+        expression: Dict[Union[ExpressionField, str], Any],
+        session: Optional[ClientSession] = None,
+        **kwargs
+    ):
+        """
+        Increment
+
+        Example:
+
+        ```python
+
+        class Sample(Document):
+            one: int
+
+        await Document.find(Sample.one == 1).inc({Sample.one: 100})
+
+        ```
+
+        Uses [Inc operator](https://roman-right.github.io/beanie/api/operators/update/#inc)
+
+        :param expression: Dict[Union[ExpressionField, str], Any]
+        :param session: Optional[ClientSession] - pymongo session
+        :return: self
+        """
+        return self.update(Inc(expression), session=session, **kwargs)
