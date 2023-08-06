90867e0ed292e4fbfcc45b466e419c852aa04e29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:01 +0000

    Add package DrawBinTree/0.1.1/DrawBinTree-0.1.1.tar.gz

---
+++ packages/DrawBinTree/DrawBinTree-0.1.1.tar.gz/DrawBinTree-0.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup,find_packages
+
+setup(
+
+    name='DrawBinTree',
+    version='0.1.1',
+    keywords=('pip','binary','tree'),
+    description="draw binary tree by matplotlib",
+    long_description="draw binary tree by matplotlib with two methods",
+    licence='MIT Licence',
+
+    url='https://github.com/jim0575/desktop-tutorial',
+    author='james',
+    author_email="jim0575@qq.com",
+
+    packages=find_packages(),
+    include_package_data=True,
+    platforms='any',
+    install_requires=[]
+    )
