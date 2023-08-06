b44d26e517d486349b7acd7b4bfe6cc56c7e427f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:23 +0000

    Add package oneNeuron-pypi-jrk90210us/0.0.3/oneNeuron_pypi-jrk90210us-0.0.3.tar.gz

---
+++ packages/oneNeuron-pypi-jrk90210us/oneNeuron_pypi-jrk90210us-0.0.3.tar.gz/oneNeuron_pypi-jrk90210us-0.0.3/README.md
@@ -0,0 +1,7 @@
+# oneNeuron_pypi
+* Packaging Perceptron - oneNeuron
+  * Making single perceptron
+  * Testing AND, OR, XOR & NAND logic gates with single perceptron
+## Version History:
+  * version=0.0.2:
+    * Added all utils to this version
---
+++ packages/oneNeuron-pypi-jrk90210us/oneNeuron_pypi-jrk90210us-0.0.3.tar.gz/oneNeuron_pypi-jrk90210us-0.0.3/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+PROJECT_NAME = "oneNeuron_pypi"
+USER_NAME = "jrk90210us"
+
+setuptools.setup(
+    name=f"{PROJECT_NAME}-{USER_NAME}",
+    version="0.0.3",
+    author="Jatin",
+    author_email="jrk90210us@gmail.com",
+    description="Packaging Perceptron - oneNeuron",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{USER_NAME}/{PROJECT_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{USER_NAME}/{PROJECT_NAME}/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7",
+    install_requires=[
+        "numpy",
+        "tqdm",
+        "matplotlib",
+        "pandas",
+        "joblib"
+    ]
+)
