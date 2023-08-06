801ee14c46a2527bfb93bf78dd0ae7eb7bd1e4ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:06 +0000

    Add package spectraframe/0.1dev/spectraframe-0.1dev.tar.gz

---
+++ packages/spectraframe/spectraframe-0.1dev.tar.gz/spectraframe-0.1dev/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+
+setup(
+    name='spectraframe',
+    version='0.1dev',
+    packages=['spectraframe'],
+    license='MIT License',
+)
---
+++ packages/spectraframe/spectraframe-0.1dev.tar.gz/spectraframe-0.1dev/spectraframe/__init__.py
@@ -0,0 +1,5 @@
+from .spectradataframe.spectradataframe import SpectraDataFrame
+from .plotting.plottingtools import plot
+from .spectradataframe.importing import read_csv, read_tsv
+from .tools.outlier_removal import pick_outliers
+from .tools.interpolation import interpolate
