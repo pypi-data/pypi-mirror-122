2d294813392c6a12c0fbc73e84a48ee1d9095800
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:49 +0000

    Add package pptb/0.1.2/pptb-0.1.2.tar.gz

---
+++ packages/pptb/pptb-0.1.2.tar.gz/pptb-0.1.2/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pptb',
+ 'pptb.nn',
+ 'pptb.optimizer',
+ 'pptb.tools',
+ 'pptb.vision',
+ 'pptb.vision.models']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'pptb',
+    'version': '0.1.2',
+    'description': '🚣 一些常用的但 paddle 里没有的小工具～',
+    'long_description': None,
+    'author': 'Nyakku Shigure',
+    'author_email': 'sigure.qaq@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
