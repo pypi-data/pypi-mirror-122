c52b8f02ef627fbf86e7a2adbadfe60f7f1ea103
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:05 +0000

    Add package pyhaversion/21.10.1/pyhaversion-21.10.1-py3-none-any.whl

---
+++ packages/pyhaversion/pyhaversion-21.10.1-py3-none-any.whl/pyhaversion/pypi.py
@@ -0,0 +1,56 @@
+"""pyhaversion package."""
+import asyncio
+from dataclasses import dataclass
+from typing import List
+
+import async_timeout
+from awesomeversion import AwesomeVersion
+
+from .base import HaVersionBase
+from .consts import (
+    DATA_INFO,
+    DATA_RELEASES,
+    DATA_VERSION,
+    DEFAULT_HEADERS,
+    HaVersionChannel,
+    HaVersionSource,
+)
+from .exceptions import HaVersionInputException
+
+URL = "https://pypi.org/pypi/homeassistant/json"
+
+
+@dataclass
+class HaVersionPypi(HaVersionBase):
+    """Handle versions for the PyPi source."""
+
+    source = HaVersionSource.PYPI
+
+    def validate_input(self) -> None:
+        """Raise HaVersionInputException if expected input are missing."""
+        if self.session is None:
+            raise HaVersionInputException("Missing aiohttp.ClientSession")
+
+    async def fetch(self, **kwargs):
+        """Logic to fetch new version data."""
+        async with async_timeout.timeout(self.timeout, loop=asyncio.get_event_loop()):
+            request = await self.session.get(url=URL, headers=DEFAULT_HEADERS)
+            self._data = await request.json()
+
+    def parse(self):
+        """Logic to parse new version data."""
+        self._version = self.data.get(DATA_INFO, {}).get(DATA_VERSION)
+
+        versions = sorted(
+            [
+                AwesomeVersion(version)
+                for version in self.data.get(DATA_RELEASES, [])
+                if version.startswith("2")
+            ],
+            reverse=True,
+        )
+        for version in versions:
+            if self.channel == HaVersionChannel.STABLE and version.beta:
+                continue
+            self._version = version
+            break
