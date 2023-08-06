43fcdecbd3ab8333c44722c7340883a71a5912b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:43:26 +0000

    Add package dinject/1.0.2/dinject-1.0.2-py3-none-any.whl

---
+++ packages/dinject/dinject-1.0.2-py3-none-any.whl/dinject/types/__init__.py
@@ -0,0 +1,9 @@
+from dinject.types.block import Block
+from dinject.types.executor import Executor
+from dinject.types.instruction import Instruction
+
+__all__ = [
+    "Block",
+    "Executor",
+    "Instruction",
+]
