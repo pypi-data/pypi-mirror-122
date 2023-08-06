b6f376d4e946b2ad26f3eb4c7e00500ef3374bc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:02 +0000

    Add package ais-dom/2021.10.0b9/ais_dom-2021.10.0b9-py3-none-any.whl

---
+++ packages/ais-dom/ais_dom-2021.10.0b9-py3-none-any.whl/homeassistant/components/ais_updater/scripts/fix_script.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+echo "echo -e ''" > /data/data/pl.sviete.dom/files/usr/bin/sudo 
+echo "echo -e '\e[40;38;5;82m Hejka tu \e[30;48;5;82m Jolka \e[0m'" >> /data/data/pl.sviete.dom/files/usr/bin/sudo 
+echo "echo -e '\e[101m sudo zostało zablokowane dla Twojego dobra\e[0m'" >> /data/data/pl.sviete.dom/files/usr/bin/sudo 
+echo "echo -e ''" >> /data/data/pl.sviete.dom/files/usr/bin/sudo 
+echo "2021-10-06" > /data/data/pl.sviete.dom/files/home/AIS/.ais_fix
