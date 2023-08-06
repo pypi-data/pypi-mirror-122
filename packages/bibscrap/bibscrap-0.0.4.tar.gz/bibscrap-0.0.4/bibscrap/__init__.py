b58593c23df695cbaa39fdad827e97d1b0121fa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:42 +0000

    Add package bibscrap/0.0.4/bibscrap-0.0.4.tar.gz

---
+++ packages/bibscrap/bibscrap-0.0.4.tar.gz/bibscrap-0.0.4/bibscrap/__init__.py
@@ -0,0 +1,29 @@
+"""Semi-automated tools for systematic literature reviews."""
+
+import argparse
+
+__import__("pkg_resources").declare_namespace(__name__)
+
+__version__ = "0.0.4"
+__version_info__ = __version__.split(".")
+
+__arg_parser = argparse.ArgumentParser(
+    prog="bibscrap",
+    description="Semi-automated tools for systematic literature reviews.",
+    epilog="""
+Submit bugs, issues, and feature requests at
+https://github.com/cotterell/bibscrap/issues.
+""",
+)
+
+__arg_parser.add_argument(
+    "--version",
+    action="version",
+    version=f"%(prog)s {__version__}",
+)
+
+
+def main() -> int:
+    args = __arg_parser.parse_args()
+    __arg_parser.parse_args(["--version"])
+    return 0
