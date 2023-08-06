caa11de3feb5b086a8da148f18e5c02f3d8efcbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:11 +0000

    Add package silverbots/0.0.6/silverbots-0.0.6.tar.gz

---
+++ packages/silverbots/silverbots-0.0.6.tar.gz/silverbots-0.0.6/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="silverbots",
+    version="0.0.6",
+    author="SrJSilver",
+    author_email="Sergey081203@ya.ru",
+    description="Silver Bots",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/WwWSrJSilverWwW/SilverBots",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
