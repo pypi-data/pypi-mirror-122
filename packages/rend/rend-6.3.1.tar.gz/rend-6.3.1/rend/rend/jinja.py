f188a23a3077621c32c6d95b75e1719ea515f7cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:41 +0000

    Add package rend/6.3.1/rend-6.3.1.tar.gz

---
+++ packages/rend/rend-6.3.1.tar.gz/rend-6.3.1/rend/rend/jinja.py
@@ -0,0 +1,36 @@
+"""
+Render jinja data
+"""
+import os
+
+import jinja2.ext
+
+import rend.exc
+
+
+async def render(hub, data):
+    """
+    Render the given data through Jinja2
+    """
+    env_args = {"extensions": [], "loader": jinja2.FileSystemLoader(os.getcwd())}
+
+    if hasattr(jinja2.ext, "do"):
+        env_args["extensions"].append("jinja2.ext.do")
+    if hasattr(jinja2.ext, "loopcontrols"):
+        env_args["extensions"].append("jinja2.ext.loopcontrols")
+
+    jinja_env = jinja2.Environment(
+        undefined=jinja2.StrictUndefined, enable_async=True, **env_args
+    )
+
+    if isinstance(data, bytes):
+        data = data.decode()
+
+    try:
+        template = jinja_env.from_string(data)
+        ret = await template.render_async(hub=hub)
+    except jinja2.exceptions.UndefinedError as exc:
+        raise rend.exc.RenderException(f"Jinja variable {exc.message}")
+    except jinja2.exceptions.TemplateSyntaxError as exc:
+        raise rend.exc.RenderException(f"Jinja syntax error {exc.message}")
+    return ret
