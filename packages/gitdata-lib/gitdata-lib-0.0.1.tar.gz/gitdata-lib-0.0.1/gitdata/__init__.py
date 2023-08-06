a2527a5471482ce19f45057c932d496693e1fb65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:11 +0000

    Add package gitdata-lib/0.0.1/gitdata-lib-0.0.1.tar.gz

---
+++ packages/gitdata-lib/gitdata-lib-0.0.1.tar.gz/gitdata-lib-0.0.1/gitdata/__init__.py
@@ -0,0 +1,14 @@
+"""
+    gitdata
+"""
+
+from .utils import Record
+from .stores.records import table_of
+from .stores.entities import store_of
+
+
+def fetch(_):
+    """Fetch data
+
+    Stub for fetch data function.
+    """
