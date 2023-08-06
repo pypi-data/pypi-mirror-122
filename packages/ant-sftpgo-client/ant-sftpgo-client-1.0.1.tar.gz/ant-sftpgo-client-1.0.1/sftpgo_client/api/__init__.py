69162a7cdd671061b2fb04ffc78a46c2a5531ee0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:10 +0000

    Add package ant-sftpgo-client/1.0.1/ant-sftpgo-client-1.0.1.tar.gz

---
+++ packages/ant-sftpgo-client/ant-sftpgo-client-1.0.1.tar.gz/ant-sftpgo-client-1.0.1/sftpgo_client/api/__init__.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from sftpgo_client.api.admins_api import AdminsApi
+from sftpgo_client.api.connections_api import ConnectionsApi
+from sftpgo_client.api.defender_api import DefenderApi
+from sftpgo_client.api.folders_api import FoldersApi
+from sftpgo_client.api.healthcheck_api import HealthcheckApi
+from sftpgo_client.api.maintenance_api import MaintenanceApi
+from sftpgo_client.api.quota_api import QuotaApi
+from sftpgo_client.api.token_api import TokenApi
+from sftpgo_client.api.users_api import UsersApi
