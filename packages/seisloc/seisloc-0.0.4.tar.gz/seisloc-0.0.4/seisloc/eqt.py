50b247e40e3681cc75af0e5fd4f41ea7f373ebe3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:01 +0000

    Add package seisloc/0.0.4/seisloc-0.0.4.tar.gz

---
+++ packages/seisloc/seisloc-0.0.4.tar.gz/seisloc-0.0.4/seisloc/eqt.py
@@ -0,0 +1,21 @@
+#!/usr/bin/evn python
+# EQTransformer related functions
+
+import obspy
+from obspy import UTCDateTime
+
+def to_mseed(trace):
+    """
+    Save the obspy trace to the format EQTransformer could recognized
+    """
+    net = trace.stats.network
+    sta = trace.stats.station
+    chn = trace.stats.channel
+    starttime = trace.stats.starttime
+    endtime = trace.stats.endtime
+    out_name = net+"."+sta+"."+chn+"__"+\
+                starttime.strftime("%Y%m%dT%H%M%SZ")+"__"+\
+                endtime.strftime("%Y%m%dT%H%M%SZ")+".mseed"\
+
+    trace.write(out_name)
+
