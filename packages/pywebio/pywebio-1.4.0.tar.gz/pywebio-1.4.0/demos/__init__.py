0ade9af4997b65bb8b9b702391e35afad8f7501f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:37 +0000

    Add package pywebio/1.4.0/pywebio-1.4.0.tar.gz

---
+++ packages/pywebio/pywebio-1.4.0.tar.gz/pywebio-1.4.0/demos/__init__.py
@@ -0,0 +1,8 @@
+r"""
+.. automodule:: demos.bmi
+.. automodule:: demos.input_usage
+.. automodule:: demos.output_usage
+.. automodule:: demos.chat_room
+.. automodule:: demos.markdown_previewer
+.. automodule:: demos.gomoku_game
+"""
