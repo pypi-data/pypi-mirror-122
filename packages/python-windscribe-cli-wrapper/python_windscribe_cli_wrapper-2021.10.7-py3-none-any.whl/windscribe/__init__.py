97852b11feb59908a52db732a63e251ea4afab99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:17 +0000

    Add package python-windscribe-cli-wrapper/2021.10.7/python_windscribe_cli_wrapper-2021.10.7-py3-none-any.whl

---
+++ packages/python-windscribe-cli-wrapper/python_windscribe_cli_wrapper-2021.10.7-py3-none-any.whl/windscribe/__init__.py
@@ -0,0 +1,19 @@
+__all__ = [
+    'account',
+    'connect',
+    'locations',
+    'login',
+    'logout',
+    'disconnect',
+    'firewall'
+]
+
+from .windscribe import (
+    account,
+    connect,
+    locations,
+    login,
+    logout,
+    disconnect,
+    firewall
+)
