f3d22470a0b0462481ac772507e023c87dd75cfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:54 +0000

    Add package chatique/1.0.1/chatique-1.0.1-py3-none-any.whl

---
+++ packages/chatique/chatique-1.0.1-py3-none-any.whl/chatique/chatique.py
@@ -0,0 +1,22 @@
+import requests
+import coolname
+
+ADDRESS = "http://82.148.30.33:7878/"
+NAME = "-".join(coolname.generate_slug().split("-")[:2])
+
+
+def change_name(new_name):
+    global NAME
+    NAME = new_name
+    print(f"Nickname changed")
+
+
+def send(message, name=NAME):
+    '''send the message to the chat'''
+    requests.post(f"{ADDRESS}", data=f"msg~~~{message}|name~~~{name}".encode('utf-8'))
+
+
+def get(top=10):
+    '''get top n messages from the chat'''
+    resp = requests.post(f"{ADDRESS}", data=f"top~~~{top}".encode('utf-8'))
+    print(resp.content.decode('utf-8'))
