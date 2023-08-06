ecb574220e25e50447a0780cd8652958172875f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:02 +0000

    Add package mlem/0.1.2/mlem-0.1.2-py3-none-any.whl

---
+++ packages/mlem/mlem-0.1.2-py3-none-any.whl/mlem/__init__.py
@@ -0,0 +1,15 @@
+"""
+MLEM is a tool to help you version and deploy your Machine Learning models:
+* Serialise any model trained in Python into ready-to-deploy format
+* Model lifecycle management using Git and GitOps principles
+* Provider-agnostic deployment
+"""
+from . import api  # noqa
+from .config import CONFIG
+from .ext import ExtensionLoader
+from .version import __version__
+
+if CONFIG.AUTOLOAD_EXTS:
+    ExtensionLoader.load_all()
+
+__all__ = ["api", "__version__"]
