ccd12b281f238d8872b4aba382e5f8ce0af0cd38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:26 +0000

    Add package aldian-probability/0.1/aldian_probability-0.1.tar.gz

---
+++ packages/aldian-probability/aldian_probability-0.1.tar.gz/aldian_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='aldian_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['aldian_probability'],
+      author='Aldian Fazrihady',
+      author_email='mobile@aldian.net',
+      zip_safe=False)
