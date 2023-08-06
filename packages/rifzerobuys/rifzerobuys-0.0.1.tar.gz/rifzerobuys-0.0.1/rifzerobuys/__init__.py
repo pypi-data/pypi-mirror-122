db0a6cb48337cadd7a92de1ca5227be962ad8291
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:07 +0000

    Add package rifzerobuys/0.0.1/rifzerobuys-0.0.1.tar.gz

---
+++ packages/rifzerobuys/rifzerobuys-0.0.1.tar.gz/rifzerobuys-0.0.1/rifzerobuys/__init__.py
@@ -0,0 +1,1 @@
+from rifzerobuys.rifzerobuy import riffer
---
+++ packages/rifzerobuys/rifzerobuys-0.0.1.tar.gz/rifzerobuys-0.0.1/rifzerobuys/rifzerobuy.py
@@ -0,0 +1,9 @@
+class riffer:
+    def __init__(self, name):
+        self.name = name
+    def upper(self):
+        self.name = self.name.upper()
+    def show(self):
+        print(self.name)
+    def show2(self):
+        print(self.name.lower())
