d976ceb905839b7bec19737d71bda80adfcc4858
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:26 +0000

    Add package giving/0.3.5/giving-0.3.5.tar.gz

---
+++ packages/giving/giving-0.3.5.tar.gz/giving-0.3.5/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['giving']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Rx>=3.2.0,<4.0.0', 'varname>=0.8.0,<0.9.0']
+
+setup_kwargs = {
+    'name': 'giving',
+    'version': '0.3.5',
+    'description': 'Reactive logging',
+    'long_description': None,
+    'author': 'Olivier Breuleux',
+    'author_email': 'breuleux@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
