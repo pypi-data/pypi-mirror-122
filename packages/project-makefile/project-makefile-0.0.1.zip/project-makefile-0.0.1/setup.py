149559b65f8a416d62cce0828d2f19946634108b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:43:11 +0000

    Add package project-makefile/0.0.1/project-makefile-0.0.1.zip

---
+++ packages/project-makefile/project-makefile-0.0.1.zip/project-makefile-0.0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages
+from setuptools import setup
+
+VERSION = "0.0.1"
+
+setup(
+    author="Alex Clark",
+    author_email="aclark@aclark.net",
+    classifiers=[],
+    description="A generic makefile for projects.",
+    keywords="",
+    license="",
+    include_package_data=True,
+    install_requires=[],
+    long_description=open("README.rst").read() + "\n" + open("CHANGES.rst").read(),
+    name="project-makefile",
+    namespace_packages=[],
+    packages=find_packages(),
+    test_suite="",
+    url="",
+    version=VERSION,
+    zip_safe=False,
+    # scripts=['project/makefile/project-makefile.py'],
+    entry_points={
+        "console_scripts": ["project-makefile=install:main"],
+    },
+    py_modules=["install"],
+)
