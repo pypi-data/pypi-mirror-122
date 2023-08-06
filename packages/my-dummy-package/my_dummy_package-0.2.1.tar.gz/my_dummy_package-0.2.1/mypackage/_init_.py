d14ad1d46ce9dde374a64cf937c0abc268454d5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:31 +0000

    Add package my-dummy-package/0.2.1/my_dummy_package-0.2.1.tar.gz

---
+++ packages/my-dummy-package/my_dummy_package-0.2.1.tar.gz/my_dummy_package-0.2.1/mypackage/_init_.py
@@ -0,0 +1,6 @@
+__version__ = "0.2.1"
+__author__ = "Masha Tukallo"
+
+
+def foo():
+    return "masha loshara"
---
+++ packages/my-dummy-package/my_dummy_package-0.2.1.tar.gz/my_dummy_package-0.2.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='my_dummy_package',
+    version='0.2.1',
+    description='My own dummy package for lab2',
+    url='',
+    install_requires=['pandas<1.0'],
+    packages=['mypackage'],
+)
