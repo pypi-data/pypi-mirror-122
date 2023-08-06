7bf270643272a29a9efda5b4d2e55aa551fb8086
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:14 +0000

    Add package VWAP/1.0.4/VWAP-1.0.4-py3-none-any.whl

---
+++ packages/VWAP/VWAP-1.0.4-py3-none-any.whl/vwap/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from .data import Data
+from .signal_gen import Signal
+from .signal_figure import signal_fig
+from .portfolio import portfolio, run
---
+++ packages/VWAP/VWAP-1.0.4-py3-none-any.whl/vwap/test.py
@@ -0,0 +1,3 @@
+from portfolio import run
+test = run(API_key='RQT8G71H1MTA5YZJ', ticker='FB', interval='5min')
+full = test.complete()
