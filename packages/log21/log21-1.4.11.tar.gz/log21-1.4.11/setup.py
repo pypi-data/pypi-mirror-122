7f5f05a7702360c1ac5d9b44307b902e89ba51ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:30 +0000

    Add package log21/1.4.11/log21-1.4.11.tar.gz

---
+++ packages/log21/log21-1.4.11.tar.gz/log21-1.4.11/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# setup.py
+
+from setuptools import setup
+
+with open('README.md', 'r') as file:
+    long_description = file.read()
+
+with open('LICENSE.txt', 'r') as file:
+    LICENSE = file.read()
+
+DESCRIPTION = 'A simple logging package that helps you log colorized messages in Windows console.'
+VERSION = '1.4.11'
+
+setup(
+    name='log21',
+    version=VERSION,
+    url='https://github.com/MPCodeWriter21/log21',
+    author='CodeWriter21(Mehrad Pooryoussof)',
+    author_email='<CodeWriter21@gmail.com>',
+    license=LICENSE,
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=['webcolors'],
+    packages=['log21'],
+    keywords=['python', 'log', 'colorize', 'color', 'logging'],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows"
+    ]
+)
