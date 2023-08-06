eb9b19ebe1e012822c016f14e41fe98d9b9cf117
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:43:58 +0000

    Add package k-probability/1.0/k_probability-1.0.tar.gz

---
+++ packages/k-probability/k_probability-1.0.tar.gz/k_probability-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='k_probability',
+      version='1.0',
+      description='Gaussian distributions',
+      packages=['k_probability'],
+      author ='Khalid Alolayan',
+      zip_safe=False)
