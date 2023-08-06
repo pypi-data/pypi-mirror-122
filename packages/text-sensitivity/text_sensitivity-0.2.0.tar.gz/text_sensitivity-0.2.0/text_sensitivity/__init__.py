c58f5ba2b123c255101932dd5eea66ad99d64d6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:08 +0000

    Add package text-sensitivity/0.2.0/text_sensitivity-0.2.0.tar.gz

---
+++ packages/text-sensitivity/text_sensitivity-0.2.0.tar.gz/text_sensitivity-0.2.0/text_sensitivity/__init__.py
@@ -0,0 +1,13 @@
+from text_sensitivity.sensitivity import compare_accuracy, compare_precision, compare_recall, input_space_robustness, input_space_robustness
+from text_sensitivity.perturbation import Perturbation, OneToOnePerturbation, OneToManyPerturbation
+from text_sensitivity.data.random.string import (RandomString, RandomAscii, RandomDigits, RandomEmojis,
+                                                 RandomLower, RandomPunctuation, RandomSpaces, RandomUpper,
+                                                 RandomWhitespace, RandomCyrillic, combine_generators)
+from text_sensitivity.data.random.entity import (RandomAddress, RandomCity, RandomCountry,
+                                                 RandomFirstName, RandomLastName, RandomName,
+                                                 RandomEmail, RandomPhoneNumber, RandomPriceTag,
+                                                 RandomYear, RandomMonth, RandomDay, RandomDayOfWeek,
+                                                 RandomCurrencySymbol, RandomCryptoCurrency, RandomLicensePlate)
+from text_sensitivity.data.generate import from_pattern, default_patterns
+
+__version__ = '0.2.0'
