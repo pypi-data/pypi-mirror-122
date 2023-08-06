ce297c060b1af4e4bd9d87e39d147dc2e13bee4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:52 +0000

    Add package yet-another-distributions-udacity/0.1/yet_another_distributions_udacity-0.1.tar.gz

---
+++ packages/yet-another-distributions-udacity/yet_another_distributions_udacity-0.1.tar.gz/yet_another_distributions_udacity-0.1/README.md
@@ -0,0 +1,3 @@
+# Info
+
+This is a collection of probabilistic distribution modules, partially written by Udacity instructors. It is incomplete.
---
+++ packages/yet-another-distributions-udacity/yet_another_distributions_udacity-0.1.tar.gz/yet_another_distributions_udacity-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='yet_another_distributions_udacity',
+      version='0.1',
+      description='Gaussian distributions exercise for Udacity ML Engineering Nanodegree',
+      packages=['distributions_udacity'],
+      zip_safe=False)
