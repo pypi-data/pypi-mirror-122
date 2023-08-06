4a3418f0eb09c4265b0bfd166bf85116adc098bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:45:46 +0000

    Add package mappable/0.1.0/mappable-0.1.0-py3-none-any.whl

---
+++ packages/mappable/mappable-0.1.0-py3-none-any.whl/mappable/static/build/precache-manifest.1531472e575d29006a7135235d6b650f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff6007e9acad53e83569a6ef8ce27f37",
+    "url": "/static/index.html"
+  },
+  {
+    "revision": "39d65cb3f02dc7d3a8d3",
+    "url": "/static/static/css/2.b0632651.chunk.css"
+  },
+  {
+    "revision": "39d65cb3f02dc7d3a8d3",
+    "url": "/static/static/js/2.f12e1718.chunk.js"
+  },
+  {
+    "revision": "8aa6aeb5b7265ef826780a9457c97cd2",
+    "url": "/static/static/js/2.f12e1718.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ecceab91c1168825962",
+    "url": "/static/static/js/main.00df9973.chunk.js"
+  },
+  {
+    "revision": "ab1c267ea20a9122648b",
+    "url": "/static/static/js/runtime-main.8082a0d0.js"
+  },
+  {
+    "revision": "2794b2bab5a665acebed06c9afbe0be4",
+    "url": "/static/static/media/logo.2794b2ba.svg"
+  }
+]);
---
+++ packages/mappable/mappable-0.1.0-py3-none-any.whl/mappable/static/precache-manifest.7d1b41451e909677aab380ba8426ab0e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fc754d758d28f09b47ec92b069400ba",
+    "url": "/static/index.html"
+  },
+  {
+    "revision": "720ed130a416f7ff21bf",
+    "url": "/static/static/css/2.9ab5f595.chunk.css"
+  },
+  {
+    "revision": "720ed130a416f7ff21bf",
+    "url": "/static/static/js/2.f12e1718.chunk.js"
+  },
+  {
+    "revision": "8aa6aeb5b7265ef826780a9457c97cd2",
+    "url": "/static/static/js/2.f12e1718.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f4eefc5ebcbdd3f274e",
+    "url": "/static/static/js/main.fc4c25d0.chunk.js"
+  },
+  {
+    "revision": "ab1c267ea20a9122648b",
+    "url": "/static/static/js/runtime-main.8082a0d0.js"
+  },
+  {
+    "revision": "2794b2bab5a665acebed06c9afbe0be4",
+    "url": "/static/static/media/logo.2794b2ba.svg"
+  }
+]);
