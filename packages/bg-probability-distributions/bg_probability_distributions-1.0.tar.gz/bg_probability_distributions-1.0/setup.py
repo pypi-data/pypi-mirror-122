2a78bbafe763e8e2ad8cbccad06413f516d66c5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:37 +0000

    Add package bg-probability-distributions/1.0/bg_probability_distributions-1.0.tar.gz

---
+++ packages/bg-probability-distributions/bg_probability_distributions-1.0.tar.gz/bg_probability_distributions-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='bg_probability_distributions',
+      version='1.0',
+      description='Binomial and Gaussian distributions',
+      author='Ifeoluwa Oderinde',
+      author_email='oderindeife@gmail.com',
+      packages=['bg_probability_distributions'],
+      zip_safe=False)
