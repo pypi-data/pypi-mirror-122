caf5b0fed5aa641231508df84895846b46e823f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:21 +0000

    Add package napari-live-recording/0.1.1/napari_live_recording-0.1.1-py3-none-any.whl

---
+++ packages/napari-live-recording/napari_live_recording-0.1.1-py3-none-any.whl/napari_live_recording/Cameras/__init__.py
@@ -0,0 +1,22 @@
+from .ICamera import *
+from .CameraError import *
+from .TestCamera import *
+from .CameraOpenCV import *
+from .CameraXimea import *
+
+supported_cameras = {
+    CAM_TEST   : TestCamera,
+    CAM_OPENCV : CameraOpenCV,
+    CAM_XIMEA  : CameraXimea
+}
+
+__all__ = [
+    "ICamera",
+    "CameraError",
+    # list of supported cameras below
+    "TestCamera",
+    "CameraOpenCV",
+    "CameraXimea",
+    # list of supported cameras above
+    "supported_cameras",
+]
