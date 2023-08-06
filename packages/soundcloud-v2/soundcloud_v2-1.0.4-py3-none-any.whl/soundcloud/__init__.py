a05f0d68c57fb153a0e3251d9b9a75533ca72ec3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:27 +0000

    Add package soundcloud-v2/1.0.4/soundcloud_v2-1.0.4-py3-none-any.whl

---
+++ packages/soundcloud-v2/soundcloud_v2-1.0.4-py3-none-any.whl/soundcloud/__init__.py
@@ -0,0 +1,8 @@
+from soundcloud.resource.playlist import AlbumPlaylist, BasicAlbumPlaylist
+from soundcloud.resource.stream import (PlaylistStreamItem,
+                                        PlaylistStreamRepostItem,
+                                        TrackStreamItem, TrackStreamRepostItem)
+from soundcloud.resource.track import BasicTrack, Track
+from soundcloud.resource.user import BasicUser, User
+from soundcloud.soundcloud import (RepostItem, SearchItem, SoundCloud,
+                                   StreamItem)
