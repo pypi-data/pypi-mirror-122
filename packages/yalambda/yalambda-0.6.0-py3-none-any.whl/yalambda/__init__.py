6b2758a8db680338eb1f601e3b942caf01df8dcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:48 +0000

    Add package yalambda/0.6.0/yalambda-0.6.0-py3-none-any.whl

---
+++ packages/yalambda/yalambda-0.6.0-py3-none-any.whl/yalambda/__init__.py
@@ -0,0 +1,22 @@
+from .core import (
+    function,
+    dispatch,
+    YaRequest,
+    YaResponse,
+
+    Context,
+    RequestContext,
+)
+from . import when
+
+__all__ = (
+    "function",
+    "dispatch",
+    "YaRequest",
+    "YaResponse",
+
+    "Context",
+    "RequestContext",
+
+    "when",
+)
