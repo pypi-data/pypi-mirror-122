bd5c7d0f97e5120de1b5930668020e1132050322
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:41 +0000

    Add package pincer/0.9.0/pincer-0.9.0.tar.gz

---
+++ packages/pincer/pincer-0.9.0.tar.gz/pincer-0.9.0/pincer/middleware/message_update.py
@@ -0,0 +1,6 @@
+# Copyright Pincer 2021-Present
+# Full MIT License can be found in `LICENSE` at the project root.
+
+"""sent when a message is updated in a subscribed text channel"""
+
+# TODO: Implement event
---
+++ packages/pincer/pincer-0.9.0.tar.gz/pincer-0.9.0/pincer/utils/api_object.py
@@ -0,0 +1,99 @@
+# Copyright Pincer 2021-Present
+# Full MIT License can be found in `LICENSE` at the project root.
+
+from __future__ import annotations
+
+import copy
+from dataclasses import dataclass, fields, _is_dataclass_instance
+from enum import Enum
+from typing import Dict, Union, Generic, TypeVar, Any
+
+from .types import MissingType
+
+T = TypeVar("T")
+
+
+def _asdict_ignore_none(obj: Generic[T]) -> Union[tuple, dict, T]:
+    """
+    Returns a dict from a dataclass that ignores
+    all values that are None
+    Modification of _asdict_inner from dataclasses
+
+    :param obj:
+        Dataclass obj
+    """
+
+    if _is_dataclass_instance(obj):
+        result = []
+        for f in fields(obj):
+            value = _asdict_ignore_none(getattr(obj, f.name))
+
+            if isinstance(value, Enum):
+                result.append((f.name, value.value))
+            # This if statement was added to the function
+            elif not isinstance(value, MissingType):
+                result.append((f.name, value))
+
+        return dict(result)
+
+    elif isinstance(obj, tuple) and hasattr(obj, '_fields'):
+        return type(obj)(*[_asdict_ignore_none(v) for v in obj])
+
+    elif isinstance(obj, (list, tuple)):
+        return type(obj)(_asdict_ignore_none(v) for v in obj)
+
+    elif isinstance(obj, dict):
+        return type(obj)(
+            (
+                _asdict_ignore_none(k),
+                _asdict_ignore_none(v)
+            ) for k, v in obj.items()
+        )
+    else:
+        return copy.deepcopy(obj)
+
+
+@dataclass
+class APIObject:
+    """
+    Represents an object which has been fetched from the Discord API.
+    """
+
+    # def __post_init__(self):
+    #     fin = {
+    #         key: _eval_type(ForwardRef(value), globals(), globals())
+    #         for key, value in self.__annotations__.items()
+    #     }
+    #
+    #     # pprint(self.__annotations__)
+    #     # pprint(get_type_hints(self))
+    #     print(fin)
+    #     print("Post init", self)
+
+    @classmethod
+    def from_dict(
+            cls: Generic[T],
+            data: Dict[str, Union[str, bool, int, Any]]
+    ) -> T:
+        """
+        Parse an API object from a dictionary.
+        """
+        if isinstance(data, cls):
+            return data
+
+        # Disable inspection for IDE because this is valid code for the
+        # inherited classes:
+        # noinspection PyArgumentList
+        return cls(
+            **{
+                key: (
+                    value.value if isinstance(value, Enum) else value
+                ) for key, value in data.items()
+            }
+        )
+
+    def to_dict(self) -> Dict:
+        """
+        Transform the current object to a dictionary representation.
+        """
+        return _asdict_ignore_none(self)
