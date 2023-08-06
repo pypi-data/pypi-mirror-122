0825cba76e1708b8d1ce7c9a6025151357e18e88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:23 +0000

    Add package xdgpspconf/0.0.4/xdgpspconf-0.0.4-py3-none-any.whl

---
+++ packages/xdgpspconf/xdgpspconf-0.0.4-py3-none-any.whl/tests/test_conf.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8; mode: python; -*-
+# Copyright © 2020-2021 Pradyumna Paranjape
+#
+# This file is part of xdgpspconf.
+#
+# xdgpspconf is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# xdgpspconf is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with xdgpspconf. If not, see <https://www.gnu.org/licenses/>.
+#
+"""
+Test config locations
+"""
+
+from pathlib import Path
+from unittest import TestCase
+
+from xdgpspconf import read_config, safe_config
+from xdgpspconf.config import write_config
+
+
+class TestRead(TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_ancestors(self):
+        """
+        check that locations are returned
+        """
+        configs = read_config('test', ancestors=True, py_bin=Path(__file__))
+        self.assertIn(Path('./.testrc').resolve(), configs)
+
+    def test_wo_ancestors(self):
+        """
+        check that locations are returned
+        """
+        configs = read_config('test')
+        self.assertNotIn(Path('../setup.cfg').resolve(), configs)
+
+
+class TestSafeConfig(TestCase):
+    def test_ancestors(self):
+        """
+        check that locations are returned
+        """
+        data_locs = safe_config('test', ancestors=True)
+        self.assertIn(Path('./.testrc').resolve(), data_locs)
+        self.assertNotIn(Path('../setup.cfg').resolve(), data_locs)
+
+    def test_wo_ancestors(self):
+        """
+        check that locations are returned
+        """
+        data_locs = safe_config('test')
+        self.assertNotIn(Path('../setup.cfg').resolve(), data_locs)
