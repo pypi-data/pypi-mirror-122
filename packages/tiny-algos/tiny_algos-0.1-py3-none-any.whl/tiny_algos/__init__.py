d335704ecbe53cd99b8fc65257fa588684899c27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:30 +0000

    Add package tiny-algos/0.1/tiny_algos-0.1-py3-none-any.whl

---
+++ packages/tiny-algos/tiny_algos-0.1-py3-none-any.whl/tiny_algos/__init__.py
@@ -0,0 +1,12 @@
+from .mod_arithmetic import mod_permutation, mod_factorial, mod_combination
+from .multiset import MultiSet
+from .minmultiset import MinMultiSet, MaxMultiSet
+
+__all__ = [
+    "mod_permutation",
+    "mod_factorial",
+    "mod_combination",
+    "MultiSet",
+    "MinMultiSet",
+    "MaxMultiSet",
+]
