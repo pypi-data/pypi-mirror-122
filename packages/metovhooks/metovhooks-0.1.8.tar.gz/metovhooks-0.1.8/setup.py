14ea237655441c989dbca7dfc3a06408aa0ca348
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:53 +0000

    Add package metovhooks/0.1.8/metovhooks-0.1.8.tar.gz

---
+++ packages/metovhooks/metovhooks-0.1.8.tar.gz/metovhooks-0.1.8/setup.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+
+from setuptools import setup, find_packages
+
+setup(
+    name="metovhooks",
+    version="0.1.8",
+    description="My personal git hooks.",
+    url="https://github.com/metov/metovhooks",
+    long_description=Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
+    author="Azat Akhmetov",
+    author_email="azatinfo@yandex.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+    ],
+    packages=find_packages(),
+    install_requires=[
+        "coloredlogs",
+        "docopt",
+        "GitPython",
+        "packaging",
+        "pre_commit_hooks",
+        "toml",
+    ],
+    entry_points={
+        "console_scripts": [
+            "require_version_bump = metovhooks.require_version_bump:main",
+            "protect_branch = metovhooks.protect_branch:main",
+        ]
+    },
+)
