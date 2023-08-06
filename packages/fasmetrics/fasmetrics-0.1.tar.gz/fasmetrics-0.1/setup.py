3b2a3aaf02e1e633dc4bc8dcec4b8ce78bea2062
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:28:49 +0000

    Add package fasmetrics/0.1/fasmetrics-0.1.tar.gz

---
+++ packages/fasmetrics/fasmetrics-0.1.tar.gz/fasmetrics-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='fasmetrics',
+      version='0.1',
+      description='FAS metrics implementations (APCER, BPCER, ACER)',
+      packages=['fasmetrics'],
+      author='Guilherme Baldo Carlos',
+      author_email='guilhermebaldocarlos@gmail.com',
+      zip_safe=False)
