e5f80260436bf92d905f8aabcfb14e16f405cf58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:22 +0000

    Add package ecoindex/1.8.0/ecoindex-1.8.0.tar.gz

---
+++ packages/ecoindex/ecoindex-1.8.0.tar.gz/ecoindex-1.8.0/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['ecoindex']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['chromedriver-binary>=94,<95',
+ 'selenium>=3.141.0,<4.0.0',
+ 'sqlmodel>=0.0.4,<0.0.5']
+
+setup_kwargs = {
+    'name': 'ecoindex',
+    'version': '1.8.0',
+    'description': 'Ecoindex module provides a simple way to measure the Ecoindex score based on the 3 parameters: The DOM elements of the page, the size of the page and the number of external requests of the page',
+    'long_description': '# ECOINDEX PYTHON\n\n![Quality check](https://github.com/cnumr/ecoindex_python/workflows/Quality%20checks/badge.svg)\n[![PyPI version](https://badge.fury.io/py/ecoindex.svg)](https://badge.fury.io/py/ecoindex)\n\nThis basic module provides a simple interface to get the [Ecoindex](http://www.ecoindex.fr) based on 3 parameters:\n\n- The number of DOM elements in the page\n- The size of the page\n- The number of external requests of the page\n\n> **Current limitation:** This does not work well with SPA.\n\n## Requirements\n\n- Python ^3.8 with [pip](https://pip.pypa.io/en/stable/installation/)\n- Google Chrome installed on your computer (or you can use [browserless/chrome](https://hub.docker.com/r/browserless/chrome), see [more about configuration](#use-remote-chrome))\n\n## Install\n\n```shell\npip install ecoindex\n```\n\n## Use\n\n```python\nfrom pprint import pprint\n\nfrom ecoindex import get_ecoindex\nfrom ecoindex import get_page_analysis\n\n# Get ecoindex from DOM elements, size of page and requests of the page\necoindex = get_ecoindex(dom=100, size=100, requests=100)\npprint(ecoindex)\n\n> Ecoindex(grade=\'B\', score=67, ges=1.66, water=2.49)\n\n# Analyse a given webpage with a resolution of 1920x1080 pixel (default)\npage_analysis = get_page_analysis(url="http://ecoindex.fr")\npprint(page_analysis)\n\n> Result(size=119.292, nodes=45, requests=7, grade=\'A\', score=89.0, ges=1.22, water=1.83, url=HttpUrl(\'http://ecoindex.fr\', scheme=\'http\', host=\'ecoindex.fr\', tld=\'fr\', host_type=\'domain\'), date=datetime.datetime(2021, 7, 29, 13, 46, 54, 396697), height=1080, width=1920, page_type=None)\n\n```\n\n## Use remote chrome\n\nYou can use a remote chrome browser such as [browserless/chrome](https://hub.docker.com/r/browserless/chrome). You just have to run the docker image:\n\n```bash\ndocker run -p 3000:3000 browserless/chrome\n```\n\nAnd then set the environment variable `REMOTE_CHROME_URL` with the url of the remote chrome browser (or set it in the `.env` file):\n\n```bash\nexport REMOTE_CHROME_URL="http://localhost:3000/webdriver"\n```\n\n## Contribute\n\nYou need [poetry](https://python-poetry.org/) to install and manage dependencies. Once poetry installed, run :\n\n```bash\npoetry install\n```\n\n## Tests\n\n```shell\npoetry run pytest\n```\n\n## [Contributing](CONTRIBUTING.md)\n\n## [Code of conduct](CODE_OF_CONDUCT.md)\n',
+    'author': 'Vincent Vatelot',
+    'author_email': 'vincent.vatelot@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'http://www.ecoindex.fr',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
