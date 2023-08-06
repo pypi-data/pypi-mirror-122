4673e6ad37c45c8689049f34c3cdf94b65f030dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:47 +0000

    Add package geoshapes/0.0.4/geoshapes-0.0.4.tar.gz

---
+++ packages/geoshapes/geoshapes-0.0.4.tar.gz/geoshapes-0.0.4/geoshapes/__init__.py
@@ -0,0 +1,6 @@
+from geoshapes.geoshapes import utils
+from geoshapes.geoshapes import gridShape
+from geoshapes.geoshapes import splitShape
+from geoshapes.geoshapes import checkShape
+from geoshapes.geoshapes import mergeShape
+from geoshapes.geoshapes import selectGeom
