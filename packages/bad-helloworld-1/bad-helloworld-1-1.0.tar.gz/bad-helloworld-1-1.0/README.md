bbc78966bc7e3cf9d3d2b5b5c056ed38c2270a08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:06 +0000

    Add package bad-helloworld-1/1.0/bad-helloworld-1-1.0.tar.gz

---
+++ packages/bad-helloworld-1/bad-helloworld-1-1.0.tar.gz/bad-helloworld-1-1.0/README.md
@@ -0,0 +1,18 @@
+## Hello world
+
+This package is called __"bad-helloworld-1"__ for it's kind of stange purpose - cause _ResolutionFailure: Your dependencies could not be resolved_.
+
+To achieve so high goal the package __"bad-helloworld-2"__ will be also created.
+
+
+## Installation
+
+Run the following to install:
+
+```python
+pip install bad-helloworld-1
+```
+
+## Usage
+
+...
---
+++ packages/bad-helloworld-1/bad-helloworld-1-1.0.tar.gz/bad-helloworld-1-1.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name = "bad-helloworld-1", # by this name people can download that project
+    version = "1.0",
+    description = "hello world to break dependencies...",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    py_modules = ["helloworld"], # modules of package
+    package_dir = {'': 'src'}, # our code under src directory
+    classifiers = [
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    install_requires = [
+         "numpy==1.21.1", # :))
+    ],
+    # extras_require = {
+    #     "dev": [
+    #         "pytest>=3.7",
+    #     ]
+    # }
+    python_requires=">=3.9", # :))
+)
