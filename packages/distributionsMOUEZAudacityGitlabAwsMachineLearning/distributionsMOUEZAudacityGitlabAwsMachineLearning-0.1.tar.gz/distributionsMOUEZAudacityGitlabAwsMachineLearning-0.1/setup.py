d30aec6a90ed2eccc0512e061375af07845af168
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:47 +0000

    Add package distributionsMOUEZAudacityGitlabAwsMachineLearning/0.1/distributionsMOUEZAudacityGitlabAwsMachineLearning-0.1.tar.gz

---
+++ packages/distributionsMOUEZAudacityGitlabAwsMachineLearning/distributionsMOUEZAudacityGitlabAwsMachineLearning-0.1.tar.gz/distributionsMOUEZAudacityGitlabAwsMachineLearning-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributionsMOUEZAudacityGitlabAwsMachineLearning',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributionsMOUEZAudacityGitlabAwsMachineLearning'],
+      zip_safe=False)
