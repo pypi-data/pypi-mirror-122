3b523ad273c5d41aef2c26d09e699e98895e24eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:45:17 +0000

    Add package customized-distributions/0.1/customized_distributions-0.1.tar.gz

---
+++ packages/customized-distributions/customized_distributions-0.1.tar.gz/customized_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='customized_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['customized_distributions'],
+      author = 'Arun A Menon',
+      author_email = 'arun.sunshine.menon@gmail.com',
+      zip_safe=False)
