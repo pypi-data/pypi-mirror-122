4d6c449b11e1beef15f443ca81189595ff428fd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:43 +0000

    Add package hyperthought/0.9.2/hyperthought-0.9.2-py3-none-any.whl

---
+++ packages/hyperthought/hyperthought-0.9.2-py3-none-any.whl/hyperthought/utils.py
@@ -0,0 +1,21 @@
+import os
+import platform
+
+
+ID_PATH_SEP = ','
+PATH_SEP = '/'
+WINDOWS_PATH_PREFIX = '//?/'
+FOLDER_TYPE = 'Folder'
+VALID_SPACES = {'group', 'project', 'user'}
+
+
+class InvalidMetadataFormatException(Exception):
+    pass
+
+
+def get_active_path(path):
+    """Get a path that can be used to get a file size, read/write a file, etc."""
+    if platform.system() != 'Windows' or path.startswith(WINDOWS_PATH_PREFIX):
+        return path
+
+    return f'{WINDOWS_PATH_PREFIX}{path}'.replace(PATH_SEP, os.path.sep)
