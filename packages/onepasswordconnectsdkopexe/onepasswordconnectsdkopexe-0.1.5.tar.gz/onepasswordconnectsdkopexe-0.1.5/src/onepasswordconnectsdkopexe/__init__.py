2ab685ad3a433a0b981163bc1ce72878dc1ea51e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:26 +0000

    Add package onepasswordconnectsdkopexe/0.1.5/onepasswordconnectsdkopexe-0.1.5.tar.gz

---
+++ packages/onepasswordconnectsdkopexe/onepasswordconnectsdkopexe-0.1.5.tar.gz/onepasswordconnectsdkopexe-0.1.5/src/onepasswordconnectsdkopexe/__init__.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+
+# flake8: noqa
+
+
+from onepasswordconnectsdkopexe.config import load
+from onepasswordconnectsdkopexe.config import load_dict
+from onepasswordconnectsdkopexe.client import new_client
+from onepasswordconnectsdkopexe.client import new_client_interactif
---
+++ packages/onepasswordconnectsdkopexe/onepasswordconnectsdkopexe-0.1.5.tar.gz/onepasswordconnectsdkopexe-0.1.5/src/onepasswordconnectsdkopexe/models/__init__.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+
+# flake8: noqa
+
+
+from __future__ import absolute_import
+
+# import models into model package
+from onepasswordconnectsdkopexe.models.op_error import OPError
+from onepasswordconnectsdkopexe.models.op_item import OPItem
+from onepasswordconnectsdkopexe.models.op_item_details import OPItemDetails
+from onepasswordconnectsdkopexe.models.op_field import OPField
+from onepasswordconnectsdkopexe.models.op_field_section import OPFieldSection
+from onepasswordconnectsdkopexe.models.op_section import OPSection
+from onepasswordconnectsdkopexe.models.op_summary_item import OPSummaryItem
+from onepasswordconnectsdkopexe.models.op_item_urls import OPItemUrls
+from onepasswordconnectsdkopexe.models.op_item_vault import OPItemVault
+from onepasswordconnectsdkopexe.models.op_vault import OPVault
