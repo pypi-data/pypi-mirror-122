f601ba7dbaa4ce523b9bc703f7c2f86def14b50e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:20 +0000

    Add package yalambda/0.4.1/yalambda-0.4.1.tar.gz

---
+++ packages/yalambda/yalambda-0.4.1.tar.gz/yalambda-0.4.1/yalambda/__init__.py
@@ -0,0 +1,17 @@
+from .core import (
+    function,
+    YaRequest,
+    YaResponse,
+
+    Context,
+    RequestContext,
+)
+
+__all__ = (
+    "function",
+    "YaRequest",
+    "YaResponse",
+
+    "Context",
+    "RequestContext",
+)
