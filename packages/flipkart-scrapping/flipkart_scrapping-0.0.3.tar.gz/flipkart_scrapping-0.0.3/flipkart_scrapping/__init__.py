0f7b139f0a3472342fd1632c5d1e2bf2ea7f1e1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:39 +0000

    Add package flipkart-scrapping/0.0.3/flipkart_scrapping-0.0.3.tar.gz

---
+++ packages/flipkart-scrapping/flipkart_scrapping-0.0.3.tar.gz/flipkart_scrapping-0.0.3/flipkart_scrapping/__init__.py
@@ -0,0 +1,1 @@
+from FlipkartScrapping import flipkartScrap
---
+++ packages/flipkart-scrapping/flipkart_scrapping-0.0.3.tar.gz/flipkart_scrapping-0.0.3/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+import os
+
+VERSION = '0.0.3'
+DESCRIPTION = "Flipkart Web Scrapping Package"
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+#with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+#    long_description = f.read()
+long_description = "This Package extracts the data from flipkart"
+
+
+#Setting up
+setup(
+    name="flipkart_scrapping",
+    version=VERSION,
+    author="Rajesh Kumar Patnala",
+    author_email="patnala04@gmail.com",
+    url='https://github.com/rajeshpatnala/flipkart-web-scrapping',
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['bs4', 'pymongo', 'bson'],
+    keywords=['python', 'web scrapping', 'flipkart', 'beautiful soup', 'data extraction'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: End Users/Desktop",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+),
+
