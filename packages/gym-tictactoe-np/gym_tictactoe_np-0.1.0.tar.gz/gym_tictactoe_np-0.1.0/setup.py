4496f38a53307eaf909661f86311afaeb6991f94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:09 +0000

    Add package gym-tictactoe-np/0.1.0/gym_tictactoe_np-0.1.0.tar.gz

---
+++ packages/gym-tictactoe-np/gym_tictactoe_np-0.1.0.tar.gz/gym_tictactoe_np-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['gym_tictactoe_np', 'gym_tictactoe_np.envs']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['gym>=0.20.0,<0.21.0', 'numpy>=1.21.2,<2.0.0']
+
+setup_kwargs = {
+    'name': 'gym-tictactoe-np',
+    'version': '0.1.0',
+    'description': "3D TicTacToe environment for OpenAI's gym written with Numpy.",
+    'long_description': None,
+    'author': 'Ishan Manchanda',
+    'author_email': 'ishanmanchanda70@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<3.11',
+}
+
+
+setup(**setup_kwargs)
