99a1dc0c24afe1134fea5e388d4104a62d503792
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:42:28 +0000

    Add package ude2-distributions/0.4/ude2_distributions-0.4.tar.gz

---
+++ packages/ude2-distributions/ude2_distributions-0.4.tar.gz/ude2_distributions-0.4/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ude2_distributions',
+      version='0.4',
+      description='Gaussian distributions',
+      packages=['ude2_distributions'],
+      zip_safe=False)
+
+
