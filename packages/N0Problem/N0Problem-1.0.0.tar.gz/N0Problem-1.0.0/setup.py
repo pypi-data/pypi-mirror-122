4926d4ff2087de856b7cff8d8d234834c0b14a02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:45 +0000

    Add package N0Problem/1.0.0/N0Problem-1.0.0.tar.gz

---
+++ packages/N0Problem/N0Problem-1.0.0.tar.gz/N0Problem-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='N0Problem',
+    version='1.0.0',
+    py_modules=['N0Program'],
+    author='N0P3',
+    author_email='n0p3@qq.com',
+    description='For leetcode'
+)
