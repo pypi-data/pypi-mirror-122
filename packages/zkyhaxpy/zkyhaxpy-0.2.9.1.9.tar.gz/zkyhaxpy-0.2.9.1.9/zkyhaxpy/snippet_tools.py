77fd6d38827fe87ecee876ecaa9d3a97173791c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:00 +0000

    Add package zkyhaxpy/0.2.9.1.9/zkyhaxpy-0.2.9.1.9.tar.gz

---
+++ packages/zkyhaxpy/zkyhaxpy-0.2.9.1.9.tar.gz/zkyhaxpy-0.2.9.1.9/zkyhaxpy/snippet_tools.py
@@ -0,0 +1,26 @@
+crop_snippet = [    
+    '!pip install rasterio utm geopandas',
+    ''
+    'from zkyhaxpy import io_tools, pd_tools, np_tools, gis_tools, colab_tools, snippet_tools, console_tools',
+    'import pandas as pd',
+    'import numpy as np',
+    'from tqdm.notebook import tqdm',
+    'import os',
+    'import matplotlib.pyplot as plt',  
+    'import seaborn as sns',
+    '',
+    '!ln -s /content/drive/MyDrive/!Surasak-PIER/Crop-Insurance/NDA-Data/vew_plant_info_official_polygon_disaster_all_rice_by_year /plant_info'
+
+]
+
+def print_snippet(module):
+    '''
+    Show snippet for specified module
+
+    Available modules
+    - crop    
+
+    '''
+    if module=='crop':
+        for txt in crop_snippet:
+            print(txt)
