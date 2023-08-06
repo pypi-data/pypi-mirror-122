9430c90d5eb82d2e0bf46e631a9b85245e75202b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:17 +0000

    Add package samutil/0.0.73/samutil-0.0.73.tar.gz

---
+++ packages/samutil/samutil-0.0.73.tar.gz/samutil-0.0.73/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="samutil",
+    description="A set of Python utility packages for developing and maintaining quality software.",
+    version="0.0.73",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=["click", "sigfig", "sortedcontainers"],
+    python_requires=">=3",
+    include_package_data=True,
+    py_modules=["cli"],
+    entry_points="""
+        [console_scripts]
+        samutil = samutil.cli:main
+    """,
+    author="Sam McElligott",
+    keyword="testing, unittesting, formatting, secure, generation, utility",
+    long_description="README.md",
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/sammce/samutil",
+    project_urls={
+        "Bug Tracker": "https://github.com/sammce/samutil/issues",
+    },
+    author_email="sammcelligott@outlook.com",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
