40f884d51017dfd25c2796b52e799fe1773ad6b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:23 +0000

    Add package HristoScraper/0.0.9/HristoScraper-0.0.9.tar.gz

---
+++ packages/HristoScraper/HristoScraper-0.0.9.tar.gz/HristoScraper-0.0.9/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name = 'HristoScraper',
+    version = '0.0.9',
+    description = 'This is a car price scraper package',
+    url = 'https://github.com/xristo01/Data_Engineering_Project',
+    author = 'Hristo Petkov',
+    license = 'MIT',
+    packages = find_packages(),
+    install_requires = ['pandas', 'selenium', 'numpy', 'sqlalchemy'])
