8dd1bac740fa5e7fdc89f5a203baa30aca675811
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:56 +0000

    Add package distributions-TWEETY/1.0/distributions_TWEETY-1.0.tar.gz

---
+++ packages/distributions-TWEETY/distributions_TWEETY-1.0.tar.gz/distributions_TWEETY-1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_TWEETY',
+      version='1.0',
+      description='Gaussian distributions',
+      packages=['distributions_TWEETY'],
+      zip_safe=False)
