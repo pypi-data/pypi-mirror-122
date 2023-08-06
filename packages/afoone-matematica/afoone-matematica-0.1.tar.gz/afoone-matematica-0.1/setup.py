9b8e47c960b85bb6d247c6047e9a11778720f0c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:12 +0000

    Add package afoone-matematica/0.1/afoone-matematica-0.1.tar.gz

---
+++ packages/afoone-matematica/afoone-matematica-0.1.tar.gz/afoone-matematica-0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="afoone-matematica",
+    version="0.1",
+    packages=["matematica"],
+    include_package_data=True,
+    description="Librería matemática para la clase de python avanzado",
+    author="Alfonso Tienda",
+    author_email="afoone@hotmail.com",
+    license="MIT",
+    url="http://my-repo.example.com",
+    python_requires=">=3.0",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3"
+    ]
+)
