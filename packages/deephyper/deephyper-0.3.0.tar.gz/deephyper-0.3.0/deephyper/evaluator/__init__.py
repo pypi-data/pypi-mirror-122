f09a79e7f10e6e7c11d45599c1967878b3e45564
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:38 +0000

    Add package deephyper/0.3.0/deephyper-0.3.0.tar.gz

---
+++ packages/deephyper/deephyper-0.3.0.tar.gz/deephyper-0.3.0/deephyper/evaluator/__init__.py
@@ -0,0 +1,3 @@
+"""
+This evaluator module asynchronously manages a series of Job objects to help execute given HPS or NAS tasks on various environments with differing system settings and properties.
+"""
