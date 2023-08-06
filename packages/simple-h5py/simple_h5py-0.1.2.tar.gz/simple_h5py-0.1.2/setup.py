cc90ef651f219372b9c456c36fe2aa48e03771fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:35 +0000

    Add package simple-h5py/0.1.2/simple_h5py-0.1.2.tar.gz

---
+++ packages/simple-h5py/simple_h5py-0.1.2.tar.gz/simple_h5py-0.1.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name='simple_h5py',
+    version='0.1.2',
+    author='Arturo Mendoza',
+    description='A simple wrapper for the h5py library.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/amq92/simple_h5py',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=['numpy', 'h5py'],
+    license='GNU Affero General Public License v3',
+)
