47a1a77b74c1a3b1af45d069afa84dd155c6f65c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:50 +0000

    Add package khi/0.0.5/khi-0.0.5.tar.gz

---
+++ packages/khi/khi-0.0.5.tar.gz/khi-0.0.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='khi',
+    version='0.0.5',
+    description='Furnish information about the great Karachi city, Pakistan.',
+    url='https://www.techtum.dev',
+    author='H. M. Bhatti',
+    author_email='python@techtum.dev',
+    license='MIT',
+    packages=['khi'],
+    install_requires=['BeautifulSoup4',
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: OS Independent',
+    ],
+)
+
