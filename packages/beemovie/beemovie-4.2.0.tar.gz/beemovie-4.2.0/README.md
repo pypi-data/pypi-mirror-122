4ae76818b16b2a2af71c9d2ff4910adbb60e12be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:22 +0000

    Add package beemovie/4.2.0/beemovie-4.2.0.tar.gz

---
+++ packages/beemovie/beemovie-4.2.0.tar.gz/beemovie-4.2.0/README.md
@@ -0,0 +1,2 @@
+# Bee
+A package to easy insert the Bee Movie script into any Python project.
---
+++ packages/beemovie/beemovie-4.2.0.tar.gz/beemovie-4.2.0/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='beemovie',
+    version='4.2.0',
+    description='A package to easy insert the Bee Movie script into any Python project.',
+    url='https://github.com/noahgarrett/BeePython',
+    author='Noah Garrett',
+    author_email='noah.garrett@jerboatechnologies.com',
+    license='MIT',
+    packages=setuptools.find_packages(where="src"),
+    install_requires=[],
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+
+    package_dir={"": "src"},
+)
