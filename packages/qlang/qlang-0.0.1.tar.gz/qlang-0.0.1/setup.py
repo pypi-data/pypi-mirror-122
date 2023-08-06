dd3d6f6afe762652cb541a09cda17c6d2cb77f4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:10 +0000

    Add package qlang/0.0.1/qlang-0.0.1.tar.gz

---
+++ packages/qlang/qlang-0.0.1.tar.gz/qlang-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='qlang',
+    version='0.0.1',
+    author='nirvanasupermind',
+    author_email='nirvana.supermind@gmail.com',
+    url='https://github.com/nirvanasupermind/qlang',
+    license='LICENSE',
+    packages=['.'],
+    description='A concise, procedural programming language',
+    install_requires=[
+    'python>=3.0.0'
+    ]
+)
