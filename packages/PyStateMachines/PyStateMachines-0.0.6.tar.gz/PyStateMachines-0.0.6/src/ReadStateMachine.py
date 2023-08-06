a8aaafb213769f34b332ca77697350b4d13489a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:31 +0000

    Add package PyStateMachines/0.0.6/PyStateMachines-0.0.6.tar.gz

---
+++ packages/PyStateMachines/PyStateMachines-0.0.6.tar.gz/PyStateMachines-0.0.6/src/ReadStateMachine.py
@@ -0,0 +1,133 @@
+from Condition import Condition
+from Conditions import Conditions
+from Actions import Actions
+from Action import Action
+from Event import Event
+from State import State
+import defusedxml.ElementTree as ET
+
+
+def ReadStateMachineFile(xml_file: str):
+    """Read the xml file and parse it"""
+    states = {}
+    initial_state = ""
+    tree = ET.parse(xml_file)
+    root = tree.getroot()
+    for states_child in root:
+        # print(states_child.tag, states_child.attrib)
+        # States Element
+        if states_child.tag == "State":
+            # print("State element")
+            # State Element
+            state_name = ""
+            events = {}
+            for state_child in states_child:
+                # print(state_child.tag, state_child.attrib)
+                if state_child.tag == "Event":
+                    # print("Event element")
+                    # State->Event Element
+                    event_name = ""
+                    to_state = ""
+                    pre_conditions = None  # dummy
+                    post_conditions = None  # dummy
+                    pre_actions = None  # dummy
+                    post_actions = None  # dummy
+
+                    for event_child in state_child:
+
+                        # print(event_child.tag, event_child.attrib)
+                        if event_child.tag == "Name":
+                            # print("Name element = ", event_child.text)
+                            # State->Event->Name Element
+                            event_name = event_child.text
+                        elif event_child.tag == "ToState":
+                            # print("ToState element = ", event_child.text)
+                            # State->Event->ToState Element
+                            to_state = event_child.text
+                        elif event_child.tag == "PreConditions":
+                            # State->Event->PreConditions Element
+                            pre_condition_elements = []
+                            for preconditions_child in event_child:
+                                # print(preconditions_child.text)
+                                # State->Event->PreConditions->Condition Element
+                                if preconditions_child.tag == "Condition":
+                                    expression = ""
+                                    result = ""
+                                    for condition_child in preconditions_child:
+                                        if condition_child.tag == "Expression":
+                                            expression = condition_child.text
+                                        elif condition_child.tag == "Result":
+                                            result = condition_child.text
+                                    pre_condition_elements.append(
+                                        Condition(expression, result))
+                            pre_conditions = Conditions(pre_condition_elements)
+                        elif event_child.tag == "PostConditions":
+                            # State->Event->PostConditions Element
+                            post_condition_elements = []
+                            for postconditions_child in event_child:
+                                # print(postconditions_child.text)
+                                # State->Event->PostConditions->Condition Element
+                                if postconditions_child.tag == "Condition":
+                                    expression = ""
+                                    result = ""
+                                    for condition_child in postconditions_child:
+                                        if condition_child.tag == "Expression":
+                                            expression = condition_child.text
+                                        elif condition_child.tag == "Result":
+                                            result = condition_child.text
+                                    post_condition_elements.append(
+                                        Condition(expression, result))
+                            post_conditions = Conditions(
+                                post_condition_elements)
+                        elif event_child.tag == "PreActions":
+                            # State->Event->PreActions Element
+                            pre_action_elements = []
+                            for preactions_child in event_child:
+                                # print(preactions_child.text)
+                                # State->Event->PreActions->Action Element
+                                if preactions_child.tag == "Action":
+                                    expression = ""
+                                    for action_child in preactions_child:
+                                        if action_child.tag == "Expression":
+                                            expression = action_child.text
+                                    pre_action_elements.append(
+                                        Action(expression))
+                            pre_actions = Actions(pre_action_elements)
+
+                        elif event_child.tag == "PostActions":
+                            # State->Event->PostActions Element
+                            post_action_elements = []
+                            for postactions_child in event_child:
+                                # print(postactions_child.text)
+                                # State->Event->PostActions->Action Element
+                                if postactions_child.tag == "Action":
+                                    expression = ""
+                                    for action_child in postactions_child:
+                                        if action_child.tag == "Expression":
+                                            expression = action_child.text
+                                    post_action_elements.append(
+                                        Action(expression))
+                            post_actions = Actions(post_action_elements)
+
+                    events[event_name] = Event(
+                        event_name,
+                        to_state,
+                        pre_conditions,
+                        post_conditions,
+                        pre_actions,
+                        post_actions
+                    )
+                    # print(event.to_string())
+                elif state_child.tag == "Name":
+                    # print("Name element = ", state_child.text)
+                    # State->Name Element
+                    state_name = state_child.text
+
+            states[state_name] = State(state_name, events)
+
+        elif states_child.tag == "Initial_State":
+            # print ("Initial_State element = ", states_child.text)
+            # Initial_State Element
+            initial_state = states_child.text
+
+    return states, initial_state
---
+++ packages/PyStateMachines/PyStateMachines-0.0.6.tar.gz/PyStateMachines-0.0.6/src/StateMachine.py
@@ -0,0 +1,215 @@
+from ReadStateMachine import ReadStateMachineFile
+import logging
+
+
+class StateMachine:
+    """
+    This is a class defines state machine operations.
+
+    Attributes:
+        xml_file (str): The name of the xml that defines the state machine.
+        states (list): list of possible states of the machine.
+        current_state (str): the current state of the machine.
+        context (dict): dictionary that contains the imported module
+            at runtime for state machine operations.
+        saved_state (list): list of variables for restore state in case of rollback.
+    """
+    def __init__(self, xml_file: str):
+        """
+        The constructor for StateMachine class.
+
+        Parameters:
+           xml_file (str): The name of the xml that defines the state machine.
+        """
+        self.xml_file = xml_file
+        self.states = None
+        self.current_state = ""
+        self.context = {}
+        self.saved_state = None
+        logging.basicConfig(
+            filename=xml_file.split(sep=".xml")[0] + '.log',
+            format='%(asctime)s - %(levelname)s - %(message)s',
+            level=logging.DEBUG
+        )
+
+    def __CheckConditions(self, conditions):
+        """
+        This Function checks the conditions passed as argument
+
+        Parameters:
+           conditions (list): List of condition to check.
+        Returns:
+            all_conditions_satisfied:
+                a boolean that indicates if all conditions are satisfied
+        """
+        all_conditions_satisfied = True
+        if conditions is not None:
+            _conditions = conditions.conditions_list
+            for condition in _conditions:
+                module, expression = self.__PrepareExpression(
+                    condition.expression)
+                if module in self.context:
+                    mod = self.context[module]
+                    try:
+                        func = getattr(mod, expression)
+                        result = None
+                        if callable(func):
+                            result = func()
+                        else:
+                            result = func
+                        if str(result) != condition.result:
+                            all_conditions_satisfied = False
+                            break
+                    except AttributeError:
+                        logging.error(
+                            "Not Found Expression %s in Context", condition.expression)
+                        all_conditions_satisfied = False
+                else:
+                    logging.error("Not Found Module %s in Context", module)
+                    all_conditions_satisfied = False
+        else:
+            logging.info("No Condition")
+        return all_conditions_satisfied
+
+    def __ExecActions(self, actions):
+        """
+        This Function executes the actions passed as argument
+
+        Parameters:
+           actions (list): List of actions to execute.
+        Returns:
+            all_action_executed: a boolean that indicates if all actions are executed
+        """
+        all_action_executed = True
+        if actions is not None:
+            _actions = actions.actions_list
+            for action in _actions:
+                module, expression = self.__PrepareExpression(
+                    action.expression)
+                if module in self.context:
+                    mod = self.context[module]
+                    try:
+                        func = getattr(mod, expression)
+                        if callable(func):
+                            func()
+                    except AttributeError:
+                        logging.error(
+                            "Not Found Expression %s in Context", action.expression)
+                        all_action_executed = False
+                else:
+                    logging.error("Not Found Module %s in Context", module)
+                    all_action_executed = False
+        else:
+            logging.info("No Action")
+        return all_action_executed
+
+    def __SaveState(self):
+        """
+        This Function saves internal state
+        """
+        self.saved_state = [self.current_state, self.context]
+
+    def __RestoreState(self):
+        """
+        This Function restores the saved state
+        """
+        self.current_state = self.saved_state[0]
+        self.context = self.saved_state[1]
+
+    @staticmethod
+    def __PrepareExpression(expression):
+        """
+        This Function split expression in module and expression
+
+        Parameters:
+           expression (str): complete expression.
+        Returns:
+            module (str): module string
+            expression (str): expression string
+        """
+        module_expression = expression.rsplit('.', 1)
+        return module_expression[0], module_expression[1]
+
+    def get_current_state(self):
+        """
+        This Function return current state
+
+        Returns:
+            current_state (str): current state
+        """
+        return self.current_state
+
+    def LoadStateMachine(self):
+        """
+        This Function load state machine configuration
+        """
+        if self.states is not None:
+            logging.error("State Machine already loaded")
+        else:
+            self.states, self.current_state = ReadStateMachineFile(
+                self.xml_file)
+            logging.info('State Machine Loaded')
+
+    def addModuleToContext(self, module: str):
+        """
+        This Function adds a module to the context of state machine
+
+        Parameters:
+           module (str): The module to add.
+        """
+        mod = __import__(module)
+        self.context[module] = mod
+
+    def InjectEvent(self, event: str):
+        """
+        This Function execute the event injected
+
+        Parameters:
+           event (str): Event injected
+        """
+        my_state = self.states[self.current_state]
+        possible_events = my_state.events
+        if event in possible_events:
+            handled_event = possible_events[event]
+            # Save Old State
+            self.__SaveState()
+            # Preconditions
+            all_pre_conditions_satisfied = self.__CheckConditions(
+                handled_event.pre_conditions)
+            if all_pre_conditions_satisfied:
+                logging.debug("PreConditions satisfied")
+                # Preactions
+                all_pre_actions_executed = self.__ExecActions(
+                    handled_event.pre_actions)
+                if all_pre_actions_executed:
+                    logging.debug("PreActions Executed")
+                    # Transition
+                    logging.debug("Transition %s ------> %s",
+                                  self.current_state, handled_event.to_state)
+                    self.current_state = handled_event.to_state
+                    # Postactions
+                    all_post_actions_executed = self.__ExecActions(
+                        handled_event.post_actions)
+                    if all_post_actions_executed:
+                        logging.debug("PostActions Executed")
+                        # Postconditions
+                        all_post_conditions_satisfied = self.__CheckConditions(
+                            handled_event.post_conditions)
+                        if all_post_conditions_satisfied:
+                            logging.debug("PostConditions Satisfied")
+                        else:
+                            logging.error(
+                                "Not all PostConditions satisfied, restore saved state")
+                            self.__RestoreState()
+                    else:
+                        logging.error(
+                            "Not all PostActions Executed, restore saved state")
+                        self.__RestoreState()
+                else:
+                    logging.error(
+                        "Not all PretActions Executed, restore saved state")
+                    self.__RestoreState()
+            else:
+                logging.error("Not all PreConditions satisfied")
+        else:
+            logging.error("Not a possible event")
