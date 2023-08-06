2cd363847854d435107c515d4e432282440d4964
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:51 +0000

    Add package rubixinsights/0.0.32/rubixinsights-0.0.32.tar.gz

---
+++ packages/rubixinsights/rubixinsights-0.0.32.tar.gz/rubixinsights-0.0.32/rubixinsights/adverity/provider.py
@@ -0,0 +1,33 @@
+import requests
+import json
+from loguru import logger
+
+
+class Provider:
+    def __init__(self, stack: str, username: str, password: str):
+        self.username = username
+        self.password = password
+        self.stack = stack
+        self.endpoint = f"{self.stack}.datatap.adverity.com"
+
+    def get_token(self):
+        payload = {
+            'username': self.username,
+            'password': self.password
+        }
+        headers = {'content-type': 'application/json'}
+        try:
+            r = requests.post(f"https://{self.endpoint}/api/auth/token/",
+                              data=json.dumps(payload), headers=headers)
+            token = json.loads(r.text)['token']
+        except:
+            logger.exception("Failed to get login token")
+        return token
+
+
+if __name__ == '__main__':
+    username = 'rohan+api@rubixagency.com'
+    password = 'AeUDm6Bp7d2jqJE6'
+    stack = 'rubix'
+    p = Provider(stack=stack, username=username, password=password)
+    print(p.get_token())
