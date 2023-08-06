5f2b519afb2463158dff8531db83b5037e75dddb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:17 +0000

    Add package geodesicDome/0.8.0/geodesicDome-0.8.0.tar.gz

---
+++ packages/geodesicDome/geodesicDome-0.8.0.tar.gz/geodesicDome-0.8.0/setup.py
@@ -0,0 +1,37 @@
+from setuptools import find_packages, setup
+
+
+
+
+
+
+setup(
+    name='geodesicDome',
+    packages=find_packages(include=['geodesicDome']),
+    version='0.8.0',
+    long_description="""
+
+About
+-------
+The purpose of this library is to assist in storing the vertices in a geodesic dome,
+and contain methods to allow neighbourhood searches and tessellation of the vertices
+to be performed efficiently.
+
+The library provides:
+* Tessellation for a geodesic dome surface
+* Provide index based storage and neighbouring search algorithm
+* Provide functionality for uneven surface tessellation
+
+Installation
+-------
+To install our library run this command in your terminal:
+pip install geodesicDome
+    """,
+    long_description_content_type="text/markdown",
+    install_requires=["numpy", "numba"],
+    description='Geodesic Dome',
+    author='Sam, Lewis, James, Kevin, Jacob, Manan',
+    license='MIT',
+    
+   
+)
