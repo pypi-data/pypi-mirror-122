f3b8ab0abbe867ff0fbecea1636ccbcb2cdd6e1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:14:00 +0000

    Add package conflict-maximgran/0.1.0/conflict_maximgran-0.1.0.tar.gz

---
+++ packages/conflict-maximgran/conflict_maximgran-0.1.0.tar.gz/conflict_maximgran-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='conflict_maximgran',
+    version='0.1.0',
+    url='https://www.google.com/',
+    author='Maksim Grankin',
+    author_email='maximgran@gmail.com',
+    requires=['numpy', 'scipy'],
+)
