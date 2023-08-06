8ecbbd436728de979efc1cbd186bab9a66bcb163
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:45 +0000

    Add package qlazy/0.2.2/qlazy-0.2.2-cp38-cp38-manylinux1_x86_64.whl

---
+++ packages/qlazy/qlazy-0.2.2-cp38-cp38-manylinux1_x86_64.whl/qlazy/CMem.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+import ctypes
+from collections import Counter
+from ctypes.util import find_library
+import warnings
+import ctypes
+
+from qlazy.config import *
+from qlazy.error import *
+
+class CMem(ctypes.Structure):
+    """ Classical Memory
+
+    Attributes
+    ----------
+    cmem_num : int
+        number of the classical register.
+    bit_array : list (int)
+        bit array of classical memory.
+
+    """
+    _fields_ = [
+        ('cmem_num', ctypes.c_int),
+        ('bit_array', ctypes.c_void_p),
+    ]
+
+    def __new__(cls, cmem_num):
+        """
+        Parameters
+        ----------
+        cmem_num : int
+            number of the classical register.
+
+        Returns
+        -------
+        cmem : instance (CMem)
+
+        """
+        # cmem = cmem_init(cmem_num)
+        obj = cmem_init(cmem_num)
+        cmem = ctypes.cast(obj.value, ctypes.POINTER(cls)).contents
+        return cmem
+            
+    def free(self):
+        """
+        free memory of classical memory.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+
+        """
+        # cmem_free(self)
+        warnings.warn("No need to call 'free' method because free automatically, or you can use 'del' to free memory explicitly.")
+        
+    def __del__(self):
+        
+        cmem_free(self)
+        
+# c-library for qstate
+from qlazy.lib.cmem_c import *
