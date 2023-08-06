4e23e8ea50f4cf7f0b754977ba3fcf98d7773713
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:57 +0000

    Add package new-project123/0.0.1/new_project123-0.0.1.tar.gz

---
+++ packages/new-project123/new_project123-0.0.1.tar.gz/new_project123-0.0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='new_project123',
+      version='0.0.1',
+      description='Gaussian distributions',
+      packages=['new_project123'],
+      zip_safe=False)
