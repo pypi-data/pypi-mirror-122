379d557c73eafde69a571295b100ba176af9c4f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:53 +0000

    Add package pyben/0.2.3/pyben-0.2.3.tar.gz

---
+++ packages/pyben/pyben-0.2.3.tar.gz/pyben-0.2.3/tests/test_classes.py
@@ -0,0 +1,239 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+
+#####################################################################
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#####################################################################
+"""Testing PyBen OOP implementation.
+
+Example of single file .torrent metadata:
+
+    tfile={
+        "announce": "http://ubuntu.com/announce",
+        "info": {
+            "name": "ubuntu.iso",
+            "length": 12845678
+            "piece length": 262144,
+            "private": 1,
+            "source": "ubuntu",
+        },
+    "created by": "mktorrent",
+    "creation date": 20398488923,
+    }
+
+"""
+
+import os
+
+import pytest
+
+from pyben.classes import Bendecoder, Benencoder
+from pyben.exceptions import DecodeError, EncodeError
+
+from . import dicts, ints, lists, rmpath, strings, testfile, testmeta
+
+
+@pytest.fixture
+def tfile():
+    """Testfile Pytest fixture."""
+    tempfile = testfile()
+    yield tempfile
+    rmpath(tempfile)
+
+
+@pytest.fixture
+def tmeta():
+    """Metadata Pytest fixture."""
+    info = testmeta()
+    return info
+
+
+@pytest.fixture
+def types():
+    """Return fixtures."""
+    return [lists, ints, strings, dicts]
+
+
+def test_types(types):
+    """Test fixture functionality."""
+    assert len(types) > 1  # nosec
+
+
+def test_decoder_constructor(strings):
+    """Test Bendecoder constructor."""
+    for string, benstring in strings:
+        decoder = Bendecoder(benstring)
+        decoded = decoder.decode()
+        assert decoded == string  # nosec
+
+
+def test_malformed_data():
+    """Test data that cannot be interpreted by decoder."""
+    data = b"li92e12:hello world!::e"
+    decoder = Bendecoder(data)
+    try:
+        _ = decoder.decode()
+    except DecodeError:
+        assert True  # nosec
+
+
+def test_improper_type():
+    """Test type that isn't interpreted by encoder."""
+    vals = [1, 2, 3, 4, 5]
+    data = [12, "hello world!", set(vals)]
+    encoder = Benencoder(data)
+    try:
+        _ = encoder.encode()
+    except EncodeError:
+        assert True  # nosec
+
+
+def test_encode_tuple_to_list():
+    """Test encoding tuple to list."""
+    data = ((130, "foobar", "foo:bar"), b"li130e6:foobar7:foo:bare")
+    assert Benencoder()._encode_list(data[0]) == data[1]  # nosec
+
+
+def test_encode_tuple_cast():
+    """Test encoding tuple to list main method."""
+    data = ((130, "foobar", "foo:bar"), b"li130e6:foobar7:foo:bare")
+    assert Benencoder().encode(data[0]) == data[1]  # nosec
+
+
+def test_decode_str(strings):
+    """Test string decoding."""
+    decoder = Bendecoder()
+    for string, benstring in strings:
+        text, feed = decoder._decode_str(benstring)
+        assert string == text  # nosec
+        assert feed == len(benstring)  # nosec
+
+
+def test_decode_int_class(ints):
+    """Test integer decoding."""
+    decoder = Bendecoder()
+    for num, benint in ints:
+        real, feed = decoder._decode_int(benint)
+        assert real == num  # nosec
+        assert feed == len(benint)  # nosec
+
+
+def test_decode_list_class(lists):
+    """Test list decoding."""
+    decoder = Bendecoder()
+    for lst, benlist in lists:
+        decoded, _ = decoder._decode_list(benlist)
+        assert decoded == lst  # nosec
+
+
+def test_decode_dict_class(dicts):
+    """Test dictionary decoding."""
+    decoder = Bendecoder()
+    for dct, bendict in dicts:
+        decoded, _ = decoder._decode_dict(bendict)
+        assert dct == decoded  # nosec
+
+
+def test_decode_class(ints, strings, lists, dicts):
+    """Test decoding."""
+    data = [lists, strings, ints, dicts]
+    decoder = Bendecoder()
+    for val in data:
+        for item, benitem in val:
+            decoded = decoder.decode(benitem)
+            assert decoded == item  # nosec
+
+
+def test_decode_load(tfile):
+    """Test inline decoding."""
+    decoder = Bendecoder()
+    output = decoder.load(tfile)
+    assert isinstance(output, dict)  # nosec
+
+
+def test_decode_loads(tfile):
+    """Test from file decoding."""
+    with open(tfile, "rb") as _fd:
+        inp = _fd.read()
+    decoder = Bendecoder()
+    out = decoder.loads(inp)
+    assert out["info"]["length"] == 12845738  # nosec
+
+
+def test_bencode_str(strings):
+    """Test string encoding."""
+    encoder = Benencoder()
+    for string, benstring in strings:
+        text = encoder._encode_str(string)
+        assert benstring == text  # nosec
+
+
+def test_bencode_int(ints):
+    """Test integer encoding."""
+    encoder = Benencoder()
+    for num, benint in ints:
+        real = encoder._encode_int(num)
+        assert real == benint  # nosec
+
+
+def test_bencode_list(lists):
+    """Test list encoding."""
+    encoder = Benencoder()
+    for lst, benlist in lists:
+        encoded = encoder._encode_list(lst)
+        assert encoded == benlist  # nosec
+
+
+def test_bencode_dump(tmeta, tfile):
+    """Test to file encoding with path string."""
+    encoder = Benencoder()
+    encoder.dump(tmeta, tfile)
+    assert os.path.exists(tfile)  # nosec
+
+
+def test_bencode_dump1(tmeta, tfile):
+    """Test to file encoding with FileIO."""
+    encoder = Benencoder()
+    with open(tfile, "wb") as a:
+        encoder.dump(tmeta, a)
+    assert os.path.exists(tfile)  # nosec
+
+
+def test_bencode_dumps(tmeta):
+    """Test inline encoding."""
+    encoder = Benencoder()
+    reg = encoder.dumps(tmeta)
+    assert isinstance(reg, bytes)  # nosec
+
+
+def test_encode_dict(dicts):
+    """Test dictionary encoding."""
+    encoder = Benencoder()
+    for dct, bendict in dicts:
+        encoded = encoder._encode_dict(dct)
+        assert bendict == encoded  # nosec
+
+
+def test_encode(ints, strings, lists, dicts):
+    """Test encoding."""
+    data = [ints, strings, lists, dicts]
+    encoder = Benencoder()
+    for val in data:
+        for item, benitem in val:
+            encoded = encoder.encode(item)
+            assert encoded == benitem  # nosec
+
+
+def test_bendecoder_load(tfile):
+    """Test inline decoding."""
+    decoder = Bendecoder()
+    with open(tfile, "rb") as _fd:
+        data = decoder.load(_fd)
+    assert data is not None  # nosec
---
+++ packages/pyben/pyben-0.2.3.tar.gz/pyben-0.2.3/tests/test_funcs.py
@@ -0,0 +1,175 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+
+#####################################################################
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#####################################################################
+"""Pytest tests for functions in pyben package."""
+
+import pytest
+
+from pyben.bencode import (bencode_dict, bencode_int, bencode_list,
+                           bencode_str, bendecode, bendecode_dict,
+                           bendecode_int, bendecode_list, bendecode_str,
+                           benencode)
+from pyben.exceptions import DecodeError, EncodeError
+
+from . import dicts, ints, lists, strings
+
+
+@pytest.fixture
+def metadata():
+    """Pytest fixture functionality test fixture."""
+    return ints, strings, dicts, lists
+
+
+def test_malformed_bytes():
+    """Test byte string literal that is not understood by decoder."""
+    data = b"dspi123elees11:hello world!spqyt"
+    try:
+        _ = bendecode(data)
+    except DecodeError:
+        assert True  # nosec
+
+
+def test_undecodable_data():
+    """Test type not understood by encoder."""
+    data = set([1, 2, 3, 4, 5])
+    try:
+        _ = benencode(data)
+    except EncodeError:
+        assert True  # nosec
+
+
+def test_matadata(metadata):
+    """Pytest fixture functionality test."""
+    assert len(metadata) > 1  # nosec
+
+
+def test_benencode_tuple_list():
+    """Test turning a tuple to a list while encoding."""
+    lst = (130, "foobar", "foo:bar")
+    assert bencode_list(lst) == b"li130e6:foobar7:foo:bare"  # nosec
+
+
+def test_benencode_tuple_to_list():
+    """Test turning a tuple to a list while encoding."""
+    lst = (130, "foobar", "foo:bar")
+    assert benencode(lst) == b"li130e6:foobar7:foo:bare"  # nosec
+
+
+def test_bendecode_str(strings):
+    """Test decoding strings."""
+    for string, benstring in strings:
+        text, _ = bendecode_str(benstring)
+        assert string == text  # nosec
+
+
+def test_bendecode_str_feed(strings):
+    """Test decoding strings feeds."""
+    for _, benstring in strings:
+        _, feed = bendecode_str(benstring)
+        assert feed == len(benstring)  # nosec
+
+
+def test_bendecode_str_equality(strings):
+    """Test equality for testing strings."""
+    for _, benstring in strings:
+        text, _ = bendecode_str(benstring)
+        pre = str(len(text)) + ":" + text
+        assert pre.encode("UTF-8") == benstring  # nosec
+
+
+def test_bendecode_int(ints):
+    """Test decoding integers."""
+    for num, benint in ints:
+        real, _ = bendecode_int(benint)
+        assert real == num  # nosec
+
+
+def test_bendecode_int_feed(ints):
+    """Test decoding integers feeds."""
+    for _, benint in ints:
+        _, feed = bendecode_int(benint)
+        assert feed == len(benint)  # nosec
+
+
+def test_bendecode_list(lists):
+    """Test decoding lists."""
+    for lst, benlist in lists:
+        decoded, _ = bendecode_list(benlist)
+        assert decoded == lst  # nosec
+
+
+def test_bendecode_list_feed(lists):
+    """Test encoding lists feeds."""
+    for _, benlist in lists:
+        _, feed = bendecode_list(benlist)
+        assert feed == len(benlist)  # nosec
+
+
+def test_bendecode_dict(dicts):
+    """Test encoding dicts."""
+    for dct, bendict in dicts:
+        decoded, _ = bendecode_dict(bendict)
+        assert dct == decoded  # nosec
+
+
+def test_bendecode_dict_feed(dicts):
+    """Test encoding dicts feeds."""
+    for _, bendict in dicts:
+        _, feed = bendecode_dict(bendict)
+        assert feed == len(bendict)  # nosec
+
+
+def test_bencode_str(strings):
+    """Test encoding strings."""
+    for string, benstring in strings:
+        text = bencode_str(string)
+        assert benstring == text  # nosec
+
+
+def test_bencode_int(ints):
+    """Test encoding integers."""
+    for num, benint in ints:
+        real = bencode_int(num)
+        assert real == benint  # nosec
+
+
+def test_bencode_list(lists):
+    """Test encoding lists."""
+    for lst, benlist in lists:
+        encoded = bencode_list(lst)
+        assert encoded == benlist  # nosec
+
+
+def test_bencode_dict(dicts):
+    """Test encoding dicts."""
+    for dct, bendict in dicts:
+        encoded = bencode_dict(dct)
+        assert bendict == encoded  # nosec
+
+
+def test_bencode_all(ints, strings, dicts, lists):
+    """Test encoding all types."""
+    data = [ints, strings, dicts, lists]
+    for category in data:
+        for unit, benunit in category:
+            encoded = benencode(unit)
+            assert benunit == encoded  # nosec
+
+
+def test_bendecode_all(ints, strings, dicts, lists):
+    """Test decoding all types."""
+    data = [ints, strings, dicts, lists]
+    for category in data:
+        for unit, benunit in category:
+            decoded, _ = bendecode(benunit)
+            assert unit == decoded  # nosec
