2b3c1d0e63230dca3914f6dd4b31c9e8709b051d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:15 +0000

    Add package deutschland/0.1.8/deutschland-0.1.8-py3-none-any.whl

---
+++ packages/deutschland/deutschland-0.1.8-py3-none-any.whl/deutschland/__init__.py
@@ -0,0 +1,10 @@
+from .config import Config
+
+module_config = Config()
+
+from .geo import Geo
+from .bundesanzeiger.bundesanzeiger import Bundesanzeiger
+from .handelsregister.handelsregister import Handelsregister
+from .lebensmittelwarnung.lebensmittelwarnung import Lebensmittelwarnung
+from .verena.verena import Verena
+from .bundesnetzagentur import *
