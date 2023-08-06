4dbf7b8b69edc87251ba2d42c7eddb2882a8bdbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:47 +0000

    Add package pyonmttok/1.29.0/pyonmttok-1.29.0-cp310-cp310-manylinux_2_12_x86_64.manylinux2010_x86_64.whl

---
+++ packages/pyonmttok/pyonmttok-1.29.0-cp310-cp310-manylinux_2_12_x86_64.manylinux2010_x86_64.whl/pyonmttok/__init__.py
@@ -0,0 +1,32 @@
+import sys
+
+if sys.platform == "win32":
+    import ctypes
+    import os
+
+    import pkg_resources
+
+    module_name = sys.modules[__name__].__name__
+    package_dir = pkg_resources.resource_filename(module_name, "")
+
+    add_dll_directory = getattr(os, "add_dll_directory", None)
+    if add_dll_directory is not None:
+        add_dll_directory(package_dir)
+
+    for filename in os.listdir(package_dir):
+        if filename.endswith(".dll"):
+            ctypes.CDLL(os.path.join(package_dir, filename))
+
+from pyonmttok._ext import (
+    BPELearner,
+    Casing,
+    SentencePieceLearner,
+    SentencePieceTokenizer,
+    SubwordLearner,
+    Token,
+    Tokenizer,
+    TokenType,
+    is_placeholder,
+    set_random_seed,
+)
+from pyonmttok.version import __version__
