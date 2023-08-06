468c884f2906c52c681b21b1bc3b67793d3cf4d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:09 +0000

    Add package fastybird-triggers-module/0.4.1/fastybird-triggers-module-0.4.1.tar.gz

---
+++ packages/fastybird-triggers-module/fastybird-triggers-module-0.4.1.tar.gz/fastybird-triggers-module-0.4.1/tests/pytests/items/test_triggers_controls.py
@@ -0,0 +1,40 @@
+#     Copyright 2021. FastyBird s.r.o.
+#
+#     Licensed under the Apache License, Version 2.0 (the "License");
+#     you may not use this file except in compliance with the License.
+#     You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#     Unless required by applicable law or agreed to in writing, software
+#     distributed under the License is distributed on an "AS IS" BASIS,
+#     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#     See the License for the specific language governing permissions and
+#     limitations under the License.
+
+# Test dependencies
+import uuid
+
+# Library libs
+from triggers_module.items import TriggerControlItem
+from triggers_module.repositories import trigger_control_repository
+
+# Tests libs
+from tests.pytests.tests import DbTestCase
+
+
+class TestTriggerControlItem(DbTestCase):
+    def test_transform_to_dict(self) -> None:
+        trigger_control_repository.initialize()
+
+        control_item = trigger_control_repository.get_by_id(
+            uuid.UUID("177d6fc7-1905-4fd9-b847-e2da8189dd6a", version=4)
+        )
+
+        self.assertIsInstance(control_item, TriggerControlItem)
+
+        self.assertEqual({
+            "id": "177d6fc7-1905-4fd9-b847-e2da8189dd6a",
+            "name": "trigger",
+            "trigger": "c64ba1c4-0eda-4cab-87a0-4d634f7b67f4",
+        }, control_item.to_dict())
