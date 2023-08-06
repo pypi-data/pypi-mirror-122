b33fc8e1d7e5e1ec052c17bebd25ea7169476c6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:59 +0000

    Add package pyreaqtive/1.0.2/pyreaqtive-1.0.2-py3-none-any.whl

---
+++ packages/pyreaqtive/pyreaqtive-1.0.2-py3-none-any.whl/pyreaqtive/layouts/rqvboxlayout.py
@@ -0,0 +1,27 @@
+from typing import Callable, Type, Union
+
+from PyQt5.QtWidgets import QBoxLayout, QWidget
+
+from .rqboxlayout import RQBoxLayout
+from ..models import RQModel, RQList
+
+
+class RQVBoxLayout(RQBoxLayout):
+    """Reactive QVBoxLayout"""
+
+    def __init__(self, model: RQList,
+                 widget: Union[Type[QWidget], Callable[[RQModel, RQList], QWidget]], *args, **kwargs):
+        """Constructor
+
+        Args:
+            model: RQList representing all the items in the layout
+
+            widget: QWidget type that represents each item.
+                Can also be a function that accepts the item model and list model as arguments,
+                and returns the widget instance
+
+            args: arguments to pass to the native pyqt layout
+
+            **kwargs: arguments to pass to the native pyqt layout
+        """
+        super().__init__(model, widget, QBoxLayout.Direction.Down, *args, **kwargs)
