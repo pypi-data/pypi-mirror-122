1098102c3ee655b4386bc2be00e8f1379544e405
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:50 +0000

    Add package kinspy/1.0.5/kinspy-1.0.5.tar.gz

---
+++ packages/kinspy/kinspy-1.0.5.tar.gz/kinspy-1.0.5/README.md
@@ -0,0 +1,6 @@
+# Name: kinspy
+# Describes: provide convenient functions and constants
+# Version: v1.0.5
+# Date: 2021-10-06
+# Author: Yunxiao Zhang
+# E-mail: yunxiao9277@gmail.com
---
+++ packages/kinspy/kinspy-1.0.5.tar.gz/kinspy-1.0.5/src/kinspy/__init__.py
@@ -0,0 +1,23 @@
+# Name: kinspy
+# Describes: provide convenient functions and constants
+# Version: v1.0.3
+# Date: 2021-04-25
+# Author: Yunxiao Zhang
+# E-mail: yunxiao9277@gmail.com
+
+# common functions
+from numpy import sqrt, sin, cos, exp, log
+
+# common constants
+from numpy import pi, e
+
+# tensor functions
+from numpy import zeros, ones, sum, dot
+
+# vector functions
+from numpy import cross
+
+# imports
+from matplotlib import pyplot as plt
+import numpy as np
+import torch as t
