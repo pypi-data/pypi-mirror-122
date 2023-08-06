27513d8ed5402ca4fcec945a42af45efc571665a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:59 +0000

    Add package getrektphineass/0.0.1/getrektphineass-0.0.1.tar.gz

---
+++ packages/getrektphineass/getrektphineass-0.0.1.tar.gz/getrektphineass-0.0.1/README.txt
@@ -0,0 +1,1 @@
+This is library which proves that I am better than Phineas
---
+++ packages/getrektphineass/getrektphineass-0.0.1.tar.gz/getrektphineass-0.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'Operating System :: MacOS',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+
+setup(
+  name='getrektphineass',
+  version='0.0.1',
+  description='This program proves that I am cool',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',
+  author='Me',
+  author_email='randommail@gmail.com',
+  license='MIT',
+  classifiers=classifiers,
+  keywords='calculator',
+  packages=find_packages(),
+  install_requires=['']
+)
