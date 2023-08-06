145188321a58725c31d954cdd9e15be09b825a52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:31 +0000

    Add package distvic/1.0/distvic-1.0.tar.gz

---
+++ packages/distvic/distvic-1.0.tar.gz/distvic-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distvic',
+      version='1.0',
+      description='Gaussian distributions',
+      packages=['distvic'],
+      author = 'Victor Huerta',
+      author_email = 'huertavictor@gmail.com',
+      zip_safe=False)
