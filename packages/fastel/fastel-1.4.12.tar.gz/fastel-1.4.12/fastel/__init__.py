bf378375b6e187e23a064cf8652d958596630f09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:01 +0000

    Add package fastel/1.4.12/fastel-1.4.12.tar.gz

---
+++ packages/fastel/fastel-1.4.12.tar.gz/fastel-1.4.12/fastel/__init__.py
@@ -0,0 +1,27 @@
+"""revtel fastapi package"""
+
+from fastel.authorizers import (
+    ClientIdAuth,
+    ClientSecretAuth,
+    Credential,
+    JWBaseAuth,
+    StaticClientAuth,
+    StaticJWTAuth,
+    StaticSecretAuth,
+)
+from fastel.cart.exceptions import CartException, cart_exception_handler
+from fastel.exceptions import APIException
+
+__version__ = "1.4.12"
+__all__ = [
+    "APIException",
+    "ClientIdAuth",
+    "ClientSecretAuth",
+    "Credential",
+    "JWBaseAuth",
+    "StaticClientAuth",
+    "StaticSecretAuth",
+    "StaticJWTAuth",
+    "cart_exception_handler",
+    "CartException",
+]
