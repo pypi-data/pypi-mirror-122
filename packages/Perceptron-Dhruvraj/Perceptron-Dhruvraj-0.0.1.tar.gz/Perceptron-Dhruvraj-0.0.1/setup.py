029bfb5a022dc485b8a46e7d81e8231003c92828
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:39 +0000

    Add package Perceptron-Dhruvraj/0.0.1/Perceptron-Dhruvraj-0.0.1.tar.gz

---
+++ packages/Perceptron-Dhruvraj/Perceptron-Dhruvraj-0.0.1.tar.gz/Perceptron-Dhruvraj-0.0.1/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+PROJECT_NAME = "Perceptron"
+USER_NAME = "Dhruvraj"
+
+setuptools.setup(
+    name= f"{PROJECT_NAME}-{USER_NAME}",
+    version="0.0.1",
+    author=USER_NAME,
+    author_email="lathiyadhruvraj44@gmail.com",
+    description="Implementation of perceptron",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lathiyadhruvraj/perceptron_pypi",
+    project_urls={
+        "Bug Tracker": "https://github.com/lathiyadhruvraj/perceptron_pypi/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7",
+    install_requires=[
+        "numpy",
+        "tqdm"
+    ]
+)
