7eef418c24936305e377ac98ce25e272c952cd27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:36 +0000

    Add package horuz/0.0.5/horuz-0.0.5.tar.gz

---
+++ packages/horuz/horuz-0.0.5.tar.gz/horuz-0.0.5/horuz/commands/cmd_search.py
@@ -0,0 +1,47 @@
+import click
+
+from horuz.cli import pass_environment
+from horuz.utils.formatting import beautify_query
+from horuz.utils.es import HoruzES
+from horuz.utils.style import rtable
+
+
+@click.command("search", short_help="Search data in ES.")
+@click.option("-v", "--verbose", is_flag=True, help="Enables verbose mode.")
+@click.option('-p', '--project', required=True, help='Project name. Example yahoo.com uber.com x.com...')
+@click.option('-q', '--query', required=True, help='Query to ElasticSeach')
+@click.option('-f', '--fields', help='Specify the fields you want.')
+@click.option('-s', '--size', default=100, type=click.IntRange(1, 10000), help='Specify the output size. Range 1-10000')
+@click.option('-o', '--order', default="time:desc", help='Specify the sorting of the query')
+@click.option('-oJ', is_flag=True, help="JSON Output")
+@pass_environment
+def cli(ctx, verbose, project, query, fields, size, order, oj):
+    """
+    Get data from ElasticSeach.
+    """
+    ctx.verbose = verbose
+    hes = HoruzES(project, ctx)
+    fields = fields.split(",") if fields else []
+    if oj:
+        # JSON Output
+        data = beautify_query(
+            hes.query(term=query, size=size, order=order, fields=fields),
+            fields,
+            output="json")
+        click.echo(data)
+    else:
+        # Interactive Output
+        # Default fields if nothing were introduced
+        if not fields:
+            fields = ["_id", "time", "session"]
+        data = beautify_query(
+            hes.query(term=query, size=size, order=order, fields=fields),
+            fields,
+            output="interactive")
+        # Adding columns
+        if data:
+            for column in data[0].keys():
+                rtable.add_column(column, style="cyan")
+            for i in data:
+                rtable.add_row(*i.values())
+            ctx.log(rtable, pager=True)
---
+++ packages/horuz/horuz-0.0.5.tar.gz/horuz-0.0.5/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+version = __import__("horuz").__version__
+
+setup(
+    name='horuz',
+    version=version,
+    description='Save and query your recon data on ElasticSearch.',
+    url="https://github.com/misalabs/horuz",
+    author='Misa G.',
+    author_email="hi@misalabs.com",
+    maintainer='Misa G.',
+    maintainer_email='hi@misalabs.com',
+    license='MIT license',
+    packages=find_packages(),
+    download_url = 'https://github.com/misalabs/horuz/archive/refs/tags/{}.tar.gz'.format(version),
+    keywords = ['recon', 'elasticsearch', 'fuzzing', 'hacking'],
+    include_package_data=True,
+    install_requires=[
+        'click',
+        'elasticsearch==7.10.0',
+        'requests==2.23.0',
+        'dpath==2.0.1',
+        'rich==9.13.0',
+    ],
+    entry_points='''
+        [console_scripts]
+        hz=horuz.cli:cli
+    ''',
+    classifiers=[
+        'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Intended Audience :: Developers',      # Define that your audience are developers
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',   # Again, pick a license
+        'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.9',  
+    ],
+)
