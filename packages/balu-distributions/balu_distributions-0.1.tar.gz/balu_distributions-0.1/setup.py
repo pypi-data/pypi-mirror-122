363ff31c576eafa1bb4fee351b0c1d450b800308
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:53 +0000

    Add package balu-distributions/0.1/balu_distributions-0.1.tar.gz

---
+++ packages/balu-distributions/balu_distributions-0.1.tar.gz/balu_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(
+name = 'balu_distributions',
+version = '0.1',
+description = 'Gaussian Distributions',
+packages = ['balu_distributions'],
+zip_safe = False
+)
