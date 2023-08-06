115c0dc33cde1aa8f2255eeee43cc78feefd9f8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:28:16 +0000

    Add package lsdpiptest/0.0.1/lsdpiptest-0.0.1.tar.gz

---
+++ packages/lsdpiptest/lsdpiptest-0.0.1.tar.gz/lsdpiptest-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(name='lsdpiptest',
+      version='0.0.1',
+      description='test python to pip',
+      author='Leesudong',
+      author_email='905412718@qq.com',
+      requires= ['numpy','matplotlib'],
+      packages=find_packages(),
+      license="apache 3.0"
+      )
