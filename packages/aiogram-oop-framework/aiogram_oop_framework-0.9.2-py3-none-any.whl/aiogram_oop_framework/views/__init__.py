4a35f177dd5ca59a59622e6e669a21d05be5f0e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:25 +0000

    Add package aiogram-oop-framework/0.9.2/aiogram_oop_framework-0.9.2-py3-none-any.whl

---
+++ packages/aiogram-oop-framework/aiogram_oop_framework-0.9.2-py3-none-any.whl/aiogram_oop_framework/views/__init__.py
@@ -0,0 +1,17 @@
+from .callback_query import CallbackQueryView
+from .inline_query import InlineQueryView
+from .message import MessageView
+from .channel_post_view import ChannelPostView
+from .chosen_inline_result import ChosenInlineResultView
+from .edited_channel_post_view import EditedChannelPostView
+from .edited_message_view import EditedMessageView
+from .poll import PollView
+from .poll_answer import PollAnswerView
+from .shipping_query import ShippingQueryView
+from .pre_chekout_query import PreCheckoutQueryView
+from .user_base_view import UserBaseView
+from .chat_member import ChatMemberView
+from .my_chat_member import MyChatMemberView
+
+from .content_types_views import *
+from .custom_views import *
