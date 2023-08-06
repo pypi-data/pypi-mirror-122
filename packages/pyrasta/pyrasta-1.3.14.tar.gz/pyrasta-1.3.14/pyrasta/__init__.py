661f6ec510f252d92b99abd478d2cba620db198a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:02 +0000

    Add package pyrasta/1.3.14/pyrasta-1.3.14.tar.gz

---
+++ packages/pyrasta/pyrasta-1.3.14.tar.gz/pyrasta-1.3.14/pyrasta/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+""" Module summary description.
+
+More detailed description.
+"""
+
+__version__ = '1.3.14'
+__author__ = 'Benjamin Pillot'
+__email__ = 'benjaminpillot@riseup.net'
+
+
+import gdal
+
+GDAL_DEFAULT_DRIVER = gdal.GetDriverByName("Gtiff")
