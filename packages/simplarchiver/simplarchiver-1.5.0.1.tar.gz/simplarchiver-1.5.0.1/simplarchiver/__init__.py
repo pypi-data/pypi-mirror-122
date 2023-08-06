46c19d77a3809a54c286075bae31f35fb06cfa8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:17 +0000

    Add package simplarchiver/1.5.0.1/simplarchiver-1.5.0.1.tar.gz

---
+++ packages/simplarchiver/simplarchiver-1.5.0.1.tar.gz/simplarchiver-1.5.0.1/simplarchiver/__init__.py
@@ -0,0 +1,3 @@
+from .abc import Feeder, FilterFeeder, AmplifierFeeder
+from .abc import Downloader, FilterDownloader, CallbackDownloader, FilterCallbackDownloader
+from .controller import Pair, Controller
