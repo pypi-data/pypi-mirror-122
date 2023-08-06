c60a4c93c49da895b7f3a21e830bff4a1cdc6442
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:12 +0000

    Add package idegui/1.0.1/idegui-1.0.1.tar.gz

---
+++ packages/idegui/idegui-1.0.1.tar.gz/idegui-1.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='idegui',
+    version='1.0.1',
+    packages=['idegui'],
+    author='stripe-python',
+    author_email='13513519246@139.com',
+    description='a library for write an IDE'
+)
