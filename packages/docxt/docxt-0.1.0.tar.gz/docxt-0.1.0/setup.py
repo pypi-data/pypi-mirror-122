d9578787beb4c1195e0199e8eabca6ed1c9bf2d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:33 +0000

    Add package docxt/0.1.0/docxt-0.1.0.tar.gz

---
+++ packages/docxt/docxt-0.1.0.tar.gz/docxt-0.1.0/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+from setuptools import setup
+setup(
+    name='docxt',
+    version='0.1.0',
+    author='barwe',
+    author_email='barwechin@163.com',
+    # url='http://barwe.top',
+    description='A simple way to generate a report from a template.',
+    packages=[
+        'docxt',
+        'docxt.utils'
+    ],
+    install_requires=['docxtpl', 'beautifulsoup4'],
+    # include_package_data=True,
+    # package_data={
+    #     "docxt": ['svg2emf.jar']
+    # }
+    # data_files=[
+    #     ('docxt', ['docxt/svg2emf.jar'])
+    # ]
+)
