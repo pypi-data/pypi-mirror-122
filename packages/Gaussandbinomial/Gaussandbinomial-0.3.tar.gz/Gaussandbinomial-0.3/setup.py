29c94c0d8eae7fea2ad3209c52e9ebc85aaa1e37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:20 +0000

    Add package Gaussandbinomial/0.3/Gaussandbinomial-0.3.tar.gz

---
+++ packages/Gaussandbinomial/Gaussandbinomial-0.3.tar.gz/Gaussandbinomial-0.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Gaussandbinomial',
+      version='0.3',
+      description='Gaussian and Binomial distributions',
+      packages=['Gaussandbinomial'],
+      author='Pranali Parab',
+      author_mail= 'pranaliparab1325@gmail.com',
+      zip_safe=False)
