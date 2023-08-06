d3d6130afb3cc6c048ee59e584bc1ade8333be93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:41 +0000

    Add package stactools-sentinel3/0.3.0/stactools_sentinel3-0.3.0-py3-none-any.whl

---
+++ packages/stactools-sentinel3/stactools_sentinel3-0.3.0-py3-none-any.whl/stactools/sentinel3/properties.py
@@ -0,0 +1,104 @@
+from pystac.extensions.sat import OrbitState
+from stactools.core.io.xml import XmlElement
+
+
+def fill_sat_properties(sat_ext, href):
+    """Fills the properties for SAR.
+
+    Based on the sat Extension.py
+
+    Args:
+        sat_ext (pystac.extensions.sat.SatExtension): The extension to be populated.
+        href (str): The HREF to the scene, this is expected to be an XML file.
+
+    Returns:
+        pystac.Asset: An asset with the SAT relevant properties.
+    """
+    # Read meta file
+    root = XmlElement.from_file(href)
+
+    sat_ext.platform_international_designator = root.findall(
+        ".//sentinel-safe:nssdcIdentifier")[0].text
+
+    orbit_state = root.find_attr("groundTrackDirection",
+                                 ".//sentinel-safe:orbitNumber")
+    sat_ext.orbit_state = OrbitState(orbit_state)
+
+    sat_ext.absolute_orbit = int(
+        root.findall(".//sentinel-safe:orbitNumber")[0].text)
+
+    relative_orbit_num = int(
+        root.findall(".//sentinel-safe:relativeOrbitNumber")[0].text)
+    if relative_orbit_num == 0:
+        sat_ext.relative_orbit = int(
+            root.findall(".//sentinel-safe:relativeOrbitNumber[@type='stop']")
+            [0].text)
+    else:
+        sat_ext.relative_orbit = relative_orbit_num
+
+
+def fill_eo_properties(eo_ext, href):
+    """Fills the properties for EO.
+
+    Based on the eo Extension.py
+
+    Args:
+        eo_ext (pystac.extensions.eo.EOExtension): The extension to be populated.
+        href (str): The HREF to the scene, this is expected to be an XML file.
+
+    Returns:
+        pystac.Asset: An asset with the EO relevant properties.
+    """
+    # Read meta file
+    root = XmlElement.from_file(href)
+    product_name = root.findall(".//sentinel3:productName")[0].text
+    if product_name.split("_")[1] == "OL" and product_name.split(
+            "_")[2] == "1":
+        pass
+    elif product_name.split("_")[1] == "OL" and product_name.split(
+            "_")[2] == "2":
+        eo_ext.cloud_cover = float(
+            root.find_attr("percentage", ".//sentinel3:cloudyPixels"))
+    elif product_name.split("_")[1] == "SL":
+        eo_ext.cloud_cover = float(
+            root.find_attr("percentage", ".//sentinel3:cloudyPixels"))
+    elif product_name.split("_")[1] == "SR" and product_name.split(
+            "_")[2] == "2":
+        pass
+    elif product_name.split("_")[1] == "SY" and product_name.split(
+            "_")[2] == "2":
+        eo_ext.cloud_cover = float(
+            root.find_attr("percentage", ".//sentinel3:cloudyPixels"))
+    else:
+        raise ValueError("Unexpected value found at "
+                         f"{product_name}: "
+                         "this was expected to follow the sentinel 3 "
+                         "naming convention, including "
+                         "ending in .SEN3")
+
+
+def fill_proj_properties(proj_ext, product_meta):
+    """Fills the properties for PROJ.
+
+    Based on the proj Extension.py
+
+    Args:
+        proj_ext (pystac.extensions.projection.ProjectionExtension): The extension to be populated.
+        href (str): The HREF to the scene, this is expected to be an XML file.
+
+    Returns:
+        pystac.Asset: An asset with the PROJECTION relevant properties.
+    """
+    # Read meta file
+
+    proj_ext.epsg = product_meta.get_epsg
+
+    if (product_meta.scene_id.split("_")[1] == "SR"
+            and product_meta.scene_id.split("_")[2] == "2"):
+        pass
+    elif (product_meta.scene_id.split("_")[1] == "SY"
+          and product_meta.scene_id.split("_")[2] == "2"
+          and product_meta.scene_id.split("_")[3] != "AOD"):
+        pass
+    else:
+        proj_ext.shape = product_meta.get_shape
---
+++ packages/stactools-sentinel3/stactools_sentinel3-0.3.0-py3-none-any.whl/stactools/sentinel3/stac.py
@@ -0,0 +1,72 @@
+import logging
+
+import pystac
+from pystac.extensions.eo import EOExtension
+from pystac.extensions.projection import ProjectionExtension
+from pystac.extensions.sat import SatExtension
+
+from .constants import SENTINEL_CONSTELLATION, SENTINEL_PROVIDER
+from .metadata_links import MetadataLinks
+from .product_metadata import ProductMetadata
+from .properties import (fill_eo_properties, fill_proj_properties,
+                         fill_sat_properties)
+
+logger = logging.getLogger(__name__)
+
+
+def create_item(granule_href: str) -> pystac.Item:
+    """Create a STC Item from a Sentinel-3 scene.
+
+    Args:
+        granule_href (str): The HREF to the granule.
+            This is expected to be a path to a SEN3 archive.
+
+    Returns:
+        pystac.Item: An item representing the Sentinel-3 OLCI or SLSTR scene.
+    """
+
+    metalinks = MetadataLinks(granule_href)
+
+    product_metadata = ProductMetadata(metalinks.product_metadata_href)
+
+    item = pystac.Item(
+        id=product_metadata.scene_id,
+        geometry=product_metadata.geometry,
+        bbox=product_metadata.bbox,
+        datetime=product_metadata.get_datetime,
+        properties={},
+        stac_extensions=[],
+    )
+
+    # ---- Add Extensions ----
+    # sat
+    sat = SatExtension.ext(item, add_if_missing=True)
+    fill_sat_properties(sat, metalinks.product_metadata_href)
+
+    # eo
+    eo = EOExtension.ext(item, add_if_missing=True)
+    fill_eo_properties(eo, metalinks.product_metadata_href)
+
+    # proj
+    proj = ProjectionExtension.ext(item, add_if_missing=True)
+    fill_proj_properties(proj, product_metadata)
+
+    # s3 properties
+    item.properties.update({**product_metadata.metadata_dict})
+
+    # --Common metadata--
+    item.common_metadata.providers = [SENTINEL_PROVIDER]
+    item.common_metadata.platform = product_metadata.platform
+    item.common_metadata.constellation = SENTINEL_CONSTELLATION
+
+    # Add assets to item
+    item.add_asset(*metalinks.create_manifest_asset())
+
+    # create band asset list
+    band_list, asset_list = metalinks.create_band_asset()
+
+    # objects for bands
+    for band, asset in zip(band_list, asset_list):
+        item.add_asset(band, asset)
+
+    return item
