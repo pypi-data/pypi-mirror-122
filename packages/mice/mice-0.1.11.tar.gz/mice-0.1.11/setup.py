402885fd7497a170257e6ea6b3efbb81b06f22f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:46 +0000

    Add package mice/0.1.11/mice-0.1.11.tar.gz

---
+++ packages/mice/mice-0.1.11.tar.gz/mice-0.1.11/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+
+with open('REQUIREMENTS') as f:
+    requirements = f.read()
+
+
+setup(
+    name='mice',
+    version='0.1.11',
+    description='Multi-iteration Stochastic Estimator',
+    long_description_content_type="text/x-rst",
+    long_description=readme,
+    author='Andre Gustavo Carlon',
+    author_email='agcarlon@gmail.com',
+    url='https://bitbucket.org/agcarlon/mice',
+    license='GPL v3',
+    install_requires=requirements,
+    setup_requires=['numpydoc',
+                    'sphinx>=1.3.1',
+                    'sphinx_rtd_theme>=0.1.7'],
+    packages=find_packages(exclude=('tests', 'docs'))
+)
