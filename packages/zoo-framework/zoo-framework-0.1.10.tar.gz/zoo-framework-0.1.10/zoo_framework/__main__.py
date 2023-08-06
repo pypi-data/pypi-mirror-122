f632dece495811de160f6b5483fbb0351bd3c2df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:06 +0000

    Add package zoo-framework/0.1.10/zoo-framework-0.1.10.tar.gz

---
+++ packages/zoo-framework/zoo-framework-0.1.10.tar.gz/zoo-framework-0.1.10/zoo_framework/__main__.py
@@ -0,0 +1,33 @@
+import json
+
+import click
+import os
+
+DEFAULT_CONF = {
+    "log": {
+        "path": "./logs"
+    }
+}
+
+
+def create():
+    pass
+
+
+@click.command()
+@click.option("--create", help="Input target object name and create it")
+@click.option("--log", is_flag=True, default=True, help="")
+def zfc(create, log):
+    object_name = create
+    if os.path.exists(object_name):
+        return
+
+    os.mkdir(object_name)
+    src_dir = object_name + '/src'
+    config_file = object_name + "config.json"
+    os.mkdir(src_dir)
+    with open(config_file, "wb") as fp:
+        json.dump(DEFAULT_CONF, fp)
+
+
+zfc()
