316fbf10f3f3eb210d2dd04724f625b4d9750034
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:43 +0000

    Add package moodle-dl/2.2.0.1/moodle-dl-2.2.0.1.tar.gz

---
+++ packages/moodle-dl/moodle-dl-2.2.0.1.tar.gz/moodle-dl-2.2.0.1/moodle_dl/ydl_extractors/extractors.py
@@ -0,0 +1,22 @@
+from youtube_dl.YoutubeDL import YoutubeDL
+
+from moodle_dl.ydl_extractors.zoomus import ZoomUSIE
+from moodle_dl.ydl_extractors.opencast import OpencastIE, OpencastPlaylistIE
+from moodle_dl.ydl_extractors.helixmedia import Helixmedia
+from moodle_dl.ydl_extractors.owncloud import Owncloud
+from moodle_dl.ydl_extractors.opencast_lti import OpencastLTI
+
+
+def add_additional_extractors(ydl: YoutubeDL):
+    additional_extractors = [
+        OpencastIE(ydl),
+        OpencastPlaylistIE(ydl),
+        ZoomUSIE(ydl),
+        Helixmedia(ydl),
+        Owncloud(ydl),
+        OpencastLTI(ydl),
+    ]
+
+    for extractor in additional_extractors:
+        ydl._ies.insert(0, extractor)
+        ydl._ies_instances[extractor.ie_key()] = extractor
