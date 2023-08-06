38537930a7c989b58922806b17a230361dcb948c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:36 +0000

    Add package django-sitegate/1.3.0/django-sitegate-1.3.0.tar.gz

---
+++ packages/django-sitegate/django-sitegate-1.3.0.tar.gz/django-sitegate-1.3.0/sitegate/toolbox.py
@@ -0,0 +1,26 @@
+from django.urls import re_path
+
+from .views import verify_email, remote_auth, remote_auth_start
+
+
+def get_sitegate_urls() -> list:
+    """Returns sitegate urlpatterns, that can be attached
+    to urlpatterns of a project:
+
+        # Example from urls.py.
+
+        from sitegate.toolbox import get_sitegate_urls
+
+        urlpatterns = patterns('',
+            ...
+            path('login/', 'apps.views.login', name='login'),
+            ...
+        ) + get_sitegate_urls()  # Attach.
+
+    """
+    urls = [
+        re_path(r'^verify_email/(?P<code>\S+)/$', verify_email, name='verify_email'),
+        re_path(r'^rauth/(?P<alias>\S+)/start/$', remote_auth_start, name='remote_auth_start'),
+        re_path(r'^rauth/(?P<alias>\S+)/$', remote_auth, name='remote_auth'),
+    ]
+    return urls
