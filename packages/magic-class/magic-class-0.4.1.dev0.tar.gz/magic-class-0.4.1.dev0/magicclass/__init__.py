602c30782a6433779f58b9f6ce9f786bb60bcc5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:06 +0000

    Add package magic-class/0.4.1.dev0/magic-class-0.4.1.dev0.tar.gz

---
+++ packages/magic-class/magic-class-0.4.1.dev0.tar.gz/magic-class-0.4.1.dev0/magicclass/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "0.4.1dev0"
+
+from .core import magicclass, magicmenu
+from .tester import Tester
+from .wrappers import set_options, click, set_design
+from .field import field
+from . import widgets
