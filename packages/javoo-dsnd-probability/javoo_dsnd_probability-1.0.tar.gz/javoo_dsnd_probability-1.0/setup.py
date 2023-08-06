54840ad82bd627785b98752fc5b4ae1e93f1c2d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:42 +0000

    Add package javoo-dsnd-probability/1.0/javoo_dsnd_probability-1.0.tar.gz

---
+++ packages/javoo-dsnd-probability/javoo_dsnd_probability-1.0.tar.gz/javoo_dsnd_probability-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='javoo_dsnd_probability',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['javoo_dsnd_probability'],
+      author = 'Andrew Paster - Javoo',
+      author_email = 'javoo.code.ando@gmail.com',
+      zip_safe=False)
