60724d30f476f07558c09651ef6ea825cbce1b7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:38 +0000

    Add package kaskade/0.1.5/kaskade-0.1.5-py3-none-any.whl

---
+++ packages/kaskade/kaskade-0.1.5-py3-none-any.whl/kaskade/app.py
@@ -0,0 +1,31 @@
+import click
+
+from kaskade.cli import Cli
+
+
+@click.command()
+@click.option("--version", is_flag=True, help="Show the app version and exit.")
+@click.argument("config_file", metavar="<config file>", nargs=1, required=False)
+def main(version, config_file):
+    """
+
+    kaskade is a terminal user interface for kafka.
+
+    \b
+    It receives an argument <config file>, in case of not passing
+    any argument it'll take any of next config file names by default:
+    kaskade.yml, kaskade.yaml, config.yml, config.yaml.
+
+    More info at https://github.com/sauljabin/kaskade.
+
+    \b
+    Examples:
+        kaskade
+        kaskade config.yml
+    """
+    cli = Cli(print_version=version, config_file=config_file)
+    cli.run()
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/kaskade/kaskade-0.1.5-py3-none-any.whl/kaskade/utils/circular_list.py
@@ -0,0 +1,22 @@
+class CircularList:
+    def __init__(self, original_list):
+        self.__list = original_list
+        self.__index = -1
+
+    def __next__(self):
+        return self.next()
+
+    def __len__(self):
+        return len(self.__list)
+
+    def next(self):
+        self.__index += 1
+        if self.__index >= len(self.__list):
+            self.__index = 0
+        return self.__list[self.__index]
+
+    def previous(self):
+        self.__index -= 1
+        if self.__index < 0:
+            self.__index = len(self.__list) - 1
+        return self.__list[self.__index]
