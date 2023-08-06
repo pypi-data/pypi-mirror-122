145e4d75452f96def96c860ce0ece2253f8bbed4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:11 +0000

    Add package datasci-distributions/1.0/datasci_distributions-1.0.tar.gz

---
+++ packages/datasci-distributions/datasci_distributions-1.0.tar.gz/datasci_distributions-1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='datasci_distributions',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['datasci_distributions'],
+      zip_safe=False)
