3a271dbf1a0f1970ca3c53203135991c1935ac92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:44 +0000

    Add package iops/0.2.3/iops-0.2.3.tar.gz

---
+++ packages/iops/iops-0.2.3.tar.gz/iops-0.2.3/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="iops",
+    version="0.2.3",
+    author="Michael du Plessis et al.",
+    author_email="author@example.com",
+    description="Package for the calculation of the IO-PS framework",
+    url='https://github.com/Michaldp/IO-PS-Test-Repository')
