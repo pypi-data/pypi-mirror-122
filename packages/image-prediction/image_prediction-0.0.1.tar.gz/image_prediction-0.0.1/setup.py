7b3739b5fd4bcee6202a441c610ac86953645b55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:57 +0000

    Add package image-prediction/0.0.1/image_prediction-0.0.1.tar.gz

---
+++ packages/image-prediction/image_prediction-0.0.1.tar.gz/image_prediction-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.txt", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="image_prediction",
+    version="0.0.1",
+    author="Dipesh Shrestha",
+    author_email="dipesh@example.com",
+    description="A simple image prediction package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
