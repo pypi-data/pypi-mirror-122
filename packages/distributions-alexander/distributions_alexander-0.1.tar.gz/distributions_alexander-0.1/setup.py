2d48cba0fe167c21675efe8e1ddc681c07ef1c45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:46 +0000

    Add package distributions-alexander/0.1/distributions_alexander-0.1.tar.gz

---
+++ packages/distributions-alexander/distributions_alexander-0.1.tar.gz/distributions_alexander-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_alexander',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_alexander'],
+      zip_safe=False)
