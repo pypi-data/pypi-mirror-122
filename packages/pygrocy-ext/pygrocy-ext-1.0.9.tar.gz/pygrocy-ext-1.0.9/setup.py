8ea99e70431378829c407eb7c6a7df2d1bfc5571
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:44 +0000

    Add package pygrocy-ext/1.0.9/pygrocy-ext-1.0.9.tar.gz

---
+++ packages/pygrocy-ext/pygrocy-ext-1.0.9.tar.gz/pygrocy-ext-1.0.9/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pygrocy-ext",
+    version="1.0.9",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "requests",
+        "iso8601~=0.1.16",
+        "pytz~=2021.1",
+        "tzlocal>=2.1,<3.0",
+        "deprecation~=2.1.0",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
