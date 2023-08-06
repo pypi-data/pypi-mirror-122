67c0786825b939905df188a414d87fdac8cf2a19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:49 +0000

    Add package gauss-bi-dist-pypi/1.0/gauss-bi-dist-pypi-1.0.tar.gz

---
+++ packages/gauss-bi-dist-pypi/gauss-bi-dist-pypi-1.0.tar.gz/gauss-bi-dist-pypi-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gauss-bi-dist-pypi',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['gauss-bi-dist-pypi'],
+      author = "Dhakshesh Thirugnanasambanthar",
+      author_email = "dhakshesh.sam@gmail.com",	
+      zip_safe=False)
