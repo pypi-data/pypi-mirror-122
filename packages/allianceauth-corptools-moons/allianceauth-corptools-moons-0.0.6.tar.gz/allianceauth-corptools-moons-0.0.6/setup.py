158f83f4d783755274b4110f3d5b4857665ca0c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:07 +0000

    Add package allianceauth-corptools-moons/0.0.6/allianceauth-corptools-moons-0.0.6.tar.gz

---
+++ packages/allianceauth-corptools-moons/allianceauth-corptools-moons-0.0.6.tar.gz/allianceauth-corptools-moons-0.0.6/setup.py
@@ -0,0 +1,38 @@
+import os
+from setuptools import find_packages, setup
+install_requires = [
+    'allianceauth>=2.8.7',
+    'allianceauth-corptools>=2.0.7'
+    'allianceauth-invoices>=0.0.5'
+]
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='allianceauth-corptools-moons',
+    version='0.0.6',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=install_requires,
+    license='GNU General Public License v3 (GPLv3)',
+    description='Alliance Auth Plugin',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url='https://github.com/The-Initiative-EvE/allianceauth-corp-tools-moons',
+    author='AaronKable',
+    author_email='aaronkable@gmail.com',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
+
