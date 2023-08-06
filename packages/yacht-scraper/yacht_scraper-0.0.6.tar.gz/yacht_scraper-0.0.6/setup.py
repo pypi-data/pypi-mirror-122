6d054ff8bdcd3978f216ac95616a7bd935c51774
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:17 +0000

    Add package yacht-scraper/0.0.6/yacht_scraper-0.0.6.tar.gz

---
+++ packages/yacht-scraper/yacht_scraper-0.0.6.tar.gz/yacht_scraper-0.0.6/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name = 'yacht_scraper',
+    version = '0.0.6',
+    description = 'Package that allows scraping of yacht data from boat24.com',
+    url = 'https://github.com/nishaalajmera/Yacht_scraper.git',
+    author = 'Nishaal Ajmera',
+    licence = 'MIT',
+    packages = find_packages(),
+    install_requires = ['selenium','pandas','tqdm','SQLAlchemy', 'psycopg2-binary']
+)
