9994ad7e765cc1b9565255623508150c7d2d63c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:11 +0000

    Add package dstoolkit-distributions/1.4/dstoolkit_distributions-1.4.tar.gz

---
+++ packages/dstoolkit-distributions/dstoolkit_distributions-1.4.tar.gz/dstoolkit_distributions-1.4/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dstoolkit_distributions',
+      version='1.4',
+      description='Binomial and Gaussian distributions',
+      packages=['dstoolkit_distributions'],
+      author = 'Gustavo Jorge Martins de Aguiar',
+      author_email = 'gustavoaguiar@id.uff.br',
+      zip_safe=False)
