6ca50650d1132350e06b9a4a6a12b308f44597ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:27 +0000

    Add package simuwater/0.0.0/simuwater-0.0.0.tar.gz

---
+++ packages/simuwater/simuwater-0.0.0.tar.gz/simuwater-0.0.0/setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  9 14:12:41 2021
+
+@author: mumuz
+"""
+
+from setuptools import setup
+
+setup(name = 'simuwater',
+      version = '0.0.0',
+      description = 'Simuwater is a control-oriented modelling software',
+      url = '',
+      author = 'Lei Zhang',
+      author_email = 'gemini.zhang@qq.com',
+      license = 'MIT',
+      packages = ['simuwater'])
