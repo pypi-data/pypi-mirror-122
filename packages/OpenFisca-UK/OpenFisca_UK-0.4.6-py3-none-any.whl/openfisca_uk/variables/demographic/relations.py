1eaadd1c33242f06338795388a7960638d07164c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:48 +0000

    Add package OpenFisca-UK/0.4.6/OpenFisca_UK-0.4.6-py3-none-any.whl

---
+++ packages/OpenFisca-UK/OpenFisca_UK-0.4.6-py3-none-any.whl/openfisca_uk/variables/demographic/relations.py
@@ -0,0 +1,24 @@
+from openfisca_core.model_api import *
+from openfisca_uk.entities import *
+from openfisca_uk.tools.general import *
+
+
+class person_benunit_id(Variable):
+    value_type = float
+    label = u"Person's benefit unit ID"
+    entity = Person
+    definition_period = YEAR
+
+
+class person_household_id(Variable):
+    value_type = float
+    label = u"Person's household ID"
+    entity = Person
+    definition_period = YEAR
+
+
+class role(Variable):
+    value_type = str
+    label = u"Role (adult/child)"
+    entity = Person
+    definition_period = YEAR
