faede0e1c0a2c143f04a3628ee7bd01bce72a0ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:46 +0000

    Add package multable-czwtest/0.0.1/multable_czwtest-0.0.1.tar.gz

---
+++ packages/multable-czwtest/multable_czwtest-0.0.1.tar.gz/multable_czwtest-0.0.1/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="multable_czwtest", # Replace with your own username  #自定义封装模块名与文件夹名相同
+    version="0.0.1", #版本号，下次修改后再提交的话只需要修改当前的版本号就可以了
+    author="陈之文", #作者
+    author_email="czw52460183@qq.com", #邮箱
+    description="调用此方法可打印9*9乘法表", #描述
+    long_description='调用此方法可以打印9*9乘法表', #描述
+    long_description_content_type="text/markdown", #markdown
+    url="https://github.com/me/myproject13", #github地址
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License", #License
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',  #支持python版本
+)
