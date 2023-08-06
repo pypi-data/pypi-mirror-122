64fa251c7b71767467ae8f9246a3fba60ac5d1bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:12 +0000

    Add package openodia/0.1.2/openodia-0.1.2-py3.9.egg

---
+++ packages/openodia/openodia-0.1.2-py3.9.egg/openodia/__init__.py
@@ -0,0 +1,19 @@
+"""Open Odia language tools
+"""
+__version__ = "0.1.2"
+
+from ._letters import Letters as alphabet
+from ._odianames import Names as name
+from ._understandData import UnderstandData as ud
+from ._translate import odia_to_other_lang, other_lang_to_odia, universal_translation
+from ._summarization import WordFrequency
+
+__all__ = [
+    "alphabet",
+    "name",
+    "ud",
+    "other_lang_to_odia",
+    "odia_to_other_lang",
+    "universal_translation",
+    "WordFrequency",
+]
