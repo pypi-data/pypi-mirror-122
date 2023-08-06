6fbcff3043d38d9f71ddbe3ae62675905bb17d8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:42:09 +0000

    Add package chin-distributions-udacity/0.2/chin_distributions_udacity-0.2.tar.gz

---
+++ packages/chin-distributions-udacity/chin_distributions_udacity-0.2.tar.gz/chin_distributions_udacity-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='chin_distributions_udacity',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['chin_distributions_udacity'],
+      author = 'Chinmay Kumar',
+      author_email = 'chinmaykumar2001@gmail.com',
+      zip_safe=False)
