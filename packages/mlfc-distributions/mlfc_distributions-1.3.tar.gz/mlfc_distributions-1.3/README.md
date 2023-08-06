6bd3192e98bfd5a8fcff5fcf1c87572e2fc72a6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:47 +0000

    Add package mlfc-distributions/1.3/mlfc_distributions-1.3.tar.gz

---
+++ packages/mlfc-distributions/mlfc_distributions-1.3.tar.gz/mlfc_distributions-1.3/README.md
@@ -0,0 +1,7 @@
+# mlfc_distributions package
+
+Binomial and Gaussian distributions package
+
+# Files
+
+One parent class and two child classes. In addition to this README file and license.txt
---
+++ packages/mlfc-distributions/mlfc_distributions-1.3.tar.gz/mlfc_distributions-1.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='mlfc_distributions',
+      version='1.3',
+      description='Gaussian distributions',
+      packages=['mlfc_distributions'],
+      author='RK',
+      author_email='rom@mailinator.com',
+      url='https://udacity.com',
+      zip_safe=False)
