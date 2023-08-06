1a3011c4b7f58bb389b2ddb0dc7e8436af327e3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:40 +0000

    Add package hawkdata/0.0.6/hawkdata-0.0.6-py3-none-any.whl

---
+++ packages/hawkdata/hawkdata-0.0.6-py3-none-any.whl/hawkdata/macro/china_cpi.py
@@ -0,0 +1,23 @@
+# -*- coding:utf-8 -*-
+
+
+from hawkdata.utils.crawlers import CStatsCrawler
+
+
+def macro_china_yearly_cpi_yoy():
+
+    csc = CStatsCrawler()
+
+    return csc.macro_china_yearly_cpi_yoy()
+
+def macro_china_yearly_cpi_index():
+
+    csc = CStatsCrawler()
+
+    return csc.macro_china_yearly_cpi_index()
+
+def macro_china_monthy_cpi_yoy():
+
+    csc = CStatsCrawler()
+
+    return csc.macro_china_monthy_cpi_yoy()
---
+++ packages/hawkdata/hawkdata-0.0.6-py3-none-any.whl/hawkdata/macro/china_gdp.py
@@ -0,0 +1,35 @@
+# -*- coding:utf-8 -*-
+
+
+from hawkdata.utils.crawlers import CStatsCrawler
+
+
+def macro_china_yearly_gdp():
+
+    csc = CStatsCrawler()
+
+    return csc.macro_china_yearly_gdp()
+
+def macro_china_yearly_gdp_yoy():
+
+    csc = CStatsCrawler()
+
+    return csc.macro_china_yearly_gdp_yoy()
+
+def macro_china_yearly_gdp_index():
+
+    csc = CStatsCrawler()
+
+    return csc.macro_china_yearly_gdp_index()
+
+def macro_china_quarterly_gdp():
+
+    csc = CStatsCrawler()
+
+    return csc.macro_china_quarterly_gdp()
+
+def macro_china_quarterly_gdp_yoy():
+
+    csc = CStatsCrawler()
+
+    return csc.macro_china_quarterly_gdp_yoy()
