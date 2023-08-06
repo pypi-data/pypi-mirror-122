24d148510b85ab6535863b00ec6389a70189bc1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:44:03 +0000

    Add package KYYdistributions/1.0/KYYdistributions-1.0.tar.gz

---
+++ packages/KYYdistributions/KYYdistributions-1.0.tar.gz/KYYdistributions-1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='KYYdistributions',
+      version='1.0',
+      description='KYYdistributions',
+      packages=['KYYdistributions'],
+      zip_safe=False)
