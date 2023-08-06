af6989092109bf6616c713ce7c4b9e1f0a7f866f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:27 +0000

    Add package paddlepaddle/2.2.0rc0/paddlepaddle-2.2.0rc0-cp38-cp38-macosx_10_14_x86_64.whl

---
+++ packages/paddlepaddle/paddlepaddle-2.2.0rc0-cp38-cp38-macosx_10_14_x86_64.whl/paddle/version.py
@@ -0,0 +1,23 @@
+# THIS FILE IS GENERATED FROM PADDLEPADDLE SETUP.PY
+#
+full_version    = '2.2.0-rc0'
+major           = '2'
+minor           = '2'
+patch           = '0-rc0'
+rc              = '0'
+istaged         = True
+commit          = 'c576169b8c4b18d4a714133e459c0380dacf84b9'
+with_mkl        = 'OFF'
+
+def show():
+    if istaged:
+        print('full_version:', full_version)
+        print('major:', major)
+        print('minor:', minor)
+        print('patch:', patch)
+        print('rc:', rc)
+    else:
+        print('commit:', commit)
+
+def mkl():
+    return with_mkl
