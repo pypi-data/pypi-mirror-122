ba3155ab265e2339d0e49da2557573ad70ce1762
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:06:14 +0000

    Add package arcalive/0.0.1/arcalive-0.0.1.tar.gz

---
+++ packages/arcalive/arcalive-0.0.1.tar.gz/arcalive-0.0.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r", encoding='UTF-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="arcalive", # Replace with your own username
+    version="0.0.1",
+    author="SZI",
+    description="Arcalive Loader",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
