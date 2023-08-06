92d70dbf46f18ba58a5ef075780e5689a1743f04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:46 +0000

    Add package stellar-model/0.2.1b0/stellar_model-0.2.1b0-py3-none-any.whl

---
+++ packages/stellar-model/stellar_model-0.2.1b0-py3-none-any.whl/stellar_model/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "0.2.1b0"
+__supported_horizon_version__ = "2.8.3"
+__url__ = "https://github.com/StellarCN/stellar-model"
+__issues__ = f"{__url__}/issues"
+
+from stellar_model.response import *
