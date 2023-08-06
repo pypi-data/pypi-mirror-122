4b188fdedf1f2500a1b10f8b9c1a58cb169fd4b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:41 +0000

    Add package jetpack-io/0.5.1.dev20211007/jetpack-io-0.5.1.dev20211007.tar.gz

---
+++ packages/jetpack-io/jetpack-io-0.5.1.dev20211007.tar.gz/jetpack-io-0.5.1.dev20211007/setup.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['jetpack',
+ 'jetpack._job',
+ 'jetpack._remote',
+ 'jetpack.config',
+ 'jetpack.console',
+ 'jetpack.console.commands',
+ 'jetpack.models',
+ 'jetpack.models.core',
+ 'jetpack.models.runtime',
+ 'jetpack.proto.runtime.v1alpha1']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['cleo>=1.0.0a4,<2.0.0',
+ 'cronitor>=4.2.0,<5.0.0',
+ 'grpcio>=1.37.1,<2.0.0',
+ 'jsonpickle>=2.0.0,<3.0.0',
+ 'protobuf>=3.17.0,<4.0.0',
+ 'redis-namespace>=3.0.1,<4.0.0',
+ 'redis==3.0.1',
+ 'schedule>=1.1.0,<2.0.0']
+
+setup_kwargs = {
+    'name': 'jetpack-io',
+    'version': '0.5.1.dev20211007',
+    'description': 'Python SDK for Jetpack.io',
+    'long_description': '### Jetpack SDK\n',
+    'author': 'jetpack.io',
+    'author_email': 'hello@jetpack.io',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://www.jetpack.io',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
