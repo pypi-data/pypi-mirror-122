c45f87fbfdc46a9ed8e1b3a19efb2f29d9520476
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:56 +0000

    Add package computeProbDens/0.2/computeProbDens-0.2.tar.gz

---
+++ packages/computeProbDens/computeProbDens-0.2.tar.gz/computeProbDens-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='computeProbDens',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['computeProbDens'],
+      zip_safe=False)
