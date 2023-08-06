7db66210758b7beabd7f2bf298aebf7a4d9a5936
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:15 +0000

    Add package manim-pptx/0.1.2/manim-pptx-0.1.2.tar.gz

---
+++ packages/manim-pptx/manim-pptx-0.1.2.tar.gz/manim-pptx-0.1.2/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['manim_pptx']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['lxml', 'manim', 'python-pptx']
+
+setup_kwargs = {
+    'name': 'manim-pptx',
+    'version': '0.1.2',
+    'description': '',
+    'long_description': None,
+    'author': 'RythenGlyth',
+    'author_email': 'rythenglyth@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.0,<4.0',
+}
+
+
+setup(**setup_kwargs)
