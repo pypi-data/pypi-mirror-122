420196034b17053107ab0968d427f90cb83e397a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:07:17 +0000

    Add package maintain-PlatoUtils-pkg-Timaos/0.1.2/maintain_PlatoUtils-pkg-Timaos-0.1.2.tar.gz

---
+++ packages/maintain-PlatoUtils-pkg-Timaos/maintain_PlatoUtils-pkg-Timaos-0.1.2.tar.gz/maintain_PlatoUtils-pkg-Timaos-0.1.2/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+from setuptools import setup
+
+NAME="maintain_PlatoUtils-pkg-Timaos"
+VERSION="0.1.2"
+PY_MODULES=["maintain_PlatoUtils.maintain_PlatoUtils"]
+
+with open("README.md", "r",encoding="utf8") as fh:
+    long_description = fh.read()
+
+setup(
+    name=NAME,
+    version=VERSION,
+    url='https://github.com/Timaos123/maintian_PlatoUtils.git',
+    license='MIT',
+    author='Timaos',
+    author_email='201436009@uibe.edu.cn',
+    description='运营PlatoDB的工具',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=['numpy',"nebula-python","pandas"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="maintain_PlatoUtils"),
+    python_requires='>=3.0',
+)
