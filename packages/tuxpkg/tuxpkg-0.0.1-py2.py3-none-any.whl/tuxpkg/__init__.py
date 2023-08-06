0104d002e3926859a51cce05fac3dc29710217da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:43 +0000

    Add package tuxpkg/0.0.1/tuxpkg-0.0.1-py2.py3-none-any.whl

---
+++ packages/tuxpkg/tuxpkg-0.0.1-py2.py3-none-any.whl/tuxpkg/__init__.py
@@ -0,0 +1,14 @@
+"""
+Release automation tool for Python projects
+"""
+
+__version__ = "0.0.1"
+
+
+from pathlib import Path
+
+
+def get_data_file(f):
+    path = Path(__file__).parent / "data" / f
+    assert path.exists()
+    return path
