ce6d802f16d4d9cb7f0873891022260249fe0ea2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:14:52 +0000

    Add package BackuPy/1.10.1/BackuPy-1.10.1.tar.gz

---
+++ packages/BackuPy/BackuPy-1.10.1.tar.gz/BackuPy-1.10.1/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+import backupy
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="BackuPy",
+    version=backupy.version(),
+    description="A simple backup program in python with an emphasis on data integrity and transparent behaviour",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/elesiuta/backupy",
+    license="GPLv3",
+    packages=['backupy'],
+    entry_points={
+        'console_scripts': [
+            'backupy = backupy:main'
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Topic :: System :: Archiving :: Backup",
+        "Topic :: System :: Archiving :: Mirroring",
+        "Topic :: Utilities",
+        "Intended Audience :: End Users/Desktop",
+        "Environment :: Console",
+        "Development Status :: 5 - Production/Stable",
+    ],
+    test_suite = 'tests.tests',
+)
