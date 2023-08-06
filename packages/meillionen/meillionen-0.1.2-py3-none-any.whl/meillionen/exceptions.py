146ee45fabe945599657e65e1d376214be5999c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:27:48 +0000

    Add package meillionen/0.1.2/meillionen-0.1.2-py3-none-any.whl

---
+++ packages/meillionen/meillionen-0.1.2-py3-none-any.whl/meillionen/exceptions.py
@@ -0,0 +1,18 @@
+class ClassNotFound(KeyError):
+    pass
+
+
+class MethodNotFound(KeyError):
+    pass
+
+
+class ResourceNotFound(KeyError):
+    pass
+
+
+class HandlerNotFound(KeyError):
+    pass
+
+
+class ExtensionHandlerNotFound(KeyError):
+    pass
