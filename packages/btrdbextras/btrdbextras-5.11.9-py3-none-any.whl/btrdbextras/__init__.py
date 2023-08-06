e2f899043e5816648f49c02ff7c02a25a485ba54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:17 +0000

    Add package btrdbextras/5.11.9/btrdbextras-5.11.9-py3-none-any.whl

---
+++ packages/btrdbextras/btrdbextras-5.11.9-py3-none-any.whl/btrdbextras/__init__.py
@@ -0,0 +1,5 @@
+from .conn import Connection
+
+__version__ = 'v5.11.9'
+
+__all__ = ["__version__", "Connection"]
---
+++ packages/btrdbextras/btrdbextras-5.11.9-py3-none-any.whl/btrdbextras/eventproc/__init__.py
@@ -0,0 +1,3 @@
+from .eventproc import hooks, list_handlers, register, deregister, upload_file, _uploads
+
+__all__ = ['hooks', 'list_handlers', 'register', 'deregister', 'upload_file', '_uploads']
