f8c159324b3ed4c1e0e82fff8439158e9457ee2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:42 +0000

    Add package aioxmlrpc/0.6.1/aioxmlrpc-0.6.1.tar.gz

---
+++ packages/aioxmlrpc/aioxmlrpc-0.6.1.tar.gz/aioxmlrpc-0.6.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['aioxmlrpc', 'aioxmlrpc.tests']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['httpx>=0.19.0,<0.20.0']
+
+setup_kwargs = {
+    'name': 'aioxmlrpc',
+    'version': '0.6.1',
+    'description': 'Source code of Sequoia API TLDPublic',
+    'long_description': None,
+    'author': 'Guillaume Gauvrit',
+    'author_email': 'guillaume@gauvr.it',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/mardiros/aioxmlrpc',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
