31cd88e1b6121d5099dc11bb88fcdd69e3608887
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:07 +0000

    Add package ds-greytheory/0.2/ds_greytheory-0.2.tar.gz

---
+++ packages/ds-greytheory/ds_greytheory-0.2.tar.gz/ds_greytheory-0.2/ds_greytheory/__init__.py
@@ -0,0 +1,6 @@
+from .models.grey_egm11 import GreyEGM_1_1
+
+class GreyTheory:
+
+    def __init__(self):
+        self.egm11 = GreyEGM_1_1
---
+++ packages/ds-greytheory/ds_greytheory-0.2.tar.gz/ds_greytheory-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ds_greytheory',
+      version='0.2',
+      description='Grey Theory models and tools',
+      packages=['ds_greytheory'],
+      author = 'Gustavo Jorge Martins de Aguiar',
+      author_email = 'gustavoaguiar@id.uff.br',
+      zip_safe=False)
