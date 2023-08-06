9ea6806f42ee8cfff400801858c7826ede4a96b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:53 +0000

    Add package var/0.0.5/var-0.0.5-py3-none-any.whl

---
+++ packages/var/var-0.0.5-py3-none-any.whl/var/auxiliary.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+HEADER
+======
+*Created on 28.06.2021 by bari_is*
+
+*For COPYING and LICENSE details, please refer to the LICENSE file*
+
+"""
+import os
+
+import pandas as pd
+
+__all__ = ["load_data"]
+__PATH__ = os.path.join(os.path.dirname(__file__), "data", "data.csv")
+
+
+def load_data():
+    """
+    A auxiliary function to load saved test data.
+
+    Returns
+    -------
+    out : DataFrame
+    """
+    data = pd.read_csv(__PATH__)
+    data["Date"] = pd.to_datetime(data["Date"], format='%Y-%m-%d')
+    data = data.set_index("Date")
+
+    return data / 100
+
