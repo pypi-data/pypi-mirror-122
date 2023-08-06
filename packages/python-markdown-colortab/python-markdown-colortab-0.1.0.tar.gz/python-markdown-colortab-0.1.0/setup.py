79f3eeccd1465040ed6d1ce268998cadf985d986
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:42 +0000

    Add package python-markdown-colortab/0.1.0/python-markdown-colortab-0.1.0.tar.gz

---
+++ packages/python-markdown-colortab/python-markdown-colortab-0.1.0.tar.gz/python-markdown-colortab-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+setup (
+    name = 'python-markdown-colortab' ,
+    version = '0.1.0' ,
+    py_modules = [ 'python-markdown-colortab' ],
+    description='Python-Markdown MkDocs python-markdown-colortab',
+    license="MIT",
+    
+    
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    keywords=['markdown', 'extensions', 'mkdocs', 'plugins'],
+
+    
+    install_requires  = [ 'markdown>=3.0' ], 
+    author="beanflame",
+)
