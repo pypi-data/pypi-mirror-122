ae439820d39017d105863e76ec635389266b010c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:15 +0000

    Add package testbybewgambler/0.0.1/testbybewgambler-0.0.1.tar.gz

---
+++ packages/testbybewgambler/testbybewgambler-0.0.1.tar.gz/testbybewgambler-0.0.1/testbybewgambler/bew.py
@@ -0,0 +1,13 @@
+class ShowUpper:
+    # constructor
+    def __init__(self, name):
+        self.name = name
+    # upper
+    def upper(self):
+        self.name = self.name.upper()
+    # lower
+    def lower(self):
+        self.name = self.name.upper()
+    # show
+    def show(self):
+        print(self.name)
