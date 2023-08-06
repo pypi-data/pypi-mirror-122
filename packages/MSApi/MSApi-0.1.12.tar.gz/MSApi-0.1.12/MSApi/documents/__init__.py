cff4942a71b6830120d3904f2bd7cad2a7ff2587
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:20 +0000

    Add package MSApi/0.1.12/MSApi-0.1.12.tar.gz

---
+++ packages/MSApi/MSApi-0.1.12.tar.gz/MSApi-0.1.12/MSApi/documents/__init__.py
@@ -0,0 +1,6 @@
+
+from MSApi.documents.DocumentMS import *
+from MSApi.documents.CustomerOrder import *
+from MSApi.documents.Demand import *
+from MSApi.documents.ProcessingOrder import *
+from MSApi.documents.Processing import *
