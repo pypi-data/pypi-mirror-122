ff7c0062d4526b614389a0fc77a567afd537cab9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:32 +0000

    Add package eazy-probability/1.0/eazy_probability-1.0.tar.gz

---
+++ packages/eazy-probability/eazy_probability-1.0.tar.gz/eazy_probability-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='eazy_probability',
+      version='1.0',
+      description='Gaussian & Binomial distributions calculator',
+      packages=['eazy_probability'],
+      author = 'Anuoluwapo Victor Adeyemi',
+      author_email = 'v.adeyemi.va@gmail.com',
+      zip_safe=False)
