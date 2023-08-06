5835b59abe48d5742a12b11a0f46aa6f34fa3f75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:24 +0000

    Add package docker-farrukh90/0.0.42/docker-farrukh90-0.0.42.tar.gz

---
+++ packages/docker-farrukh90/docker-farrukh90-0.0.42.tar.gz/docker-farrukh90-0.0.42/src/dockers/docker_setup/ticket1.py
@@ -0,0 +1,23 @@
+import os 
+import time
+import sys
+
+sys.path.append('../')
+
+from dockers.shared_scripts.modules import *
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+def taskchecker():
+    isserviceactive("docker")
+    isserviceenabled("docker")
