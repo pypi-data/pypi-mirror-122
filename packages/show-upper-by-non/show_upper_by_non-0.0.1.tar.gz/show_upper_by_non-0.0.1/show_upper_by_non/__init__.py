60c3df872e5698dd91529d2ceada7780f694a741
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:07 +0000

    Add package show-upper-by-non/0.0.1/show_upper_by_non-0.0.1.tar.gz

---
+++ packages/show-upper-by-non/show_upper_by_non-0.0.1.tar.gz/show_upper_by_non-0.0.1/show_upper_by_non/__init__.py
@@ -0,0 +1,1 @@
+from show_upper_by_non.show_upper import ShowUpper
---
+++ packages/show-upper-by-non/show_upper_by_non-0.0.1.tar.gz/show_upper_by_non-0.0.1/show_upper_by_non/show_upper.py
@@ -0,0 +1,13 @@
+class ShowUpper:
+    # constructor
+    def __init__(self, name):
+        self.name = name
+    # string to upper
+    def str_upper(self):
+        self.name = self.name.upper()
+    # string to upper
+    def str_lower(self):
+        self.name = self.name.lower()    
+    # show
+    def show(self):
+        print(self.name)
