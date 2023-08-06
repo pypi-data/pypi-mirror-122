ffa6483d15d4fef182419482e19159609245dbdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:33 +0000

    Add package mice/0.1.13/mice-0.1.13.tar.gz

---
+++ packages/mice/mice-0.1.13.tar.gz/mice-0.1.13/setup.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+
+with open('REQUIREMENTS') as f:
+    requirements = f.read()
+
+
+classifiers=[  # Optional
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Education',
+        'Intended Audience :: Information Technology',
+        'Topic :: Scientific/Engineering',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3 :: Only',
+    ]
+
+setup(
+    name='mice',
+    version='0.1.13',
+    description='Multi-iteration Stochastic Estimator',
+    long_description_content_type="text/x-rst",
+    long_description=readme,
+    author='Andre Gustavo Carlon',
+    author_email='agcarlon@gmail.com',
+    url='https://bitbucket.org/agcarlon/mice',
+    keywords='stochastic optimization, hierarchical methods, Monte Carlo',
+    classifiers=classifiers,
+    license='GPLv3',
+    install_requires=requirements,
+    setup_requires=['numpydoc',
+                    'sphinx>=1.3.1',
+                    'sphinx_rtd_theme>=0.1.7'],
+    packages=find_packages(exclude=('tests', 'docs'))
+)
