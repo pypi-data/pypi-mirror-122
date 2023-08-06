f31feef8cdcf1a04451398e9d38039f6a1c0e889
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:22 +0000

    Add package pytextclassifier/1.0.4/pytextclassifier-1.0.4.tar.gz

---
+++ packages/pytextclassifier/pytextclassifier-1.0.4.tar.gz/pytextclassifier-1.0.4/pytextclassifier/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+@author:XuMing(xuming624@qq.com)
+@description: 
+"""
+
+from pytextclassifier.textclassifier import TextClassifier, load_data
+from pytextclassifier.textcluster import TextCluster
