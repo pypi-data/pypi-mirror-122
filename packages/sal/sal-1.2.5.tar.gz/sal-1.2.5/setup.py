79d20258d274c86b3b34a989b807eedbc276314c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:06:11 +0000

    Add package sal/1.2.5/sal-1.2.5.tar.gz

---
+++ packages/sal/sal-1.2.5.tar.gz/sal-1.2.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+from os import path
+
+# parse the package version number
+with open(path.join(path.dirname(__file__), 'sal/core/VERSION')) as version_file:
+    VERSION = version_file.read().strip()
+
+setup(
+    name='sal',
+    version=VERSION,
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['flask', 'flask_restful', 'itsdangerous', 'numpy',
+                      'requests'],
+    extras_require={"LDAP": ["ldap3"]},
+    url='https://github.com/simpleaccesslayer',
+    license='EUPL v1.2',
+    author='Dr Alex Meakins',
+    author_email='alex.meakins@ukaea.uk',
+    description='Simple Access Layer: A general purpose data system framework.'
+
+)
