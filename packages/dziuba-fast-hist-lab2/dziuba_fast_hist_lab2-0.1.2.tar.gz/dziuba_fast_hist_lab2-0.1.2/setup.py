e24dabca6bf0da4f5542de8b49f37e948c7b5653
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:10 +0000

    Add package dziuba-fast-hist-lab2/0.1.2/dziuba_fast_hist_lab2-0.1.2.tar.gz

---
+++ packages/dziuba-fast-hist-lab2/dziuba_fast_hist_lab2-0.1.2.tar.gz/dziuba_fast_hist_lab2-0.1.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='dziuba_fast_hist_lab2',
+    version='0.1.2',    
+    author='Maria Dziuba',
+    packages=['dziuba_fast_hist_lab2'],
+    install_requires=[
+                      'numpy<1.14',
+                      'matplotlib',
+                      ],
+) 
