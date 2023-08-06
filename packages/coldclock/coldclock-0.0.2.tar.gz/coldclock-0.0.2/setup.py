c88e1f6b0bf05633bf21edd486d778bb6018a128
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:50 +0000

    Add package coldclock/0.0.2/coldclock-0.0.2.tar.gz

---
+++ packages/coldclock/coldclock-0.0.2.tar.gz/coldclock-0.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='coldclock',
+    version='0.0.2',
+    description='GUI clock for tkinter based projects',
+    long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    url='',
+    author='coldsober irene',
+    author_email='nseirene3@gmail.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords=['clock', 'time', 'date', 'datetime'],
+    packages=find_packages(),
+    install_requires=[]
+)
