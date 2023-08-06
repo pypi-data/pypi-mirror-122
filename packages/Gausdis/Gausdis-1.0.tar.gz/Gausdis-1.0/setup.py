623d6d6f404a30c33030234054c9d6077943493d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:43:27 +0000

    Add package Gausdis/1.0/Gausdis-1.0.tar.gz

---
+++ packages/Gausdis/Gausdis-1.0.tar.gz/Gausdis-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Gausdis',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['Gausdis'],
+      author='Wenjie Wang',
+      author_email='wang2021@brandeis.edu',
+      zip_safe=False)
