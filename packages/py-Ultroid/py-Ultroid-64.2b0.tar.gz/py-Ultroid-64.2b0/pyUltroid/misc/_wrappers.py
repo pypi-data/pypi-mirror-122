3d53ef449a27ee7250e7fdaa4eee40c5f1fb3e0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:28 +0000

    Add package py-Ultroid/64.2b0/py-Ultroid-64.2b0.tar.gz

---
+++ packages/py-Ultroid/py-Ultroid-64.2b0.tar.gz/py-Ultroid-64.2b0/pyUltroid/misc/_wrappers.py
@@ -0,0 +1,44 @@
+# Ultroid - UserBot
+# Copyright (C) 2021 TeamUltroid
+#
+# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+# PLease read the GNU Affero General Public License in
+# <https://github.com/TeamUltroid/pyUltroid/blob/main/LICENSE>.
+
+import asyncio
+
+# edit or reply
+
+DEL_TIME = []
+
+
+async def eor(event, text, **args):
+    link_preview = args.get("link_preview", False)
+    parse_mode = args.get("parse_mode", "md")
+    time = args.get("time", None)
+    if not event.out:
+        if event.is_reply:
+            event = await event.get_reply_message()
+        if event:
+            ok = await event.reply(
+                text, link_preview=link_preview, parse_mode=parse_mode
+            )
+    else:
+        ok = await event.edit(text, link_preview=link_preview, parse_mode=parse_mode)
+    if not DEL_TIME:
+        from .. import udB
+
+        ut = udB.get("DEL_DELAY_TIME")
+        DEL_TIME.append(ut)
+    else:
+        ut = DEL_TIME[0]
+    if time and ut != "None":
+        time = ut or time
+        await asyncio.sleep(int(time))
+        return await ok.delete()
+    return ok
+
+
+async def eod(event, text=None, **kwargs):
+    kwargs["time"] = kwargs.get("time", 5)
+    return await eor(event, text, **kwargs)
