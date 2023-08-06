0eef229e73a9005eec6fd7918f30e7fe75581b1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:50 +0000

    Add package distributions-wol/0.1/distributions_wol-0.1.tar.gz

---
+++ packages/distributions-wol/distributions_wol-0.1.tar.gz/distributions_wol-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_wol',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_wol'],
+      zip_safe=False)
