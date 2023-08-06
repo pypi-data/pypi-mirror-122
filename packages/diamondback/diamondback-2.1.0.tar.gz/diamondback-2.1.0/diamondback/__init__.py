6f1cab8649c9cd3e2c6c4c08d11e0067035eb4b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:36 +0000

    Add package diamondback/2.1.0/diamondback-2.1.0.tar.gz

---
+++ packages/diamondback/diamondback-2.1.0.tar.gz/diamondback-2.1.0/diamondback/__init__.py
@@ -0,0 +1,63 @@
+""" **Description**
+        Initialize.
+
+    **License**
+        `BSD-3C. <https://github.com/larryturner/diamondback/blob/master/license>`_
+        © 2018 - 2021 Larry Turner, Schneider Electric Industries SAS. All rights reserved.
+
+    **Author**
+        Larry Turner, Schneider Electric, Analytics & AI, 2018-03-22.
+"""
+
+from .commons import Log  # noqa: F401
+from .commons import RestClient  # noqa: F401
+from .commons import Serial  # noqa: F401
+from .filters import FirFilter  # noqa: F401
+from .filters import IirFilter  # noqa: F401
+from .filters import ComplexBandPassFilter  # noqa: F401
+from .filters import ComplexExponentialFilter  # noqa: F401
+from .filters import ComplexFrequencyFilter  # noqa: F401
+from .filters import DerivativeFilter  # noqa: F401
+from .filters import GoertzelFilter  # noqa: F401
+from .filters import IntegralFilter  # noqa: F401
+from .filters import PidFilter  # noqa: F401
+from .filters import PolynomialRateFilter  # noqa: F401
+from .filters import PolyphaseRateFilter  # noqa: F401
+from .filters import RankFilter  # noqa: F401
+from .filters import WindowFilter  # noqa: F401
+from .interfaces import IA  # noqa: F401
+from .interfaces import IB  # noqa: F401
+from .interfaces import IClear  # noqa: F401
+from .interfaces import IConfigure  # noqa: F401
+from .interfaces import ICount  # noqa: F401
+from .interfaces import IData  # noqa: F401
+from .interfaces import IDate  # noqa: F401
+from .interfaces import IFrequency  # noqa: F401
+from .interfaces import IIdentity  # noqa: F401
+from .interfaces import ILabel  # noqa: F401
+from .interfaces import ILive  # noqa: F401
+from .interfaces import IModel  # noqa: F401
+from .interfaces import IPeriod # noqa: F401
+from .interfaces import IPath  # noqa: F401
+from .interfaces import IPhase  # noqa: F401
+from .interfaces import IProxy  # noqa: F401
+from .interfaces import IQ  # noqa: F401
+from .interfaces import IRate  # noqa: F401
+from .interfaces import IReady  # noqa: F401
+from .interfaces import IReset  # noqa: F401
+from .interfaces import IS  # noqa: F401
+from .interfaces import IStream  # noqa: F401
+from .interfaces import ITimeOut  # noqa: F401
+from .interfaces import ITimeZone  # noqa: F401
+from .interfaces import IUrl  # noqa: F401
+from .interfaces import IValid  # noqa: F401
+from .interfaces import IVersion  # noqa: F401
+from .models import DiversityModel  # noqa: F401
+from .models import PrincipalComponentModel  # noqa: F401
+from .transforms import ComplexTransform  # noqa: F401
+from .transforms import FourierTransform  # noqa: F401
+from .transforms import PowerSpectrumTransform  # noqa: F401
+from .transforms import WaveletTransform  # noqa: F401
+from .transforms import ZTransform  # noqa: F401
+
+__version__ = '2.1.0'
---
+++ packages/diamondback/diamondback-2.1.0.tar.gz/diamondback-2.1.0/diamondback/models/PrincipalComponentModel.py
@@ -0,0 +1,135 @@
+""" **Description**
+        A principal component model analyzes an incident signal to define
+        transformation matrices which consume an incident signal to produce a
+        reference signal, normalized and ordered to define orthogonal axes of
+        descending variance.
+
+        A principal component model is a supervised learning model which
+        analyzes an incident signal representing a training set to learn a mean
+        vector, standard deviation vector, and a collection of eigenvectors
+        associated with an incident signal.
+
+        .. math::
+            \\vec{\mu_{i}} = \matrix{\ \\frac{\sum_{n=0}^{N}\\vec{x_{i,n}}}{N}}
+
+        .. math::
+            \\vec{\sigma_{i}} = \matrix{\ \\frac{\sum_{n=0}^{N}(\ \\vec{x_{i,n}} - \\vec{\mu_{i}}\ )^{2}}{N}}^{0.5}
+
+        .. math::
+            \Lambda_{n} = eig\matrix{\ cov\matrix{\ \matrix{\\frac{\ X_{n}^{T} - \\vec{\mu}\ }{\\vec{\sigma}}\ }\ }^{T}\ }^{T}
+
+        An incident signal which is not a part of an inital training set is
+        transformed without modifying a principal component model, by
+        translation, normalization, and rotation to produce a reference signal
+        which is a candidate for dimension reduction, in which higher order
+        dimensions are discarded, reducing the order of the reference signal,
+        while preserving significant and often sufficient information.
+
+        .. math::
+            Y_{n} = \Lambda_{n} \ \matrix{\\frac{\ X_{n}^{T} - \\vec{\mu}\ }{\\vec{\sigma}}\ }^{T}
+
+        Principal component analysis and dimension reduction has application in
+        clustering, classification, pattern recognition, and visualization.
+
+    **Example**
+       
+        ::
+        
+            from diamondback import PrincipalComponentModel
+
+            # Create an instance.
+
+            obj = PrincipalComponentModel( )
+
+            # Model an incident signal and extract eigenvalue, standard deviation, means, and rotation arrays.
+
+            x = numpy.random.rand( 3, 32 )
+            y = obj.model( x )
+            e, s, u, v = obj.e, obj.s, obj.u, obj.v
+
+    **License**
+        `BSD-3C.  <https://github.com/larryturner/diamondback/blob/master/license>`_
+        © 2018 - 2021 Larry Turner, Schneider Electric Industries SAS. All rights reserved.
+
+    **Author**
+        Larry Turner, Schneider Electric, Analytics & AI, 2019-01-25.
+"""
+
+from diamondback.interfaces.IClear import IClear
+from diamondback.interfaces.IS import IS
+from typing import List, Union
+import numpy
+
+class PrincipalComponentModel( IClear, IS ) :
+
+    """ Principal component model.
+    """
+
+    @property
+    def e( self ) :
+
+        """ e : numpy.ndarray - eigenvalues.
+        """
+
+        return self._e
+
+    @property
+    def u( self ) :
+
+        """ mean : numpy.ndarray.
+        """
+
+        return self._u
+
+    @property
+    def v( self ) :
+
+        """ rotation : numpy.ndarray
+        """
+
+        return self._v
+
+    def __init__( self ) -> None :
+
+        """ Initialize.
+        """
+
+        super( ).__init__( )
+        self._e, self._u, self._v = numpy.array( [ ] ), numpy.array( [ ] ), numpy.array( [ ] )
+        self.s = numpy.array( [ ] )
+
+    def clear( self ) -> None :
+
+        """ Clears an instance.
+        """
+
+        self._e, self._u, self._v = numpy.array( [ ] ), numpy.array( [ ] ), numpy.array( [ ] )
+        self.s = numpy.array( [ ] )
+
+    def model( self, x : Union[ List, numpy.ndarray ] ) -> numpy.ndarray :
+
+        """ Models an incident signal and produces a reference signal.
+
+            Arguments :
+                x : Union[ List, numpy.ndarray ] - incident signal.
+
+            Returns :
+                y : numpy.ndarray - reference signal.
+        """
+
+        if ( ( not numpy.isscalar( x ) ) and ( not isinstance( x, numpy.ndarray ) ) ) :
+            x = numpy.array( list( x ) )
+        if ( ( len( x.shape ) != 2 ) or ( not len( x ) ) ) :
+            raise ValueError( f'X = {x}' )
+        if ( not len( self.s ) ) :
+            self._u = x.mean( 1 )
+            self.s = x.std( 1 )
+            z = ( ( x.T - self.u ) / self.s ).T
+            self._e, self._v = numpy.linalg.eig( numpy.matmul( z, z.T ) / z.shape[ 1 ] )
+            self._v = self._v.T
+        else :
+            z = ( ( x.T - self.u ) / self.s ).T
+        rows, cols = x.shape
+        if ( ( rows != len( self._u ) ) or ( ( rows, rows ) != self._v.shape ) or ( cols <= 0 ) ):
+            raise ValueError( f'Rows = {rows} Columns = {cols}' )
+        return numpy.matmul( self.v, z )
