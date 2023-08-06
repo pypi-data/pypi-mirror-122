a6972157911c2a055f738efe9b341e666baf2a2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:49 +0000

    Add package PyCatApi/1.0.6/PyCatApi-1.0.6.tar.gz

---
+++ packages/PyCatApi/PyCatApi-1.0.6.tar.gz/PyCatApi-1.0.6/main.py
@@ -0,0 +1,16 @@
+from pycatapi import Client
+
+#Define our client here
+c = Client()
+
+
+"""Lets get a random cat image UwU"""
+
+img = c.get_cat()
+print(f"Cat\n{img}")
+
+"""Get an image of a cat belonging to a specific breed"""
+
+breedimg = c.get_breed("beng")
+print(f"\nCat by breed\n{breedimg}")
+
