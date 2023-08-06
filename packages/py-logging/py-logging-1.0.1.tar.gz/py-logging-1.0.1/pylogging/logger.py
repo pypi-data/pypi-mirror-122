7ce1a0eada552432e70d19c1eaae7a4677eff8c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:36 +0000

    Add package py-logging/1.0.1/py-logging-1.0.1.tar.gz

---
+++ packages/py-logging/py-logging-1.0.1.tar.gz/py-logging-1.0.1/pylogging/logger.py
@@ -0,0 +1,28 @@
+from pylogging.formatters.level import get_log_level_formatter
+from pylogging.formatters.scope import get_scope
+from pylogging.formatters.list import format_list
+from pylogging.formatters.dictionary import format_dict
+
+
+class Logger:
+    def __init__(self):
+        self.scope = False
+
+    def set_options(self, value: dict):
+        if "scope" in value.keys():
+            self.scope = value.get("scope")
+
+    def log(self, level, content):
+        final_string = ""
+        if self.scope:
+            final_string += get_scope()
+        final_string += get_log_level_formatter(level)
+        if type(content) is list:
+            print(final_string + "List:")
+            format_list(content)
+        elif type(content) is dict:
+            print(final_string + "Dictionary:")
+            format_dict(content)
+        else:
+            print(final_string + str(content))
+            return
---
+++ packages/py-logging/py-logging-1.0.1.tar.gz/py-logging-1.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+from pylogging import __version__
+
+setup(
+    name='py-logging',
+    url='https://github.com/Szczurowsky/py-loggingr',
+    author='Kamil Szczurowski',
+    author_email='kamil@szczurowsky.pl',
+    packages=['pylogging'],
+    version=__version__,
+    license='MPL-2.0',
+    description='Configurable and easy extendable python logger.',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown"
+)
