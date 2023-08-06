92d66c531d3d00ab2a955b7387595637f5c27e76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:12 +0000

    Add package xz29-probability/0.1/xz29_probability-0.1.tar.gz

---
+++ packages/xz29-probability/xz29_probability-0.1.tar.gz/xz29_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='xz29_probability',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['xz29_probability'],
+      author='Kaitlyn Martinez',
+      author_email='martinez.kaitlyn55@gmail.com',
+      zip_safe=False)
