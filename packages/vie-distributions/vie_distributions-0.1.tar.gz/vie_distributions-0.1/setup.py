164713d13d3a8c0c563e65c8bf6b19a289d77adf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:29 +0000

    Add package vie-distributions/0.1/vie_distributions-0.1.tar.gz

---
+++ packages/vie-distributions/vie_distributions-0.1.tar.gz/vie_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='vie_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['vie_distributions'],
+      author='Aidoo Enoch Kwadwo',
+      author_email='aidooenochkwadwo@gmail.com',
+      zip_safe=False)
