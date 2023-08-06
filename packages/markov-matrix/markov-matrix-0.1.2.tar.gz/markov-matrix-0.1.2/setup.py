be00b4b943f8051d109ae85a317dccbc7e260fe0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:04 +0000

    Add package markov-matrix/0.1.2/markov-matrix-0.1.2.tar.gz

---
+++ packages/markov-matrix/markov-matrix-0.1.2.tar.gz/markov-matrix-0.1.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['markov_matrix']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['numpy>=1.21.2,<2.0.0']
+
+setup_kwargs = {
+    'name': 'markov-matrix',
+    'version': '0.1.2',
+    'description': 'A package for generating markov matrices',
+    'long_description': None,
+    'author': 'peder2911',
+    'author_email': 'pglandsverk@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<=3.10',
+}
+
+
+setup(**setup_kwargs)
