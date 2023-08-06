0fd3475cb4526d0345969b9f0b121ca19b9bfe8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:07:49 +0000

    Add package wbaran-probability/1.0/wbaran_probability-1.0.tar.gz

---
+++ packages/wbaran-probability/wbaran_probability-1.0.tar.gz/wbaran_probability-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='wbaran_probability',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['wbaran_probability'],
+      author='Wren Baran',
+      author_email='wren@wabzs.com',
+      zip_safe=False)
