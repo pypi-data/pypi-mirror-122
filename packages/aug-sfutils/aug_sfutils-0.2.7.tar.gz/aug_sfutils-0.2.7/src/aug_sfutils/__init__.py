209fbdc4c818ff97e099314764844e79f3822731
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:03 +0000

    Add package aug-sfutils/0.2.7/aug_sfutils-0.2.7.tar.gz

---
+++ packages/aug-sfutils/aug_sfutils-0.2.7.tar.gz/aug_sfutils-0.2.7/src/aug_sfutils/__init__.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+# sfutils
+
+"""Shotfile reading with pure python
+
+https://www.aug.ipp.mpg.de/~git/pyaug/sfread.html
+
+"""
+__author__  = 'Giovanni Tardini (Tel. 1898)'
+__version__ = '0.2.7'
+__date__    = '07.10.2021'
+
+import sys
+
+from .sfread import *
+from .sf2equ import *
+from .libddc import ddcshotnr, previousshot
+from .mapeq import *
+try:
+    from .ww import *
+    from .sfh import *
+except:
+    print('ww and sfh not loaded, SFREAD and EQU available')
+    pass
+
+# Backward compatibility.
+
+# On Windows, we encode and decode deep enough that something goes wrong and
+# the encodings.utf_8 module is loaded and then unloaded, I don't know why.
+# Adding a reference here prevents it from being unloaded.  Yuk.
+import encodings.utf_8      # pylint: disable=wrong-import-position, wrong-import-order
