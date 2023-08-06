aa5df097ba99cb2be2ff461bfd04bf0172797e98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:01 +0000

    Add package justanunsuspectingmodule/0.1.1/justanunsuspectingmodule-0.1.1.tar.gz

---
+++ packages/justanunsuspectingmodule/justanunsuspectingmodule-0.1.1.tar.gz/justanunsuspectingmodule-0.1.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup_args = dict(
+    name = 'justanunsuspectingmodule',
+    version = '0.1.1',
+    description='Class with a module for lab',
+    license='MIT',
+    packages=find_packages(),
+    author = 'Mekatto', 
+    author_email = 'igor@mekatto.com',
+    url = 'https://github.com/Igor-Sviridov/justanunsuspectingmodule',
+    download_url = 'https://pypi.org/project/justanunsuspectingmodule/',
+)
+
+install_requires = [
+          'scikit-commpy',
+          'numpy',
+          'soundfile',
+          'bitstring',
+          'scikit-learn',
+          'matplotlib'
+]
+
+if __name__ == '__main__':
+    setup(**setup_args, install_requires=install_requires)
