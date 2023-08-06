b7262c9ed71fabb6d078b891f0a639dc8a189d06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:50 +0000

    Add package cmes/1.0.0/cmes-1.0.0.tar.gz

---
+++ packages/cmes/cmes-1.0.0.tar.gz/cmes-1.0.0/cmes/__init__.py
@@ -0,0 +1,1 @@
+from cmes.chatic import send, get
---
+++ packages/cmes/cmes-1.0.0.tar.gz/cmes-1.0.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name='cmes',
+    packages=['cmes'],
+    version='1.0.0',
+    license='MIT',
+    description='Just something not important',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='lecrkohuty',
+    author_email='lecrkohuty@gmail.com',
+    url='https://github.com/lecrkohuty/cmes',
+    download_url='https://github.com/lecrkohuty/cmes/archive/v_1.0.0.tar.gz',
+    keywords=['colab'],
+    install_requires=[
+        'requests~=2.26.0',
+        'coolname'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+)
