5218c0fc9fde5a02c20f203ece125fbee0aba9be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:34 +0000

    Add package newshound/0.0.1/newshound-0.0.1.tar.gz

---
+++ packages/newshound/newshound-0.0.1.tar.gz/newshound-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='newshound',
+    version='0.0.1',
+    url='https://github.com/johnbumgarner/newshound',
+    author="John Bumgarner",
+    author_email="wordhoardproject@gmail.com",
+    description="A future news extractor package for Python 3",
+    long_description='',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
