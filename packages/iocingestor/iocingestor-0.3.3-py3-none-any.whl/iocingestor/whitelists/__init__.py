40c59152b98f8d1970d61694e0647fb93099bd06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:09 +0000

    Add package iocingestor/0.3.3/iocingestor-0.3.3-py3-none-any.whl

---
+++ packages/iocingestor/iocingestor-0.3.3-py3-none-any.whl/iocingestor/whitelists/__init__.py
@@ -0,0 +1,28 @@
+import json
+from pathlib import Path
+from typing import List, Set
+
+
+class Whitelist:
+    """Base class for Whitelist plugin."""
+
+    def __init__(self, paths: List[str]):
+        self.paths = paths
+        self.values: Set[str] = set()
+        self._load_paths()
+
+    def contains(self, value: str) -> bool:
+        return value in self.values
+
+    def _load_path(self, path: str):
+        if not Path(path).is_file():
+            return
+
+        with open(path) as f:
+            data = json.load(f)
+            list_ = data.get("list", [])
+            self.values.update(list_)
+
+    def _load_paths(self):
+        for path in self.paths:
+            self._load_path(path)
