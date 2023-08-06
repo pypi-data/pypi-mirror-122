6fdfd72df151e21ef061a94f096e53d5e071aa50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:44:41 +0000

    Add package dist-prob-oop/0.2/dist_prob_oop-0.2.tar.gz

---
+++ packages/dist-prob-oop/dist_prob_oop-0.2.tar.gz/dist_prob_oop-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dist_prob_oop',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['dist_prob_oop'],
+      zip_safe=False,
+      author = 'Vanessa Esaw',
+      author_email='esawv.studwent@gmail.com')
