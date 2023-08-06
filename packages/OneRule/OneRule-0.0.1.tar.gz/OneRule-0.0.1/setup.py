0b43cc53e292ccade557bfe310410167f08d8552
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:06:04 +0000

    Add package OneRule/0.0.1/OneRule-0.0.1.tar.gz

---
+++ packages/OneRule/OneRule-0.0.1.tar.gz/OneRule-0.0.1/setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  8 11:47:27 2021
+
+@author: Hangzhou Yiyuan Technology Co., LTD Email yuanyang_teng@163.com
+"""
+
+import setuptools
+setuptools.setup(name='OneRule',
+      version='0.0.1',
+      description="One rule algorithm, Author orgnized according to llh_1178's csdn blog",
+      url='https://github.com/tengyuanyang/Onerule.git',
+      author='Dr Li',
+      author_email='303984464@qq.com',
+      license='MIT',
+      packages=setuptools.find_packages(),
+      zip_safe=False)
