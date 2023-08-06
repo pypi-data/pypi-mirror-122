d47357b5c5fc0002bb20bb24d19d00ef86e9552a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:49 +0000

    Add package codema-dev-tasks/0.2.0/codema_dev_tasks-0.2.0.tar.gz

---
+++ packages/codema-dev-tasks/codema_dev_tasks-0.2.0.tar.gz/codema_dev_tasks-0.2.0/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['codema_dev_tasks']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['aiohttp>=3.7.4,<4.0.0',
+ 'fsspec>=2021.8.1,<2022.0.0',
+ 'requests>=2.26.0,<3.0.0']
+
+setup_kwargs = {
+    'name': 'codema-dev-tasks',
+    'version': '0.2.0',
+    'description': 'A collection of Python helper functions for use in ETL pipelines',
+    'long_description': None,
+    'author': 'Rowan Molony',
+    'author_email': 'rowan.molony@codema.ie',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
