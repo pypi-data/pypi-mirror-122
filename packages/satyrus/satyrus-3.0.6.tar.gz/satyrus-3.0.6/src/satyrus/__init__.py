c7e2ec0a4bd820eecd35fd031005024beff3b965
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:11 +0000

    Add package satyrus/3.0.6/satyrus-3.0.6.tar.gz

---
+++ packages/satyrus/satyrus-3.0.6.tar.gz/satyrus-3.0.6/src/satyrus/__init__.py
@@ -0,0 +1,6 @@
+from .api import SatAPI
+from .cli import SatCLI
+from .satyrus import Satyrus
+from .satlib import Posiform
+
+__all__ = ["Satyrus", "SatAPI", "SatCLI", "Posiform"]
---
+++ packages/satyrus/satyrus-3.0.6.tar.gz/satyrus-3.0.6/src/satyrus/cli/help.py
@@ -0,0 +1,14 @@
+from ..api import SatAPI
+
+HELP = {
+    "debug": "Enters debug mode. (Also sets verbosity to maximum level)",
+    "source": "Source code file path.",
+    "legacy": "Intended to compile SATish code in legacy syntax.",
+    "solver": f"Solver Selection, choose from {SatAPI.options()!r}. Declare extra interfaces with the '-a, --api' parameter.",
+    "output": "Output file destination",
+    "verbose": "Compiler output verbosity, from 0 (default, no output, except errors and results) to 3 (complete compiler log).",
+    "version": "Display compiler version",
+    "report": "Shows detailed sectioned elapsed time.",
+    "params": "Path to JSON file containing parameters for passing to Solver API.",
+    "api": "Selects Python file (.py, .pyw) where SatAPI interfaces are declared.",
+}
