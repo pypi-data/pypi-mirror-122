464c8d1836a9741bbae713a4734c3998e2829ef5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:47 +0000

    Add package russian-cost/0.0.2/russian_cost-0.0.2.tar.gz

---
+++ packages/russian-cost/russian_cost-0.0.2.tar.gz/russian_cost-0.0.2/src/russian_coast/__init__.py
@@ -0,0 +1,2 @@
+# flake8: noqa: F401
+from .russian_cost import RussianCost
---
+++ packages/russian-cost/russian_cost-0.0.2.tar.gz/russian_cost-0.0.2/src/russian_coast/test.py
@@ -0,0 +1,7 @@
+from russian_cost import RussianCost
+
+cost = RussianCost(-4.24)
+print(cost)
+# минус четыре рубля двадцать четыре копейки
+print(cost.strfcoast('%s %R %p'))
+# - четыре рубля 24 копейки
