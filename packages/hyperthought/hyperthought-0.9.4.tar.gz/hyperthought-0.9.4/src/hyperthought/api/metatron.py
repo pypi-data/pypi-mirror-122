4ed8166e0f67214a2b96992ede1cbf840b1859ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:41:39 +0000

    Add package hyperthought/0.9.4/hyperthought-0.9.4.tar.gz

---
+++ packages/hyperthought/hyperthought-0.9.4.tar.gz/hyperthought-0.9.4/src/hyperthought/api/metatron.py
@@ -0,0 +1,38 @@
+from functools import partial
+
+import requests
+
+from .base import GenericAPI, ERROR_THRESHOLD
+
+
+class MetatronAPI(GenericAPI):
+    """
+    Metatron (parsers) API switchboard.
+
+    Contains methods that correspond to endpoints for HyperThought™ parsers.
+
+    Parameters
+    ----------
+    auth : auth.Authorization
+        Authorization object used to get headers and cookies needed to call
+        HyperThought endpoints.
+    """
+
+    def __init__(self, auth):
+        super().__init__(auth)
+
+    def get_parsers(self):
+        """Get information on parsers available to the current user."""
+        curried_request = partial(
+            requests.get,
+            url=f'{self._base_url}/api/metatron/parsers/',
+            headers=self._auth.get_headers(),
+            cookies=self._auth.get_cookies(),
+            verify=self._auth.verify,
+        )
+        r = self.attempt_api_call(curried_request=curried_request)
+
+        if r.status_code < ERROR_THRESHOLD:
+            return r.json()
+        else:
+            self._report_api_error(response=r)
