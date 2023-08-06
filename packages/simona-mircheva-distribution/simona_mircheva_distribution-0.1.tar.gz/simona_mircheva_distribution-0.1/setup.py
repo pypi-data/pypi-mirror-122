94ced6a548d4079969e996da55aff3661633740a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:49 +0000

    Add package simona-mircheva-distribution/0.1/simona_mircheva_distribution-0.1.tar.gz

---
+++ packages/simona-mircheva-distribution/simona_mircheva_distribution-0.1.tar.gz/simona_mircheva_distribution-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='simona_mircheva_distribution',
+      version='0.1',
+      description='Gaussian simona_mircheva',
+      packages=['simona_mircheva'],
+      zip_safe=False)
