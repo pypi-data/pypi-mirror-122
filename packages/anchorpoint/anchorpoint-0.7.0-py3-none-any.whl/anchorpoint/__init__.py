497fc39d2cf532f54b624f9e71de36dda32eeee5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:31 +0000

    Add package anchorpoint/0.7.0/anchorpoint-0.7.0-py3-none-any.whl

---
+++ packages/anchorpoint/anchorpoint-0.7.0-py3-none-any.whl/anchorpoint/__init__.py
@@ -0,0 +1,6 @@
+"""Anchorpoint is a library that supplies text substring selectors for anchoring annotations."""
+
+from anchorpoint.textselectors import TextQuoteSelector, TextPositionSelector
+from anchorpoint.textselectors import TextPositionSet, TextPositionSetFactory
+
+__version__ = "0.7.0"
