c428f46bddb0dd38efe37158989f972281d864fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:24 +0000

    Add package pax3/0.4.0rc1/pax3-0.4.0rc1-py3-none-any.whl

---
+++ packages/pax3/pax3-0.4.0rc1-py3-none-any.whl/pax/__init__.py
@@ -0,0 +1,59 @@
+from pax._src.module import Module, PaxFieldKind
+from pax._src.pure import pure
+from pax._src.rng import next_rng_key, seed_rng_key
+from pax._src.transforms import (
+    apply_gradients,
+    apply_mp_policy,
+    apply_updates,
+    enable_eval_mode,
+    enable_train_mode,
+    flatten_module,
+    freeze_parameters,
+    scan_bugs,
+    select_kind,
+    select_parameters,
+    select_states,
+    transform_gradients,
+    unfreeze_parameters,
+    update_parameters,
+    update_states,
+)
+from pax._src.utils import build_update_fn, dropout, grad_parameters, scan
+
+from . import initializers, nets, nn, utils
+
+__all__ = [
+    "apply_gradients",
+    "apply_mp_policy",
+    "apply_updates",
+    "build_update_fn",
+    "dropout",
+    "enable_eval_mode",
+    "enable_train_mode",
+    "flatten_module",
+    "freeze_parameters",
+    "grad_parameters",
+    "initializers",
+    "Module",
+    "nets",
+    "next_rng_key",
+    "nn",
+    "PaxFieldKind",
+    "pure",
+    "scan_bugs",
+    "scan",
+    "seed_rng_key",
+    "select_kind",
+    "select_parameters",
+    "select_states",
+    "transform_gradients",
+    "unfreeze_parameters",
+    "update_parameters",
+    "update_states",
+    "utils",
+]
+
+try:
+    del _src
+except NameError:
+    pass
