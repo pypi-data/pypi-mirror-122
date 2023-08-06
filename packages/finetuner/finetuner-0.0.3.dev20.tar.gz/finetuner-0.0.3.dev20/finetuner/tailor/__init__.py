4abb0be73c1f2e6d95eb8189fbdefd9fe215c765
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:43 +0000

    Add package finetuner/0.0.3.dev20/finetuner-0.0.3.dev20.tar.gz

---
+++ packages/finetuner/finetuner-0.0.3.dev20.tar.gz/finetuner-0.0.3.dev20/finetuner/tailor/__init__.py
@@ -0,0 +1,31 @@
+from typing import Optional, Tuple
+
+from ..helper import get_framework, AnyDNN
+
+
+def to_embedding_model(
+    model: AnyDNN,
+    input_size: Optional[Tuple[int, ...]] = None,
+    input_dtype: str = 'float32',
+    layer_name: Optional[str] = None,
+    output_dim: Optional[int] = None,
+    freeze: bool = False,
+) -> AnyDNN:
+    f_type = get_framework(model)
+
+    if f_type == 'keras':
+        from .keras import KerasTailor
+
+        ft = KerasTailor
+    elif f_type == 'torch':
+        from .pytorch import PytorchTailor
+
+        ft = PytorchTailor
+    elif f_type == 'paddle':
+        from .paddle import PaddleTailor
+
+        ft = PaddleTailor
+
+    return ft(model, input_size, input_dtype).to_embedding_model(
+        layer_name=layer_name, output_dim=output_dim, freeze=freeze
+    )
