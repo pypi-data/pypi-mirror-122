2b4c9a982a4bbb7b92c5ef01650b6e6fba719394
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:33 +0000

    Add package fake-bpy-module-latest/20211006/fake-bpy-module-latest-20211006.tar.gz

---
+++ packages/fake-bpy-module-latest/fake-bpy-module-latest-20211006.tar.gz/fake-bpy-module-latest-20211006/bl_operators/__init__.py
@@ -0,0 +1,46 @@
+import sys
+import typing
+from . import node
+from . import image
+from . import file
+from . import freestyle
+from . import object_quick_effects
+from . import geometry_nodes
+from . import object_align
+from . import object
+from . import add_mesh_torus
+from . import sequencer
+from . import console
+from . import constraint
+from . import presets
+from . import clip
+from . import view3d
+from . import userpref
+from . import anim
+from . import bmesh
+from . import object_randomize_transform
+from . import uvcalc_lightmap
+from . import screen_play_rendered_anim
+from . import rigidbody
+from . import assets
+from . import spreadsheet
+from . import uvcalc_follow_active
+from . import vertexpaint_dirt
+from . import wm
+from . import mesh
+
+
+def register():
+    ''' 
+
+    '''
+
+    pass
+
+
+def unregister():
+    ''' 
+
+    '''
+
+    pass
