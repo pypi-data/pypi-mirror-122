2918bee767e96c9ac37cbae9a89c6abeba5157bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:56 +0000

    Add package Arbitrage-Master-Sheet-Py/0.0.1/Arbitrage-Master-Sheet-Py-0.0.1.tar.gz

---
+++ packages/Arbitrage-Master-Sheet-Py/Arbitrage-Master-Sheet-Py-0.0.1.tar.gz/Arbitrage-Master-Sheet-Py-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+requirements = ["xlwings==0.24.7","Excelutilities","openpyxl","PySimpleGUI"]
+#The xlwings version has to be strict to work with the workbook setup
+
+setup(
+    name="Arbitrage-Master-Sheet-Py",
+    version="0.0.1",
+    author="Ethan Horsfall",
+    author_email="ethan.horsfall@gmail.com",
+    description="Arbitrage master sheet for amazon",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
