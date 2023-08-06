1447eade2cce78bb64efa9eeb691aea7fa7f935d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:22 +0000

    Add package inspr/0.0.14/inspr-0.0.14.tar.gz

---
+++ packages/inspr/inspr-0.0.14.tar.gz/inspr-0.0.14/inspr/rest.py
@@ -0,0 +1,64 @@
+import requests
+import json
+import sys
+from flask.wrappers import Response
+from requests.models import HTTPError
+
+def send_post_request(url:str, body = {}, headers = {}) -> Response:
+    try:
+        resp = requests.post(url, data = json.dumps(body), headers=headers)
+        resp.raise_for_status()
+    
+    except requests.exceptions.RequestException as e:
+        print(e, file=sys.stderr)
+        raise HTTPError
+    
+    print("Response (send_post_request) = ", resp, file=sys.stderr)
+    return resp
+
+def send_update_request(url:str, body = {}, headers = {}) -> Response:
+    try:
+        resp = requests.put(url, data = json.dumps(body), headers=headers)
+        resp.raise_for_status()
+    
+    except requests.exceptions.RequestException as e:
+        print(e, file=sys.stderr)
+        raise HTTPError
+    
+    return resp
+
+def send_get_request(url:str, body = {}, headers = {}) -> Response:
+    try:
+        resp = requests.get(url, data=json.dumps(body), headers=headers)
+        resp.raise_for_status()
+    
+    except requests.exceptions.RequestException as e:
+        print(e, file=sys.stderr)
+        raise HTTPError
+    
+    return resp
+
+def send_delete_request(url:str, body = {}, headers = {}) -> Response:
+    try:
+        resp = requests.delete(url, data=json.dumps(body), headers=headers)
+        resp.raise_for_status()
+    
+    except requests.exceptions.RequestException as e:
+        print(e, file=sys.stderr)
+        raise HTTPError
+    
+    return resp
+
+
+def send_new_request(url:str, method:str, body = {}, headers = {}) -> Response:
+    if method == 'POST':
+        return send_post_request(url,body,headers)
+    elif method == 'GET':
+        return send_get_request(url,body,headers)
+    elif method == 'PUT':
+        return send_update_request(url,body,headers)
+    elif method == 'DELETE':
+        return send_delete_request(url,body,headers)
+    else:
+        raise Exception(f"Error while send request: Method '{method}' not allowed. The allowed methods are POST,GET,UPDATE,DELETE.")
+                
