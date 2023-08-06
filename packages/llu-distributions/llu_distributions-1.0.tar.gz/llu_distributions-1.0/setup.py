9af6eefbba18f0720d759abd4f4d41892a5012db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:49 +0000

    Add package llu-distributions/1.0/llu_distributions-1.0.tar.gz

---
+++ packages/llu-distributions/llu_distributions-1.0.tar.gz/llu_distributions-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='llu_distributions',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['llu_distributions'],
+	  author='Leqi Lu',
+	  author_email='leqilu67@gmail.com',
+	  zip_safe=False)
