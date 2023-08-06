a2f8c943794d48886a1e20c0980a9af882aa1f66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:09 +0000

    Add package ensmallen/0.6.5/ensmallen-0.6.5.tar.gz

---
+++ packages/ensmallen/ensmallen-0.6.5.tar.gz/ensmallen-0.6.5/src/lib.rs
@@ -0,0 +1,30 @@
+use numpy::{PyArray, PyArray1, PyArray2};
+use pyo3::exceptions::{PyAttributeError, PyTypeError, PyValueError};
+use pyo3::prelude::*;
+use pyo3::types::PyDict;
+use std::collections::{HashMap, HashSet};
+
+use graph::{EdgeT, EdgeTypeT, NodeT, NodeTypeT, Result, WeightT};
+use tags::*;
+
+mod macros;
+pub(crate) use crate::macros::*;
+mod edge_file_writer;
+mod hash;
+mod node_file_writer;
+mod preprocessing;
+mod subgraphs;
+mod trees;
+mod utilities;
+pub(crate) use crate::preprocessing::*;
+mod types;
+pub(crate) use crate::types::*;
+mod laplacian;
+mod operators;
+mod walks;
+
+// automatically generated files
+mod auto_generated_bindings;
+pub use auto_generated_bindings::*;
+mod method_names_list;
+pub use method_names_list::*;
---
+++ packages/ensmallen/ensmallen-0.6.5.tar.gz/ensmallen-0.6.5/src/utilities.rs
@@ -0,0 +1,36 @@
+use super::*;
+use graph::{NodeT, Result, WalksParameters, WeightT};
+
+use std::collections::HashMap;
+
+impl Graph {
+    pub(crate) fn build_walk_parameters(
+        &self,
+        walk_length: u64,
+        kwargs: &PyDict,
+    ) -> Result<WalksParameters> {
+        Ok(WalksParameters::new(walk_length)?
+            .set_change_edge_type_weight(extract_value_rust_result!(
+                kwargs,
+                "change_edge_type_weight",
+                WeightT
+            ))?
+            .set_change_node_type_weight(extract_value_rust_result!(
+                kwargs,
+                "change_node_type_weight",
+                WeightT
+            ))?
+            .set_explore_weight(extract_value_rust_result!(
+                kwargs,
+                "explore_weight",
+                WeightT
+            ))?
+            .set_return_weight(extract_value_rust_result!(kwargs, "return_weight", WeightT))?
+            .set_random_state(extract_value_rust_result!(kwargs, "random_state", usize))
+            .set_max_neighbours(extract_value_rust_result!(kwargs, "max_neighbours", NodeT))?
+            .set_iterations(extract_value_rust_result!(kwargs, "iterations", NodeT))?
+            .set_dense_node_mapping(
+                extract_value_rust_result!(kwargs, "dense_node_mapping", HashMap<NodeT, NodeT>),
+            ))
+    }
+}
