84d0f9f900fa1e78909424c9e6cc45e48db24ce2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:17 +0000

    Add package gscdk/0.1.4/gscdk-0.1.4.tar.gz

---
+++ packages/gscdk/gscdk-0.1.4.tar.gz/gscdk-0.1.4/setup.py
@@ -0,0 +1,34 @@
+
+import os
+import setuptools
+# from skbuild import setup
+from distutils.core import setup
+
+from distutils.sysconfig import get_python_lib
+import glob
+
+release_files = []
+for root, dirs, files in os.walk("pysrc/tinygo"):
+    for f in files:
+        release_files.append(os.path.join(root.replace('pysrc/', ''), f))    
+
+print(release_files)
+
+setup(
+    name="gscdk",
+    version="0.1.4",
+    description="Go Smart Contract Development Kit",
+    author='The UUOSIO Team',
+    license="BSD-3-Clause",
+    packages=['gscdk'],
+    package_dir={'gscdk': 'pysrc'},
+    package_data={
+#        "": ["*"],
+        'gscdk': release_files,
+    },
+    setup_requires=['wheel']
+    # scripts=['compiler/build/release/tinygo/bin/eosio-go'],
+    # install_requires=[
+    # ],
+    # include_package_data=True
+)
