db5c66bd7262a64a4b4f769ec201a71b94e83841
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:15 +0000

    Add package docker-farrukh90/0.0.41/docker-farrukh90-0.0.41.tar.gz

---
+++ packages/docker-farrukh90/docker-farrukh90-0.0.41.tar.gz/docker-farrukh90-0.0.41/src/dockers/containers/ticket1.py
@@ -0,0 +1,50 @@
+import sys
+import docker
+import os
+import time
+import subprocess
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+client = docker.from_env()
+container = client.containers.list(all=True, filters={'name':'jenkins'})
+
+CONTAINERSTATE=""
+PORT=""
+IMAGE=""
+
+for i in container:
+    DATA = i.attrs
+    IMAGE = str(i.image)
+    CONTAINERSTATE= DATA["State"]["Status"]
+    NETWORKDATA = DATA["NetworkSettings"]["Ports"]
+    PORT = NETWORKDATA["8080/tcp"][0]["HostPort"]
+
+
+
+if CONTAINERSTATE == "running":
+    print("Jenkins container is running")
+else:
+    print("Container might be named improperly or not running. Please rename it as jenkins")
+
+
+if PORT == "8080":
+    print("Jenkins container is running on the correct port")
+else:
+    print("Jenkins container is not running on the correct port")
+
+
+if "jenkins" in IMAGE:
+    print("Jenkins container is running with right image")
+else:
+    print("Jenkins container is not running with right image")
