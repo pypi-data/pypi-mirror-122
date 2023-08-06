395532d55627071259efa43f3e9a8ce5cc262bba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:42 +0000

    Add package py-dist/0.1/py_dist-0.1.tar.gz

---
+++ packages/py-dist/py_dist-0.1.tar.gz/py_dist-0.1/py_dist/example.py
@@ -0,0 +1,4 @@
+from py_dist import Gaussian
+gaussian_one = Gaussian(25, 2)
+print(f'Gaussian_one mean: {gaussian_one.mean}')
+print('new distribution info: ', (gaussian_one + gaussian_one).__repr__())
---
+++ packages/py-dist/py_dist-0.1.tar.gz/py_dist-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='py_dist',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['py_dist'],
+      author = 'Thuy Trinh',
+      author_email = 'thuytrinht4@gmail.com',
+      zip_safe=False)
