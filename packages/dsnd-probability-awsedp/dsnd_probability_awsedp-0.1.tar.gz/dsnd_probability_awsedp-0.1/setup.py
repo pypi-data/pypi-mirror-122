455d03d2c5fc2c14779f0782276adcf595c3a68f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:31 +0000

    Add package dsnd-probability-awsedp/0.1/dsnd_probability_awsedp-0.1.tar.gz

---
+++ packages/dsnd-probability-awsedp/dsnd_probability_awsedp-0.1.tar.gz/dsnd_probability_awsedp-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dsnd_probability_awsedp',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['dsnd_probability_awsedp'],
+      zip_safe=False)
