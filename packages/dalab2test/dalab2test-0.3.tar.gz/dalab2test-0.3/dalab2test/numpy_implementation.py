254db9af39e1b279f79f797f1d56b97c3020143d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:02 +0000

    Add package dalab2test/0.3/dalab2test-0.3.tar.gz

---
+++ packages/dalab2test/dalab2test-0.3.tar.gz/dalab2test-0.3/dalab2test/numpy_implementation.py
@@ -0,0 +1,16 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import numpy as np
+import numpy.ma
+
+
+def numpy_diag(diag):
+    return numpy.ma.diag(diag)
+
+
+def numpy_randint(min_value, max_value, num):
+    return np.random.randint(min_value, max_value, num)
+
+print(numpy_diag(1))
