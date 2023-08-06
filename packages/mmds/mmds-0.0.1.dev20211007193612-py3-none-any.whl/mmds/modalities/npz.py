a667473ff03f41c0c88b1dc15ae7791f157bc53d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:16 +0000

    Add package mmds/0.0.1.dev20211007193612/mmds-0.0.1.dev20211007193612-py3-none-any.whl

---
+++ packages/mmds/mmds-0.0.1.dev20211007193612-py3-none-any.whl/mmds/modalities/npz.py
@@ -0,0 +1,11 @@
+import attr
+import numpy as np
+
+from .modality import Modality
+
+
+@attr.define
+class NpzModality(Modality):
+    def _load_impl(self):
+        assert self.path, f"got {self.path}."
+        return np.load(self.path)["arr_0"]
