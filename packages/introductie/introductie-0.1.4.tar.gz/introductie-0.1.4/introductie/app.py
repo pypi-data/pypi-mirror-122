86eec6b62e7074a52ee8ddfb1402735a2cf98c5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:05 +0000

    Add package introductie/0.1.4/introductie-0.1.4.tar.gz

---
+++ packages/introductie/introductie-0.1.4.tar.gz/introductie-0.1.4/introductie/app.py
@@ -0,0 +1,5 @@
+from namen.app import resolve_name_by_index
+
+
+def say_hello_from_name(index):
+    return "Hello from " + resolve_name_by_index(index)
---
+++ packages/introductie/introductie-0.1.4.tar.gz/introductie-0.1.4/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['introductie']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['namen>=0.4.0,<0.5.0']
+
+setup_kwargs = {
+    'name': 'introductie',
+    'version': '0.1.4',
+    'description': '',
+    'long_description': None,
+    'author': 'Dennis van Bohemen',
+    'author_email': 'Dennis.Van.Bohemen@topicus.nl',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
