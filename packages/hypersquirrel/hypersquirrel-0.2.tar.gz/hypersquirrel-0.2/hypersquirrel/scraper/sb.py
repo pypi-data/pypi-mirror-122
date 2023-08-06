4b9c108ce6774abb9bc11f2ba603257ca5ec6161
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:16 +0000

    Add package hypersquirrel/0.2/hypersquirrel-0.2.tar.gz

---
+++ packages/hypersquirrel/hypersquirrel-0.2.tar.gz/hypersquirrel-0.2/hypersquirrel/scraper/sb.py
@@ -0,0 +1,21 @@
+from commmons import html
+from commmons.url import get_host_url
+
+
+def scrape(url):
+    tree = html.from_url(url)
+    host_url = get_host_url(url)
+
+    divs = tree.xpath("//div[contains(@class, 'video-item')]")
+    for div in divs:
+        if "data-id" in div.attrib:
+            dataid = div.attrib["data-id"]
+            atags = div.xpath("./a[@class='n']")
+            if atags:
+                img = div.xpath("./a/picture/img")[0]
+                yield {
+                    "fileid": "sb" + dataid,
+                    "sourceurl": host_url + atags[0].attrib["href"],
+                    "filename": img.attrib["alt"],
+                    "thumbnailurl": img.attrib["data-src"]
+                }
