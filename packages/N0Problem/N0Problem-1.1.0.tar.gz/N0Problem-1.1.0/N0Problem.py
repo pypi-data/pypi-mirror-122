288833eaf8e0e4252bee3571b6435841550be736
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:13 +0000

    Add package N0Problem/1.1.0/N0Problem-1.1.0.tar.gz

---
+++ packages/N0Problem/N0Problem-1.1.0.tar.gz/N0Problem-1.1.0/N0Problem.py
@@ -0,0 +1,33 @@
+class Answer:
+    def __init__(self,title,Description,author):
+        self.Title=title
+        self.Description=Description
+        self.Author=author
+    Challenges=[]
+    Responses=[]
+    def set(self,response,**kwargs):
+        self.Challenges.append(kwargs)
+        self.Responses.append(response)
+    
+    def check(self,expFunc):
+        correct=0
+        wrong=0
+        print("\n["+self.Title+"]")
+        print('"'+self.Description+'"\n')
+
+        for challengeId in range(0,len(self.Challenges)):
+            challenge=self.Challenges[challengeId]
+            #print(challenge)
+            res=expFunc(*challenge)
+            if res==self.Responses[challengeId]:
+                print("[Correct]:%s --> %s" % (challenge,res))
+                correct+=1
+            else:
+                print("[Wrong]:%s --> %s Answer: %s" % (challenge,res,self.Responses[challengeId]))
+                wrong+=1
+        print("\nCorrect: %s/%s" % (correct,len(self.Challenges)))
+        print("Wrong: %s/%s" % (wrong,len(self.Challenges)))
+        if wrong==0:
+            print("\nThis Challenge Has Been Solved By "+self.Author)
+        else:
+            print("\n"+self.Author+"! Try again!")
---
+++ packages/N0Problem/N0Problem-1.1.0.tar.gz/N0Problem-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='N0Problem',
+    version='1.1.0',
+    py_modules=["N0Problem"],
+    author='N0P3',
+    author_email='n0p3@qq.com',
+    description='For leetcode'
+)
