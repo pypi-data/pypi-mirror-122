5c47604739a4119a2351159ef952437e9f01a468
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:01 +0000

    Add package inspr/0.0.13/inspr-0.0.13.tar.gz

---
+++ packages/inspr/inspr-0.0.13.tar.gz/inspr-0.0.13/inspr/client.py
@@ -0,0 +1,71 @@
+import os
+import json
+import sys
+from flask import Flask, request
+from http import HTTPStatus
+from .rest import *
+from typing import Callable, Any
+
+SIDECAR_READ_PORT = "INSPR_SCCLIENT_READ_PORT"
+SIDECAR_WRITE_PORT = "INSPR_LBSIDECAR_WRITE_PORT"
+
+class Client:
+    def __init__(self) -> None:
+        self.read_port = os.getenv(SIDECAR_READ_PORT)
+        self.write_address = "http://localhost:" + str(os.getenv(SIDECAR_WRITE_PORT))
+        self.app = Flask(__name__)
+
+    def write_message(self, channel:str, msg) -> None:
+        msg_body = {
+            "data": msg
+        }
+        try:
+            send_post_request(self.write_address + "/channel/" + channel, msg_body)
+        except Exception as e:
+            print(f"Error while trying to write message: {e}")
+            raise Exception("failed to deliver message: channel: {}".format(channel))
+
+    def handle_channel(self, channel:str) -> Callable[[Callable[[Any], Any]], Callable[[Any], Any]]:
+        def wrapper(handle_func: Callable[[Any], Any]):
+            def route_func():
+                data = request.get_json(force=True)
+                try:
+                    handle_func(data["data"])
+                except:
+                    err = "Error handling message"
+                    return err, HTTPStatus.INTERNAL_SERVER_ERROR
+
+                return '', HTTPStatus.OK
+
+            self.app.add_url_rule("/channel/" + channel, endpoint = channel, view_func = route_func, methods=["POST"])
+            return handle_func
+        return wrapper
+    
+    def handle_route(self, path:str) -> Callable[[Callable[[Any], Any]], Callable[[Any], Any]]:
+        def wrapper(handle_func: Callable[[Any], Any]):
+            route = remove_prefix(path, '/')
+            self.app.add_url_rule("/route/" + route, endpoint = "/route/" + route, view_func = handle_func, methods=["GET", "DELETE", "POST", "PUT"])
+            return handle_func
+        return wrapper
+
+    def send_request(self, node_name:str, path:str, method:str, body) -> None:
+        try:
+            url = self.write_address + "/route/" + node_name + "/" + path
+            send_new_request(url, method, body)
+        except Exception as e:
+            print(f"Error while trying to send request: {e}")
+            raise Exception("failed to deliver message: route: {}".format(url))
+
+    def run(self) -> None:
+        links = []
+        for rule in self.app.url_map.iter_rules():
+            links.append(rule.endpoint)
+        print("registered routes =", links, file=sys.stderr)
+
+        self.app.run(port=self.read_port)
+
+
+def remove_prefix(text, prefix):
+    if text.startswith(prefix):
+        return text[len(prefix):]
+    return text
---
+++ packages/inspr/inspr-0.0.13.tar.gz/inspr-0.0.13/inspr/rest.py
@@ -0,0 +1,69 @@
+import requests
+import json
+import sys
+from flask.wrappers import Response
+from requests.models import HTTPError
+
+def send_post_request(url:str, body = {}, headers = {}) -> Response:
+    try:
+
+        print("AQUUUIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII")
+        print(url, file=sys.stderr)
+        print(body, file=sys.stderr)
+        print(headers, file=sys.stderr)
+
+        resp = requests.post(url, data = json.dumps(body), headers=headers)
+        resp.raise_for_status()
+    
+    except requests.exceptions.RequestException as e:
+        print(e, file=sys.stderr)
+        raise HTTPError
+    
+    return resp
+
+def send_update_request(url:str, body = {}, headers = {}) -> Response:
+    try:
+        resp = requests.put(url, data = json.dumps(body), headers=headers)
+        resp.raise_for_status()
+    
+    except requests.exceptions.RequestException as e:
+        print(e, file=sys.stderr)
+        raise HTTPError
+    
+    return resp
+
+def send_get_request(url:str, body = {}, headers = {}) -> Response:
+    try:
+        resp = requests.get(url, data=json.dumps(body), headers=headers)
+        resp.raise_for_status()
+    
+    except requests.exceptions.RequestException as e:
+        print(e, file=sys.stderr)
+        raise HTTPError
+    
+    return resp
+
+def send_delete_request(url:str, body = {}, headers = {}) -> Response:
+    try:
+        resp = requests.delete(url, data=json.dumps(body), headers=headers)
+        resp.raise_for_status()
+    
+    except requests.exceptions.RequestException as e:
+        print(e, file=sys.stderr)
+        raise HTTPError
+    
+    return resp
+
+
+def send_new_request(url:str, method:str, body = {}, headers = {}) -> Response:
+    if method == 'POST':
+        return send_post_request(url,body,headers)
+    elif method == 'GET':
+        return send_get_request(url,body,headers)
+    elif method == 'PUT':
+        return send_update_request(url,body,headers)
+    elif method == 'DELETE':
+        return send_delete_request(url,body,headers)
+    else:
+        raise Exception(f"Error while send request: Method '{method}' not allowed. The allowed methods are POST,GET,UPDATE,DELETE.")
+                
