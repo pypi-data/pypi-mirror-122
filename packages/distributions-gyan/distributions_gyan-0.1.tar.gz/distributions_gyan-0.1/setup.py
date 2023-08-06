b4a9841cb9fb7a3b6fff46caf6a467326ed19a4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:36 +0000

    Add package distributions-gyan/0.1/distributions_gyan-0.1.tar.gz

---
+++ packages/distributions-gyan/distributions_gyan-0.1.tar.gz/distributions_gyan-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_gyan',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_gyan'],
+      zip_safe=False)
