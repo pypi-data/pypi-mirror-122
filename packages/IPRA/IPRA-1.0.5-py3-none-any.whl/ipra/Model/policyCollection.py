e39cbebda6cafcfa4640d865c57b2cc33a53d655
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:59 +0000

    Add package IPRA/1.0.5/IPRA-1.0.5-py3-none-any.whl

---
+++ packages/IPRA/IPRA-1.0.5-py3-none-any.whl/ipra/Model/policyCollection.py
@@ -0,0 +1,42 @@
+
+class PolicyCollection:
+        
+    def __init__(self):
+        self.AIA = ['AIA']
+        self.AXA = ['AXA']
+        self.BOCG = ['BOCG']
+        self.CHINALIFE = ['CHINA LIFE']
+        self.PRU = ['PRU']
+        self.FWD = ['FWD']
+        self.totalInsurranceList = [
+            self.AIA,
+            self.AXA,
+            self.BOCG,
+            self.CHINALIFE,
+            self.PRU,
+            self.FWD,
+        ]
+        self.supportedList = ['AIA','AXA','BOCG','CHINA LIFE','PRU','FWD']
+        pass
+    
+    def cleanAllPolicy(self):
+        self.AIA = ['AIA']
+        self.AXA = ['AXA']
+        self.BOCG = ['BOCG']
+        self.CHINALIFE = ['CHINA LIFE']
+        self.PRU = ['PRU']
+        self.FWD = ['FWD']
+        self.totalInsurranceList = [
+            self.AIA,
+            self.AXA,
+            self.BOCG,
+            self.CHINALIFE,
+            self.PRU,
+            self.FWD,
+        ]
+    
+    def getTotalList(self):
+        return self.totalInsurranceList
+
+    def getSupportedList(self):
+        return self.supportedList
