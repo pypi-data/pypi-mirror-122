8a9803461658f9006bdd4989199ae4ea56f39daf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:37 +0000

    Add package bethicfuncs/0.0.2/bethicfuncs-0.0.2-py3-none-any.whl

---
+++ packages/bethicfuncs/bethicfuncs-0.0.2-py3-none-any.whl/bethic/utils.py
@@ -0,0 +1,18 @@
+def flatten_json(nested_json, exclude=['']):
+
+    out = {}
+
+    def flatten(x, name='', exclude=exclude):
+        if type(x) is dict:
+            for a in x:
+                if a not in exclude: flatten(x[a], name + a + '_')
+        elif type(x) is list:
+            i = 0
+            for a in x:
+                flatten(a, name + str(i) + '_')
+                i += 1
+        else:
+            out[name[:-1]] = x
+
+    flatten(nested_json)
+    return out
