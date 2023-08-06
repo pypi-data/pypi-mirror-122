9ef74eb0ec364eb453ecb4d2a61e0785f82166f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:41:40 +0000

    Add package openap/1.2/openap-1.2-py3-none-any.whl

---
+++ packages/openap/openap-1.2-py3-none-any.whl/openap/casadi/numpy_override.py
@@ -0,0 +1,8 @@
+from numpy import *
+from casadi import *  # this override the previous
+
+# numpy functions -> casadi functions
+where = casadi.if_else
+maximum = casadi.fmax
+minimum = casadi.fmin
+interp = lambda x, xp, yp: casadi.interpolant("LUT", "linear", [xp], yp)(x)
