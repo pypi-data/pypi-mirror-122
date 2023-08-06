0b26fadda5c3e9ec063e9b7139422323c08c2ec1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:31 +0000

    Add package inspr/0.0.15/inspr-0.0.15.tar.gz

---
+++ packages/inspr/inspr-0.0.15.tar.gz/inspr-0.0.15/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="inspr",
+    version="0.0.15",
+    author="Inspr LLC",
+    description="This module define APIs to interact with the Inspr environment",
+    long_description=long_description,      # Long description read from the the readme file
+    long_description_content_type="text/markdown",
+    python_requires='>=3.8',
+    packages=[
+        "inspr",
+        "inspr.controller"
+    ],
+    requires=[
+        "requests",
+        "flask"
+    ],
+    install_requires=[
+        "requests",
+        "flask"
+    ]
+)
