a1a88a835a5f8a83c305c75448b4fd346b4a6182
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:52 +0000

    Add package estipy/0.0.1/estipy-0.0.1.tar.gz

---
+++ packages/estipy/estipy-0.0.1.tar.gz/estipy-0.0.1/src/estipy/main.py
@@ -0,0 +1,10 @@
+import time
+from src.estipy.eta import ETA
+
+# Some data to be processed
+data = list(range(42))
+
+for num, _ in ETA(data):
+    # Do something useful here that takes a little time
+    print(num)
+    time.sleep(0.1)
