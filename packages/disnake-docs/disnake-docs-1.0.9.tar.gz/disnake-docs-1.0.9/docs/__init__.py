9fda6eafc2145026d92ae8765f8ce141a640c4b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:57 +0000

    Add package disnake-docs/1.0.9/disnake-docs-1.0.9.tar.gz

---
+++ packages/disnake-docs/disnake-docs-1.0.9.tar.gz/disnake-docs-1.0.9/docs/__init__.py
@@ -0,0 +1,18 @@
+from typing import Union
+
+from disnake import Client
+from disnake.ext.commands import Bot
+
+__version__ = '1.0.9'
+
+MAX_SIGNATURE_AMOUNT = 3
+PRIORITY_PACKAGES = (
+    "python",
+)
+NAMESPACE = "doc"
+
+
+def setup(bot: Union[Client, Bot]) -> None:
+    """Load the Doc cog."""
+    from .cog import Docs
+    bot.add_cog(Docs(bot))
