fd85d601b8dcb1cc8103c79e4c7b8c3ac9757d04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:45:30 +0000

    Add package Colorful-Distributions/0.1/Colorful_Distributions-0.1.tar.gz

---
+++ packages/Colorful-Distributions/Colorful_Distributions-0.1.tar.gz/Colorful_Distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='Colorful_Distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['Colorful_Distributions'],
+      zip_safe=False)
