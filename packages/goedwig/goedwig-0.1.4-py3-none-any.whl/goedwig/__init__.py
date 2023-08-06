798b181617e7506c283243c5aea551628cecfead
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:21 +0000

    Add package goedwig/0.1.4/goedwig-0.1.4-py3-none-any.whl

---
+++ packages/goedwig/goedwig-0.1.4-py3-none-any.whl/goedwig/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Importable package definitions.
+"""
+
+from .cypher import CypherItem
+from .query import Query
+from .version import __version__
