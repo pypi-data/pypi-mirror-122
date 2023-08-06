f6e6f8ee657f99504b4009455e31bd673ec18270
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:45:09 +0000

    Add package siriuscommon/2.1.0/siriuscommon-2.1.0.tar.gz

---
+++ packages/siriuscommon/siriuscommon-2.1.0.tar.gz/siriuscommon-2.1.0/test/test_spreadsheet.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import os
+import unittest
+
+import conscommon.spreadsheet.parser as spreadsheet_parser
+from conscommon.spreadsheet import SheetName
+
+
+class TestDataModel(unittest.TestCase):
+    def test_spreadsheetParser(self):
+        spreadsheet_path = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)), "Redes e Beaglebones.xlsx"
+        )
+        self.assertIs(os.path.exists(spreadsheet_path), True)
+        data = spreadsheet_parser.loadSheets(spreadsheet_path)
+
+        self.assertIsNot(data[SheetName.AGILENT].__len__(), 0)
+        self.assertIsNot(data[SheetName.MKS].__len__(), 0)
+        self.assertIsNot(data[SheetName.MBTEMP].__len__(), 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
