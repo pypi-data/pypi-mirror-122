5e098484a494129b46295388dc8ee152fc5170e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:38 +0000

    Add package sphinx-exercise/0.2.1/sphinx_exercise-0.2.1-py3-none-any.whl

---
+++ packages/sphinx-exercise/sphinx_exercise-0.2.1-py3-none-any.whl/sphinx_exercise/directive.py
@@ -0,0 +1,144 @@
+"""
+sphinx_exercise.directive
+~~~~~~~~~~~~~~~~~~~~~~~~~
+
+A custom Sphinx Directive
+
+:copyright: Copyright 2020 by the QuantEcon team, see AUTHORS
+:licences: see LICENSE for details
+"""
+from typing import List
+from docutils.nodes import Node
+
+from sphinx.util.docutils import SphinxDirective
+from docutils.parsers.rst import directives
+from .nodes import exercise_node, exercise_unenumerable_node, solution_node
+from docutils import nodes
+from sphinx.util import logging
+
+logger = logging.getLogger(__name__)
+
+
+class CustomDirective(SphinxDirective):
+    """ A custom Sphinx directive """
+
+    name = ""
+
+    def run(self) -> List[Node]:
+        env = self.env
+        typ = self.name
+        if typ == "solution" and env.app.config.hide_solutions:
+            return []
+
+        serial_no = env.new_serialno()
+
+        if not hasattr(env, "exercise_list"):
+            env.exercise_list = {}
+
+        classes, class_name = [typ], self.options.get("class", "")
+        if class_name:
+            classes.extend(class_name)
+
+        # Have a dummy title text if no title specified, as 'std' domain needs
+        # a title to process it as enumerable node.
+        if typ == "exercise":
+            title_text = f"{self.name.title()} "
+
+            if self.arguments != []:
+                title_text = f"({self.arguments[0]})"
+        else:
+            title_text = f"{self.name.title()} to "
+            target_label = self.arguments[0]
+
+        # selecting the type of node
+        if typ == "exercise":
+            if "nonumber" in self.options:
+                node = exercise_unenumerable_node()
+            else:
+                node = exercise_node()
+        else:
+            node = solution_node()
+
+        # state parsing
+        section = nodes.section(ids=[f"{typ}-content"])
+        textnodes, messages = self.state.inline_text(title_text, self.lineno)
+        self.state.nested_parse(self.content, self.content_offset, section)
+
+        node += nodes.title(title_text, "", *textnodes)
+        node += section
+
+        label = self.options.get("label", "")
+        if label:
+            self.options["noindex"] = False
+        else:
+            self.options["noindex"] = True
+            label = f"{env.docname}-{typ}-{serial_no}"
+
+        # Duplicate label warning
+        if not label == "" and label in env.exercise_list.keys():
+            docpath = env.doc2path(env.docname)
+            path = docpath[: docpath.rfind(".")]
+            other_path = env.doc2path(env.exercise_list[label]["docname"])
+            msg = f"duplicate label: {label}; other instance in {other_path}"
+            logger.warning(msg, location=path, color="red")
+            return []
+
+        self.options["name"] = label
+
+        # Set node attributes
+        node["classes"].extend(classes)
+        node["ids"].append(label)
+        node["label"] = label
+        node["docname"] = env.docname
+        node["title"] = title_text
+        node["type"] = typ
+        node["hidden"] = True if "hidden" in self.options else False
+        node.document = self.state.document
+
+        if typ == "solution":
+            node["target_label"] = target_label
+
+        self.add_name(node)
+        env.exercise_list[label] = {
+            "type": typ,
+            "docname": env.docname,
+            "node": node,
+            "title": title_text,
+            "hidden": node.get("hidden", bool),
+        }
+
+        if node.get("hidden", bool):
+            return []
+
+        return [node]
+
+
+class ExerciseDirective(CustomDirective):
+    """ A custom exercise directive """
+
+    name = "exercise"
+    has_content = True
+    required_arguments = 0
+    optional_arguments = 1
+    final_argument_whitespace = True
+    option_spec = {
+        "label": directives.unchanged_required,
+        "class": directives.class_option,
+        "nonumber": directives.flag,
+        "hidden": directives.flag,
+    }
+
+
+class SolutionDirective(CustomDirective):
+    """ A custom solution directive """
+
+    name = "solution"
+    has_content = True
+    required_arguments = 1
+    optional_arguments = 0
+    final_argument_whitespace = False
+    option_spec = {
+        "label": directives.unchanged_required,
+        "class": directives.class_option,
+        "hidden": directives.flag,
+    }
---
+++ packages/sphinx-exercise/sphinx_exercise-0.2.1-py3-none-any.whl/sphinx_exercise/nodes.py
@@ -0,0 +1,150 @@
+"""
+sphinx_exercise.nodes
+~~~~~~~~~~~~~~~~~~~~~
+
+Enumerable and unenumerable nodes
+
+:copyright: Copyright 2020 by the QuantEcon team, see AUTHORS
+:licences: see LICENSE for details
+"""
+from docutils.nodes import Node
+from sphinx.util import logging
+from docutils import nodes as docutil_nodes
+from sphinx.writers.latex import LaTeXTranslator
+from .utils import get_node_number, find_parent, list_rindex
+
+logger = logging.getLogger(__name__)
+
+CR = "\n"
+latex_admonition_start = CR + "\\begin{sphinxadmonition}{note}"
+latex_admonition_end = "\\end{sphinxadmonition}" + CR
+
+
+class exercise_node(docutil_nodes.Admonition, docutil_nodes.Element):
+    pass
+
+
+class solution_node(docutil_nodes.Admonition, docutil_nodes.Element):
+    pass
+
+
+class exercise_unenumerable_node(docutil_nodes.Admonition, docutil_nodes.Element):
+    pass
+
+
+def _visit_nodes_latex(self, node, find_parent):
+    """ Function to handle visit_node for latex. """
+    docname = find_parent(self.builder.env, node, "section")
+    self.body.append(
+        "\\phantomsection \\label{" + f"{docname}:{node.attributes['label']}" + "}"
+    )
+    self.body.append(latex_admonition_start)
+
+
+def _depart_nodes_latex(self, node, title, pop_index=False):
+    """ Function to handle depart_node for latex. """
+    idx = list_rindex(self.body, latex_admonition_start) + 2
+    if pop_index:
+        self.body.pop(idx)
+    self.body.insert(idx, title)
+    self.body.append(latex_admonition_end)
+
+
+def _remove_placeholder_title_exercise(typ, node):
+    """ Removing the exercise placeholder we put in title earlier."""
+    for title in node.traverse(docutil_nodes.title):
+        if typ.title() in title.astext():
+            title[0] = docutil_nodes.Text("")
+
+
+def visit_enumerable_node(self, node: Node) -> None:
+    typ = node.attributes.get("type", "")
+    if isinstance(self, LaTeXTranslator):
+        _remove_placeholder_title_exercise(typ, node)
+        _visit_nodes_latex(self, node, find_parent)
+    else:
+        _remove_placeholder_title_exercise(typ, node)
+        self.body.append(self.starttag(node, "div", CLASS="admonition"))
+
+
+def depart_enumerable_node(self, node: Node) -> None:
+    typ = node.attributes.get("type", "")
+    if isinstance(self, LaTeXTranslator):
+        number = get_node_number(self, node, typ)
+        _depart_nodes_latex(self, node, f"{typ.title()} {number} ")
+    else:
+        number = get_node_number(self, node, typ)
+        idx = list_rindex(self.body, f"{typ.title()} {number} ")
+        self.body[idx] = f"{typ.title()} {number} "
+        self.body.append("</div>")
+
+
+def visit_exercise_unenumerable_node(self, node: Node) -> None:
+    typ = node.attributes.get("type", "")
+    if isinstance(self, LaTeXTranslator):
+        _remove_placeholder_title_exercise(typ, node)
+        _visit_nodes_latex(self, node, find_parent)
+    else:
+        _remove_placeholder_title_exercise(typ, node)
+        self.body.append(self.starttag(node, "div", CLASS="admonition"))
+
+
+def depart_exercise_unenumerable_node(self, node: Node) -> None:
+    typ = node.attributes.get("type", "")
+    if isinstance(self, LaTeXTranslator):
+        _depart_nodes_latex(self, node, f"{typ.title()} ")
+    else:
+        idx = list_rindex(self.body, '<p class="admonition-title">') + 1
+        element = f"<span>{typ.title()} </span>"
+        self.body.insert(idx, element)
+        self.body.append("</div>")
+
+
+def visit_solution_node(self, node: Node) -> None:
+    if isinstance(self, LaTeXTranslator):
+        _visit_nodes_latex(self, node, find_parent)
+    else:
+        self.body.append(self.starttag(node, "div", CLASS="admonition"))
+
+
+def depart_solution_node(self, node: Node) -> None:
+    typ = node.attributes.get("type", "")
+    if isinstance(self, LaTeXTranslator):
+        _depart_nodes_latex(self, node, f"{typ.title()} to ", True)
+    else:
+        number = get_node_number(self, node, typ)
+        idx = list_rindex(self.body, f"{typ.title()} {number} ")
+        self.body.pop(idx)
+        self.body.append("</div>")
+
+
+def is_exercise_node(node):
+    return isinstance(node, exercise_node)
+
+
+def is_exercise_unenumerable_node(node):
+    return isinstance(node, exercise_unenumerable_node)
+
+
+def is_solution_node(node):
+    return isinstance(node, solution_node)
+
+
+def is_extension_node(node):
+    return (
+        is_exercise_node(node)
+        or is_exercise_unenumerable_node(node)
+        or is_solution_node(node)
+    )
+
+
+def rreplace(s, old, new, occurrence):
+    # taken from https://stackoverflow.com/a/2556252
+    li = s.rsplit(old, occurrence)
+    return new.join(li)
+
+
+NODE_TYPES = {
+    "exercise": {"node": exercise_node, "type": "exercise"},
+    "solution": {"node": solution_node, "type": "solution"},
+}
