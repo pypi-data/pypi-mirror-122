f2d7c2762cb27621ac86febb9a64637464564db2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:15 +0000

    Add package tweet-requester/0.0.1.5/tweet-requester-0.0.1.5.tar.gz

---
+++ packages/tweet-requester/tweet-requester-0.0.1.5.tar.gz/tweet-requester-0.0.1.5/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+setup(
+  install_requires = [
+    "ipython>=7.25.0",
+    "google-cloud-translate>=3.3.1",
+    "pandas",
+  ],
+  classifiers = [
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent'
+  ]
+)
---
+++ packages/tweet-requester/tweet-requester-0.0.1.5.tar.gz/tweet-requester-0.0.1.5/src/tweet_requester/__init__.py
@@ -0,0 +1,10 @@
+from .display import \
+    JsonLInteractiveClassifier, \
+    TweetInteractiveClassifier, \
+    prepare_google_credentials
+from .analysis import \
+    TweetAnalyzer,TweetMedia, TweetPhoto, TweetVideo
+from .session import TSess
+from .cache import Cache
+
+__version__="0.0.1.5"
