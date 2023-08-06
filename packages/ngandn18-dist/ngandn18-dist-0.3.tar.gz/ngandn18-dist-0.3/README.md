6b18241aeb5643842ef5f62e62040b78f14335c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:26 +0000

    Add package ngandn18-dist/0.3/ngandn18-dist-0.3.tar.gz

---
+++ packages/ngandn18-dist/ngandn18-dist-0.3.tar.gz/ngandn18-dist-0.3/README.md
@@ -0,0 +1,7 @@
+# Example Package
+
+This is a simple example package.
+My first test package.
+## Contains
+1. Gaussian distribution.
+2. Binomial distribution. 
---
+++ packages/ngandn18-dist/ngandn18-dist-0.3.tar.gz/ngandn18-dist-0.3/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='ngandn18-dist',
+      version='0.3',
+      description='Gaussian & Binomial distributions',
+      packages=['distributions'],
+      zip_safe=False)
