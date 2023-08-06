01efd22218968cec56b32e154766df97308e7003
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:31 +0000

    Add package tracardi-plugin-sdk/0.6.15/tracardi_plugin_sdk-0.6.15-py3-none-any.whl

---
+++ packages/tracardi-plugin-sdk/tracardi_plugin_sdk-0.6.15-py3-none-any.whl/tracardi_plugin_sdk/domain/register.py
@@ -0,0 +1,63 @@
+from typing import List, Optional
+from pydantic import BaseModel
+
+
+class FieldValidation(BaseModel):
+    regex: str
+    message: str
+
+
+class Component(BaseModel):
+    type: str = 'text'
+    props: Optional[dict] = {}
+
+
+class FormFields(BaseModel):
+    id: str
+    name: str
+    description: Optional[str]
+    component: Component
+    validation: Optional[FieldValidation]
+
+
+class FormGroup(BaseModel):
+    name: Optional[str]
+    description: Optional[str]
+    fields: List[FormFields]
+
+
+class Form(BaseModel):
+    title: str
+    groups: List[FormGroup]
+
+
+class Spec(BaseModel):
+    className: str
+    module: str
+    inputs: Optional[List[str]] = []
+    outputs: Optional[List[str]] = []
+    init: Optional[dict] = None
+    form: Optional[Form]
+    manual: Optional[str] = None
+    author: Optional[str] = None
+    license: Optional[str] = "MIT"
+    version: Optional[str] = '0.0.1'
+
+
+class MetaData(BaseModel):
+    name: str
+    desc: Optional[str] = ""
+    keywords: Optional[List[str]] = []
+    type: str
+    width: int
+    height: int
+    icon: str
+    editor: str = 'json'
+    group: Optional[List[str]] = ["General"]
+
+
+class Plugin(BaseModel):
+    start: bool = False
+    debug: bool = False
+    spec: Spec
+    metadata: MetaData
