87f07499960bad5e43c9342cf431327cd59b9b22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:45:27 +0000

    Add package em-distributions/1.0/em_distributions-1.0.tar.gz

---
+++ packages/em-distributions/em_distributions-1.0.tar.gz/em_distributions-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='em_distributions',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['em_distributions'],
+      author = 'Emilio Mantilla',
+      author_email = 'emiliomantilla25@gmail.com',
+      zip_safe=False)
