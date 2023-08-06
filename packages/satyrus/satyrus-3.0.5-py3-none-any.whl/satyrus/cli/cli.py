7da80bc05e7a382106c3bd9055cd2740883d159a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:59 +0000

    Add package satyrus/3.0.5/satyrus-3.0.5-py3-none-any.whl

---
+++ packages/satyrus/satyrus-3.0.5-py3-none-any.whl/satyrus/cli/cli.py
@@ -0,0 +1,338 @@
+"""\
+Satyrus Compiler v{version}
+"""
+from __future__ import annotations
+
+__version__ = "3.0.5    "
+
+## Standard Library
+import argparse
+import json
+from pathlib import Path
+from functools import wraps
+from gettext import gettext
+
+## Third-Party
+from cstream import Stream, stdlog, stdwar, stderr, stdout
+
+## Local
+from .help import HELP
+from ..api import SatAPI
+from ..error import EXIT_SUCCESS, EXIT_FAILURE
+from ..assets import SAT_BANNER, SAT_CRITICAL
+from ..satlib import Timing, log
+
+
+def load_json(parser, path: Path) -> object:
+    if not path.exists() or not path.is_file():
+        parser.error(f"File '{path}' doesn't exists")
+
+    with path.open(mode="r") as file:
+        try:
+            data = json.load(file)
+        except json.JSONDecodeError as error:
+            parser.error(f"In '{path}':\n{error.msg}")
+        else:
+            return data
+
+
+class ArgParser(argparse.ArgumentParser):
+    @wraps(argparse.ArgumentParser.__init__)
+    def __init__(self, *args, **kwargs):
+        self._rank = {}
+        argparse.ArgumentParser.__init__(self, *args, **kwargs)
+
+    @wraps(argparse.ArgumentParser.parse_args)
+    def parse_args(self, args: list = None, namespace: argparse.Namespace = None):
+        args = argparse.ArgumentParser.parse_args(self, args, namespace)
+
+        for p in sorted(self._rank.keys()):
+            for action, params in self._rank[p]:
+                action(*params)
+
+        return args
+
+    def print_help(self, from_help: bool = True):
+        if from_help:
+            stdlog[0] << SAT_BANNER
+        argparse.ArgumentParser.print_help(self, stdlog[0])
+
+    def error(self, message: str, code: int = 1):
+        stderr << f"Error: {gettext(message)}"
+        self.print_help(from_help=False)
+        exit(code)
+
+    @classmethod
+    def enqueue(cls, priority: int = 0):
+        def decor(action):
+            @wraps(action)
+            def enqueued_action(self, parser, namespace, values, option_string=None):
+                if isinstance(parser, cls):
+                    params = (self, parser, namespace, values, option_string)
+                    if priority in parser._rank:
+                        parser._rank[priority].append((action, params))
+                    else:
+                        parser._rank[priority] = [(action, params)]
+                else:
+                    return action(self, parser, namespace, values, option_string)
+
+            return enqueued_action
+
+        return decor
+
+
+class GetVersion(argparse._VersionAction):
+    @wraps(argparse._VersionAction.__init__)
+    def __init__(self, *args, **kwargs):
+        argparse._VersionAction.__init__(self, *args, **kwargs)
+
+    @wraps(argparse._VersionAction.__call__)
+    def __call__(self, parser, namespace, values, option_string=None):
+        from sys import version_info
+
+        stdout[0] << f"satyrus {__version__} (python {version_info.major}.{version_info.minor})"
+        exit(0)
+
+
+class SetVerbosity(argparse._StoreAction):
+    @wraps(argparse._StoreAction.__init__)
+    def __init__(self, *args, **kwargs):
+        argparse._StoreAction.__init__(self, *args, **kwargs)
+
+    @ArgParser.enqueue(0)
+    @wraps(argparse._StoreAction.__call__)
+    def __call__(self, parser, namespace, values, option_string=None):
+        argparse._StoreAction.__call__(self, parser, namespace, values, option_string)
+        Stream.set_lvl(getattr(namespace, self.dest))
+
+
+class DebugMode(argparse._StoreTrueAction):
+    @wraps(argparse._StoreTrueAction.__init__)
+    def __init__(self, *args, **kwargs):
+        argparse._StoreTrueAction.__init__(self, *args, **kwargs)
+
+    @ArgParser.enqueue(1)
+    @wraps(argparse._StoreTrueAction.__call__)
+    def __call__(self, parser, namespace, values, option_string=None):
+        argparse._StoreTrueAction.__call__(self, parser, namespace, values, option_string)
+        setattr(namespace, "verbose", 3)
+        Stream.set_lvl(3)
+
+
+class IncludeSatAPI(argparse._StoreAction):
+    @wraps(argparse._StoreAction.__init__)
+    def __init__(self, *args, **kwargs):
+        argparse._StoreAction.__init__(self, *args, **kwargs)
+
+    @ArgParser.enqueue(2)
+    @wraps(argparse._StoreAction.__call__)
+    def __call__(self, parser, namespace, values, option_string=None):
+        argparse._StoreAction.__call__(self, parser, namespace, values, option_string)
+        fname = getattr(namespace, self.dest)
+        if fname is not None:
+            SatAPI.include(str.strip(fname))
+
+
+class SetSolver(argparse._StoreAction):
+    @wraps(argparse._StoreAction.__init__)
+    def __init__(self, *args, **kwargs):
+        argparse._StoreAction.__init__(self, *args, **kwargs)
+
+    @ArgParser.enqueue(3)
+    @wraps(argparse._StoreAction.__call__)
+    def __call__(self, parser: ArgParser, namespace, values, option_string=None):
+        argparse._StoreAction.__call__(self, parser, namespace, values, option_string)
+        solver = str.strip(getattr(namespace, self.dest))
+        if solver not in SatAPI.options():
+            parser.error(f"Invalid solver choice '{solver}' (choose from {SatAPI.options()!r})")
+
+
+class readParams(argparse._StoreAction):
+    @wraps(argparse._StoreAction.__init__)
+    def __init__(self, *args, **kwargs):
+        argparse._StoreAction.__init__(self, *args, **kwargs)
+
+    @ArgParser.enqueue(4)
+    @wraps(argparse._StoreAction.__call__)
+    def __call__(self, parser: ArgParser, namespace, values, option_string=None):
+        argparse._StoreAction.__call__(self, parser, namespace, values, option_string)
+        params = getattr(namespace, self.dest)
+
+        print("PARAMS:", params)
+
+        if params is None:
+            stdwar[1] << "Warning: No solver parameters passed."
+            setattr(namespace, self.dest, {})
+
+        params_path = Path(params)
+
+        if isinstance(params, dict):
+            setattr(namespace, self.dest, params)
+        else:
+            parser.error(f"In '{params_path}':\nParameter object must be mapping, not '{type(params)}'")
+
+
+class SatCLI:
+    r"""
+      Satyrus Stack
+      ┌───────────┐
+    │ │  SatCLI   │
+    │ ├───────────┤
+    │ │  SatAPI   │
+    │ ├───────────┤
+    │ │  Satyrus  │
+    │ ├───────────┤
+    │ │SatCompiler│
+    │ ├───────────┤
+    ▼ │ SatParser │
+      └───────────┘
+    """
+
+    @classmethod
+    def infer_output(cls, solver: SatAPI.SatAPISolver, args: argparse.Namespace, answer: tuple[dict, float] | str | object) -> tuple[str, str]:
+        if answer is None:
+            exit(EXIT_FAILURE)
+
+        if SatAPI.complete(answer):
+            answer = json.dumps({"solution": answer[0], "energy": answer[1]}, indent=4)
+            output = f"{Path(args.source[0]).stem}.{solver.ext}"
+        else:
+            answer = str(answer)
+            output = f"{Path(args.source[0]).stem}.{solver.ext}"
+
+        if args.output is not None:
+            output = args.output
+
+        return output, answer
+
+    @classmethod
+    def run(cls, argv: list = None):
+        """
+        Parameters
+        ----------
+
+        source : str
+            Path to source code.
+        -o, --out : str
+            Output Destination.
+        --legacy : bool
+            If True, uses the legacy parser.
+        -v, --verbose : int
+            Compiler verbosity level.
+        -d, --debug : bool
+            If True, enables debug mode.
+        -a, --api : str
+            If present, include solver interfaces defined in python file.
+        -p, --params : str
+            Path to JSON file containing parameters for passing to Solver API.
+        """
+
+        parser = ArgParser(prog="satyrus", description=__doc__.format(version=__version__))
+
+        ## Mandatory - Source file path
+        parser.add_argument("source", help=HELP["source"], nargs="+")
+
+        # Optional - Compiler Version
+        parser.add_argument("--version", help=HELP["version"], action=GetVersion)
+
+        ## Optional - Compiler Verbosity
+        parser.add_argument(
+            "-v",
+            "--verbose",
+            type=int,
+            dest="verbose",
+            choices=[0, 1, 2, 3],
+            default=1,
+            help=HELP["verbose"],
+            action=SetVerbosity,
+        )
+
+        # Optional - Debug Mode
+        parser.add_argument("-d", "--debug", dest="debug", help=argparse.SUPPRESS, action=DebugMode)
+
+        # Optional - Augmented API
+        parser.add_argument("-a", "--api", type=str, dest="api", help=HELP["api"], action=IncludeSatAPI)
+
+        # Optional - Solver Option
+        parser.add_argument(
+            "-s",
+            "--solver",
+            type=str,
+            dest="solver",
+            help=HELP["solver"],
+            default="text",
+            action=SetSolver,
+        )
+
+        # Optional - Output File
+        parser.add_argument(
+            "-o",
+            "--output",
+            type=str,
+            dest="output",
+            help=HELP["output"],
+            action="store",
+        )
+
+        # Optional - Legacy Syntax
+        parser.add_argument("-l", "--legacy", dest="legacy", action="store_true", help=HELP["legacy"])
+
+        # Optional - Timing Report
+        parser.add_argument("-r", "--report", dest="report", action="store_true", help=HELP["report"])
+
+        # Optional - Solver parameters
+        parser.add_argument("-p", "--params", dest="params", help=HELP["params"], action=readParams)
+
+        # Set base output verbosity level
+        Stream.set_lvl(1)
+
+        if argv is None:
+            args = parser.parse_args()
+        else:
+            args = parser.parse_args(argv)
+
+        # Exhibits Compiler Command line arguments
+        stdlog[3] << f'Command line args:\n{";".join(f"{k}={v!r}" for k, v in vars(args).items())}'
+
+        # Warning: Debug mode
+        if args.debug:
+            stdwar[1] << f"Warning: Debug mode enabled."
+
+        # Warning: Legacy mode
+        if args.legacy:
+            stdwar[1] << f"Warning: Parser is running in legacy mode."
+
+        try:
+            # Launch API
+            solver = SatAPI(*args.source, legacy=args.legacy)[args.solver]
+
+            # Solve in desired way
+            if args.params is None:
+                answer: tuple[dict, float] | str | object = solver.solve()
+            else:
+                answer: tuple[dict, float] | str | object = solver.solve(**args.params)
+
+            # Retrieve Output Destination
+            args.output, answer = cls.infer_output(solver, args, answer)
+
+            with open(args.output, mode="w") as file:
+                file.write(answer)
+
+            # Report
+            if args.report:
+                Timing.timer.show_report(level=0)
+
+            exit(EXIT_SUCCESS)
+        except RuntimeError as exc:
+            stderr[0] << exc
+            exit(EXIT_FAILURE)
+        except Exception:
+            trace = log(target="satyrus.log")
+            if args.debug:
+                stderr[0] << trace
+            else:
+                stderr[0] << SAT_CRITICAL
+            exit(EXIT_FAILURE)
+
+
+__all__ = ["SatCLI"]
---
+++ packages/satyrus/satyrus-3.0.5-py3-none-any.whl/satyrus/satlib/posiform.py
@@ -0,0 +1,413 @@
+""""""
+# Future
+from __future__ import annotations
+
+# Standard Library
+import json
+import numbers
+
+# Third-Party
+import numpy as np
+
+
+class Posiform(dict):
+    r"""
+    This object is intended to represent a sum of products under a psudo-boolean domain.
+
+    Assumptions
+    -----------
+    1. Every variable is boolean (i.e. $x \in \{0, 1\}$)
+    2. Posiform is a mutable type.
+    """
+
+    def __init__(self, buffer: dict | float = None):
+        """\
+        Parameters
+        ----------
+        buffer : dict
+            Dictionary containing pairs (variables, constant).
+        """
+        if buffer is not None:
+            if isinstance(buffer, dict):
+                __buffer = {}
+                for k, v in buffer.items():
+                    if not isinstance(v, numbers.Number) or isinstance(v, complex):
+                        raise TypeError("Posiform buffer values must be real numbers")
+                    elif float(v) == 0.0:
+                        continue
+
+                    if k is None:
+                        __buffer[k] = float(v)
+                    else:
+                        if not isinstance(k, (tuple, set, frozenset)) or not all(isinstance(s, str) for s in k):
+                            raise TypeError("Posiform buffer keys must be 'None' or be of type 'tuple' or 'set' and contain only 'strings'.")
+                        else:
+                            __buffer[frozenset(map(str, k))] = float(v)
+            elif isinstance(buffer, numbers.Number):
+                if buffer == 0.0:
+                    __buffer = {}
+                else:
+                    __buffer = {None: float(buffer)}
+            else:
+                raise TypeError(f"Posiform buffer must be of type 'dict', 'float', 'int' or 'None', not '{type(buffer)}'")
+        else:
+            __buffer = {}
+        dict.__init__(self, __buffer)
+        self.__aux = 0
+
+    def __bool__(self) -> bool:
+        return len(self) > 0
+
+    def __str__(self) -> str:
+        if self:
+            terms = []
+            for k, v in self:
+                if k is None:
+                    if v >= 0.0:
+                        terms.extend(["+", str(v)])
+                    else:
+                        terms.extend(["-", str(abs(v))])
+                else:
+                    if v == 1.0:
+                        terms.extend(["+", " * ".join(sorted(k))])
+                    elif v == -1.0:
+                        terms.extend(["-", " * ".join(sorted(k))])
+                    elif v >= 0.0:
+                        terms.extend(["+", " * ".join([str(v), *sorted(k)])])
+                    else:
+                        terms.extend(["-", " * ".join([str(abs(v)), *sorted(k)])])
+            else:
+                return " ".join(terms[1:] if terms[0] == "+" else terms)
+        else:
+            return "0.0"
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({dict.__repr__({k if k is None else tuple(sorted(k)) : v for k, v in self})})"
+
+    def __iter__(self):
+        return iter(dict.items(self))
+
+    def copy(self) -> Posiform:
+        """Deep Posiform copy."""
+        return Posiform({k: v for k, v in self})
+
+    def __add__(self, other) -> Posiform:
+        posiform = self.copy()
+        posiform = posiform.__iadd__(other)
+        return posiform
+
+    def __iadd__(self, other) -> Posiform:
+        if isinstance(other, dict):
+            try:
+                other = self.cls(other)
+            except TypeError as type_error:
+                raise TypeError("Unable to cast operand to Posiform type.") from type_error
+        if isinstance(other, type(self)):
+            for k, v in other:
+                if k in self:
+                    w = self[k] + v
+                    if w == 0.0:
+                        del self[k]
+                    else:
+                        self[k] = w
+                else:
+                    self[k] = v
+            return self
+        elif isinstance(other, numbers.Number) and not isinstance(other, complex):
+            if None in self:
+                self[None] += float(other)
+            else:
+                self[None] = float(other)
+            return self
+        else:
+            return NotImplemented
+
+    def __neg__(self) -> Posiform:
+        posiform = self.cls()
+        for k, v in self:
+            posiform[k] = -v
+        return posiform
+
+    def __rsub__(self, other) -> Posiform:
+        return self.__sub__(other).__neg__()
+
+    def __sub__(self, other) -> Posiform:
+        posiform = self.copy()
+        posiform = posiform.__isub__(other)
+        return posiform
+
+    def __isub__(self, other) -> Posiform:
+        if isinstance(other, dict):
+            try:
+                other = self.cls(other)
+            except TypeError as type_error:
+                raise TypeError("Unable to cast operand to Posiform type.") from type_error
+        if isinstance(other, type(self)):
+            for k, v in other.items():
+                if k in self:
+                    w = self[k] - v
+                    if w == 0.0:
+                        del self[k]
+                    else:
+                        self[k] = w
+                else:
+                    self[k] = -v
+            return self
+        elif isinstance(other, numbers.Number) and not isinstance(other, complex):
+            if None in self:
+                self[None] -= float(other)
+            else:
+                self[None] = -float(other)
+            return self
+        else:
+            return NotImplemented
+
+    def __truediv__(self, other) -> Posiform:
+        posiform = self.copy()
+        posiform = posiform.__itruediv__(other)
+        return posiform
+
+    def __itruediv__(self, other) -> Posiform:
+        if isinstance(other, numbers.Number):
+            c = float(other)
+            if c != 0.0:
+                for k, v in self:
+                    self[k] = v / c
+                return self
+            else:
+                raise ZeroDivisionError("division by zero")
+        else:
+            return NotImplemented
+
+    def __mul__(self, other) -> Posiform:
+        posiform = self.copy()
+        posiform = posiform.__imul__(other)
+        return posiform
+
+    def __imul__(self, other):
+        if isinstance(other, dict):
+            try:
+                other = self.cls(other)
+            except TypeError as type_error:
+                raise TypeError("Unable to cast operand to Posiform type") from type_error
+        if isinstance(other, type(self)):
+            posiform = Posiform()
+            for kx, vx in self:
+                for ky, vy in other:
+                    if kx is None:
+                        k = ky
+                    elif ky is None:
+                        k = kx
+                    else:
+                        k = frozenset({*kx, *ky})
+
+                    v = vx * vy
+
+                    if k in posiform:
+                        w = posiform[k] + v
+                        if w == 0.0:
+                            del posiform[k]
+                        else:
+                            posiform[k] = w
+                    else:
+                        posiform[k] = v
+            return posiform
+        elif isinstance(other, numbers.Number) and not isinstance(other, complex):
+            w = float(other)
+            if w == 0.0:
+                self.clear()
+            else:
+                for k, v in self:
+                    self[k] = w * v
+            return self
+        else:
+            return NotImplemented
+
+    __radd__ = __add__
+    __rmul__ = __mul__
+
+    @property
+    def aux(self) -> str:
+        self.__aux += 1
+        return f"${self.__aux}"
+
+    def reduce_degree(self) -> Posiform:
+        """"""
+        ## Reset ancillary variable counter
+        self.__aux = 0
+
+        posiform = Posiform()
+        for X, a in self:
+            posiform += self.__reduce_term(X, a)
+        return posiform
+
+    def __reduce_term(self, X: frozenset, a: float) -> Posiform:
+        """
+        Parameters
+        ----------
+        X : frozenset[str], tuple[str], list[str], set[str]
+            Tuple with the string variable names (or None for constant term).
+        a : float
+            Multiplicative constant.
+
+        Returns
+        -------
+        Posiform
+        """
+        if X is None or len(X) <= 2:
+            return self.cls({X: a})
+        else:
+            ## Reduction by minimum selection
+            w = self.aux
+            x, y, *z = X
+
+            if self.__minimum_selection():  ## minimum selection
+                if a < 0:
+                    ## if a < 0: a (x y z) => a w (x + y + z - 2)
+                    return self.cls({(x, w): a, (y, w): a, (w,): -2.0 * a}) + self.__reduce_term((*z, w), a)
+                else:
+                    ## if a > 0: a (x y z) => a (x w + y w + z w + x y + x z + y z - x - y - z - w + 1)
+                    return (
+                        self.cls(
+                            {
+                                (x, w): a,
+                                (y, w): a,
+                                (x, y): a,
+                                (x,): -a,
+                                (y,): -a,
+                                (w,): -a,
+                                None: a,
+                            }
+                        )
+                        + (self.__reduce_term((x, *z), a) + self.__reduce_term((y, *z), a) + self.__reduce_term((*z, w), a) + self.__reduce_term((*z,), -a))
+                    )
+            elif self.__substitution():
+                alpha = 2.0  ## TODO: How can I compute alpha? (besides alpha > 1)
+                return self.__reduce_term((*z, w), 1.0) + alpha * self.__P(x, y, w)
+            else:
+                raise NotImplementedError("Not an option.")
+
+    @classmethod
+    def __P(cls: type, x: str, y: str, w: str):
+        """"""
+        return cls({(x, y): 1.0, (x, w): -2.0, (y, w): -2.0, (w,): 3.0})
+
+    def __minimum_selection(self, *args) -> bool:
+        return True
+
+    def __substitution(self, *args) -> bool:
+        return True
+
+    @property
+    def variables(self):
+        return sorted(set(x for k in self.keys() if k is not None for x in k))
+
+    @property
+    def cls(self):
+        return self.__class__
+
+    def toMiniJSON(self) -> str:
+        return json.dumps([[k if k is None else " ".join(k), v] for k, v in self])
+
+    @classmethod
+    def fromMiniJSON(cls, data: str) -> Posiform:
+        json_data = json.loads(data)
+
+        buffer = {}
+
+        if not isinstance(json_data, list):
+            raise json.decoder.JSONDecodeError("Data must be of Array type (Python list)", data, 0)
+        else:
+            for item in json_data:
+                if not isinstance(item, list):
+                    raise json.decoder.JSONDecodeError("Items must be of Array type (Python list)", data, 0)
+                elif len(item) != 2:
+                    raise json.decoder.JSONDecodeError("Items must contain two entries", data, 0)
+                elif item[0] is not None and not isinstance(item[0], str):
+                    raise json.decoder.JSONDecodeError("Items 1st entry must be of String type (Python str)", data, 0)
+                elif not isinstance(item[1], (int, float)):
+                    raise json.decoder.JSONDecodeError("Items 2nd entry must be of Number type (Python float)", data, 0)
+                else:
+                    key = tuple(item[0].split(" ")) if item[0] is not None else None
+                    val = float(item[1])
+                    if key in buffer:
+                        buffer[key] += val
+                    else:
+                        buffer[key] = val
+            else:
+                return cls(buffer)
+
+    def toJSON(self, indent: int = None) -> str:
+        if indent is None:
+            return json.dumps([{"term": sorted(k) if k is not None else None, "cons": v} for k, v in self])
+        else:
+            return json.dumps([{"term": sorted(k) if k is not None else None, "cons": v} for k, v in self], indent=indent)
+
+    @classmethod
+    def fromJSON(cls, data: str) -> Posiform:
+        json_data = json.loads(data)
+
+        buffer = {}
+
+        if not isinstance(json_data, list):
+            raise json.decoder.JSONDecodeError("Data must be of Array type (Python list)", data, 0)
+        else:
+            for item in json_data:
+                if not isinstance(item, dict):
+                    raise json.decoder.JSONDecodeError("Items must be of Object type (Python dict)", data, 0)
+                elif "term" not in item or "cons" not in item:
+                    raise json.decoder.JSONDecodeError("Items must contain both 'term' and 'cons' keys", data, 0)
+                elif not isinstance(item["term"], list) and item["term"] is not None:
+                    raise json.decoder.JSONDecodeError("Items 'term' value must be of Array type (Python list)", data, 0)
+                elif item["term"] is not None and not all(isinstance(var, str) for var in item["term"]):
+                    raise json.decoder.JSONDecodeError("Items 'term' value must contain only entries of type String (Python str)", data, 0)
+                elif not isinstance(item["cons"], (int, float)):
+                    raise json.decoder.JSONDecodeError("Items 'cons' value must be of Number type (Python float)", data, 0)
+                else:
+                    key = tuple(item["term"]) if item["term"] is not None else None
+                    val = float(item["cons"])
+                    if key in buffer:
+                        buffer[key] += val
+                    else:
+                        buffer[key] = val
+            else:
+                return cls(buffer)
+
+    def qubo(self) -> tuple[dict[str, int], np.ndarray[float], float]:
+        """
+        Returns
+        -------
+        dict[str, int]
+            Mapping between variables and respective indexes.
+        np.ndarray
+            Symmetric Matrix representing QUBO instance.
+        float
+            Ground state energy.
+        """
+        reduced = self.reduce_degree()
+        variables = reduced.variables
+
+        n = len(variables)
+
+        x = {v: i for i, v in enumerate(variables)}
+        Q = np.zeros((n, n), dtype=float)
+        c = 0.0
+
+        for k, a in reduced:
+            if k is None:
+                c = a
+            else:
+                I = tuple(map(x.get, k))
+                if len(I) == 1:
+                    i = I[0]
+                    Q[i, i] += a
+                elif len(I) == 2:
+                    i, j = I
+                    Q[i, j] += a
+                    Q[j, i] += a
+                else:
+                    raise ValueError("Degree reduction failed.")
+        return x, Q, c
+
+
+__all__ = ["Posiform"]
