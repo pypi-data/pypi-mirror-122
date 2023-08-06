0d4a547e2701407e916b709a89db7fe86ae0b8cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:16 +0000

    Add package runtype/0.2.3/runtype-0.2.3-py3-none-any.whl

---
+++ packages/runtype/runtype-0.2.3-py3-none-any.whl/runtype/utils.py
@@ -0,0 +1,15 @@
+import inspect
+from collections import deque
+
+
+def bfs(initial, expand):
+    open_q = deque(list(initial))
+    visited = set(open_q)
+    while open_q:
+        node = open_q.popleft()
+        yield node
+        for next_node in expand(node):
+            if next_node not in visited:
+                visited.add(next_node)
+                open_q.append(next_node)
+
