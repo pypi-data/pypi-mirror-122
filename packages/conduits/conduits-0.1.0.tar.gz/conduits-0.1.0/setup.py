25cd7ed80f4a86184bc15fb282306201c44f039a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:58 +0000

    Add package conduits/0.1.0/conduits-0.1.0.tar.gz

---
+++ packages/conduits/conduits-0.1.0.tar.gz/conduits-0.1.0/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="conduits",
+    version="0.1.0",
+    author="Kale Miller",
+    url="https://github.com/kmiller96/conduits",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    install_requires=[
+        "pandas",
+        "networkx[default]",
+        "joblib",
+    ],
+)
