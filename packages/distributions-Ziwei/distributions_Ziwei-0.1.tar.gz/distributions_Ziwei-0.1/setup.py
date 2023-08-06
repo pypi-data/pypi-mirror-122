b342783449ce050345fc3cfd973755fd4ac9f109
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:34 +0000

    Add package distributions-Ziwei/0.1/distributions_Ziwei-0.1.tar.gz

---
+++ packages/distributions-Ziwei/distributions_Ziwei-0.1.tar.gz/distributions_Ziwei-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_Ziwei',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_Ziwei'],
+      author_email = 'testemail@gmail.com',
+      zip_safe=False)
