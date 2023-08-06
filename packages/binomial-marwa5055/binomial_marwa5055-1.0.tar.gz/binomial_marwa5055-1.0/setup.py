b873e75029c1fea2868f8594bd58b65543d8c2bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:21 +0000

    Add package binomial-marwa5055/1.0/binomial_marwa5055-1.0.tar.gz

---
+++ packages/binomial-marwa5055/binomial_marwa5055-1.0.tar.gz/binomial_marwa5055-1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='binomial_marwa5055',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['binomial_marwa5055'],
+      zip_safe=False)
