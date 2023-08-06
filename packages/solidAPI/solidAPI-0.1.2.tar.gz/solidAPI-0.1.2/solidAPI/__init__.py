6f8276b3b552fa38de35e2da2ba82e03fd2a37ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:48 +0000

    Add package solidAPI/0.1.2/solidAPI-0.1.2.tar.gz

---
+++ packages/solidAPI/solidAPI-0.1.2.tar.gz/solidAPI-0.1.2/solidAPI/__init__.py
@@ -0,0 +1,6 @@
+from .chats import add_chat, get_chat, set_lang, del_chat
+from .others import get_available_language, get_message
+from .sudos import add_sudo, del_sudo, get_sudos
+
+
+BASE_URL = "https://solidapis.azurewebsites.net"
