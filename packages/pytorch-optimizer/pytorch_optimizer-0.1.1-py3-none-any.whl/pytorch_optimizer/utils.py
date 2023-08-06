b5322aa15cd51681b737a233f466819f07b13b35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:27 +0000

    Add package pytorch-optimizer/0.1.1/pytorch_optimizer-0.1.1-py3-none-any.whl

---
+++ packages/pytorch-optimizer/pytorch_optimizer-0.1.1-py3-none-any.whl/pytorch_optimizer/utils.py
@@ -0,0 +1,58 @@
+from typing import List, Optional, Tuple, Union
+
+import torch
+from torch import nn
+
+from pytorch_optimizer.types import PARAMETERS
+
+
+def is_valid_parameters(parameters: PARAMETERS) -> bool:
+    return isinstance(parameters, (list, tuple)) and len(parameters) > 0 and isinstance(parameters[0], dict)
+
+
+def normalize_gradient(x: torch.Tensor, use_channels: bool = False, epsilon: float = 1e-8) -> torch.Tensor:
+    """normalize gradient with stddev
+    :param x: torch.Tensor. gradient.
+    :param use_channels: bool. channel-wise normalization.
+    :param epsilon: float. eps.
+    :return: torch.Tensor. normalized gradient.
+    """
+    size: int = x.dim()
+    if size > 1 and use_channels:
+        s = x.std(dim=tuple(range(1, size)), keepdim=True) + epsilon
+        x.div_(s)
+    elif torch.numel(x) > 2:
+        s = x.std() + epsilon
+        x.div_(s)
+    return x
+
+
+def unit_norm(x: torch.Tensor, norm: float = 2.0) -> torch.Tensor:
+    keep_dim: bool = True
+    dim: Optional[Union[int, Tuple[int, ...]]] = None
+
+    x_len: int = len(x.shape)
+    if x_len <= 1:
+        keep_dim = False
+    elif x_len in (2, 3):  # linear layers
+        dim = 1
+    elif x_len == 4:  # conv kernels
+        dim = (1, 2, 3)
+    else:
+        dim = tuple(range(1, x_len))
+
+    return x.norm(dim=dim, keepdim=keep_dim, p=norm)
+
+
+def get_optimizer_parameters(
+    model: nn.Module, weight_decay: float, wd_ban_list: List[str] = ('bias', 'LayerNorm.bias', 'LayerNorm.weight')
+) -> PARAMETERS:
+    param_optimizer: List[Tuple[str, nn.Parameter]] = list(model.named_parameters())
+
+    return [
+        {
+            'params': [p for n, p in param_optimizer if not any(nd in n for nd in wd_ban_list)],
+            'weight_decay': weight_decay,
+        },
+        {'params': [p for n, p in param_optimizer if any(nd in n for nd in wd_ban_list)], 'weight_decay': 0.0},
+    ]
