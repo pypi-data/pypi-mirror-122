781e3e3a22158b0034b2fe2b0aa6b536bfb9704b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:41 +0000

    Add package koil/0.1.24/koil-0.1.24.tar.gz

---
+++ packages/koil/koil-0.1.24.tar.gz/koil-0.1.24/koil/checker/base.py
@@ -0,0 +1,42 @@
+
+
+from abc import abstractmethod
+import abc
+from typing import Union
+from koil.exceptions import KoilException
+
+from koil.state import KoilState
+
+
+
+
+
+class CheckerExpection(KoilException):
+    pass
+
+
+class BaseChecker(abc.ABC):
+    pass
+
+    @abstractmethod
+    def force_state(self) -> Union[None, KoilState]:
+        """Force State
+
+        Please overwrite this method in your Checker. A Checkers 
+        purpose is to identify environment in which we choose
+        different runstates for the koil. 
+
+        Your checker should either return None if it does identify
+        a certain set of conditions for a certain desired state or
+        should return the desired state
+
+        Raises:
+            CheckerExpection: An exception if there is a mismatch between a nesecarry state and the current environment we are in (e.g no desired library)
+
+        Returns:
+            Union[None, KoilState]: The derised State or None if no state is desired
+        """
+
+
+
+        raise CheckerExpection("Checker did not overwrite the proper function")
