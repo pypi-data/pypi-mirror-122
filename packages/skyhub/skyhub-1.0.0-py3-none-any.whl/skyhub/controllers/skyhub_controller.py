aca8d7b1eec1fa3e22676c92db52a3d9235c9dc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:05 +0000

    Add package skyhub/1.0.0/skyhub-1.0.0-py3-none-any.whl

---
+++ packages/skyhub/skyhub-1.0.0-py3-none-any.whl/skyhub/controllers/skyhub_controller.py
@@ -0,0 +1,44 @@
+import logging
+
+import requests
+
+from skyhub.models.produto import Produto
+
+logger = logging.Logger(__name__)
+
+
+class SkyhubController:
+    def __init__(self, credentials: dict, base_url: str, request: requests) -> None:
+        self.credentials = credentials
+        self.base_url = base_url
+        self.request = request
+
+    def get_produto(self, sku: str) -> Produto:
+        url = f"{self.base_url}/{sku}"
+        response = self.request.get(url, headers=self.credentials)
+
+        if response.status_code == 200:
+            produto = response.json()
+            qty = produto["qty"]  # Quantidade
+            price = produto["price"]  # Preço
+            return Produto(sku, price, qty)
+
+        elif response.status_code == 404:
+            logger.warning(f"O produto {sku} não foi encontrado!")
+            return False
+
+        return response
+
+    def atualiza_produto(self, produto: Produto) -> bool:
+        produtoJSON = produto.toJson()
+        url = f"{self.base_url}/{produto.sku}"
+        response = self.request.put(url, data=produtoJSON, headers=self.credentials)
+
+        if response.status_code == 204:
+            logger.info(f"O produto '{produto.sku}' foi atualizado.")
+            return True
+
+        else:
+            print(response.text)
+            logger.warning(f"O produto '{produto.sku}' não pode ser atualizado.")
+            return False
