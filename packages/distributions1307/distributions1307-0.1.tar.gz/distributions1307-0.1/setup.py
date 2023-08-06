5ca065323617ef8106d59eea89376c952ffc2d2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:36 +0000

    Add package distributions1307/0.1/distributions1307-0.1.tar.gz

---
+++ packages/distributions1307/distributions1307-0.1.tar.gz/distributions1307-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions1307',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions1307'],
+      author = 'Alexander Julius Wolters',
+      author_email = 'alexander.woltersgo@gmail.com',
+      zip_safe=False)
