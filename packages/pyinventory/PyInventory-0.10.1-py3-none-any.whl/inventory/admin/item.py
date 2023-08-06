94e253b22011a33d2ff0bf671a7898ebdf0e2282
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:55 +0000

    Add package pyinventory/0.10.1/PyInventory-0.10.1-py3-none-any.whl

---
+++ packages/pyinventory/PyInventory-0.10.1-py3-none-any.whl/inventory/admin/item.py
@@ -0,0 +1,204 @@
+import logging
+
+import tagulous
+from adminsortable2.admin import SortableInlineAdminMixin
+from django.contrib import admin
+from django.template.loader import render_to_string
+from django.utils.html import format_html
+from django.utils.translation import gettext_lazy as _
+from import_export.admin import ImportExportMixin
+from import_export.resources import ModelResource
+
+from inventory.admin.base import BaseUserAdmin
+from inventory.forms import ItemModelModelForm
+from inventory.models import ItemLinkModel, ItemModel
+from inventory.models.item import ItemFileModel, ItemImageModel
+
+
+logger = logging.getLogger(__name__)
+
+
+class UserInlineMixin:
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+
+        if not request.user.is_superuser:
+            # Display only own created entries
+            qs = qs.filter(user=request.user)
+
+        return qs
+
+
+class ItemLinkModelInline(UserInlineMixin, SortableInlineAdminMixin, admin.TabularInline):
+    model = ItemLinkModel
+    extra = 0
+
+
+class ItemImageModelInline(UserInlineMixin, SortableInlineAdminMixin, admin.TabularInline):
+    def preview(self, instance):
+        return format_html(
+            (
+                '<a href="{url}" title="{name}"'
+                ' target="_blank" class="image_file_input_preview">'
+                '<img style="width:9em;" src="{url}"></a>'
+            ),
+            url=instance.image.url,
+            name=instance.name,
+        )
+    model = ItemImageModel
+    extra = 0
+    fields = (
+        'position', 'preview', 'image', 'name', 'tags'
+    )
+    readonly_fields = ('preview',)
+
+
+class ItemFileModelInline(UserInlineMixin, SortableInlineAdminMixin, admin.TabularInline):
+    model = ItemFileModel
+    extra = 0
+    fields = (
+        'position', 'file', 'name', 'tags'
+    )
+
+
+class ItemModelResource(ModelResource):
+    class Meta:
+        model = ItemModel
+
+
+class GroupItemsListFilter(admin.SimpleListFilter):
+    title = _('Group Items')
+    parameter_name = 'grouping'
+
+    GET_KEY_AUTO = 'auto'
+    GET_KEY_NO = 'no'
+
+    def lookups(self, request, model_admin):
+        return (
+            (self.GET_KEY_AUTO, _('Automatic')),
+            (self.GET_KEY_NO, _('No')),
+        )
+
+    def value(self):
+        return super().value() or self.GET_KEY_AUTO
+
+    def queryset(self, request, queryset):
+        auto_mode = self.value() == self.GET_KEY_AUTO
+        if auto_mode:
+            request.group_items = not request.GET.keys()
+        else:
+            request.group_items = self.value() != self.GET_KEY_NO
+
+        logger.info('Group items: %r (auto mode: %r)', request.group_items, auto_mode)
+
+        if request.group_items:
+            queryset = queryset.filter(parent__isnull=True)
+
+        return queryset
+
+    def choices(self, changelist):
+        for lookup, title in self.lookup_choices:
+            if lookup == self.GET_KEY_AUTO:
+                query_string = changelist.get_query_string(remove=[self.parameter_name])
+            else:
+                query_string = changelist.get_query_string({self.parameter_name: lookup})
+
+            yield {
+                'selected': self.value() == lookup,
+                'query_string': query_string,
+                'display': title,
+            }
+
+
+@admin.register(ItemModel)
+class ItemModelAdmin(ImportExportMixin, BaseUserAdmin):
+    form = ItemModelModelForm
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        qs = qs.select_related(
+            'user',
+        )
+        qs = qs.prefetch_related(
+            'kind',
+            'producer',
+        )
+        return qs
+
+    def column_item(self, obj):
+        context = {
+            'base_item': obj,
+        }
+
+        if self.request.group_items:  # Attribute added in GroupItemsListFilter.queryset()
+            logger.debug('Display sub items inline')
+            # TODO: annotate "sub_items" !
+            qs = ItemModel.objects.filter(
+                user=self.user  # user added in BaseUserAdmin.get_changelist()
+            )
+            qs = qs.filter(parent=obj).sort()
+            context['sub_items'] = qs
+
+        return render_to_string(
+            template_name='admin/inventory/item/column_item.html',
+            context=context,
+        )
+
+    column_item.short_description = _('ItemModel.verbose_name_plural')
+
+    date_hierarchy = 'create_dt'
+    list_display = (
+        'kind', 'producer',
+        'column_item',
+        'location',
+        'received_date', 'update_dt'
+    )
+    ordering = ('kind', 'producer', 'name')
+    list_display_links = None
+    list_filter = (GroupItemsListFilter, 'kind', 'location', 'producer', 'tags')
+    search_fields = ('name', 'description', 'kind__name', 'tags__name')
+    fieldsets = (
+        (_('Internals'), {
+            'classes': ('collapse',),
+            'fields': (
+                'id',
+                'user',
+            )
+        }),
+        (_('Meta'), {
+            'classes': ('collapse',),
+            'fields': (
+                'create_dt', 'update_dt'
+            )
+        }),
+        (_('Basic'), {'fields': (
+            'kind',
+            ('producer', 'name'),
+            'description',
+            'tags',
+            'fcc_id',
+            'parent',
+            'location',
+        )}),
+        (_('Lent'), {
+            'classes': ('collapse',),
+            'fields': (
+                'lent_to',
+                ('lent_from_date', 'lent_until_date',)
+            )}),
+        (_('Received'), {
+            'classes': ('collapse',),
+            'fields': (
+                ('received_from', 'received_date', 'received_price'),
+            )}),
+        (_('Handed over'), {
+            'classes': ('collapse',),
+            'fields': (
+                ('handed_over_to', 'handed_over_date', 'handed_over_price'),
+            )}),
+    )
+    readonly_fields = ('id', 'create_dt', 'update_dt', 'user')
+    inlines = (ItemImageModelInline, ItemFileModelInline, ItemLinkModelInline)
+
+
+tagulous.admin.enhance(ItemModel, ItemModelAdmin)
---
+++ packages/pyinventory/PyInventory-0.10.1-py3-none-any.whl/inventory_project/tests/test_admin_item.py
@@ -0,0 +1,222 @@
+import datetime
+import logging
+from unittest import mock
+
+from bx_django_utils.test_utils.datetime import MockDatetimeGenerator
+from bx_django_utils.test_utils.html_assertion import HtmlAssertionMixin
+from django.contrib.auth.models import User
+from django.template.defaulttags import CsrfTokenNode
+from django.test import TestCase
+from django.utils import timezone
+from django_tools.unittest_utils.mockup import ImageDummy
+from model_bakery import baker
+
+from inventory import __version__
+from inventory.models import ItemImageModel, ItemModel
+from inventory.permissions import get_or_create_normal_user_group
+from inventory_project.tests.temp_utils import assert_html_response_snapshot
+
+
+class AdminAnonymousTests(TestCase):
+    def test_login(self):
+        response = self.client.get('/admin/inventory/itemmodel/add/', HTTP_ACCEPT_LANGUAGE='en')
+        self.assertRedirects(
+            response,
+            expected_url='/admin/login/?next=/admin/inventory/itemmodel/add/'
+        )
+
+
+class AdminTestCase(HtmlAssertionMixin, TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.normaluser = baker.make(
+            User, username='NormalUser',
+            is_staff=True, is_active=True, is_superuser=False
+        )
+        assert cls.normaluser.user_permissions.count() == 0
+        group = get_or_create_normal_user_group()[0]
+        cls.normaluser.groups.set([group])
+
+    def test_normal_user_create_minimal_item(self):
+        self.client.force_login(self.normaluser)
+
+        response = self.client.get('/admin/inventory/itemmodel/add/')
+        assert response.status_code == 200
+        self.assert_html_parts(response, parts=(
+            f'<title>Add Item | PyInventory v{__version__}</title>',
+        ))
+
+        assert ItemModel.objects.count() == 0
+
+        response = self.client.post(
+            path='/admin/inventory/itemmodel/add/',
+            data={
+                'kind': 'kind',
+                'name': 'name',
+
+                'itemimagemodel_set-TOTAL_FORMS': '0',
+                'itemimagemodel_set-INITIAL_FORMS': '0',
+                'itemimagemodel_set-MIN_NUM_FORMS': '0',
+                'itemimagemodel_set-MAX_NUM_FORMS': '1000',
+                'itemimagemodel_set-__prefix__-position': '0',
+
+                'itemfilemodel_set-TOTAL_FORMS': '0',
+                'itemfilemodel_set-INITIAL_FORMS': '0',
+                'itemfilemodel_set-MIN_NUM_FORMS': '0',
+                'itemfilemodel_set-MAX_NUM_FORMS': '1000',
+                'itemfilemodel_set-__prefix__-position': '0',
+
+                'itemlinkmodel_set-TOTAL_FORMS': '0',
+                'itemlinkmodel_set-INITIAL_FORMS': '0',
+                'itemlinkmodel_set-MIN_NUM_FORMS': '0',
+                'itemlinkmodel_set-MAX_NUM_FORMS': '1000',
+                'itemlinkmodel_set-__prefix__-position': '0',
+
+                '_save': 'Save',
+            },
+        )
+        self.assertRedirects(response, expected_url='/admin/inventory/itemmodel/')
+
+        data = list(ItemModel.objects.values_list('kind__name', 'name'))
+        assert data == [('kind', 'name')]
+
+        item = ItemModel.objects.first()
+
+        self.assert_messages(response, expected_messages=[
+            f'The Item “<a href="/admin/inventory/itemmodel/{item.pk}/change/"> - name</a>”'
+            ' was added successfully.'
+        ])
+
+        assert item.user_id == self.normaluser.pk
+
+    def test_new_item_with_image(self):
+        """
+        https://github.com/jedie/PyInventory/issues/33
+        """
+        self.client.force_login(self.normaluser)
+
+        img = ImageDummy(width=1, height=1, format='png').in_memory_image_file(filename='test.png')
+
+        response = self.client.post(
+            path='/admin/inventory/itemmodel/add/',
+            data={
+                'kind': 'kind',
+                'name': 'name',
+
+                'itemimagemodel_set-TOTAL_FORMS': '1',
+                'itemimagemodel_set-INITIAL_FORMS': '0',
+                'itemimagemodel_set-MIN_NUM_FORMS': '0',
+                'itemimagemodel_set-MAX_NUM_FORMS': '1000',
+                'itemimagemodel_set-0-position': '0',
+                'itemimagemodel_set-__prefix__-position': '0',
+                'itemimagemodel_set-0-image': img,
+
+                'itemfilemodel_set-TOTAL_FORMS': '0',
+                'itemfilemodel_set-INITIAL_FORMS': '0',
+                'itemfilemodel_set-MIN_NUM_FORMS': '0',
+                'itemfilemodel_set-MAX_NUM_FORMS': '1000',
+                'itemfilemodel_set-__prefix__-position': '0',
+
+                'itemlinkmodel_set-TOTAL_FORMS': '0',
+                'itemlinkmodel_set-INITIAL_FORMS': '0',
+                'itemlinkmodel_set-MIN_NUM_FORMS': '0',
+                'itemlinkmodel_set-MAX_NUM_FORMS': '1000',
+                'itemlinkmodel_set-__prefix__-position': '0',
+
+                '_save': 'Save',
+            },
+        )
+        self.assertRedirects(response, expected_url='/admin/inventory/itemmodel/')
+
+        data = list(ItemModel.objects.values_list('kind__name', 'name'))
+        assert data == [('kind', 'name')]
+
+        item = ItemModel.objects.first()
+
+        self.assert_messages(response, expected_messages=[
+            f'The Item “<a href="/admin/inventory/itemmodel/{item.pk}/change/"> - name</a>”'
+            ' was added successfully.'
+        ])
+
+        assert item.user_id == self.normaluser.pk
+
+        assert ItemImageModel.objects.count() == 1
+        image = ItemImageModel.objects.first()
+        assert image.name == 'test.png'
+        assert image.item == item
+        assert image.user_id == self.normaluser.pk
+
+    def test_auto_group_items(self):
+        self.client.force_login(self.normaluser)
+
+        offset = datetime.timedelta(minutes=1)
+        with mock.patch.object(timezone, 'now', MockDatetimeGenerator(offset=offset)):
+            for main_item_no in range(1, 3):
+                main_item = ItemModel.objects.create(
+                    id=f'00000000-000{main_item_no}-0000-0000-000000000000',
+                    user=self.normaluser,
+                    name=f'main item {main_item_no}'
+                )
+                main_item.full_clean()
+                for sub_item_no in range(1, 3):
+                    sub_item = ItemModel.objects.create(
+                        id=f'00000000-000{main_item_no}-000{sub_item_no}-0000-000000000000',
+                        user=self.normaluser,
+                        parent=main_item,
+                        name=f'sub item {main_item_no}.{sub_item_no}'
+                    )
+                    sub_item.full_clean()
+
+        names = list(ItemModel.objects.order_by('id').values_list('name', flat=True))
+        assert names == [
+            'main item 1', 'sub item 1.1', 'sub item 1.2',
+            'main item 2', 'sub item 2.1', 'sub item 2.2',
+        ]
+
+        # Default mode, without any GET parameter -> group "automatic":
+
+        with mock.patch.object(CsrfTokenNode, 'render', return_value='MockedCsrfTokenNode'), \
+                self.assertLogs(logger='inventory', level=logging.DEBUG) as logs:
+            response = self.client.get(
+                path='/admin/inventory/itemmodel/',
+            )
+            assert response.status_code == 200
+        self.assert_html_parts(response, parts=(
+            f'<title>Select Item to change | PyInventory v{__version__}</title>',
+
+            '<a href="/admin/inventory/itemmodel/00000000-0001-0000-0000-000000000000/change/">'
+            'main item 1</a>',
+
+            '<li><a href="/admin/inventory/itemmodel/00000000-0001-0001-0000-000000000000/change/">'
+            'sub item 1.1</a></li>',
+        ))
+        assert logs.output == [
+            'INFO:inventory.admin.item:Group items: True (auto mode: True)',
+            'DEBUG:inventory.admin.item:Display sub items inline',
+            'DEBUG:inventory.admin.item:Display sub items inline'
+        ]
+        assert_html_response_snapshot(response=response, validate=False)
+
+        # Search should disable grouping:
+
+        with mock.patch.object(CsrfTokenNode, 'render', return_value='MockedCsrfTokenNode'), \
+                self.assertLogs(logger='inventory', level=logging.DEBUG) as logs:
+            response = self.client.get(
+                path='/admin/inventory/itemmodel/?q=sub+item+2.',
+            )
+            assert response.status_code == 200
+        self.assert_html_parts(response, parts=(
+            '<input type="text" size="40" name="q" value="sub item 2." id="searchbar" autofocus>',
+            '2 results (<a href="?">6 total</a>)',
+
+            '<a href="/admin/inventory/itemmodel/00000000-0002-0001-0000-000000000000/change/">'
+            'sub item 2.1</a>',
+
+            '<a href="/admin/inventory/itemmodel/00000000-0002-0002-0000-000000000000/change/">'
+            'sub item 2.2</a>',
+        ))
+        assert logs.output == [
+            # grouping disabled?
+            'INFO:inventory.admin.item:Group items: False (auto mode: True)'
+        ]
+        assert_html_response_snapshot(response=response, validate=False)
