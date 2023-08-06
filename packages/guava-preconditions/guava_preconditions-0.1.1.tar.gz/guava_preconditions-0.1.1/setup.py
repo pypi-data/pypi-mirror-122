336734e2e7f21d2ecdefa1d4255cede1abb73d98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:22 +0000

    Add package guava-preconditions/0.1.1/guava_preconditions-0.1.1.tar.gz

---
+++ packages/guava-preconditions/guava_preconditions-0.1.1.tar.gz/guava_preconditions-0.1.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['guava_preconditions']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'guava-preconditions',
+    'version': '0.1.1',
+    'description': "A python version of guava's preconditions",
+    'long_description': None,
+    'author': 'Elliana',
+    'author_email': 'me@mause.me',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
