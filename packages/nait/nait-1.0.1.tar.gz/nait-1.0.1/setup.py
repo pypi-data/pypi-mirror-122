902503c9157e407dc16179efd183dc8faa6bb0ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:02 +0000

    Add package nait/1.0.1/nait-1.0.1.tar.gz

---
+++ packages/nait/nait-1.0.1.tar.gz/nait-1.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+
+setup(
+  name='nait',
+  version='1.0.1',
+  description='Easy to use Neural AI Tool',
+  author='DanishDeveloper',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='nait', 
+  packages='s',
+  #packages=find_packages(),
+  install_requires=['numpy'] 
+)
