514bf4f04d9f1479bd4520d61d27440d512b60be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:38 +0000

    Add package afoonematematica/0.1/afoonematematica-0.1.tar.gz

---
+++ packages/afoonematematica/afoonematematica-0.1.tar.gz/afoonematematica-0.1/README.md
@@ -0,0 +1,4 @@
+#Matemática
+
+Este paquete es un paquete matemático para el curso de python
+avanzado de **Imagina**
---
+++ packages/afoonematematica/afoonematematica-0.1.tar.gz/afoonematematica-0.1/matematica/aritmetica.py
@@ -0,0 +1,16 @@
+# Modulo aritmética
+
+def sumar(a: float, b: float) -> float:
+    return a + b
+
+
+def restar(a: float, b: float) -> float:
+    return a - b
+
+
+def multiplicar(a: float, b: float) -> float:
+    return a * b
+
+
+def dividir(a: float, b: float) -> float:
+    return a / b
