1d9fe1b53c59fcf4d10a2bf69d1ae493c620c7d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:42 +0000

    Add package pincer/0.9.1/pincer-0.9.1-py3-none-any.whl

---
+++ packages/pincer/pincer-0.9.1-py3-none-any.whl/pincer/middleware/voice_state_update.py
@@ -0,0 +1,9 @@
+# Copyright Pincer 2021-Present
+# Full MIT License can be found in `LICENSE` at the project root.
+
+"""
+sent when a user's voice state changes in a subscribed voice channel
+(mute, volume, etc.)
+"""
+
+# TODO: Implement event
