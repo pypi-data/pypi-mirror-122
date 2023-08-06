b9a6b6a3455cf59ed70af7517b9d8f1f7fd993cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:22 +0000

    Add package dvclive/0.4.0/dvclive-0.4.0-py2.py3-none-any.whl

---
+++ packages/dvclive/dvclive-0.4.0-py2.py3-none-any.whl/dvclive/keras.py
@@ -0,0 +1,26 @@
+from tensorflow.keras.callbacks import (  # pylint: disable=no-name-in-module
+    Callback,
+)
+
+from dvclive import Live
+
+
+class DvcLiveCallback(Callback):
+    def __init__(
+        self, model_file=None, save_weights_only: bool = False, **kwargs
+    ):
+        super().__init__()
+        self.model_file = model_file
+        self.save_weights_only = save_weights_only
+        self.dvclive = Live(**kwargs)
+
+    def on_epoch_end(self, epoch: int, logs: dict = None):
+        logs = logs or {}
+        for metric, value in logs.items():
+            self.dvclive.log(metric, value)
+        if self.model_file:
+            if self.save_weights_only:
+                self.model.save_weights(self.model_file)
+            else:
+                self.model.save(self.model_file)
+        self.dvclive.next_step()
