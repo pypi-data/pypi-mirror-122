fcb33bfc0d28301056ca4161f60328d7c92429b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:49 +0000

    Add package dns-distributions/1.0/dns_distributions-1.0.tar.gz

---
+++ packages/dns-distributions/dns_distributions-1.0.tar.gz/dns_distributions-1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dns_distributions',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions'],
+      zip_safe=False)
