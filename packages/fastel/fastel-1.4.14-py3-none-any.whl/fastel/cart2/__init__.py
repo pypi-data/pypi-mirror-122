eb6ec9ce6c068b90a58f1b25c7c8268bbd729235
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:59 +0000

    Add package fastel/1.4.14/fastel-1.4.14-py3-none-any.whl

---
+++ packages/fastel/fastel-1.4.14-py3-none-any.whl/fastel/cart2/__init__.py
@@ -0,0 +1,6 @@
+from .cart import Cart
+from .collections import set_collections
+from .product_item import ProductItem
+from .product_items import ProductItems
+
+__all__ = ["set_collections", "ProductItems", "ProductItem", "Cart"]
