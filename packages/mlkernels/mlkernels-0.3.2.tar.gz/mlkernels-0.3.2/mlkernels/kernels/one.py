6df6ae9baed8c029686a9a81d63a656df1cbb68f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:02 +0000

    Add package mlkernels/0.3.2/mlkernels-0.3.2.tar.gz

---
+++ packages/mlkernels/mlkernels-0.3.2.tar.gz/mlkernels-0.3.2/mlkernels/kernels/one.py
@@ -0,0 +1,27 @@
+import lab as B
+from algebra import OneFunction
+from matrix import Constant
+
+from . import _dispatch
+from .. import Kernel
+from ..util import num_elements
+
+__all__ = ["OneKernel"]
+
+
+class OneKernel(Kernel, OneFunction):
+    """Constant kernel of `1`."""
+
+    @property
+    def _stationary(self):
+        return True
+
+
+@_dispatch
+def pairwise(k: OneKernel, x: B.Numeric, y: B.Numeric):
+    return Constant(B.one(x), num_elements(x), num_elements(y))
+
+
+@_dispatch
+def elwise(k: OneKernel, x: B.Numeric, y: B.Numeric):
+    return B.ones(B.dtype(x), num_elements(x), 1)
