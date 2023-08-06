9d6983e0d23dd1e190d16c4efc6b75ec79f6f962
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:00 +0000

    Add package Gaussia-Binomial-Dist/0.2/Gaussia_Binomial_Dist-0.2.tar.gz

---
+++ packages/Gaussia-Binomial-Dist/Gaussia_Binomial_Dist-0.2.tar.gz/Gaussia_Binomial_Dist-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Gaussia_Binomial_Dist',
+      version='0.2',
+      description='Gaussian distributions',
+      author="Eslam Abdelghany",
+      author_email="eslam322_1@hotmail.com",
+      packages=['Gaussia_Binomial_Dist'],
+      zip_safe=False)
