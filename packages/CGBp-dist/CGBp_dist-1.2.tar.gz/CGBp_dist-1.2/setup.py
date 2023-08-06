05c257fc869e9cf449a72655a904d6d3d071cb52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:42 +0000

    Add package CGBp-dist/1.2/CGBp_dist-1.2.tar.gz

---
+++ packages/CGBp-dist/CGBp_dist-1.2.tar.gz/CGBp_dist-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='CGBp_dist',
+      version='1.2',
+      description='Gaussian and Binomial distributions',
+      packages=['CGBp_dist'],
+      author = 'Vy Thuy Nguyen',
+      author_email = 'thuyvy11998@gmail.com',
+      zip_safe=False)
