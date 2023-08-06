de24efee7e59f6230ffd006656d8618a3a74e3ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:01 +0000

    Add package ggb-probability/0.1/ggb_probability-0.1.tar.gz

---
+++ packages/ggb-probability/ggb_probability-0.1.tar.gz/ggb_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ggb_probability',
+      version='0.1',
+      description='Distributions',
+      packages=['ggb_probability'],
+      author = 'Tushar Mehta',
+      author_email = 'tusharmehta0810@gmail.com',
+      zip_safe=False)
