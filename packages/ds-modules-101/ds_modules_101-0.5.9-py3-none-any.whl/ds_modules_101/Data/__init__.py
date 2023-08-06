07cc3b3cd4ea68c352d233491c63900593d0764b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:25 +0000

    Add package ds-modules-101/0.5.9/ds_modules_101-0.5.9-py3-none-any.whl

---
+++ packages/ds-modules-101/ds_modules_101-0.5.9-py3-none-any.whl/ds_modules_101/Data/__init__.py
@@ -0,0 +1,8 @@
+import pkg_resources
+import pandas as pd
+
+titanic_f = pkg_resources.resource_filename(__name__, 'titanic/titanic.csv')
+titanic_df = pd.read_csv(titanic_f)
+
+ibd_f = pkg_resources.resource_filename(__name__, 'ibd/IBD.csv')
+ibd_df = pd.read_csv(titanic_f)
