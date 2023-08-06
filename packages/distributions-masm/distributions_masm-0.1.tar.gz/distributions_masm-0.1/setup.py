c8564814edf4ccca64f31c1c8df7fd1351509844
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:39 +0000

    Add package distributions-masm/0.1/distributions_masm-0.1.tar.gz

---
+++ packages/distributions-masm/distributions_masm-0.1.tar.gz/distributions_masm-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_masm',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_masm'],
+      author='Miguel Soriano',
+      author_email = 'msorianomontero@gmail.com',
+      zip_safe=False)
