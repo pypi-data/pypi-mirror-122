45b280fff30b55d26d8f64775fc25e27c4842afe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:43 +0000

    Add package kalshi/0.2.0/kalshi-0.2.0.tar.gz

---
+++ packages/kalshi/kalshi-0.2.0.tar.gz/kalshi-0.2.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='kalshi',
+      version='0.2.0',
+      description='Library for accessing Kalshi API',
+      url='https://github.com/mlucy/kalshi_py',
+      author='Michael Lucy',
+      author_email='michaelglucy@gmail.com',
+      license='MIT',
+      packages=['kalshi'],
+      zip_safe=True,
+
+      install_requires=['requests'],
+      python_requires='>=3',
+)
