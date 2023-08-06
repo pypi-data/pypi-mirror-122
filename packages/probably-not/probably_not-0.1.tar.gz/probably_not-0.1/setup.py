2d74096e4c6c320dc68778272259468462286f82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:48 +0000

    Add package probably-not/0.1/probably_not-0.1.tar.gz

---
+++ packages/probably-not/probably_not-0.1.tar.gz/probably_not-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='probably_not',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['probably_not'],
+      zip_safe=False)
