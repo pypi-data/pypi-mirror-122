c073e83e38cbf9d151becbbf5cd7c2b7ddf13fb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:38 +0000

    Add package afpy/0.0.6/afpy-0.0.6.tar.gz

---
+++ packages/afpy/afpy-0.0.6.tar.gz/afpy-0.0.6/README.md
@@ -0,0 +1,2 @@
+# afpy
+A personal python package for increasing python efficiency by simplifying repeatable code.
---
+++ packages/afpy/afpy-0.0.6.tar.gz/afpy-0.0.6/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="afpy",
+    version="0.0.6",
+    author="Andrew Ford",
+    author_email="author@example.com",
+    description="A personal python package for increasing python efficiency by simplifying repeatable code.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/fordaj/afpy",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
