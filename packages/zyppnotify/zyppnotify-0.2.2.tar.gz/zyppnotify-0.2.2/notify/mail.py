24877d84ed3df82e0f3d5ead0b048eec20159237
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:56 +0000

    Add package zyppnotify/0.2.2/zyppnotify-0.2.2.tar.gz

---
+++ packages/zyppnotify/zyppnotify-0.2.2.tar.gz/zyppnotify-0.2.2/notify/mail.py
@@ -0,0 +1,107 @@
+import logging
+import os
+import smtplib
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.utils import formatdate
+from pathlib import Path
+from typing import Union
+
+import pandas as pd
+
+from notify.utils import check_environment_variables, dataframe_to_html
+
+
+class NotifyMail:
+    def __init__(
+        self,
+        to: str,
+        subject: str,
+        message: str,
+        cc: str = None,
+        files: Union[str, list] = None,
+        df: pd.DataFrame = pd.DataFrame(),
+        server: str = "smtp.office365.com",
+        port: str = 587,
+        user_tls: bool = True,
+    ):
+        """
+        This function sends an e-mail from Microsoft Exchange server
+
+        Parameters
+        ----------
+        to: str
+            the e-mail adress to send email to
+        subject: str
+            subject of the message
+        message:
+            HTML or plain text content of the message
+        cc: str
+            e-mail adress to add as cc
+        files: str, list
+            Path(s) to file(s) to add as attachment
+        df: pd.DataFrame
+            dataframe that needs to be added to the HTML message.
+        """
+
+        self.to = to
+        self.cc = cc
+        self.subject = subject
+        self.message = message
+        self.files = [files] if isinstance(files, str) else files
+
+        check_environment_variables(["EMAIL_USER", "EMAIL_PW"])
+        self.username = os.environ.get("EMAIL_USER")
+        self.pw = os.environ.get("EMAIL_PW")
+        self.df = df
+
+        self.server = server
+        self.port = port
+        self.use_tls = user_tls
+
+    def send_email(self) -> None:
+        """
+        This function sends an e-mail from Microsoft Exchange server
+
+        Returns
+        -------
+        None
+        """
+        msg = MIMEMultipart()
+        msg["From"] = self.username
+        msg["To"] = self.to
+        msg["Cc"] = self.cc
+        msg["Date"] = formatdate(localtime=True)
+        msg["Subject"] = self.subject
+
+        if self.df.shape[0] in range(1, 30):
+            html_table = dataframe_to_html(df=self.df)
+        elif self.df.shape[0] > 30:
+            logging.warning(f"Only first 30 records will be added. ({self.df.shape[0]} > the limit of 30).")
+            html_table = dataframe_to_html(df=self.df.head(n=30))
+        else:
+            html_table = ""  # no data in dataframe (0 records)
+
+        self.message += html_table
+        msg.attach(MIMEText(self.message, "html"))
+
+        # attach files if these are given else ignore
+        if self.files:
+            for path in self.files:
+                part = MIMEBase("application", "octet-stream")
+                with open(path, "rb") as file:
+                    part.set_payload(file.read())
+                encoders.encode_base64(part)
+                # Path(path).name grabs the filename from the path
+                part.add_header("Content-Disposition", f"attachment; filename={Path(path).name}")
+                msg.attach(part)
+
+        smtp = smtplib.SMTP(self.server, self.port)
+        if self.use_tls:
+            smtp.starttls()
+
+        smtp.login(self.username, self.pw)
+        smtp.send_message(msg=msg)
+        smtp.quit()
---
+++ packages/zyppnotify/zyppnotify-0.2.2.tar.gz/zyppnotify-0.2.2/notify/utils.py
@@ -0,0 +1,140 @@
+import os
+
+import pandas as pd
+from babel.numbers import format_currency, format_decimal
+
+from notify.exceptions import EnvironmentVariablesError
+
+
+def format_numbers(df: pd.DataFrame, currency_columns: list = None, number_columns: list = None):
+    """
+    Deze functie converteerd currency (bedrag) en number (getal) kolommen naar geformatteerde tekstvelden.
+
+    Parameters
+    ----------
+    df: pd.DataFrame
+        dataset waarin kolommen staan die geconverteerd dienen te worden
+    currency_columns: list
+        lijst met kolomnamen die geconverteerd worden naar € kolommen en formats.
+    number_columns: list
+        lijst met kolomnamen die geconverteerd worden naar nummer kolommen met nederlandse annotatie.
+
+    Returns
+    -------
+    df: pd.DataFrame
+        dataset met kolommen die gebruikt kunnen worden voor het presenteren van
+        bedragen en nummers (locale=NL).
+    """
+
+    # format de bedrag kolommen
+    if number_columns is None:
+        number_columns = []
+
+    if currency_columns is None:
+        currency_columns = []
+
+    for col in currency_columns:
+        df[col] = df[col].apply(lambda x: format_currency(number=x, currency="EUR", locale="nl_NL"))
+
+    # format de nummer kolommen
+    for col in number_columns:
+        df[col] = df[col].apply(lambda x: format_decimal(number=x, locale="nl_NL"))
+
+    return df
+
+
+def check_environment_variables(required_variables: list):
+    """
+    Test if environment variables are set.
+
+    Parameters
+    ----------
+    required_variables: list
+        list of required variables that need to be present in environment variables.
+
+    Returns
+    -------
+    None
+    """
+    values = [os.environ.get(x) for x in required_variables]
+
+    if not all(values):
+        raise EnvironmentVariablesError(f"One of the environment variables {', '.join(required_variables)} is not set")
+
+
+def dataframe_to_html(df: pd.DataFrame) -> str:
+    """
+    Deze functie zet een dataframe om in een opgemaakte HTML table. Wanneer de gebruiker zelfstandig een HTML bericht
+    opbouwt, kan deze functie uitkomst bieden voor het invoegen van html tabellen.
+
+    Parameters
+    ----------
+    df: pd.DataFrame
+        dataframe die in een HTML table geconverteerd dient te worden.
+
+    Returns
+    -------
+    pretty_html_table: str
+        html body voor de gegeneerde HTML tabel
+    """
+
+    html_table = df.to_html(index=False, classes="styled-table", justify="center")
+
+    pretty_html_table = (
+        """
+        <head>
+            <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+            <title>Dataframe report</title>
+            <style type="text/css" media="screen">
+                h1 {
+                    background-color: #a8a8a8;
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: center;
+                    text-align: center;
+                }
+
+                .styled-table {
+                    border-collapse: collapse;
+                    margin: 25px 0;
+                    font-size: 0.9em;
+                    font-family: sans-serif;
+                    min-width: 400px;
+                    box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+                }
+
+                .styled-table thead tr {
+                    background-color: #009879;
+                    color: #ffffff;
+                    text-align: left;
+                }
+
+                .styled-table th,
+                .styled-table td {
+                    padding: 12px 15px;
+                }
+
+                .styled-table tbody tr {
+                    border-bottom: thin solid #dddddd;
+                }
+
+                .styled-table tbody tr:nth-of-type(even) {
+                    background-color: #f3f3f3;
+                }
+
+                .styled-table tbody tr.active-row {
+                    font-weight: bold;
+                    color: #009879;
+                }
+
+                .styled-table tbody tr:last-of-type {
+                    border-bottom: 2px solid #009879;
+                }
+            </style>
+        </head>
+        <body>"""
+        + html_table
+        + "</body>"
+    )
+
+    return pretty_html_table
