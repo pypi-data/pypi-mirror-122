45badef821c52faa222f9a457029f9c47e9b4fa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:43:20 +0000

    Add package pybasics/0.1b2/pybasics-0.1b2.tar.gz

---
+++ packages/pybasics/pybasics-0.1b2.tar.gz/pybasics-0.1b2/src/pybasics/__init__.py
@@ -0,0 +1,6 @@
+from pybasics.main import read_file
+from pybasics.main import read_pickle
+from pybasics.main import write_file
+from pybasics.main import write_pickle
+
+from pybasics.globals import AAS
