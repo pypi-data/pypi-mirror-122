3963cd8fb46ba5676eaf9fe6d1e12ea29b9d0a66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:30 +0000

    Add package jsonwriter/0.1.1/jsonwriter-0.1.1.tar.gz

---
+++ packages/jsonwriter/jsonwriter-0.1.1.tar.gz/jsonwriter-0.1.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='jsonwriter',
+    version='0.1.1',
+    description='Easy JSON Writer',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    author='Nawaf Alqari',
+    author_email='contact@nawaf.cf',
+    packages=find_packages(where='jsonwriter'),
+    python_requires='>=3.6',
+    license='MIT',
+    url = 'https://github.com/nawafalqari/jsonwriter',
+    project_urls = {
+        'Github': 'https://github.com/nawafalqari/jsonwriter',
+        'Issues': 'https://github.com/nawafalqari/jsonwriter/issues',
+    },
+    keywords=['jsondb', 'json database', 'jsondatabase', 'json editor', 'json', 'json writer'],
+    classifiers=[
+    'Programming Language :: Python :: 3',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+  ],
+)
+
+# python setup.py sdist
+# twine upload dist/jsonwriter-[version].tar.gz
