9987f7dc13b92c59bcead74740a9e5bfd022e26b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:16 +0000

    Add package bubbleio/0.1/bubbleio-0.1.tar.gz

---
+++ packages/bubbleio/bubbleio-0.1.tar.gz/bubbleio-0.1/setup.py
@@ -0,0 +1,23 @@
+#! python3  # noqa: E265
+
+from setuptools import find_packages, setup
+
+# package (to get version)
+from bubbleio import __about__
+
+setup(
+    name="bubbleio",
+    version=__about__.__version__,
+    author=__about__.__author__,
+    author_email=__about__.__email__,
+    description=__about__.__summary__,
+    py_modules=["sqlitetoairtable"],
+    # packaging
+    packages=find_packages(
+        exclude=["contrib", "docs", "*.tests", "*.tests.*", "tests.*", "tests"]
+    ),
+    include_package_data=True,
+    install_requires=[
+        "requests",
+    ],
+)
