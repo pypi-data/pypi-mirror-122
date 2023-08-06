67f4030fddf16a660f300c7e87780b3a2cfcc1cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:25 +0000

    Add package neml/1.4.0/neml-1.4.0.tar.gz

---
+++ packages/neml/neml-1.4.0.tar.gz/neml-1.4.0/include/neml_interface.h
@@ -0,0 +1,11 @@
+#ifndef NEML_INTERFACE_H
+#define NEML_INTERFACE_H
+
+#include "nemlerror.h"
+
+namespace neml {
+std::unique_ptr<NEMLModel> parse_xml_unique(std::string fname, std::string mname);
+std::unique_ptr<NEMLModel> parse_string_unique(std::string input, std::string mname);
+}
+
+#endif // NEML_INTERFACE_H
