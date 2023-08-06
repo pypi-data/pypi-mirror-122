91504d82c2e9298d96f71167ef4c1a60ff3eea93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:05 +0000

    Add package meerschaum/0.4.0.dev1/meerschaum-0.4.0.dev1.tar.gz

---
+++ packages/meerschaum/meerschaum-0.4.0.dev1.tar.gz/meerschaum-0.4.0.dev1/meerschaum/api/dash/callbacks/__init__.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+
+"""
+Define callbacks for pages.
+"""
+
+import meerschaum.api.dash.callbacks.dashboard
+import meerschaum.api.dash.callbacks.login
+import meerschaum.api.dash.callbacks.plugins
