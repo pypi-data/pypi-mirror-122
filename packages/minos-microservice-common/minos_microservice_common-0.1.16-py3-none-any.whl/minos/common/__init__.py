b6bf03a50aa2f2879caf16ecc9fe861bab4ce227
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:12 +0000

    Add package minos-microservice-common/0.1.16/minos_microservice_common-0.1.16-py3-none-any.whl

---
+++ packages/minos-microservice-common/minos_microservice_common-0.1.16-py3-none-any.whl/minos/common/__init__.py
@@ -0,0 +1,164 @@
+__version__ = "0.1.16"
+
+from .configuration import (
+    BROKER,
+    COMMANDS,
+    DISCOVERY,
+    QUEUE,
+    REPOSITORY,
+    REST,
+    SAGA,
+    SERVICE,
+    SNAPSHOT,
+    STORAGE,
+    MinosConfig,
+    MinosConfigAbstract,
+)
+from .database import (
+    PostgreSqlMinosDatabase,
+    PostgreSqlPool,
+)
+from .datetime import (
+    NULL_DATETIME,
+    current_datetime,
+)
+from .exceptions import (
+    DataDecoderException,
+    DataDecoderMalformedTypeException,
+    DataDecoderRequiredValueException,
+    DataDecoderTypeException,
+    EmptyMinosModelSequenceException,
+    MinosAttributeValidationException,
+    MinosBrokerException,
+    MinosBrokerNotProvidedException,
+    MinosConfigException,
+    MinosConfigNotProvidedException,
+    MinosException,
+    MinosHandlerException,
+    MinosHandlerNotProvidedException,
+    MinosImmutableClassException,
+    MinosImportException,
+    MinosMalformedAttributeException,
+    MinosMessageException,
+    MinosModelAttributeException,
+    MinosModelException,
+    MinosParseAttributeException,
+    MinosPreviousVersionSnapshotException,
+    MinosProtocolException,
+    MinosRepositoryException,
+    MinosRepositoryNotProvidedException,
+    MinosReqAttributeException,
+    MinosSagaManagerException,
+    MinosSagaManagerNotProvidedException,
+    MinosSnapshotAggregateNotFoundException,
+    MinosSnapshotDeletedAggregateException,
+    MinosSnapshotException,
+    MinosSnapshotNotProvidedException,
+    MinosTypeAttributeException,
+    MultiTypeMinosModelSequenceException,
+)
+from .importlib import (
+    classname,
+    import_module,
+)
+from .injectors import (
+    DependencyInjector,
+)
+from .launchers import (
+    EntrypointLauncher,
+)
+from .meta import (
+    classproperty,
+    property_or_classproperty,
+    self_or_classmethod,
+)
+from .model import (
+    Action,
+    Aggregate,
+    AggregateDiff,
+    AggregateRef,
+    AvroDataDecoder,
+    AvroDataEncoder,
+    AvroSchemaDecoder,
+    AvroSchemaEncoder,
+    BucketModel,
+    Command,
+    CommandReply,
+    CommandStatus,
+    DataTransferObject,
+    DeclarativeModel,
+    DynamicModel,
+    Entity,
+    EntitySet,
+    EntitySetDiff,
+    EntitySetDiffEntry,
+    Event,
+    Field,
+    FieldDiff,
+    FieldDiffContainer,
+    GenericTypeProjector,
+    IncrementalFieldDiff,
+    MinosModel,
+    MissingSentinel,
+    Model,
+    ModelField,
+    ModelRef,
+    ModelRefExtractor,
+    ModelRefInjector,
+    ModelType,
+    NoneType,
+    TypeHintBuilder,
+    TypeHintComparator,
+    ValueObject,
+    ValueObjectSet,
+    ValueObjectSetDiff,
+    ValueObjectSetDiffEntry,
+)
+from .networks import (
+    MinosBroker,
+    MinosHandler,
+)
+from .pools import (
+    MinosPool,
+)
+from .protocol import (
+    MinosAvroDatabaseProtocol,
+    MinosAvroMessageProtocol,
+    MinosAvroProtocol,
+    MinosBinaryProtocol,
+    MinosJsonBinaryProtocol,
+)
+from .queries import (
+    Condition,
+    Ordering,
+)
+from .repository import (
+    InMemoryRepository,
+    MinosRepository,
+    PostgreSqlRepository,
+    RepositoryEntry,
+)
+from .saga import (
+    MinosSagaManager,
+)
+from .setup import (
+    MinosSetup,
+)
+from .snapshot import (
+    InMemorySnapshot,
+    MinosSnapshot,
+    PostgreSqlSnapshot,
+    PostgreSqlSnapshotQueryBuilder,
+    PostgreSqlSnapshotReader,
+    PostgreSqlSnapshotSetup,
+    PostgreSqlSnapshotWriter,
+    SnapshotEntry,
+)
+from .storage import (
+    MinosStorage,
+    MinosStorageLmdb,
+)
+from .uuid import (
+    NULL_UUID,
+    UUID_REGEX,
+)
---
+++ packages/minos-microservice-common/minos_microservice_common-0.1.16-py3-none-any.whl/minos/common/repository/__init__.py
@@ -0,0 +1,12 @@
+from .abc import (
+    MinosRepository,
+)
+from .entries import (
+    RepositoryEntry,
+)
+from .memory import (
+    InMemoryRepository,
+)
+from .pg import (
+    PostgreSqlRepository,
+)
