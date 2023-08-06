f05c89b1ce78810050fd3607d408aa25047a4a01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:40 +0000

    Add package backup-uploader/0.4.0/backup_uploader-0.4.0.tar.gz

---
+++ packages/backup-uploader/backup_uploader-0.4.0.tar.gz/backup_uploader-0.4.0/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="backup_uploader",
+    packages=find_packages(include=[
+        "backup_uploader",
+        "backup_uploader.clients.dropbox",
+        "backup_uploader.clients.mega",
+    ]),
+    license="MIT",
+    version="0.4.0",
+    url="https://github.com/aspedrosa/BackupsUploader",
+    author="André Pedrosa",
+    author_email="aspedr0sa@protonmail.com",
+    keywords=["backup", "uploader"],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Utilities',
+    ],
+    include_package_data=True,
+    install_requires=["click"],
+    extras_require={
+        "dropbox": ["dropbox"],
+        "mega": ["mega.py"],
+    },
+    entry_points="""
+    [console_scripts]
+    backup_uploader=backup_uploader.cli:main
+    """,
+)
