08727a9694e0415b9fd1101a23776e35bdb02c7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:41:34 +0000

    Add package rwx824550565-udacity/0.1/rwx824550565_udacity-0.1.tar.gz

---
+++ packages/rwx824550565-udacity/rwx824550565_udacity-0.1.tar.gz/rwx824550565_udacity-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='rwx824550565_udacity',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['rwx824550565_udacity'],
+      zip_safe=False)
