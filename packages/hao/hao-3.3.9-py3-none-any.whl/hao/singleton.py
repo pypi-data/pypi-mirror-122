bd13b5a6cebdcdfc5cc8041ac169c62b4e222b75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:15 +0000

    Add package hao/3.3.9/hao-3.3.9-py3-none-any.whl

---
+++ packages/hao/hao-3.3.9-py3-none-any.whl/hao/singleton.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from multiprocessing import Lock
+
+
+class Singleton(type):
+    _instances = {}
+    _lock: Lock = Lock()
+
+    def __call__(cls, *args, **kwargs):
+        with cls._lock:
+            instance = cls._instances.get(cls)
+            if not instance:
+                instance = super().__call__(*args, **kwargs)
+                cls._instances[cls] = instance
+            return instance
+
+
+class Multiton(type):
+    _instances = {}
+    _lock: Lock = Lock()
+
+    def __call__(cls, *args, **kwargs):
+        with cls._lock:
+            key = (list(args) + list(kwargs.values()) + [None])[0]
+            instance = cls._instances.get((cls, key))
+            if instance is None:
+                instance = super().__call__(*args, **kwargs)
+                cls._instances[(cls, key)] = instance
+            return instance
