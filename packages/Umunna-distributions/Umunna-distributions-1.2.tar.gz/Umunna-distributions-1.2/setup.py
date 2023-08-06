25156e9b3e5b41748878c37bb03e083bab3c3f04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:28 +0000

    Add package Umunna-distributions/1.2/Umunna-distributions-1.2.tar.gz

---
+++ packages/Umunna-distributions/Umunna-distributions-1.2.tar.gz/Umunna-distributions-1.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='Umunna-distributions',
+      version='1.2',
+      description='Gaussian distributions',
+      packages=['Umunna-distributions'],
+      author = 'Victor Umunna',
+      zip_safe=False)
