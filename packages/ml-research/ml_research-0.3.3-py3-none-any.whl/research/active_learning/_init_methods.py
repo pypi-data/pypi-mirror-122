db9cad4d5be2c7389268ace1f08856a9a9205a71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:34 +0000

    Add package ml-research/0.3.3/ml_research-0.3.3-py3-none-any.whl

---
+++ packages/ml-research/ml_research-0.3.3-py3-none-any.whl/research/active_learning/_init_methods.py
@@ -0,0 +1,57 @@
+"""
+Wrapper for cluster-based initialization methods and random initialization.
+"""
+import numpy as np
+from scipy.special import softmax
+from ._selection_methods import UNCERTAINTY_FUNCTIONS
+
+
+def init_strategy(
+    X,
+    y,
+    n_initial,
+    clusterer=None,
+    selection_method=None,
+    random_state=None
+):
+    """
+    Defaults to random.
+
+    Selection method is only relevant if a clusterer object is passed.
+    Possible selection methods:
+    - None (default): random selection
+    - centroid: Gets observations close to the centroids of
+      the clusters.
+    -
+    """
+
+    unlabeled_ids = np.indices(X.shape[:1]).squeeze()
+
+    # Random selection
+    if clusterer is None or selection_method in ['random', None]:
+        rng = np.random.RandomState(random_state)
+        ids = rng.choice(unlabeled_ids, n_initial, replace=False)
+        # There must be at least 2 different initial classes
+        if len(np.unique(y[ids])) == 1:
+            ids[-1] = rng.choice(unlabeled_ids[y != y[ids][0]], 1, replace=False)
+
+        return None, ids
+
+    # Cluster-based selection
+    clusterer.fit(X)
+
+    if hasattr(clusterer, 'predict_proba'):
+        # Use probabilities to compute uncertainty
+        probs = clusterer.predict_proba(X)
+    else:
+        # Use cluster distances to compute probabilities
+        dist = clusterer.transform(X)
+        # The first one is another possible alternative
+        # dist_inv = 1 - (dist / np.expand_dims(dist.max(1), 1))
+        dist_inv = (np.expand_dims(dist.max(1), 1) / dist) - 1
+        probs = softmax(dist_inv, axis=1)
+
+    uncertainty = UNCERTAINTY_FUNCTIONS[selection_method](probs)
+    ids = unlabeled_ids[np.argsort(uncertainty)[::-1][:n_initial]]
+
+    return clusterer, ids
---
+++ packages/ml-research/ml_research-0.3.3-py3-none-any.whl/research/utils/_check_pipelines.py
@@ -0,0 +1,80 @@
+from rlearn.utils import check_random_states
+from itertools import product
+from imblearn.pipeline import Pipeline
+from sklearn.base import clone
+
+
+def check_pipelines(objects_list, random_state, n_runs):
+    """Extract estimators and parameters grids."""
+
+    # Create random states
+    random_states = check_random_states(random_state, n_runs)
+
+    pipelines = []
+    param_grid = []
+    for comb, rs in product(product(*objects_list), random_states):
+        name = '|'.join([i[0] for i in comb])
+
+        # name, object, sub grid
+        comb = [(nm, ob, sg) for nm, ob, sg in comb if ob is not None]
+
+        if name not in [n[0] for n in pipelines]:
+            pipelines.append((name, Pipeline([(nm, ob) for nm, ob, _ in comb])))
+
+        grid = {'est_name': [name]}
+        for obj_name, obj, sub_grid in comb:
+            if 'random_state' in obj.get_params().keys():
+                grid[f'{name}__{obj_name}__random_state'] = [rs]
+            for param, values in sub_grid.items():
+                grid[f'{name}__{obj_name}__{param}'] = values
+
+        # Avoid multiple runs over pipelines without random state
+        if grid not in param_grid:
+            param_grid.append(grid)
+
+    return pipelines, param_grid
+
+
+def check_pipelines_wrapper(
+    objects_list,
+    wrapper,
+    random_state,
+    n_runs,
+    wrapped_only=False
+):
+    wrapper_label = wrapper[0]
+    wrapper_obj = wrapper[1]
+    wrapper_grid = wrapper[2]
+
+    estimators, param_grids = check_pipelines(
+        objects_list, random_state, n_runs
+    )
+
+    wrapped_estimators = [
+        (
+            f'{wrapper_label}|{name}',
+            clone(wrapper_obj).set_params(**{'classifier': pipeline})
+        )
+        for name, pipeline in estimators
+    ]
+
+    wrapped_param_grids = [
+        {
+            'est_name': [f'{wrapper_label}|{d["est_name"][0]}'],
+            **{
+                k.replace(
+                    d["est_name"][0],
+                    f'{wrapper_label}|{d["est_name"][0]}__classifier'
+                ):v
+                for k, v in d.items() if k != 'est_name'},
+            **{f'{wrapper_label}|{d["est_name"][0]}__{k}': v
+                for k, v in wrapper_grid.items()}
+        } for d in param_grids
+    ]
+    if wrapped_only:
+        return wrapped_estimators, wrapped_param_grids
+    else:
+        return (
+            estimators + wrapped_estimators,
+            param_grids + wrapped_param_grids
+        )
