b808b4ea6c1c1471198043f232e82bed7be50ad5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:41:25 +0000

    Add package pytinyrenderer/0.0.8/pytinyrenderer-0.0.8.tar.gz

---
+++ packages/pytinyrenderer/pytinyrenderer-0.0.8.tar.gz/pytinyrenderer-0.0.8/python/CMakeFiles/pytinyrenderer.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/erwincoumans/develop/tinyrenderer/geometry.cpp" "/Users/erwincoumans/develop/tinyrenderer/python/CMakeFiles/pytinyrenderer.dir/__/geometry.cpp.o"
+  "/Users/erwincoumans/develop/tinyrenderer/model.cpp" "/Users/erwincoumans/develop/tinyrenderer/python/CMakeFiles/pytinyrenderer.dir/__/model.cpp.o"
+  "/Users/erwincoumans/develop/tinyrenderer/our_gl.cpp" "/Users/erwincoumans/develop/tinyrenderer/python/CMakeFiles/pytinyrenderer.dir/__/our_gl.cpp.o"
+  "/Users/erwincoumans/develop/tinyrenderer/tgaimage.cpp" "/Users/erwincoumans/develop/tinyrenderer/python/CMakeFiles/pytinyrenderer.dir/__/tgaimage.cpp.o"
+  "/Users/erwincoumans/develop/tinyrenderer/tinyrenderer.cpp" "/Users/erwincoumans/develop/tinyrenderer/python/CMakeFiles/pytinyrenderer.dir/__/tinyrenderer.cpp.o"
+  "/Users/erwincoumans/develop/tinyrenderer/python/pytinyrenderer.cc" "/Users/erwincoumans/develop/tinyrenderer/python/CMakeFiles/pytinyrenderer.dir/pytinyrenderer.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "_USE_MATH_DEFINES"
+  "pytinyrenderer_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "python/.."
+  "python/../third_party/pybind11/include"
+  "/Library/Frameworks/Python.framework/Versions/3.6/include/python3.6m"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
