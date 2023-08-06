da54f13a04d7cfb480ea394a24bccfb97d6a40e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:21 +0000

    Add package dsnd-probabilitysk1905/0.1/dsnd_probabilitysk1905-0.1.tar.gz

---
+++ packages/dsnd-probabilitysk1905/dsnd_probabilitysk1905-0.1.tar.gz/dsnd_probabilitysk1905-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd_probabilitysk1905',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dsnd_probabilitysk1905'],
+      author= 'Shreya Kanodia',
+      author_email='shreyakanodia19@gmail.com',
+      zip_safe=False)
