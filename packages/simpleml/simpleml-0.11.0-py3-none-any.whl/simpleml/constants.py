1acfd63ec1d20d8db68397026dc87a10e08ce3ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:44:18 +0000

    Add package simpleml/0.11.0/simpleml-0.11.0-py3-none-any.whl

---
+++ packages/simpleml/simpleml-0.11.0-py3-none-any.whl/simpleml/constants.py
@@ -0,0 +1,11 @@
+'''
+Module to centralize all referenced constants in SimpleML
+'''
+
+__author__ = 'Elisha Yadgaran'
+
+
+# Package Constants
+TRAIN_SPLIT: str = 'TRAIN'
+VALIDATION_SPLIT: str = 'VALIDATION'
+TEST_SPLIT: str = 'TEST'
