20d881b5a6786dcd5146919aec65310be0a96353
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:42 +0000

    Add package distributions-srg-001/0.1/distributions_srg_001-0.1.tar.gz

---
+++ packages/distributions-srg-001/distributions_srg_001-0.1.tar.gz/distributions_srg_001-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name = 'distributions_srg_001',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_srg_001'],
+      author='Somya Rajiv Gupta',
+      author_email='thesomyagupta@gmail.com',
+      zip_safe=False)
