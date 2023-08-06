ba6116ad7b12c3ca4a6a52a8d10cc828d18e9b4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:10 +0000

    Add package karan-distributions/0.0/karan_distributions-0.0.tar.gz

---
+++ packages/karan-distributions/karan_distributions-0.0.tar.gz/karan_distributions-0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name = 'karan_distributions',
+      version = '0.0',
+      description = 'Gaussian and Binomial Distribution',
+      package = ['karan_distributions'],
+      author = 'Karan Kumar',
+      author_email = 'karan220595@gmail.com',
+      zip_safe = False)
