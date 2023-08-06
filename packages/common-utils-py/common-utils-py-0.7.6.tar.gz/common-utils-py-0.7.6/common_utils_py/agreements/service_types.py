e2f4f75dbbb4a232fa0dcf5728cc9f42f46689f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:05 +0000

    Add package common-utils-py/0.7.6/common-utils-py-0.7.6.tar.gz

---
+++ packages/common-utils-py/common-utils-py-0.7.6.tar.gz/common-utils-py-0.7.6/common_utils_py/agreements/service_types.py
@@ -0,0 +1,28 @@
+class ServiceTypes:
+    """Types of Service allowed in ocean protocol DDO services."""
+    AUTHORIZATION = 'authorization'
+    METADATA = 'metadata'
+    ASSET_ACCESS = 'access'
+    CLOUD_COMPUTE = 'compute'
+    DID_SALES = 'did-sales'
+    NFT_SALES = 'nft-sales'
+    NFT_ACCESS = 'nft-access'
+    NFT721_ACCESS = 'nft721-access'
+
+
+class ServiceTypesIndices:
+    DEFAULT_METADATA_INDEX = 0
+    DEFAULT_PROVENANCE_INDEX = 1
+    DEFAULT_AUTHORIZATION_INDEX = 2
+    DEFAULT_ACCESS_INDEX = 3
+    DEFAULT_COMPUTING_INDEX = 4
+    DEFAULT_DID_SALES_INDEX = 5
+    DEFAULT_NFT_SALES_INDEX = 6
+    DEFAULT_NFT_ACCESS_INDEX = 7
+    DEFAULT_NFT721_ACCESS_INDEX = 9
+
+
+class ServiceAuthorizationTypes:
+    SECRET_STORE = 'SecretStore'
+    PSK_ECDSA = 'PSK-ECDSA'
+    PSK_RSA = 'PSK-RSA'
