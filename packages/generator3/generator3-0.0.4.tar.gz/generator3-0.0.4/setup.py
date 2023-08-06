3e624ae8d7f938f1386a0ccff66d7c11938867a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:53 +0000

    Add package generator3/0.0.4/generator3-0.0.4.tar.gz

---
+++ packages/generator3/generator3-0.0.4.tar.gz/generator3-0.0.4/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+long_description = open('README.md', encoding='utf-8').read()
+setuptools.setup(
+    name="generator3",
+    version="0.0.4",
+    author="themysticsavages",
+    author_email="someemail@emailprovider.com",
+    description="Parse Scratch's large project JSONs and convert it to scratchblocks notation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/themysticsavages/generator3",
+    project_urls={
+        "Bug Tracker": "https://github.com/themysticsavages/generator3/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+      "flask", 
+      "requests"
+    ]
+)
