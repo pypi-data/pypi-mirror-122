fe7738f17461641816786ee9319ddf76be3b6754
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:07 +0000

    Add package myfirstdummypackage/0.1/myfirstdummypackage-0.1.tar.gz

---
+++ packages/myfirstdummypackage/myfirstdummypackage-0.1.tar.gz/myfirstdummypackage-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='myfirstdummypackage',
+    version='0.1',
+    description='My own dummy package for lab2',
+    url='',
+    install_requires=['pandas<1.0'],
+    packages=['simple_package'],
+)
