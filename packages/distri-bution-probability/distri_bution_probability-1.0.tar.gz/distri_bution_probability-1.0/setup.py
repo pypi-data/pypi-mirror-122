18aaf34bd9832951f13c1017dcb7d4f6e3c41000
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:43:49 +0000

    Add package distri-bution-probability/1.0/distri_bution_probability-1.0.tar.gz

---
+++ packages/distri-bution-probability/distri_bution_probability-1.0.tar.gz/distri_bution_probability-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distri_bution_probability',
+      version='1.0',
+      description='Gaussian and bionomial distributions',
+      packages=['distri_bution_probability'],
+      author = 'Marina Lovenita',
+      author_email = 'lovenita.marina@gmail.com',
+      zip_safe=False)
