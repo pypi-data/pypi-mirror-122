c03a47212d7faf57a60588cee5b7eeb2911611a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:14 +0000

    Add package N0Code/1.1.3/N0Code-1.1.3.tar.gz

---
+++ packages/N0Code/N0Code-1.1.3.tar.gz/N0Code-1.1.3/N0Code.py
@@ -0,0 +1,39 @@
+from leetcode_api import *
+class Answer:
+    def __init__(self,title,Description,author):
+        self.Title=title
+        self.Description=Description
+        self.Author=author
+    Challenges=[]
+    Responses=[]
+    def set(self,response,**kwargs):
+        self.Challenges.append(kwargs)
+        self.Responses.append(response)
+    
+    def check(self,expFunc):
+        correct=0
+        wrong=0
+        print("\n["+self.Title+"]")
+        print('"'+self.Description+'"\n')
+
+        for challengeId in range(0,len(self.Challenges)):
+            challenge=self.Challenges[challengeId]
+            #print(challenge)
+            try:
+                res=expFunc(**challenge)
+            except Exception as e:
+                print("Do not specify a parameter name.")
+            if res==self.Responses[challengeId]:
+                print("[Correct]:%s --> %s" % (challenge,res))
+                correct+=1
+            else:
+                print("[Wrong]:%s --> %s Answer: %s" % (challenge,res,self.Responses[challengeId]))
+                wrong+=1
+        print("\nCorrect: %s/%s" % (correct,len(self.Challenges)))
+        print("Wrong: %s/%s" % (wrong,len(self.Challenges)))
+        if wrong==0:
+            print("\nThis Challenge Has Been Solved By "+self.Author)
+        else:
+            print("\n"+self.Author+"! Try again!")
+
+print("Done.")
