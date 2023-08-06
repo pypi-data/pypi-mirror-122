d59b4ef97e1f8c687a4429d26d49ca4bb9a68ab5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:04 +0000

    Add package pymchain/0.1/pymchain-0.1.tar.gz

---
+++ packages/pymchain/pymchain-0.1.tar.gz/pymchain-0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt", "r") as fh:
+    reqs = fh.read()
+
+setup(
+    name='pymchain',
+    version="0.1",
+    description='Python Markov Chain',
+    python_requires='>=3.8',
+    author='Yurasov Nikita',
+    author_email='n.yurasov@yahoo.com',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=reqs,
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    url='https://github.com/NikitaYurasov/pymchain',
+    include_package_data=True,
+    zip_safe=False
+)
