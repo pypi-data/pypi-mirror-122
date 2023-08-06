34e29663ef8ff8319ce23d0e371dc50385fe67ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:13 +0000

    Add package dsnd-prob-vd/1.0/dsnd_prob_vd-1.0.tar.gz

---
+++ packages/dsnd-prob-vd/dsnd_prob_vd-1.0.tar.gz/dsnd_prob_vd-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd_prob_vd',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['dsnd_prob_gauss_binom'],
+      author = 'David Yacoub',
+      author_email= 'daveshtine2010@yahoo.com',
+      zip_safe=False)
